7e67f4e8164e5f8ae8f834187d15b02bcefe260a
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-03-04 11:18:13 +0100

    Generalize msgToText to use Istring and rename to sshow

--- local-cluster/test/Test/LocalClusterSpec.hs
+++ local-cluster/test/Test/LocalClusterSpec.hs
@@ -3,18 +3,18 @@ module Test.LocalClusterSpec where
 import Cardano.Prelude
 
 import Lib (ClusterConfig(..), withCluster)
+import Logging (Severity(..), withStdoutTracer)
 import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec (Spec, describe, it)
-import Logging (withStdoutTracer, Severity(..))
 
 import qualified Data.Text as T
 
 spec :: Spec
 spec = describe "Hydra local cluster" $ do
   it "should start" $ do
-    withStdoutTracer "local-cluster" Info msgToText $ \tr ->
+    withStdoutTracer "local-cluster" Info sshow $ \tr ->
       withSystemTempDirectory "hydra-local-cluster" $ \tmp -> do
         withCluster tr (ClusterConfig tmp) $ \_ -> pure ()
 
-msgToText :: Show msg => msg -> Text
-msgToText = T.pack . show
+sshow :: (IsString s, Show a) => a -> s
+sshow = fromString . show
