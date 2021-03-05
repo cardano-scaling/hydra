f56cb7e2e7d4e2fa8651d157429913501d46fd94
KtorZ <matthias.benkort@gmail.com>
2021-03-05 11:20:57 +0100

    Bubble up NodeIs into the 'RunningNode'
    So that callers upstream can use it to instantiate log messages with
    context.

--- local-cluster/test/Test/LocalClusterSpec.hs
+++ local-cluster/test/Test/LocalClusterSpec.hs
@@ -1,23 +1,27 @@
 module Test.LocalClusterSpec where
 
-
 import Cardano.Prelude
 
-import Data.String (IsString(..))
-import Lib (ClusterConfig(..), RunningCluster(..), withCluster, ClusterLog)
-import Logging (Severity(..), withStdoutTracer, Tracer)
+import Data.String (IsString (..))
+import Lib (ClusterConfig (..), ClusterLog (..), RunningCluster (..), withCluster)
+import Logging (Severity (..), Tracer, contramap, withStdoutTracer)
+import Node (RunningNode (..), cliQueryTip)
 import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec (Spec, describe, it)
 
 spec :: Spec
 spec = describe "Hydra local cluster" $ do
-  it "should start" $ do
-    withStdoutTracer "local-cluster" Info sshow $ \tr ->
-      withSystemTempDirectory "hydra-local-cluster" $ \tmp -> do
-        withCluster tr (ClusterConfig tmp) $ assertNetworkIsUp tr
+    it "should start" $ do
+        withStdoutTracer "local-cluster" Info sshow $ \tr ->
+            withSystemTempDirectory "hydra-local-cluster" $ \tmp -> do
+                withCluster tr (ClusterConfig tmp) $ assertNetworkIsUp tr
 
 assertNetworkIsUp :: Tracer IO ClusterLog -> RunningCluster -> IO ()
-assertNetworkIsUp _ _ = panic "cluster has no running node"
+assertNetworkIsUp tracer = \case
+    RunningCluster _ (RunningNode nodeId socket : _) ->
+        cliQueryTip (contramap (MsgFromNode nodeId) tracer) socket
+    _ ->
+        panic "empty cluster?"
 
 sshow :: (IsString s, Show a) => a -> s
 sshow = fromString . show
