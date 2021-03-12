5eff758cb8ee9bb4df4f5f2715fb19131e3ff72a
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-03-12 15:57:41 +0000

    Can start and stop a dummy Hydra node

--- hydra-node/src/Hydra/Node.hs
+++ hydra-node/src/Hydra/Node.hs
@@ -1,22 +1,37 @@
--- | Top-level module to run a single Hydra node
 {-# LANGUAGE DerivingStrategies #-}
+
+-- | Top-level module to run a single Hydra node
 module Hydra.Node where
 
 import Cardano.Prelude
 
 runHydra :: IO ()
-runHydra = panic "Not implemented"
+runHydra = do
+  node <- startHydraNode
+  putStrLn ("Hydra started" :: Text)
+  void $ stopHydraNode node
 
-nodeStatus :: HydraNode -> IO NodeStatus
-nodeStatus = panic "not implemented"
+-- | Current state of a Hydra node
+newtype HydraNode = HydraNode {nodeThread :: Maybe (Async ())}
 
-stopHydraNode :: HydraNode -> IO ()
-stopHydraNode = panic "not implemented"
-
-startHydraNode :: IO HydraNode
-startHydraNode = panic "not implemented"
-
-data HydraNode = HydraNode
-
+-- | High-level lifecycle states for a Hydra node
 data NodeStatus = NotReady | Ready
   deriving stock (Eq, Show)
+
+stopHydraNode :: HydraNode -> IO HydraNode
+stopHydraNode node =
+  case nodeThread node of
+    Just thread -> cancel thread >> pure (HydraNode Nothing)
+    Nothing -> pure node
+
+startHydraNode :: IO HydraNode
+startHydraNode = do
+  signal <- newEmptyMVar
+  thread <- async $ takeMVar signal
+  pure $ HydraNode (Just thread)
+
+nodeStatus :: HydraNode -> IO NodeStatus
+nodeStatus node =
+  case nodeThread node of
+    Just{} -> pure Ready
+    Nothing -> pure NotReady
