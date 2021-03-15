bccdf1e7006b9517fd114a8d16716f9ce76b1b99
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-03-15 12:32:23 +0100

    Setup flake.nix according to haskell.nix guide

--- flake.nix
+++ flake.nix
@@ -2,16 +2,40 @@
   description = "Hydra project flake";
 
   inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    haskell-nix.url = "github:input-output-hk/haskell.nix";
-    nixpkgs.follows = "haskell-nix/nixpkgs-2009";
   };
 
-  outputs = { self, nixpkgs, flake-utils, haskell-nix }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "hydra-node";
-      shell = ./shell.nix;
-      preOverlays = [ haskell-nix.overlay ];
-    };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            # REVIEW(SN): why an overlay?
+            hydraProject =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc8104";
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        flake = pkgs.hydraProject.flake { };
+      in
+      flake // {
+        # Built by `nix build`
+        defaultPackage = flake.packages."local-cluster:exe:local-cluster";
+
+        # Shell provided to `nix develop`
+        devShell = pkgs.hydraProject.shellFor {
+          tools = {
+            cabal = "latest";
+            hlint = "latest";
+            haskell-language-server = "latest";
+          };
+        };
+      });
 }
