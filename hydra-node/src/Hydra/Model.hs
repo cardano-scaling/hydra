3b33752157bcce52d4eb6fab57458a8b20e7c460
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-03-23 17:48:44 +0100

    Add some more details to the domain modelling

--- hydra-node/src/Hydra/Model.hs
+++ hydra-node/src/Hydra/Model.hs
@@ -1,6 +1,9 @@
+-- | A first take on our problem domain and how we would model things. This is a
+-- DRAFT and most things are likely incomplete.
 module Hydra.Model where
 
 import Cardano.Prelude
+import Network.TypedProtocol.FireForget.Server (FireForgetServer)
 
 data Tx
 
@@ -10,6 +13,12 @@ data ClientCommand
   | NewTx
   | Close
   | Contest
+
+data ClientInstruction
+  = InitTxToSign
+  | CommitTxDraft
+  | HeadClosed
+  | TxConfirmed
 
 -- | Messages which are sent through the hydra node-to-node network
 data HydraMessage
@@ -32,12 +41,17 @@ data Event
   | HydraEvent HydraMessage
   | OnChainEvent OnChainTx
 
+data Output
+  = ClientOutput ClientInstruction
+  | HydraOutput HydraMessage
+  | OnChainOutput OnChainTx
+
 data HeadState
 
 -- | The heart of the Hydra head protocol, a handler of all kinds of 'Event' in
 -- the Hydra head. This may also be split into multiple handlers, i.e. one for
 -- hydra network events, one for client events and one for main chain events.
-hydraHeadProtocolHandler :: HeadState -> Event -> (HeadState, [HydraMessage], [OnChainTx])
+hydraHeadProtocolHandler :: HeadState -> Event -> (HeadState, [Output])
 hydraHeadProtocolHandler = panic "not implemented"
 
 -- | Verification used to authenticate main chain transactions that are
@@ -58,15 +72,18 @@ type Party = (OnChainVerificationKey, HydraVerificationKey)
 -- bob :: Party
 -- carol :: Party
 
+-- | Contains at least the contestation period and other things.
 data HeadParameters
+
+-- | Decides when, how often and who is in charge of creating snapshots.
+data SnapshotStrategy
 
 -- | Assume: We know the party members and their verification keys. These need
 -- to be exchanged somehow, eventually.
-initHead :: [Party] -> HeadParameters -> HeadState
-initHead = panic "not implemented"
+createHeadState :: [Party] -> HeadParameters -> SnapshotStrategy -> HeadState
+createHeadState = panic "not implemented"
 
--- | Either valid or an error which we get from the ledger-specs tx validation.
-data ValidationResult
+-- * Ledger stuff
 
 -- | A set of unspent transaction outputs.
 data UTxO
@@ -78,6 +95,69 @@ data ProtocolParameters
 -- mainchain protocol parameters.
 type LedgerState = (UTxO, ProtocolParameters)
 
--- | Validate a transaction given a ledger state and mainchain protocol parameters.
+-- | Either valid or an error which we get from the ledger-specs tx validation.
+data ValidationResult
+  = Valid
+  | Invalid ValidationError
+
+-- | Validate a transaction given a ledger state and mainchain protocol
+-- parameters. TODO: required if we have applyTx?
 validateTx :: LedgerState -> Tx -> ValidationResult
 validateTx = panic "not implemented"
+
+data ValidationError
+
+-- | Validate and apply a transaction if valid to a given ledger.
+applyTx :: LedgerState -> Tx -> Either ValidationError LedgerState
+applyTx = panic "not implemented"
+
+-- * Too detailed stuff
+
+main :: IO ()
+main = do
+  hn <- panic "undefined"
+  mc <- panic "undefined"
+  hh <- panic "undefined"
+
+  forever $ do
+    e <- getEvent
+    runHydraHeadProtocol hn mc hh e
+
+-- | Connects to a configured set of peers and sets up the whole network stack.
+setupHydraNetwork :: MainChain IO -> HydraHead IO -> IO (HydraNetworkClient IO)
+setupHydraNetwork mc hh = do
+  let client = panic "create HydraNetworkClient interface out of ourobouros client"
+  let server = hydraMessageServer (runHydraHeadProtocol client mc hh . HydraEvent)
+  -- ourobouros network stuff, setting up mux, protocols and whatnot
+  panic "not implemented"
+
+hydraMessageServer :: (HydraMessage -> IO ()) -> FireForgetServer HydraMessage IO ()
+hydraMessageServer action = do
+  -- FireForgetServer
+  --   { recvMsg = \msg -> action msg >> hydraMessageServer
+  --   , recvMsgDone = putTextLn "Done."
+  --   }
+  panic "not implemented"
+
+-- | Monadic interface around 'hydraHeadProtocolHandler'.
+runHydraHeadProtocol :: Monad m => HydraNetworkClient m -> MainChain m -> HydraHead m -> Event -> m ()
+runHydraHeadProtocol HydraNetworkClient{broadcast} MainChain{postTx} HydraHead{getState, putState} e = do
+  -- s <- getState
+  -- let (s', msgs, txs) = hydraHeadProtocolHandler s e
+  -- mapM_ broadcast msgs
+  -- mapM_ postTx txs
+  -- putState s'
+  panic "not implemented"
+
+data HydraHead m = HydraHead
+  { getState :: m HeadState
+  , putState :: HeadState -> m ()
+  }
+
+newtype HydraNetworkClient m = HydraNetworkClient
+  { broadcast :: HydraMessage -> m ()
+  }
+
+newtype MainChain m = MainChain
+  { postTx :: OnChainTx -> m ()
+  }
