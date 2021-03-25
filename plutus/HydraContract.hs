e8368abe10c82c845d50824471440a6212e77f64
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-03-25 11:48:51 +0000

    Fourmolu formatting

--- plutus/HydraContract.hs
+++ plutus/HydraContract.hs
@@ -1,34 +1,36 @@
-import           Ledger                    (Address, Validator, ValidatorCtx, Value, scriptAddress)
-import qualified Ledger.Constraints        as Constraints
+{-# LANGUAGE TypeApplications #-}
+
+import Control.Monad (guard, void)
+import Language.Plutus.Contract
+import qualified Language.PlutusTx as PlutusTx
+import Language.PlutusTx.Prelude
+import Ledger (Address, Validator, ValidatorCtx, Value, scriptAddress)
+import qualified Ledger.Constraints as Constraints
+import qualified Ledger.Typed.Scripts as Scripts
+import Numeric.Natural (Natural)
+import Playground.Contract
+import Prelude (Num)
 import qualified Prelude
-import           Language.PlutusTx.Prelude
-import           Playground.Contract
-import           Control.Monad(guard, void)
-import Prelude(Num)
-import           Numeric.Natural(Natural)
-import qualified Language.PlutusTx         as PlutusTx
-import qualified Ledger.Typed.Scripts      as Scripts
-import           Language.Plutus.Contract
 
-data Datum =
-  Open OpenState
-    deriving (Generic)
+data Datum
+  = Open OpenState
+  deriving (Generic)
 
-data OpenState = OpenState {
-  keyAggregate :: MultisigPublicKey,
-  eta :: Eta
+data OpenState = OpenState
+  { keyAggregate :: MultisigPublicKey
+  , eta :: Eta
   -- hMT :: MerkleTreeRoot,
   -- numberOfMembers :: Integer,
   -- contestationPeriod :: Integer
   }
-  deriving Generic
+  deriving (Generic)
 
 data MultisigPublicKey = MultisigPublicKey
 
-data Eta = Eta {
-  utxos :: UTXO,  -- u
-  snapshotNumber :: Integer, -- s
-  transactions :: [Transaction] -- morally a Set
+data Eta = Eta
+  { utxos :: UTXO -- u
+  , snapshotNumber :: Integer -- s
+  , transactions :: [Transaction] -- morally a Set
   }
 
 data UTXO = UTXO
@@ -51,36 +53,37 @@ data Redeemer = Redeemer Xi -- Pi
 
 data Pi
 
-data Xi =
-  Xi { xiUtxos :: UTXO,
-       xiSnapshotNumber :: Integer,
-       signatures :: MultiSignature,
-       confirmedTransactions :: [TransactionObject] -- morally a Set
-     }
+data Xi = Xi
+  { xiUtxos :: UTXO
+  , xiSnapshotNumber :: Integer
+  , signatures :: MultiSignature
+  , confirmedTransactions :: [TransactionObject] -- morally a Set
+  }
 
 validateClose :: Datum -> Redeemer -> ValidatorCtx -> Bool
-validateClose (Open OpenState{keyAggregate,eta}) (Redeemer xi)  _ctx =
+validateClose (Open OpenState{keyAggregate, eta}) (Redeemer xi) _ctx =
   isJust (close keyAggregate eta xi)
 
-{-# INLINABLE close #-}
+{-# INLINEABLE close #-}
 close :: MultisigPublicKey -> Eta -> Xi -> Maybe Eta
 close kAgg eta xi = do
   let (Xi u s sigma txs) = xi
   guard (all (verifyMultisignature kAgg) txs)
   guard (s /= 0 && verifySnapshot kAgg u s sigma)
-  let realU = if s == 0
-              then utxos eta
-              else u
+  let realU =
+        if s == 0
+          then utxos eta
+          else u
       mainchainTxs = map tx txs
   guard (isJust $ applyTransactions realU mainchainTxs)
   pure $ Eta realU s mainchainTxs
 
-{-# INLINABLE verifyMultisignature #-}
+{-# INLINEABLE verifyMultisignature #-}
 verifyMultisignature :: MultisigPublicKey -> TransactionObject -> Bool
-verifyMultisignature kAgg TransactionObject{sigma,tx} =
+verifyMultisignature kAgg TransactionObject{sigma, tx} =
   msAVerify kAgg (hash tx) sigma
 
-{-# INLINABLE verifySnapshot #-}
+{-# INLINEABLE verifySnapshot #-}
 verifySnapshot :: MultisigPublicKey -> UTXO -> Integer -> MultiSignature -> Bool
 verifySnapshot kAgg u s sigma =
   msAVerify kAgg (hash u <> hash s) sigma
@@ -104,45 +107,51 @@ msAVerify _ _ _ = True -- TODO
 
 data Hydra
 instance Scripts.ScriptType Hydra where
-    type instance RedeemerType Hydra = Redeemer
-    type instance DatumType Hydra = Datum
+  type RedeemerType Hydra = Redeemer
+  type DatumType Hydra = Datum
 
+{- ORMOLU_DISABLE -}
 contractInstance :: Scripts.ScriptInstance Hydra
 contractInstance = Scripts.validator @Hydra
     $$(PlutusTx.compile [|| validateClose ||])
     $$(PlutusTx.compile [|| wrap ||]) where
         wrap = Scripts.wrapValidator @Datum @Redeemer
-        
+{- ORMOLU_ENABLE -}
+
 data CollectComParams = CollectComParams
-    { amount     :: Value
-    }
-    deriving stock (Prelude.Eq, Prelude.Show, Generic)
-    deriving anyclass (FromJSON, ToJSON, IotsType, ToSchema, ToArgument)
+  { amount :: Value
+  }
+  deriving stock (Prelude.Eq, Prelude.Show, Generic)
+  deriving anyclass (FromJSON, ToJSON, IotsType, ToSchema, ToArgument)
 
 -- | Our mocked "collectCom" endpoint
 collectComEndpoint :: AsContractError e => Contract Schema e ()
 collectComEndpoint = do
-    CollectComParams amt <- endpoint @"collectCom" @CollectComParams
-    let tx         = Constraints.mustPayToTheScript datum amt
-    void (submitTxConstraints contractInstance tx)
+  CollectComParams amt <- endpoint @"collectCom" @CollectComParams
+  let tx = Constraints.mustPayToTheScript datum amt
+  void (submitTxConstraints contractInstance tx)
  where
-  datum = Open $ OpenState { keyAggregate = MultisigPublicKey
-                           , eta = Eta UTXO 0 []
-                           }
+  datum =
+    Open $
+      OpenState
+        { keyAggregate = MultisigPublicKey
+        , eta = Eta UTXO 0 []
+        }
 
 -- | Our "close" endpoint to trigger a close
 closeEndpoint :: AsContractError e => Contract Schema e ()
 closeEndpoint = pure ()
-    -- GuessParams theGuess <- endpoint @"guess" @GuessParams
-    -- unspentOutputs <- utxoAt gameAddress
-    -- let redeemer = clearString theGuess
-    --     tx       = collectFromScript unspentOutputs redeemer
-    -- void (submitTxConstraintsSpending gameInstance unspentOutputs tx)
-        
-type Schema = 
-    BlockchainActions
-        .\/ Endpoint "collectCom" CollectComParams
-        .\/ Endpoint "close" ()
+
+-- GuessParams theGuess <- endpoint @"guess" @GuessParams
+-- unspentOutputs <- utxoAt gameAddress
+-- let redeemer = clearString theGuess
+--     tx       = collectFromScript unspentOutputs redeemer
+-- void (submitTxConstraintsSpending gameInstance unspentOutputs tx)
+
+type Schema =
+  BlockchainActions
+    .\/ Endpoint "collectCom" CollectComParams
+    .\/ Endpoint "close" ()
 
 endpoints :: AsContractError e => Contract Schema e ()
 endpoints = collectComEndpoint `select` closeEndpoint
