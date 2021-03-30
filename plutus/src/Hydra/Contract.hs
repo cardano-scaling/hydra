bc1970e313b7d601ef6ad1e6527080046ecda423
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-03-30 13:35:41 +0000

    Copy/pasted types from Contract because Plutus

--- plutus/src/Hydra/Contract.hs
+++ plutus/src/Hydra/Contract.hs
@@ -7,22 +7,6 @@ module Hydra.Contract where
 
 import Control.Monad (guard, void)
 import qualified Data.Map as Map
-
-import Hydra.Core (
-  Eta (..),
-  HydraInput (..),
-  HydraState (..),
-  MultiSignature (..),
-  MultisigPublicKey (..),
-  OpenState (..),
-  Transaction (..),
-  TransactionObject (..),
-  UTXO (..),
-  Xi (..),
- )
-
---   close,
---  )
 import Ledger (Address, Validator, ValidatorCtx, Value, scriptAddress)
 import qualified Ledger.Constraints as Constraints
 import Ledger.Tx (TxOut (..), TxOutTx (..))
@@ -33,11 +17,106 @@ import qualified PlutusTx
 import PlutusTx.Prelude
 import qualified Prelude
 
+data Datum
+  = Open OpenState
+  | Closed
+  deriving stock (Prelude.Eq, Generic)
+
+data OpenState = OpenState
+  { keyAggregate :: MultisigPublicKey
+  , eta :: Eta
+  -- hMT :: MerkleTreeRoot,
+  -- numberOfMembers :: Integer,
+  -- contestationPeriod :: Integer
+  }
+  deriving (Prelude.Eq, Generic)
+
+data MultisigPublicKey = MultisigPublicKey
+  deriving (Prelude.Eq, Generic)
+
+data Eta = Eta
+  { utxos :: UTXO -- u
+  , snapshotNumber :: Integer -- s
+  , transactions :: [Transaction] -- morally a Set
+  }
+  deriving (Prelude.Eq, Generic)
+
+data UTXO = UTXO
+  deriving (Prelude.Eq, Generic)
+
+-- | The transaction as handled in the hydra head, i.e. the tx which we have put
+-- into Hydra. According to isomorphism property of Hydra, it could also have
+-- been put on the main chain.
+data Transaction = Transaction
+  deriving (Prelude.Eq, Generic)
+
+data TransactionObject = TransactionObject
+  { sigma :: MultiSignature
+  , tx :: Transaction
+  }
+
+data MultiSignature = MultiSignature
+
+data MerkleTreeRoot = MerkleTreeRoot
+
+data Redeemer = Redeemer Xi -- Pi
+
+data Pi
+
+data Xi = Xi
+  { xiUtxos :: UTXO
+  , xiSnapshotNumber :: Integer
+  , signatures :: MultiSignature
+  , confirmedTransactions :: [TransactionObject] -- morally a Set
+  }
+
 {-# INLINEABLE validate #-}
-validate :: HydraState -> HydraInput -> ValidatorCtx -> Bool
-validate (Open OpenState{keyAggregate, eta}) (HydraInput xi) _ctx =
+validate :: Datum -> Redeemer -> ValidatorCtx -> Bool
+validate (Open OpenState{keyAggregate, eta}) (Redeemer xi) _ctx =
   isJust (close keyAggregate eta xi)
 validate Closed _ _ = False
+
+{-# INLINEABLE close #-}
+close :: MultisigPublicKey -> Eta -> Xi -> Maybe Eta
+close kAgg eta xi = do
+  let (Xi u s sigma txs) = xi
+  guard (all (verifyMultisignature kAgg) txs)
+  guard (s == 0 || verifySnapshot kAgg u s sigma)
+  let realU =
+        if s == 0
+          then utxos eta
+          else u
+      mainchainTxs = map tx txs
+  guard (isJust $ applyTransactions realU mainchainTxs)
+  pure $ Eta realU s mainchainTxs
+
+{-# INLINEABLE verifyMultisignature #-}
+verifyMultisignature :: MultisigPublicKey -> TransactionObject -> Bool
+verifyMultisignature kAgg TransactionObject{sigma, tx} =
+  msAVerify kAgg (hash $ serialize tx) sigma
+
+{-# INLINEABLE verifySnapshot #-}
+verifySnapshot :: MultisigPublicKey -> UTXO -> Integer -> MultiSignature -> Bool
+verifySnapshot kAgg u s sigma =
+  msAVerify kAgg (hash $ serialize u <> serialize s) sigma
+
+-- | This is only about folding the transactions onto a UTXO and no evaluation
+-- whatsoever.
+applyTransactions :: UTXO -> [Transaction] -> Maybe UTXO
+applyTransactions u _ = Just u -- TODO
+
+--
+-- Primitives we need
+--
+
+serialize :: a -> ByteString
+serialize = const "reuse plutus tx's isData stuff" -- TODO
+
+hash :: ByteString -> ByteString
+hash = const "hashed bytestring" -- TODO
+
+msAVerify :: MultisigPublicKey -> ByteString -> MultiSignature -> Bool
+msAVerify _ _ _ = True -- TODO
 
 --
 -- Boilerplate
@@ -45,15 +124,15 @@ validate Closed _ _ = False
 
 data Hydra
 instance Scripts.ScriptType Hydra where
-  type RedeemerType Hydra = HydraInput
-  type DatumType Hydra = HydraState
+  type RedeemerType Hydra = Redeemer
+  type DatumType Hydra = Datum
 
 {- ORMOLU_DISABLE -}
 contractInstance :: Scripts.ScriptInstance Hydra
 contractInstance = Scripts.validator @Hydra
     $$(PlutusTx.compile [|| validate ||])
     $$(PlutusTx.compile [|| wrap ||]) where
-        wrap = Scripts.wrapValidator @HydraState @HydraInput
+        wrap = Scripts.wrapValidator @Datum @Redeemer
 {- ORMOLU_ENABLE -}
 
 -- | The validator script of the contract.
@@ -100,7 +179,7 @@ closeEndpoint = do
   void (submitTxConstraintsSpending contractInstance utxoMap tx)
  where
   datum = Closed -- TODO add more things
-  redeemer = HydraInput $ Xi UTXO 0 MultiSignature []
+  redeemer = Redeemer $ Xi UTXO 0 MultiSignature []
 
 type Schema =
   BlockchainActions
@@ -111,47 +190,27 @@ hydraHead :: AsContractError e => Contract () Schema e ()
 hydraHead = collectComEndpoint `select` closeEndpoint
 
 --
--- State Transitions
+-- Template Haskell
 --
 
-{-# INLINEABLE close #-}
-close :: MultisigPublicKey -> Eta -> Xi -> Maybe Eta
-close kAgg eta xi = do
-  let (Xi u s sigma txs) = xi
-  guard (all (verifyMultisignature kAgg) txs)
-  guard (s == 0 || verifySnapshot kAgg u s sigma)
-  let realU =
-        if s == 0
-          then utxos eta
-          else u
-      mainchainTxs = map tx txs
-  guard (isJust $ applyTransactions realU mainchainTxs)
-  pure $ Eta realU s mainchainTxs
+PlutusTx.makeLift ''Datum
+PlutusTx.makeLift ''OpenState
+PlutusTx.makeLift ''MultisigPublicKey
+PlutusTx.makeLift ''Eta
+PlutusTx.makeLift ''MerkleTreeRoot
+PlutusTx.makeLift ''TransactionObject
+PlutusTx.makeLift ''Transaction
+PlutusTx.makeLift ''UTXO
+PlutusTx.makeLift ''MultiSignature
 
-{-# INLINEABLE verifyMultisignature #-}
-verifyMultisignature :: MultisigPublicKey -> TransactionObject -> Bool
-verifyMultisignature kAgg TransactionObject{sigma, tx} =
-  msAVerify kAgg (hash $ serialize tx) sigma
-
-{-# INLINEABLE verifySnapshot #-}
-verifySnapshot :: MultisigPublicKey -> UTXO -> Integer -> MultiSignature -> Bool
-verifySnapshot kAgg u s sigma =
-  msAVerify kAgg (hash $ serialize u <> serialize s) sigma
-
--- | This is only about folding the transactions onto a UTXO and no evaluation
--- whatsoever.
-applyTransactions :: UTXO -> [Transaction] -> Maybe UTXO
-applyTransactions u _ = Just u -- TODO
-
---
--- Primitives we need
---
-
-serialize :: a -> ByteString
-serialize = const "reuse plutus tx's isData stuff" -- TODO
-
-hash :: ByteString -> ByteString
-hash = const "hashed bytestring" -- TODO
-
-msAVerify :: MultisigPublicKey -> ByteString -> MultiSignature -> Bool
-msAVerify _ _ _ = True -- TODO
+PlutusTx.unstableMakeIsData ''Datum
+PlutusTx.unstableMakeIsData ''OpenState
+PlutusTx.unstableMakeIsData ''MultisigPublicKey
+PlutusTx.unstableMakeIsData ''Eta
+PlutusTx.unstableMakeIsData ''MerkleTreeRoot
+PlutusTx.unstableMakeIsData ''TransactionObject
+PlutusTx.unstableMakeIsData ''Transaction
+PlutusTx.unstableMakeIsData ''UTXO
+PlutusTx.unstableMakeIsData ''MultiSignature
+PlutusTx.unstableMakeIsData ''Redeemer
+PlutusTx.unstableMakeIsData ''Xi
--- plutus/src/Hydra/ContractStateMachine.hs
+++ plutus/src/Hydra/ContractStateMachine.hs
@@ -5,11 +5,12 @@
 
 module Hydra.ContractStateMachine where
 
-import Control.Monad (guard, void)
-import qualified Data.Map as Map
-import Ledger (Address, Validator, ValidatorCtx, Value, scriptAddress)
-import qualified Ledger.Constraints as Constraints
-import Ledger.Tx (TxOut (..), TxOutTx (..))
+import Control.Monad (guard)
+
+import Ledger (Address, Validator, Value, scriptAddress)
+
+--import qualified Ledger.Constraints as Constraints
+-- import Ledger.Tx (TxOut (..), TxOutTx (..))
 import qualified Ledger.Typed.Scripts as Scripts
 import Playground.Contract
 import Plutus.Contract
@@ -21,9 +22,100 @@ import qualified Prelude
 
 import Hydra.Contract ()
 
-data HydraState = Closed
+data HydraState
+  = Open OpenState
+  | Closed
+  deriving stock (Prelude.Eq, Generic)
 
-data HydraInput = HydraInput
+data OpenState = OpenState
+  { keyAggregate :: MultisigPublicKey
+  , eta :: Eta
+  -- hMT :: MerkleTreeRoot,
+  -- numberOfMembers :: Integer,
+  -- contestationPeriod :: Integer
+  }
+  deriving (Prelude.Eq, Generic)
+
+data MultisigPublicKey = MultisigPublicKey
+  deriving (Prelude.Eq, Generic)
+
+data Eta = Eta
+  { utxos :: UTXO -- u
+  , snapshotNumber :: Integer -- s
+  , transactions :: [Transaction] -- morally a Set
+  }
+  deriving (Prelude.Eq, Generic)
+
+data UTXO = UTXO
+  deriving (Prelude.Eq, Generic)
+
+-- | The transaction as handled in the hydra head, i.e. the tx which we have put
+-- into Hydra. According to isomorphism property of Hydra, it could also have
+-- been put on the main chain.
+data Transaction = Transaction
+  deriving (Prelude.Eq, Generic)
+
+data TransactionObject = TransactionObject
+  { sigma :: MultiSignature
+  , tx :: Transaction
+  }
+
+data MultiSignature = MultiSignature
+
+data MerkleTreeRoot = MerkleTreeRoot
+
+data HydraInput = HydraInput Xi -- Pi
+
+data Pi
+
+data Xi = Xi
+  { xiUtxos :: UTXO
+  , xiSnapshotNumber :: Integer
+  , signatures :: MultiSignature
+  , confirmedTransactions :: [TransactionObject] -- morally a Set
+  }
+
+{-# INLINEABLE close #-}
+close :: MultisigPublicKey -> Eta -> Xi -> Maybe Eta
+close kAgg eta xi = do
+  let (Xi u s sigma txs) = xi
+  guard (all (verifyMultisignature kAgg) txs)
+  guard (s == 0 || verifySnapshot kAgg u s sigma)
+  let realU =
+        if s == 0
+          then utxos eta
+          else u
+      mainchainTxs = map tx txs
+  guard (isJust $ applyTransactions realU mainchainTxs)
+  pure $ Eta realU s mainchainTxs
+
+{-# INLINEABLE verifyMultisignature #-}
+verifyMultisignature :: MultisigPublicKey -> TransactionObject -> Bool
+verifyMultisignature kAgg TransactionObject{sigma, tx} =
+  msAVerify kAgg (hash $ serialize tx) sigma
+
+{-# INLINEABLE verifySnapshot #-}
+verifySnapshot :: MultisigPublicKey -> UTXO -> Integer -> MultiSignature -> Bool
+verifySnapshot kAgg u s sigma =
+  msAVerify kAgg (hash $ serialize u <> serialize s) sigma
+
+-- | This is only about folding the transactions onto a UTXO and no evaluation
+-- whatsoever.
+applyTransactions :: UTXO -> [Transaction] -> Maybe UTXO
+applyTransactions u _ = Just u -- TODO
+
+--
+-- Primitives we need
+--
+
+serialize :: a -> ByteString
+serialize = const "reuse plutus tx's isData stuff" -- TODO
+
+hash :: ByteString -> ByteString
+hash = const "hashed bytestring" -- TODO
+
+msAVerify :: MultisigPublicKey -> ByteString -> MultiSignature -> Bool
+msAVerify _ _ _ = True -- TODO
 
 {-# INLINEABLE transition #-}
 transition ::
@@ -101,7 +193,7 @@ closeEndpoint = do
 --  void (submitTxConstraintsSpending contractInstance utxoMap tx)
 -- where
 --  datum    = Closed -- TODO add more things
---  redeemer = Redeemer $ Xi UTXO 0 MultiSignature []
+--  redeemer = HydraInput $ Xi UTXO 0 MultiSignature []
 
 type Schema =
   BlockchainActions
@@ -115,7 +207,24 @@ hydraHead = collectComEndpoint `select` closeEndpoint -- TODO loop here?
 -- Template Haskell
 --
 
-PlutusTx.unstableMakeIsData ''HydraState
 PlutusTx.makeLift ''HydraState
+PlutusTx.makeLift ''OpenState
+PlutusTx.makeLift ''MultisigPublicKey
+PlutusTx.makeLift ''Eta
+PlutusTx.makeLift ''MerkleTreeRoot
+PlutusTx.makeLift ''TransactionObject
+PlutusTx.makeLift ''Transaction
+PlutusTx.makeLift ''UTXO
+PlutusTx.makeLift ''MultiSignature
+
+PlutusTx.unstableMakeIsData ''HydraState
+PlutusTx.unstableMakeIsData ''OpenState
+PlutusTx.unstableMakeIsData ''MultisigPublicKey
+PlutusTx.unstableMakeIsData ''Eta
+PlutusTx.unstableMakeIsData ''MerkleTreeRoot
+PlutusTx.unstableMakeIsData ''TransactionObject
+PlutusTx.unstableMakeIsData ''Transaction
+PlutusTx.unstableMakeIsData ''UTXO
+PlutusTx.unstableMakeIsData ''MultiSignature
 PlutusTx.unstableMakeIsData ''HydraInput
-PlutusTx.makeLift ''HydraInput
+PlutusTx.unstableMakeIsData ''Xi
