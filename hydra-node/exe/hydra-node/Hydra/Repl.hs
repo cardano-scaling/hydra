a743911e3db80e9e3ee9202204d7176be2ae9df0
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-05-07 12:35:33 +0200

    Fix Repl

--- hydra-node/exe/hydra-node/Hydra/Repl.hs
+++ hydra-node/exe/hydra-node/Hydra/Repl.hs
@@ -2,16 +2,11 @@ module Hydra.Repl where
 
 import Cardano.Prelude
 
-import Hydra.Ledger (LedgerState)
 import Hydra.Logic (ClientRequest (..))
-import Hydra.Node (HydraNode, handleCommand)
+import Hydra.Node (HydraNode, handleClientRequest)
 import System.Console.Repline (CompleterStyle (Word0), ExitDecision (Exit), evalRepl)
 
-startHydraRepl ::
-  Show (LedgerState tx) => -- TODO(SN): leaky abstraction of HydraHead
-  Show tx =>
-  HydraNode tx IO ->
-  IO ()
+startHydraRepl :: HydraNode tx IO -> IO ()
 startHydraRepl node = link =<< async runRepl
  where
   runRepl = evalRepl (const $ pure prompt) replCommand [] Nothing Nothing (Word0 replComplete) replInit (pure Exit)
@@ -22,15 +17,11 @@ startHydraRepl node = link =<< async runRepl
   commands = ["init", "commit", "newtx", "close", "contest"]
 
   replCommand c
-    | c == "init" =
-      liftIO $
-        handleCommand node Init >>= \case
-          Left e -> putText $ "You dummy.. " <> show e
-          Right _ -> pure ()
-    | c == "close" = liftIO $ void $ handleCommand node Close
+    | c == "init" = liftIO $ handleClientRequest node Init
+    | c == "close" = liftIO $ handleClientRequest node Close
     -- c == "commit" =
     | c == "newtx" = liftIO $ do
-      loadTx "hardcoded/file/path" >>= void . handleCommand node . NewTx
+      loadTx "hardcoded/file/path" >>= handleClientRequest node . NewTx
     -- c == "contest" =
     | otherwise = liftIO $ putText $ "Unknown command, use any of: " <> show commands
 
