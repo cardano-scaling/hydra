1ee862815f7fdc5cb2cdbdb9668cab3d245a07ae
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-05-17 11:43:04 +0000

    Introduce test for FireForget protocol

---
+++ hydra-node/test/Hydra/FireForgetSpec.hs
@@ -0,0 +1,9 @@
+module Hydra.FireForgetSpec where
+
+import Cardano.Prelude
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Fire-Forget Ouroboros Protocol" $ do
+  it "client can send 'Hail Hydra!' to server" $ do
+    pending
