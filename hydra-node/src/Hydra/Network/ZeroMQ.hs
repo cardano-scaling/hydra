3d1152f6891b61f1b1c7f3f056f9e273d1ba4f19
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-05-18 14:23:17 +0000

    Invert bind/connect for Sub/Pub sockets

--- hydra-node/src/Hydra/Network/ZeroMQ.hs
+++ hydra-node/src/Hydra/Network/ZeroMQ.hs
@@ -19,28 +19,27 @@ withZeroMQHydraNetwork ::
   IO ()
 withZeroMQHydraNetwork localHost remoteHosts incomingCallback continuation = do
   mvar <- newEmptyTMVarIO
-  race_ (runServer localHost mvar) $
-    race_ (runClients remoteHosts incomingCallback) $ do
+  race_ (runServer mvar) $
+    race_ (runClients incomingCallback) $ do
       continuation $ HydraNetwork (atomically . putTMVar mvar)
  where
   toZMQAddress (hostName, port) = "tcp://" <> hostName <> ":" <> port
 
-  runServer host queue = runZMQ $ do
-    let hostAddress = toZMQAddress host
+  runServer queue = runZMQ $ do
+    let peerAddresses = map toZMQAddress remoteHosts
     pub <- socket Pub
-    bind pub hostAddress
+    forM_ peerAddresses (connect pub)
     forever $ do
       hydraMessage <- liftIO $ atomically $ takeTMVar queue
       let encoded = serialise hydraMessage
       -- TODO use proper tracers
-      hPutStrLn @Text stdout $ show host <> ": sending " <> show hydraMessage <> ", encoded as: " <> show encoded
+      hPutStrLn @Text stdout $ show localHost <> ": sending " <> show hydraMessage <> ", encoded as: " <> show encoded
       send pub [] $ LBS.toStrict encoded
 
-  runClients peers callback = runZMQ $ do
-    let peerAddresses = map toZMQAddress peers
+  runClients callback = runZMQ $ do
     sub <- socket Sub
     subscribe sub ""
-    forM_ peerAddresses (connect sub)
+    bind sub $ toZMQAddress localHost
     forever $ do
       msg <- receive sub
       hPutStrLn @Text stdout $ show localHost <> ": receiving " <> show msg
