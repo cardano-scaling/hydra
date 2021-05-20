15422f3e4a121981b9508965b1c8b93cd8c2c0e3
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-05-20 15:45:32 +0200

    Properly accept connections on the unix socket
    This is done very dirty as it only allows a single connection and
    explodes after that connection is closed

--- hydra-node/exe/hydra-node/Main.hs
+++ hydra-node/exe/hydra-node/Main.hs
@@ -9,27 +9,30 @@ import Data.Text (pack)
 import qualified Hydra.Ledger.Mock as Ledger
 import Hydra.Logic (Party (Party))
 import Hydra.Node (HydraNode, createHydraNode, handleClientRequest, runHydraNode)
-import Network.Socket (Family (AF_UNIX), SockAddr (SockAddrUnix), SocketType (Stream), bind, defaultProtocol, listen, socket, socketToHandle)
+import Network.Socket (Family (AF_UNIX), SockAddr (SockAddrUnix), SocketType (Stream), accept, bind, defaultProtocol, listen, socket, socketToHandle)
 import System.Directory (removeFile)
 import System.IO (hGetLine, hPrint)
 
 main :: IO ()
 main = do
   [nodeId] <- getArgs
+  -- TODO(SN): this is leaking resources (definitely)
   h <- openUnixSocket ("hydra.socket." <> nodeId)
   case readMaybe nodeId of
-      _ <- async $ runAPIServer h node
-      runHydraNode node
     Nothing -> panic $ "invalid nodeId argument, should be a number: " <> pack nodeId
     Just n -> do
       node <- createHydraNode (Party n) Ledger.mockLedger (hPrint h)
+      race_
+        (runAPIServer @Ledger.MockTx h node)
+        (runHydraNode node)
 
 runAPIServer :: Read tx => Handle -> HydraNode tx IO -> IO ()
 runAPIServer h node = forever $ do
-  input <- hGetLine h
-  case readMaybe input of
-    Just command -> handleClientRequest node command
-    Nothing -> hPutStrLn h $ "Invalid command: " <> input
+  try @IOException (hGetLine h) >>= \case
+    Left ex -> putText $ "[API] error reading: " <> show ex
+    Right input -> case readMaybe input of
+      Just command -> handleClientRequest node command
+      Nothing -> hPutStrLn h $ "[API] Invalid command: " <> input
 
 openUnixSocket :: FilePath -> IO Handle
 openUnixSocket socketPath = do
@@ -37,4 +40,5 @@ openUnixSocket socketPath = do
   s <- socket AF_UNIX Stream defaultProtocol
   bind s $ SockAddrUnix socketPath
   listen s 1
-  socketToHandle s ReadWriteMode
+  (conn, _peer) <- accept s
+  socketToHandle conn ReadWriteMode
