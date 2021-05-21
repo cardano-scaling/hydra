1dbefe0ef3ee6dd29db04a3a3dee7f75875788b6
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-05-21 08:02:24 +0200

    Update to cabal 3.4.0.0 and remove freeze file
    Not sure why my cabal builds don't produce such a freeze file and so far
    it only conflicted with my cabal.project.local re-configuration (for the
    'development' flag)

--- shell.nix
+++ shell.nix
@@ -17,27 +17,27 @@
       "https://github.com/input-output-hk/cardano-node/archive/8fe46140a52810b6ca456be01d652ca08fe730bf.tar.gz")
     { gitrev = "8fe46140a52810b6ca456be01d652ca08fe730bf"; }
 }:
-with pkgs;
 let
-  hls = haskell-nix.tool compiler "haskell-language-server" "latest";
-  ghc = haskell-nix.compiler.${compiler};
-  fourmolu = haskell-nix.tool compiler "fourmolu" "latest";
+  hls = pkgs.haskell-nix.tool compiler "haskell-language-server" "latest";
+  ghc = pkgs.haskell-nix.compiler.${compiler};
+  cabal-install = pkgs.haskell-nix.tool compiler "cabal-install" "3.4.0.0";
+  fourmolu = pkgs.haskell-nix.tool compiler "fourmolu" "latest";
   ghcid = pkgs.haskellPackages.ghcid;
   hspec-discover = pkgs.haskellPackages.hspec-discover;
-  libsodium-vrf = libsodium.overrideAttrs (oldAttrs: {
+  libsodium-vrf = pkgs.libsodium.overrideAttrs (oldAttrs: {
     name = "libsodium-1.0.18-vrf";
-    src = fetchFromGitHub {
+    src = pkgs.fetchFromGitHub {
       owner = "input-output-hk";
       repo = "libsodium";
       # branch tdammers/rebased-vrf
       rev = "66f017f16633f2060db25e17c170c2afa0f2a8a1";
       sha256 = "12g2wz3gyi69d87nipzqnq4xc6nky3xbmi2i2pb2hflddq8ck72f";
     };
-    nativeBuildInputs = [ autoreconfHook ];
+    nativeBuildInputs = [ pkgs.autoreconfHook ];
     configureFlags = "--enable-static";
   });
 in
-mkShell rec {
+pkgs.mkShell rec {
   name = "hydra-node-env";
 
   tools = [
@@ -48,7 +48,7 @@ mkShell rec {
     ghcid
     hspec-discover
     # For discovering libs (below)
-    pkgconfig
+    pkgs.pkgconfig
     # Used in local-cluster
     cardanoNodePkgs.cardano-node
     cardanoNodePkgs.cardano-cli
@@ -56,15 +56,15 @@ mkShell rec {
 
   libs = [
     libsodium-vrf
-    systemd
-    zlib
-    zeromq
+    pkgs.systemd
+    pkgs.zlib
+    pkgs.zeromq
   ];
 
   buildInputs = tools ++ libs;
 
   # Ensure that libz.so and other libraries are available to TH splices.
-  LD_LIBRARY_PATH = lib.makeLibraryPath libs;
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
 
   # Force a UTF-8 locale because many Haskell programs and tests
   # assume this.
@@ -72,6 +72,6 @@ mkShell rec {
 
   # Make the shell suitable for the stack nix integration
   # <nixpkgs/pkgs/development/haskell-modules/generic-stack-builder.nix>
-  GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
   STACK_IN_NIX_SHELL = "true";
 }
