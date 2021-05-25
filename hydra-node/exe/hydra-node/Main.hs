281a945dece767a45b0a8a75d8e6d0c8483a7da4
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-05-25 12:23:12 +0000

    Actually send responses to all connected clients

--- hydra-node/exe/hydra-node/Main.hs
+++ hydra-node/exe/hydra-node/Main.hs
@@ -4,10 +4,12 @@ module Main where
 
 import Cardano.Prelude
 
+import Control.Concurrent.STM (TChan, dupTChan, newBroadcastTChanIO, readTChan, writeTChan)
 import Data.Text (pack)
 import qualified Data.Text as Text
 import qualified Hydra.Ledger.Mock as Ledger
 import Hydra.Logic (
+  ClientResponse,
   Environment (..),
   Event (..),
   HeadParameters (..),
@@ -26,7 +28,7 @@ import Hydra.Node (
   handleClientRequest,
   runHydraNode,
  )
-import Network.WebSockets (acceptRequest, receiveData, runServer, withPingThread)
+import Network.WebSockets (acceptRequest, receiveData, runServer, sendTextData, withPingThread)
 
 main :: IO ()
 main = do
@@ -40,31 +42,41 @@ main = do
       hh <- createHydraHead headState Ledger.mockLedger
       oc <- createMockChainClient env eq
       withZeroMQHydraNetwork (me n) (them n) (putEvent eq . NetworkEvent) $ \hn -> do
-        let sendResponse = putText . ("Should send response: " <>) . show
+        responseChannel <- newBroadcastTChanIO
+        let sendResponse = atomically . writeTChan responseChannel
         let node = HydraNode{eq, hn, hh, oc, sendResponse, env}
         race_
-          (runAPIServer node)
+          (runAPIServer responseChannel node)
           (runHydraNode node)
  where
   -- HACK(SN): Obviously we should configure the node instead
   me nodeId = ("127.0.0.1", show $ 5000 + nodeId)
   them nodeId = [("127.0.0.1", show $ 5000 + id) | id <- [1 .. 3], id /= nodeId]
 
-runAPIServer :: (Show tx, Read tx) => HydraNode tx IO -> IO ()
-runAPIServer node = do
+runAPIServer :: (Show tx, Read tx) => TChan (ClientResponse tx) -> HydraNode tx IO -> IO ()
+runAPIServer responseChannel node = do
   logAPI $ "Listening on port " <> show port
   runServer "0.0.0.0" port $ \pending -> do
     con <- acceptRequest pending
+    chan <- atomically $ dupTChan responseChannel
     logAPI "Accepted new connection"
     withPingThread con 30 (pure ()) $
-      forever $ do
-        msg <- receiveData con
-        case readMaybe (Text.unpack msg) of
-          Just request -> do
-            logAPI $ "Received request: " <> show request
-            handleClientRequest node request
-          Nothing -> logAPI $ "Invalid request: " <> msg
+      race_ (receiveRequests con) (sendResponses chan con)
  where
+  sendResponses chan con = forever $ do
+    response <- atomically $ readTChan chan
+    sendTextData con (show @_ @Text response)
+
+  receiveRequests con = forever $ do
+    msg <- receiveData con
+    case readMaybe (Text.unpack msg) of
+      Just request -> do
+        logAPI $ "Received request: " <> show request
+        handleClientRequest node request
+      Nothing -> logAPI $ "Invalid request: " <> msg
+
   nodeId = party $ env node
+
   port = fromIntegral $ 4000 + nodeId
+
   logAPI t = putText $ "[API:" <> show nodeId <> "] " <> t
