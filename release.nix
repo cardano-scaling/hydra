a77aece348414e41c9312916adaa54fe81228487
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-06-01 17:29:19 +0200

    [nix] Try use iohk-nix.nixpkgs instead

--- release.nix
+++ release.nix
@@ -1,12 +1,12 @@
 # Hydra release jobset
 
-{ pkgs ? import
-    (builtins.fetchTarball {
-      url = "https://github.com/nixos/nixpkgs/archive/1232484e2bbd1c84f968010ea5043a73111f7549.tar.gz";
-      sha256 = "0d455yjl9z2vmgxxkn5ib490sia8051myr1zsk6dspyvkpmm5j88";
-    })
-    { }
-}:
+{ ... }:
+let
+  sources = import ./nix/sources.nix { };
+  iohkNix = import sources.iohk-nix { };
+  nixpkgs = iohkNix.nixpkgs;
+  pkgs = import nixpkgs { };
+in
 {
   # Keep this until we have a proper job
   hello = pkgs.hello;
