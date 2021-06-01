56e61d236bd3e6155cec4425fee2b18e12e243ad
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-06-01 16:39:12 +0200

    [nix] Add a sha256 to fetchTarball

--- release.nix
+++ release.nix
@@ -1,8 +1,10 @@
 # Hydra release jobset
 
 { pkgs ? import
-    (builtins.fetchTarball
-      "https://github.com/nixos/nixpkgs/archive/1232484e2bbd1c84f968010ea5043a73111f7549.tar.gz")
+    (builtins.fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/1232484e2bbd1c84f968010ea5043a73111f7549.tar.gz";
+      sha256 = "0d455yjl9z2vmgxxkn5ib490sia8051myr1zsk6dspyvkpmm5j88";
+    })
     { }
 }:
 {
