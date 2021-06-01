2efef86fded41720cc575682e5f955123ef4e345
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-06-01 15:49:44 +0200

    Use pinned nixpkgs for 'hello' job

--- release.nix
+++ release.nix
@@ -1,6 +1,10 @@
 # Hydra release jobset
 
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import
+    (builtins.fetchTarball
+      "https://github.com/nixos/nixpkgs/archive/1232484e2bbd1c84f968010ea5043a73111f7549.tar.gz")
+    { }
+}:
 {
   # Keep this until we have a proper job
   hello = pkgs.hello;
