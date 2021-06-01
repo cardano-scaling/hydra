4235cd9d5695d8801d530393166faa3bae8fdf5e
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-06-01 17:46:29 +0200

    [nix] Add 'shell' job using as-is shell.nix

--- release.nix
+++ release.nix
@@ -12,10 +12,8 @@ let
 in
 {
   # Keep this until we have a proper job
-  hello = pkgs.hello.overrideAttrs (oldAttrs: rec {
-    separateDebugInfo = true;
-  });
+  hello = pkgs.hello;
 
   # TODO(SN): Build shell derivation to cache it, but mkShell can't be built?
-  # shell = ./shell.nix { };
+  shell = import ./shell.nix { };
 }
