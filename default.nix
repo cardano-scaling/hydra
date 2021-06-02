79fed19d4fb8ec56a46856444bc91dd844e9723b
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-06-02 08:31:41 +0200

    [nix] Bump haskell.nix for cabal 3.4 support

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { compiler ? "ghc8104"
 , haskellNix ? import
     (builtins.fetchTarball
-      "https://github.com/input-output-hk/haskell.nix/archive/abb289ff961fb1bd98eb0340088c432f15109807.tar.gz")
+      "https://github.com/input-output-hk/haskell.nix/archive/532e71470b41fc0fd0d3d858ea98d7f07f37d309.tar.gz")
     { }
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@
   # Latest haskell.nix for more likely cache hits
 , haskellNix ? import
     (builtins.fetchTarball
-      "https://github.com/input-output-hk/haskell.nix/archive/abb289ff961fb1bd98eb0340088c432f15109807.tar.gz")
+      "https://github.com/input-output-hk/haskell.nix/archive/532e71470b41fc0fd0d3d858ea98d7f07f37d309.tar.gz")
     { }
   # Use same pkgs as haskell.nix for more likely cache hits
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
