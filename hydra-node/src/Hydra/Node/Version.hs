5f760237010d67013025db233c7927cafd050e9c
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-06-04 07:58:11 +0000

    Remove use of file-embed monkey patching hack

--- hydra-node/src/Hydra/Node/Version.hs
+++ hydra-node/src/Hydra/Node/Version.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
--- | Provides version number optionally with Git SHA1
---
--- Shamelessly stolen from <cardano-wallet https://github.com/input-output-hk/cardano-wallet/blob/master/lib/core/src/Cardano/Wallet/Version.hs>
+-- | Provides version number with optional Git SHA1.
 module Hydra.Node.Version (
   -- * Values computed at compile-time
   version,
@@ -17,15 +15,9 @@ module Hydra.Node.Version (
 ) where
 
 import Cardano.Prelude
-import Data.FileEmbed (
-  dummySpaceWith,
- )
 import Data.String (
   String,
   fromString,
- )
-import Data.Text.Encoding (
-  decodeLatin1,
  )
 import Data.Version (
   Version,
@@ -47,21 +39,12 @@ showFullVersion v (GitRevision r) =
 
 -- | The Git revision ID (40 character hex string) of this build.
 --
--- This requires @git@ do be available when building. Alternatively, the git
--- revision of the @cardano-wallet@ binary can be updated post-build using
--- "Data.FileEmbed.injectWith".
+-- This requires @git@ do be available when building.
 gitRevision :: GitRevision
 gitRevision
-  | gitRevEmbed /= zeroRev = GitRevision gitRevEmbed
   | T.null fromGit = GitRevision zeroRev
   | otherwise = GitRevision fromGit
  where
-  -- Git revision embedded after compilation using
-  -- Data.FileEmbed.injectWith. If nothing has been injected,
-  -- this will be filled with 0 characters.
-  gitRevEmbed :: Text
-  gitRevEmbed = decodeLatin1 $(dummySpaceWith "gitrev" 40)
-
   -- Git revision found during compilation by running git. If
   -- git could not be run, then this will be empty.
   fromGit = T.strip (fromString $(gitHash))
