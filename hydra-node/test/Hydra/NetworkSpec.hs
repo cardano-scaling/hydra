49bd67e3d15dc9a6748170c96f445f7748c999ff
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-06-09 15:09:31 +0000

    Make NetworkSpec more reliable
    As Heartbeat logic has moved out of Network layer, ensuring messages
    are properly broadcasted requires actively retrying until they are
    seen on all nodes, as startup time might lead to messages being
    dropped.

--- hydra-node/test/Hydra/NetworkSpec.hs
+++ hydra-node/test/Hydra/NetworkSpec.hs
@@ -10,12 +10,15 @@ import Cardano.Binary (FromCBOR, ToCBOR, fromCBOR, toCBOR)
 import Codec.CBOR.Read (deserialiseFromBytes)
 import Codec.CBOR.Write (toLazyByteString)
 import Codec.Serialise (Serialise, deserialiseOrFail, serialise)
+import Control.Monad.Class.MonadAsync (concurrently_)
 import Control.Monad.Class.MonadTime (DiffTime)
 import Control.Monad.Class.MonadTimer (timeout)
 import Hydra.HeadLogic (HydraMessage (..))
 import Hydra.Logging (nullTracer)
+import Hydra.Network (HydraNetwork)
 import Hydra.Network.Ouroboros (broadcast, withOuroborosHydraNetwork)
 import Hydra.Network.ZeroMQ (withZeroMQHydraNetwork)
+import Test.HUnit.Lang (HUnitFailure)
 import Test.Hspec (Spec, describe, expectationFailure, it, pendingWith, shouldReturn)
 import Test.QuickCheck (Arbitrary (..), Positive (getPositive), arbitrary, oneof, property)
 
@@ -45,17 +48,10 @@ spec = describe "Networking layer" $ do
         withOuroborosHydraNetwork @(HydraMessage Integer) (lo, 45678) [(lo, 45679), (lo, 45680)] (putMVar node1received) $ \hn1 ->
           withOuroborosHydraNetwork (lo, 45679) [(lo, 45678), (lo, 45680)] (putMVar node2received) $ \hn2 -> do
             withOuroborosHydraNetwork (lo, 45680) [(lo, 45678), (lo, 45679)] (putMVar node3received) $ \hn3 -> do
-              broadcast hn1 requestTx
-              failAfter 1 $ takeMVar node2received `shouldReturn` requestTx
-              failAfter 1 $ takeMVar node3received `shouldReturn` requestTx
-
-              broadcast hn2 requestTx
-              failAfter 1 $ takeMVar node1received `shouldReturn` requestTx
-              failAfter 1 $ takeMVar node3received `shouldReturn` requestTx
-
-              broadcast hn3 requestTx
-              failAfter 1 $ takeMVar node1received `shouldReturn` requestTx
-              failAfter 1 $ takeMVar node2received `shouldReturn` requestTx
+              concurrently_ (assertBroadcastFrom requestTx hn1 [node2received, node3received]) $
+                concurrently_
+                  (assertBroadcastFrom requestTx hn2 [node1received, node3received])
+                  (assertBroadcastFrom requestTx hn3 [node2received, node1received])
 
     it "broadcasts messages to itself" $ do
       pendingWith "not tested yet"
@@ -65,25 +61,26 @@ spec = describe "Networking layer" $ do
       node1received <- newEmptyMVar
       node2received <- newEmptyMVar
       node3received <- newEmptyMVar
-      failAfter 3 $ do
+      failAfter 10 $ do
         withZeroMQHydraNetwork (lo, 55677) [(lo, 55678), (lo, 55679)] nullTracer (putMVar node1received) $ \hn1 ->
           withZeroMQHydraNetwork (lo, 55678) [(lo, 55677), (lo, 55679)] nullTracer (putMVar node2received) $ \hn2 ->
             withZeroMQHydraNetwork (lo, 55679) [(lo, 55677), (lo, 55678)] nullTracer (putMVar node3received) $ \hn3 -> do
-              broadcast hn1 requestTx
-              failAfter 1 $ takeMVar node2received `shouldReturn` requestTx
-              failAfter 1 $ takeMVar node3received `shouldReturn` requestTx
-
-              broadcast hn2 requestTx
-              failAfter 1 $ takeMVar node1received `shouldReturn` requestTx
-              failAfter 1 $ takeMVar node3received `shouldReturn` requestTx
-
-              broadcast hn3 requestTx
-              failAfter 1 $ takeMVar node1received `shouldReturn` requestTx
-              failAfter 1 $ takeMVar node2received `shouldReturn` requestTx
+              concurrently_ (assertBroadcastFrom requestTx hn1 [node2received, node3received]) $
+                concurrently_
+                  (assertBroadcastFrom requestTx hn2 [node1received, node3received])
+                  (assertBroadcastFrom requestTx hn3 [node2received, node1received])
 
   describe "Serialisation" $ do
     it "can roundtrip serialisation of HydraMessage" $ property $ prop_canRoundtripSerialise @(HydraMessage Integer)
     it "can roundtrip CBOR encoding/decoding of HydraMessage" $ property $ prop_canRoundtripCBOREncoding @(HydraMessage Integer)
+
+assertBroadcastFrom :: Eq a => Show a => a -> HydraNetwork IO a -> [MVar a] -> IO ()
+assertBroadcastFrom requestTx network receivers =
+  tryBroadcast `catch` \(_ :: HUnitFailure) -> tryBroadcast
+ where
+  tryBroadcast = do
+    broadcast network requestTx
+    forM_ receivers $ \var -> failAfter 1 $ takeMVar var `shouldReturn` requestTx
 
 instance Arbitrary (HydraMessage Integer) where
   arbitrary =
