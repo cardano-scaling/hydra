44b623182ef40bfcb62b00aebd1cd17b9753f4bc
KtorZ <matthias.benkort@gmail.com>
2021-06-16 09:55:30 +0200

    Show network logs on failure in Network specs.

--- hydra-node/test/Hydra/NetworkSpec.hs
+++ hydra-node/test/Hydra/NetworkSpec.hs
@@ -4,18 +4,25 @@
 -- | Test the real networking layer
 module Hydra.NetworkSpec where
 
-import Cardano.Prelude hiding (threadDelay)
+import Cardano.Prelude hiding (atomically, onException, threadDelay)
 
 import Cardano.Binary (FromCBOR, ToCBOR, fromCBOR, toCBOR)
 import Codec.CBOR.Read (deserialiseFromBytes)
 import Codec.CBOR.Write (toLazyByteString)
 import Control.Monad.Class.MonadAsync (concurrently_)
+import Control.Monad.Class.MonadSTM (
+  MonadSTM (..),
+  newTVarIO,
+  readTVar,
+ )
+import Control.Monad.Class.MonadThrow (MonadCatch, onException)
 import Hydra.HeadLogic (HydraMessage (..), Snapshot (..))
 import Hydra.Ledger.Mock (MockTx (..))
-import Hydra.Logging (nullTracer)
+import Hydra.Logging (Tracer, traceInTVar)
 import Hydra.Network (Network)
 import Hydra.Network.Ouroboros (broadcast, withOuroborosNetwork)
 import Hydra.Network.ZeroMQ (withZeroMQNetwork)
+import Say (say)
 import Test.HUnit.Lang (HUnitFailure)
 import Test.Hspec (Spec, describe, it, shouldReturn)
 import Test.QuickCheck (
@@ -37,10 +44,9 @@ spec = describe "Networking layer" $ do
   describe "Ouroboros Network" $ do
     it "broadcasts messages to single connected peer" $ do
       received <- newEmptyMVar
-      failAfter 10 $ do
-        -- TODO(MB): Capture the trace and print it on failures.
-        withOuroborosNetwork nullTracer (lo, 45678) [(lo, 45679)] (const @_ @(HydraMessage MockTx) $ pure ()) $ \hn1 ->
-          withOuroborosNetwork @(HydraMessage MockTx) nullTracer (lo, 45679) [(lo, 45678)] (putMVar received) $ \_ -> do
+      showLogsOnFailure $ \tracer -> failAfter 10 $ do
+        withOuroborosNetwork tracer (lo, 45678) [(lo, 45679)] (const @_ @(HydraMessage MockTx) $ pure ()) $ \hn1 ->
+          withOuroborosNetwork @(HydraMessage MockTx) tracer (lo, 45679) [(lo, 45678)] (putMVar received) $ \_ -> do
             broadcast hn1 requestTx
             takeMVar received `shouldReturn` requestTx
 
@@ -48,11 +54,10 @@ spec = describe "Networking layer" $ do
       node1received <- newEmptyMVar
       node2received <- newEmptyMVar
       node3received <- newEmptyMVar
-      failAfter 10 $ do
-        -- TODO(MB): Capture the trace and print it on failures.
-        withOuroborosNetwork @(HydraMessage MockTx) nullTracer (lo, 45678) [(lo, 45679), (lo, 45680)] (putMVar node1received) $ \hn1 ->
-          withOuroborosNetwork nullTracer (lo, 45679) [(lo, 45678), (lo, 45680)] (putMVar node2received) $ \hn2 -> do
-            withOuroborosNetwork nullTracer (lo, 45680) [(lo, 45678), (lo, 45679)] (putMVar node3received) $ \hn3 -> do
+      showLogsOnFailure $ \tracer -> failAfter 10 $ do
+        withOuroborosNetwork @(HydraMessage MockTx) tracer (lo, 45678) [(lo, 45679), (lo, 45680)] (putMVar node1received) $ \hn1 ->
+          withOuroborosNetwork tracer (lo, 45679) [(lo, 45678), (lo, 45680)] (putMVar node2received) $ \hn2 -> do
+            withOuroborosNetwork tracer (lo, 45680) [(lo, 45678), (lo, 45679)] (putMVar node3received) $ \hn3 -> do
               concurrently_ (assertBroadcastFrom requestTx hn1 [node2received, node3received]) $
                 concurrently_
                   (assertBroadcastFrom requestTx hn2 [node1received, node3received])
@@ -63,11 +68,10 @@ spec = describe "Networking layer" $ do
       node1received <- newEmptyMVar
       node2received <- newEmptyMVar
       node3received <- newEmptyMVar
-      failAfter 10 $ do
-        -- TODO(MB): Capture the trace and print it on failures.
-        withZeroMQNetwork nullTracer (lo, 55677) [(lo, 55678), (lo, 55679)] (putMVar node1received) $ \hn1 ->
-          withZeroMQNetwork nullTracer (lo, 55678) [(lo, 55677), (lo, 55679)] (putMVar node2received) $ \hn2 ->
-            withZeroMQNetwork nullTracer (lo, 55679) [(lo, 55677), (lo, 55678)] (putMVar node3received) $ \hn3 -> do
+      showLogsOnFailure $ \tracer -> failAfter 10 $ do
+        withZeroMQNetwork tracer (lo, 55677) [(lo, 55678), (lo, 55679)] (putMVar node1received) $ \hn1 ->
+          withZeroMQNetwork tracer (lo, 55678) [(lo, 55677), (lo, 55679)] (putMVar node2received) $ \hn2 ->
+            withZeroMQNetwork tracer (lo, 55679) [(lo, 55677), (lo, 55678)] (putMVar node3received) $ \hn3 -> do
               concurrently_ (assertBroadcastFrom requestTx hn1 [node2received, node3received]) $
                 concurrently_
                   (assertBroadcastFrom requestTx hn2 [node1received, node3received])
@@ -83,6 +87,16 @@ assertBroadcastFrom requestTx network receivers =
   tryBroadcast = do
     broadcast network requestTx
     forM_ receivers $ \var -> failAfter 1 $ takeMVar var `shouldReturn` requestTx
+
+-- TODO(MB): Possibly relocate this function for re-use?
+showLogsOnFailure ::
+  (MonadSTM m, MonadCatch m, MonadIO m, Show msg) =>
+  (Tracer m msg -> m a) ->
+  m a
+showLogsOnFailure action = do
+  tvar <- newTVarIO []
+  action (traceInTVar tvar)
+    `onException` (atomically (readTVar tvar) >>= mapM_ (say . show))
 
 instance Arbitrary (HydraMessage MockTx) where
   arbitrary =
