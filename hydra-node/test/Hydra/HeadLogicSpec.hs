1f591ba72aa02d0d19ed60bea0c42cd8a2af18b8
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-06-16 15:07:30 +0000

    Replace MockTx with SimpleTx

--- hydra-node/test/Hydra/HeadLogicSpec.hs
+++ hydra-node/test/Hydra/HeadLogicSpec.hs
@@ -38,7 +38,7 @@ import Hydra.HeadLogic (
   update,
  )
 import Hydra.Ledger (Ledger (..), Party, Tx)
-import Hydra.Ledger.Mock (MockTx (ValidTx), mockLedger)
+import Hydra.Ledger.Simple (SimpleTx (..), TxIn (..), simpleLedger)
 import Test.Hspec (
   Spec,
   describe,
@@ -55,16 +55,19 @@ import Test.QuickCheck.Property (collect)
 spec :: Spec
 spec = describe "Hydra Head Logic" $ do
   let threeParties = Set.fromList [1, 2, 3]
-      ledger = mockLedger
+      ledger = simpleLedger
       env =
         Environment
           { party = 2
           , snapshotStrategy = NoSnapshots
           }
+      -- NOTE: This unrealistic Tx is just there to be always valid as
+      -- it does not require any input
+      simpleTx = SimpleTx 1 mempty (Set.fromList [TxIn 3, TxIn 4])
 
   it "confirms tx given it receives AckTx from all parties" $ do
-    let reqTx = NetworkEvent $ ReqTx (ValidTx 1)
-        ackFrom p = NetworkEvent $ AckTx p (ValidTx 1)
+    let reqTx = NetworkEvent $ ReqTx simpleTx
+        ackFrom p = NetworkEvent $ AckTx p simpleTx
         s0 = initialState threeParties ledger
 
     s1 <- assertNewState $ update env ledger s0 reqTx
@@ -73,7 +76,7 @@ spec = describe "Hydra Head Logic" $ do
     getConfirmedTransactions s3 `shouldBe` []
 
     s4 <- assertNewState $ update env ledger s3 (ackFrom 2)
-    getConfirmedTransactions s4 `shouldBe` [ValidTx 1]
+    getConfirmedTransactions s4 `shouldBe` [simpleTx]
 
   it "notifies client when it receives a ping" $ do
     update env ledger (initialState threeParties ledger) (NetworkEvent $ Ping 2) `hasEffect` ClientEffect (PeerConnected 2)
@@ -86,34 +89,34 @@ spec = describe "Hydra Head Logic" $ do
     s2 <- assertNewState $ update env ledger s1 (ackFrom 3)
     s3 <- assertNewState $ update env ledger s2 (ackFrom 1)
 
-    getConfirmedSnapshot s3 `shouldBe` Just (Snapshot 0 [] [])
+    getConfirmedSnapshot s3 `shouldBe` Just (Snapshot 0 mempty [])
 
     s4 <- assertNewState $ update env ledger s3 (ackFrom 2)
-    getConfirmedSnapshot s4 `shouldBe` Just (Snapshot 1 [] [])
+    getConfirmedSnapshot s4 `shouldBe` Just (Snapshot 1 mempty [])
 
   it "does not confirm snapshots from non-leaders" pending
   it "does not confirm old snapshots" pending
 
   prop "can handle OnChainEvent in any state" prop_handleOnChainEventInAnyState
 
-genOnChainTx :: Gen (OnChainTx MockTx)
+genOnChainTx :: Gen (OnChainTx SimpleTx)
 genOnChainTx =
   elements
     [ InitTx mempty
-    , CommitTx 1 [ValidTx 10]
-    , CollectComTx []
+    , CommitTx 1 (Set.fromList [TxIn 1, TxIn 2])
+    , CollectComTx mempty
     , CloseTx (Snapshot 0 mempty mempty) mempty
     , ContestTx (Snapshot 0 mempty mempty) mempty
-    , FanoutTx [ValidTx 1]
+    , FanoutTx (Set.fromList [TxIn 1, TxIn 2])
     ]
 
-genHeadStatus :: Gen (HeadStatus MockTx)
+genHeadStatus :: Gen (HeadStatus SimpleTx)
 genHeadStatus =
   elements
     [ InitState
     , FinalState
     , CollectingState mempty mempty
-    , OpenState (SimpleHeadState [] mempty mempty (Snapshot 0 mempty mempty) Nothing)
+    , OpenState (SimpleHeadState mempty mempty mempty (Snapshot 0 mempty mempty) Nothing)
     ]
 
 defaultHeadParameters :: HeadParameters
@@ -131,7 +134,7 @@ prop_handleOnChainEventInAnyState =
           Error _ -> False
  where
   env = Environment 1 NoSnapshots
-  ledger = mockLedger
+  ledger = simpleLedger
 
 hasEffect :: Tx tx => Outcome tx -> Effect tx -> IO ()
 hasEffect (NewState _ effects) effect
@@ -166,7 +169,7 @@ getConfirmedSnapshot HeadState{headStatus} = case headStatus of
   OpenState SimpleHeadState{confirmedSnapshot} -> Just confirmedSnapshot
   _ -> Nothing
 
-assertNewState :: Outcome MockTx -> IO (HeadState MockTx)
+assertNewState :: Outcome SimpleTx -> IO (HeadState SimpleTx)
 assertNewState = \case
   NewState st _ -> pure st
   Error e -> fail (show e)
