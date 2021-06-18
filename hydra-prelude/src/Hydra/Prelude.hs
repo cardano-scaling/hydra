9719b0a2235f59d9a7df472e88b363ddec072004
KtorZ <matthias.benkort@gmail.com>
2021-06-18 13:46:41 +0200

    Write hydra-prelude.
      Done as a separate package, since we have multiple packagess in the project. If we ever consolidate hydra-plutus and local-cluster into hydra-node, we may also merge the prelude into it.

---
+++ hydra-prelude/src/Hydra/Prelude.hs
@@ -0,0 +1,93 @@
+module Hydra.Prelude (
+  module Relude,
+  module Control.Monad.Class.MonadSTM,
+  module Control.Monad.Class.MonadTime,
+  module Control.Monad.Class.MonadST,
+  module Control.Monad.Class.MonadAsync,
+  module Control.Monad.Class.MonadEventlog,
+  module Control.Monad.Class.MonadTimer,
+  module Control.Monad.Class.MonadFork,
+  module Control.Monad.Class.MonadThrow,
+) where
+
+import Control.Monad.Class.MonadAsync (
+  MonadAsync,
+ )
+import Control.Monad.Class.MonadEventlog (
+  MonadEventlog,
+ )
+import Control.Monad.Class.MonadFork (
+  MonadFork,
+  MonadThread,
+ )
+import Control.Monad.Class.MonadST (
+  MonadST,
+ )
+import Control.Monad.Class.MonadSTM (
+  MonadSTM,
+  TBQueue,
+  TMVar,
+  TQueue,
+  TVar,
+  atomically,
+ )
+import Control.Monad.Class.MonadThrow (
+  MonadCatch,
+  MonadEvaluate,
+  MonadMask,
+  MonadThrow,
+ )
+import Control.Monad.Class.MonadTime (
+  DiffTime,
+  MonadMonotonicTime (..),
+  MonadTime (..),
+  NominalDiffTime,
+  Time (..),
+  UTCTime,
+  addTime,
+  addUTCTime,
+  diffTime,
+  diffUTCTime,
+ )
+import Control.Monad.Class.MonadTimer (
+  MonadDelay (..),
+  MonadTimer (..),
+ )
+import Relude hiding (
+  MVar,
+  Nat,
+  STM,
+  TMVar,
+  TVar,
+  atomically,
+  catchSTM,
+  isEmptyTMVar,
+  mkWeakTMVar,
+  modifyTVar',
+  newEmptyMVar,
+  newEmptyTMVar,
+  newEmptyTMVarIO,
+  newMVar,
+  newTMVar,
+  newTMVarIO,
+  newTVar,
+  newTVarIO,
+  putMVar,
+  putTMVar,
+  readMVar,
+  readTMVar,
+  readTVar,
+  readTVarIO,
+  swapMVar,
+  swapTMVar,
+  takeMVar,
+  takeTMVar,
+  throwSTM,
+  tryPutMVar,
+  tryPutTMVar,
+  tryReadMVar,
+  tryReadTMVar,
+  tryTakeMVar,
+  tryTakeTMVar,
+  writeTVar,
+ )
