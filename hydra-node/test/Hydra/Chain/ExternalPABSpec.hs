6047719bb13bef4146ca4783212c073b5be937d5
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-06-22 15:33:09 +0200

    Mark ExternalPABSpec as pending

--- hydra-node/test/Hydra/Chain/ExternalPABSpec.hs
+++ hydra-node/test/Hydra/Chain/ExternalPABSpec.hs
@@ -7,12 +7,13 @@ import Hydra.Chain.ExternalPAB (withExternalPAB)
 import Hydra.HeadLogic (OnChainTx (..))
 import Hydra.Ledger.Simple (SimpleTx)
 import Hydra.Logging (nullTracer)
-import Test.Hspec.Core.Spec (Spec, describe, it)
+import Test.Hspec.Core.Spec (Spec, describe, it, pendingWith)
 
 spec :: Spec
 spec =
   describe "ExternalPAB" $ do
     it "publishes init tx using wallet 1" $ do
+      pendingWith "launch hydra-pab as process"
       -- TODO(SN): launch hydra-pab as process
       withExternalPAB nullTracer (error "called back") $ \Chain{postTx} ->
         postTx $ InitTx @SimpleTx (error "unused")
