c866dd4d86bd5fc65f58910de1a9c35e91a4a9ab
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-06-29 16:41:51 +0000

    Add counter metric for number of confirmed txs

--- hydra-node/src/Hydra/Logging/Monitoring.hs
+++ hydra-node/src/Hydra/Logging/Monitoring.hs
@@ -7,14 +7,15 @@ import Hydra.Prelude
 
 import Control.Tracer (Tracer (Tracer))
 import Data.Map.Strict as Map
+import Hydra.HeadLogic (Effect (ClientEffect), Event (NetworkEvent), HydraMessage (ReqTx), ServerOutput (SnapshotConfirmed), Snapshot (confirmed))
 import Hydra.Logging.Messages (HydraLog (..))
 import Hydra.Network (PortNumber)
-import Hydra.Node (HydraNodeLog (ProcessedEvent))
+import Hydra.Node (HydraNodeLog (ProcessedEffect, ProcessedEvent, ProcessingEvent))
 import System.Metrics.Prometheus.Http.Scrape (serveMetrics)
-import System.Metrics.Prometheus.Metric (Metric (CounterMetric))
-import System.Metrics.Prometheus.Metric.Counter (inc)
+import System.Metrics.Prometheus.Metric (Metric (CounterMetric, HistogramMetric))
+import System.Metrics.Prometheus.Metric.Counter (add, inc)
 import System.Metrics.Prometheus.MetricId (Name (Name))
-import System.Metrics.Prometheus.Registry (Registry, new, registerCounter, sample)
+import System.Metrics.Prometheus.Registry (Registry, new, registerCounter, registerHistogram, sample)
 
 -- | Wraps a monadic action using a `Tracer` and capture metrics based on traces.
 -- Given a `portNumber`, this wrapper starts a Prometheus-compliant server on this port.
@@ -41,16 +42,38 @@ withMonitoring (Just monitoringPort) (Tracer tracer) action = do
 prepareRegistry :: MonadIO m => m (HydraLog tx net -> m (), Registry)
 prepareRegistry = first monitor <$> registerMetrics
  where
+  monitor metricsMap (Node (ProcessingEvent _ (NetworkEvent (ReqTx _ _tx)))) =
+    tick "hydra_head_requested_tx" metricsMap
+  monitor metricsMap (Node (ProcessedEffect _ (ClientEffect (SnapshotConfirmed snapshot)))) =
+    tickN (length $ confirmed snapshot) "hydra_head_confirmed_tx" metricsMap
   monitor metricsMap (Node (ProcessedEvent _ _)) =
-    case Map.lookup "hydra_head_events" metricsMap of
+    tick "hydra_head_event" metricsMap
+  monitor _ _ = pure ()
+
+  tick metricName metricsMap =
+    case Map.lookup metricName metricsMap of
       (Just (CounterMetric c)) -> liftIO $ inc c
       _ -> pure ()
-  monitor _ _ = pure ()
+
+  tickN num metricName metricsMap =
+    case Map.lookup metricName metricsMap of
+      (Just (CounterMetric c)) -> liftIO $ add num c
+      _ -> pure ()
 
   registerMetrics = foldlM registerMetric (mempty, new) allMetrics
 
-  allMetrics = [(Name "hydra_head_events", CounterMetric, flip registerCounter mempty)]
+  allMetrics :: [MetricDefinition]
+  allMetrics =
+    [ MetricDefinition (Name "hydra_head_events") CounterMetric $ flip registerCounter mempty
+    , MetricDefinition (Name "hydra_head_requested_tx") CounterMetric $ flip registerCounter mempty
+    , MetricDefinition (Name "hydra_head_confirmed_tx") CounterMetric $ flip registerCounter mempty
+    , MetricDefinition (Name "hydra_head_tx_confirmation_time_ms") HistogramMetric $ \n -> registerHistogram n mempty [100, 200 .. 2000]
+    ]
 
-  registerMetric (metricsMap, registry) (name, ctor, registration) = do
+  registerMetric (metricsMap, registry) (MetricDefinition name ctor registration) = do
     (metric, registry') <- liftIO $ registration name registry
     pure (Map.insert name (ctor metric) metricsMap, registry')
+
+-- | Existential wrapper around different kind of metrics construction logic.
+data MetricDefinition where
+  MetricDefinition :: forall a. Name -> (a -> Metric) -> (Name -> Registry -> IO (a, Registry)) -> MetricDefinition
---
+++ hydra-node/test/Hydra/Logging/MonitoringSpec.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Hydra.Logging.MonitoringSpec where
+
+import qualified Data.Text as Text
+import Hydra.HeadLogic (
+  Effect (ClientEffect),
+  Event (NetworkEvent),
+  HydraMessage (ReqTx),
+  ServerOutput (SnapshotConfirmed),
+  Snapshot (..),
+ )
+import Hydra.Ledger.Builder (aValidTx, utxoRefs)
+import Hydra.Logging (nullTracer, traceWith)
+import Hydra.Logging.Messages (HydraLog (Node))
+import Hydra.Logging.Monitoring
+import Hydra.Network.Ports (withFreePort)
+import Hydra.Node (HydraNodeLog (ProcessedEffect, ProcessingEvent))
+import Hydra.Prelude
+import Network.HTTP.Req (GET (..), NoReqBody (..), bsResponse, defaultHttpConfig, http, port, req, responseBody, runReq, (/:))
+import Test.Hspec
+import Test.Util (failAfter)
+
+spec :: Spec
+spec = describe "Prometheus Metrics" $ do
+  it "provides count of confirmed transactions from ReqTx and SnapshotConfirmed events" $ do
+    failAfter 3 $
+      withFreePort $ \p ->
+        withMonitoring (Just $ fromIntegral p) nullTracer $ \tracer -> do
+          traceWith tracer (Node $ ProcessingEvent 1 (NetworkEvent (ReqTx 1 (aValidTx 42))))
+          traceWith tracer (Node $ ProcessingEvent 1 (NetworkEvent (ReqTx 1 (aValidTx 43))))
+          traceWith tracer (Node $ ProcessedEffect 1 (ClientEffect (SnapshotConfirmed $ Snapshot 1 (utxoRefs [1]) [aValidTx 43, aValidTx 42])))
+
+          response <- runReq @IO defaultHttpConfig $ req GET (http "localhost" /: "metrics") NoReqBody bsResponse (port p)
+
+          Text.lines (decodeUtf8 $ responseBody response) `shouldContain` ["hydra_head_confirmed_tx  2"]
