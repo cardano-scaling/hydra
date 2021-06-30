9c952b582a11545dc36d637e2ad2878bb7736967
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-06-30 07:54:50 +0000

    Address PR review comments and document use
    * Use \case instead of arguments pattern-matching
    * Inline transactions map
    * Remove confirmed transactions from map and add NOTE to future selves

--- hydra-node/src/Hydra/Logging/Monitoring.hs
+++ hydra-node/src/Hydra/Logging/Monitoring.hs
@@ -1,4 +1,13 @@
 -- | Provides Prometheus-based metrics server based on `Tracer` collection.
+--
+-- To add a new metric, one needs to:
+--
+--  * Add a 'MetricDefinition' to the 'allMetrics' list, providing a unique 'Name', the
+--    relevant constructor for the 'Metric' value and a registration function,
+--  * Update the 'monitor' function to Handle relevant 'HydraLog' entries and update
+--    underlying Prometheus metrics store. Nested helpers are provided to increase a
+--    'Counter' by one (@tick@), by some integral value (@tickN@), and to 'observe'
+--    some value in an 'Histogram'.
 module Hydra.Logging.Monitoring (
   withMonitoring,
 ) where
@@ -32,8 +41,7 @@ withMonitoring ::
   m ()
 withMonitoring Nothing tracer action = action tracer
 withMonitoring (Just monitoringPort) (Tracer tracer) action = do
-  txMap <- newTVarIO mempty
-  (traceMetric, registry) <- prepareRegistry txMap
+  (traceMetric, registry) <- prepareRegistry
   withAsync (serveMetrics (fromIntegral monitoringPort) ["metrics"] (sample registry)) $ \_ ->
     let wrappedTracer = Tracer $ \msg -> do
           traceMetric msg
@@ -43,50 +51,12 @@ withMonitoring (Just monitoringPort) (Tracer tracer) action = do
 -- | Register all relevant metrics.
 -- Returns an updated `Registry` which is needed to `serveMetrics` or any other form of publication
 -- of metrics, whether push or pull, and a function for updating metrics given some trace event.
-prepareRegistry :: (MonadIO m, MonadSTM m, MonadMonotonicTime m, Tx tx) => TVar m (Map (TxId tx) Time) -> m (HydraLog tx net -> m (), Registry)
-prepareRegistry txMap = first monitor <$> registerMetrics
+prepareRegistry :: (MonadIO m, MonadSTM m, MonadMonotonicTime m, Tx tx) => m (HydraLog tx net -> m (), Registry)
+prepareRegistry = do
+  transactionsMap <- newTVarIO mempty
+  first (monitor transactionsMap) <$> registerMetrics
  where
-  monitor metricsMap (Node (ProcessingEvent _ (NetworkEvent (ReqTx _ tx)))) = do
-    t <- getMonotonicTime
-    atomically $ modifyTVar' txMap (Map.insert (txId tx) t)
-    tick "hydra_head_requested_tx" metricsMap
-  monitor metricsMap (Node (ProcessedEffect _ (ClientEffect (SnapshotConfirmed snapshot)))) = do
-    t <- getMonotonicTime
-    forM_ (confirmed snapshot) $ \tx -> do
-      let tid = txId tx
-      txsStartTime <- readTVarIO txMap
-      case Map.lookup tid txsStartTime of
-        Just start -> histo "hydra_head_tx_confirmation_time_ms" (diffTime t start) metricsMap
-        Nothing -> pure ()
-    tickN (length $ confirmed snapshot) "hydra_head_confirmed_tx" metricsMap
-  monitor metricsMap (Node (ProcessedEvent _ _)) =
-    tick "hydra_head_events" metricsMap
-  monitor _ _ = pure ()
-
-  tick metricName metricsMap =
-    case Map.lookup metricName metricsMap of
-      (Just (CounterMetric c)) -> liftIO $ inc c
-      _ -> pure ()
-
-  tickN num metricName metricsMap =
-    case Map.lookup metricName metricsMap of
-      (Just (CounterMetric c)) -> liftIO $ add num c
-      _ -> pure ()
-
-  histo metricName time metricsMap =
-    case Map.lookup metricName metricsMap of
-      (Just (HistogramMetric h)) -> liftIO $ observe (fromRational $ toRational $ time * 1000) h
-      _ -> pure ()
-
   registerMetrics = foldlM registerMetric (mempty, new) allMetrics
-
-  allMetrics :: [MetricDefinition]
-  allMetrics =
-    [ MetricDefinition (Name "hydra_head_events") CounterMetric $ flip registerCounter mempty
-    , MetricDefinition (Name "hydra_head_requested_tx") CounterMetric $ flip registerCounter mempty
-    , MetricDefinition (Name "hydra_head_confirmed_tx") CounterMetric $ flip registerCounter mempty
-    , MetricDefinition (Name "hydra_head_tx_confirmation_time_ms") HistogramMetric $ \n -> registerHistogram n mempty [100, 200 .. 2000]
-    ]
 
   registerMetric (metricsMap, registry) (MetricDefinition name ctor registration) = do
     (metric, registry') <- liftIO $ registration name registry
@@ -95,3 +65,58 @@ prepareRegistry txMap = first monitor <$> registerMetrics
 -- | Existential wrapper around different kind of metrics construction logic.
 data MetricDefinition where
   MetricDefinition :: forall a. Name -> (a -> Metric) -> (Name -> Registry -> IO (a, Registry)) -> MetricDefinition
+
+-- |All custom 'MetricDefinition's for Hydra
+allMetrics :: [MetricDefinition]
+allMetrics =
+  [ MetricDefinition (Name "hydra_head_events") CounterMetric $ flip registerCounter mempty
+  , MetricDefinition (Name "hydra_head_requested_tx") CounterMetric $ flip registerCounter mempty
+  , MetricDefinition (Name "hydra_head_confirmed_tx") CounterMetric $ flip registerCounter mempty
+  , MetricDefinition (Name "hydra_head_tx_confirmation_time_ms") HistogramMetric $ \n -> registerHistogram n mempty [100, 200 .. 2000]
+  ]
+
+-- | Main monitoring function that updates metrics store given some log entries.
+monitor ::
+  (MonadIO m, MonadSTM m, MonadMonotonicTime m, Tx tx) =>
+  TVar m (Map (TxId tx) Time) ->
+  Map Name Metric ->
+  HydraLog tx net ->
+  m ()
+monitor transactionsMap metricsMap = \case
+  (Node (ProcessingEvent _ (NetworkEvent (ReqTx _ tx)))) -> do
+    t <- getMonotonicTime
+    -- NOTE: If a requested transaction never gets confirmed, it might stick
+    -- forever in the transactions map which could lead to unbounded growth and
+    -- memory leak. We might want to have a 'cleaner' thread run that will remove
+    -- transactions after some timeout expires
+    atomically $ modifyTVar' transactionsMap (Map.insert (txId tx) t)
+    tick "hydra_head_requested_tx"
+  (Node (ProcessedEffect _ (ClientEffect (SnapshotConfirmed snapshot)))) -> do
+    t <- getMonotonicTime
+    forM_ (confirmed snapshot) $ \tx -> do
+      let tid = txId tx
+      txsStartTime <- readTVarIO transactionsMap
+      case Map.lookup tid txsStartTime of
+        Just start -> do
+          atomically $ modifyTVar' transactionsMap $ Map.delete tid
+          histo "hydra_head_tx_confirmation_time_ms" (diffTime t start)
+        Nothing -> pure ()
+    tickN "hydra_head_confirmed_tx" (length $ confirmed snapshot)
+  (Node (ProcessedEvent _ _)) ->
+    tick "hydra_head_events"
+  _ -> pure ()
+ where
+  tick metricName =
+    case Map.lookup metricName metricsMap of
+      (Just (CounterMetric c)) -> liftIO $ inc c
+      _ -> pure ()
+
+  tickN metricName num =
+    case Map.lookup metricName metricsMap of
+      (Just (CounterMetric c)) -> liftIO $ add num c
+      _ -> pure ()
+
+  histo metricName time =
+    case Map.lookup metricName metricsMap of
+      (Just (HistogramMetric h)) -> liftIO $ observe (fromRational $ toRational $ time * 1000) h
+      _ -> pure ()
