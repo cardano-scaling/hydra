94ab2b7b56dcee19db68c8125c39c0df7b265c81
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-07-02 16:16:10 +0000

    Write test results in XML and publish in CI

--- hydra-node/test/Main.hs
+++ hydra-node/test/Main.hs
@@ -1,1 +1,9 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import Hydra.Prelude
+import qualified Spec
+import Test.HSpec.JUnit
+import Test.Hspec.Runner
+
+main :: IO ()
+main = hspecWith defaultConfig{configFormat = Just $ junitFormat "test-results.xml" "my-tests"} Spec.spec
