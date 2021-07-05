2a76bde648a1b1817852684f2a3047172220c838
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-07-05 08:07:42 +0000

    Output tests results to XML file and console

--- hydra-node/test/Main.hs
+++ hydra-node/test/Main.hs
@@ -3,7 +3,15 @@ module Main where
 import Hydra.Prelude
 import qualified Spec
 import Test.HSpec.JUnit
+import Test.Hspec.Core.Format (Format, FormatConfig)
+import Test.Hspec.Core.Formatters (formatterToFormat, specdoc)
 import Test.Hspec.Runner
 
 main :: IO ()
-main = hspecWith defaultConfig{configFormat = Just $ junitFormat "test-results.xml" "my-tests"} Spec.spec
+main = hspecWith defaultConfig{configFormat = Just dualFormatter} Spec.spec
+
+dualFormatter :: FormatConfig -> IO Format
+dualFormatter config = do
+  junit <- junitFormat "test-results.xml" "hydra-node" config
+  docSpec <- formatterToFormat specdoc config
+  pure $ \e -> junit e >> docSpec e
--- local-cluster/test/Main.hs
+++ local-cluster/test/Main.hs
@@ -1,1 +1,20 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import Hydra.Prelude
+import qualified Spec
+import Test.HSpec.JUnit
+import Test.Hspec.Core.Format (Format, FormatConfig)
+import Test.Hspec.Core.Formatters (formatterToFormat, specdoc)
+import Test.Hspec.Runner
+
+main :: IO ()
+main = hspecWith defaultConfig{configFormat = Just dualFormatter} Spec.spec
+
+-- NOTE: This is duplicated from hydra-node's tests, we should provide this as part
+-- of a standard 'Hydra.Test.Prelude' but should this live in hydra-prelude or in
+-- a new package?
+dualFormatter :: FormatConfig -> IO Format
+dualFormatter config = do
+  junit <- junitFormat "test-results.xml" "local-cluster" config
+  docSpec <- formatterToFormat specdoc config
+  pure $ \e -> junit e >> docSpec e
