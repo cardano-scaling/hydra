e825ddfd90d5a56b823d905d0b643f814f37ae6f
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-07-20 09:41:19 +0200

    Make hydra-node compile

--- hydra-node/src/Hydra/Ledger/MaryTest.hs
+++ hydra-node/src/Hydra/Ledger/MaryTest.hs
@@ -10,7 +10,7 @@ import Hydra.Prelude
 -- REVIEW(SN): use a more consistent set of ledger imports, but some things not
 -- in the API?
 
-import Cardano.Ledger.BaseTypes (UnitInterval, mkActiveSlotCoeff, mkUnitInterval)
+import Cardano.Ledger.BaseTypes (mkActiveSlotCoeff)
 import Cardano.Ledger.Keys (KeyPair (KeyPair), asWitness)
 import Cardano.Ledger.SafeHash (hashAnnotated)
 import Cardano.Ledger.ShelleyMA.Timelocks (ValidityInterval (..))
@@ -47,7 +47,7 @@ import Shelley.Spec.Ledger.LedgerState (UTxOState (..))
 import Shelley.Spec.Ledger.Tx (addrWits)
 import Shelley.Spec.Ledger.UTxO (UTxO (..), makeWitnessesVKey, txid)
 import Test.Cardano.Ledger.EraBuffet (MaryEra, TestCrypto, Value)
-import Test.Shelley.Spec.Ledger.Utils (mkAddr, mkKeyPair)
+import Test.Shelley.Spec.Ledger.Utils (RawSeed(..), mkAddr, mkKeyPair, unsafeBoundRational)
 import Text.Read (Read (..))
 
 type MaryTest = MaryEra TestCrypto
@@ -136,15 +136,10 @@ globals =
     , quorum = 5
     , maxMajorPV = 1000
     , maxLovelaceSupply = 45 * 1000 * 1000 * 1000 * 1000 * 1000
-    , activeSlotCoeff = mkActiveSlotCoeff . unsafeMkUnitInterval $ 0.9
+    , activeSlotCoeff = mkActiveSlotCoeff . unsafeBoundRational $ 0.9
     , networkId = Testnet
     , systemStart = SystemStart $ posixSecondsToUTCTime 0
     }
-
--- | You vouch that argument is in [0; 1].
-unsafeMkUnitInterval :: Ratio Word64 -> UnitInterval
-unsafeMkUnitInterval r =
-  fromMaybe (error "could not construct unit interval") $ mkUnitInterval r
 
 -- * Test functions
 
@@ -197,14 +192,14 @@ testUTxO =
 -- | Alice's payment key pair
 alicePay :: KeyPair 'Payment TestCrypto
 alicePay = KeyPair vk sk
- where
-  (sk, vk) = mkKeyPair (0, 0, 0, 0, 0)
+  where
+    (sk, vk) = mkKeyPair (RawSeed 0 0 0 0 0)
 
 -- | Alice's stake key pair
 aliceStake :: KeyPair 'Staking TestCrypto
 aliceStake = KeyPair vk sk
- where
-  (sk, vk) = mkKeyPair (1, 1, 1, 1, 1)
+  where
+    (sk, vk) = mkKeyPair (RawSeed 1 1 1 1 1)
 
 -- | Alice's base address
 aliceAddr :: Addr TestCrypto
@@ -213,14 +208,14 @@ aliceAddr = mkAddr (alicePay, aliceStake)
 -- | Bob's payment key pair
 bobPay :: KeyPair 'Payment TestCrypto
 bobPay = KeyPair vk sk
- where
-  (sk, vk) = mkKeyPair (2, 2, 2, 2, 2)
+  where
+    (sk, vk) = mkKeyPair (RawSeed 2 2 2 2 2)
 
 -- | Bob's stake key pair
 bobStake :: KeyPair 'Staking TestCrypto
 bobStake = KeyPair vk sk
- where
-  (sk, vk) = mkKeyPair (3, 3, 3, 3, 3)
+  where
+    (sk, vk) = mkKeyPair (RawSeed 3 3 3 3 3)
 
 -- | Bob's address
 bobAddr :: Addr TestCrypto
