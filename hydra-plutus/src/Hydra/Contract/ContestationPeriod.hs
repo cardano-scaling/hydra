fc767f6c15f5bf3e14d7ad32292a215caa9d93dd
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-07-29 11:40:51 +0200

    Fix serialization of the Hydra.Contract.ContestationPeriod

--- hydra-plutus/src/Hydra/Contract/ContestationPeriod.hs
+++ hydra-plutus/src/Hydra/Contract/ContestationPeriod.hs
@@ -5,10 +5,19 @@ module Hydra.Contract.ContestationPeriod where
 
 import Hydra.Prelude
 
+import Data.Time (diffTimeToPicoseconds, picosecondsToDiffTime)
 import qualified PlutusTx
 
-newtype ContestationPeriod = UnsafeContestationPeriod {seconds :: Integer}
+newtype ContestationPeriod = UnsafeContestationPeriod {picoseconds :: Integer}
   deriving stock (Generic, Eq, Ord, Show)
-  deriving newtype (Num, FromJSON, ToJSON)
+  deriving newtype (Num)
 
 PlutusTx.unstableMakeIsData ''ContestationPeriod
+
+instance FromJSON ContestationPeriod where
+  parseJSON =
+    fmap (UnsafeContestationPeriod . diffTimeToPicoseconds) . parseJSON
+
+instance ToJSON ContestationPeriod where
+  toJSON =
+    toJSON . picosecondsToDiffTime . picoseconds
