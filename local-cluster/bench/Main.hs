e39763380d4066ea3e7275a0fc30cb577112f8d5
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-08-09 16:56:26 +0000

    Fix test prelude import in benchmark

--- local-cluster/bench/Main.hs
+++ local-cluster/bench/Main.hs
@@ -5,6 +5,7 @@ import Hydra.Prelude
 import Bench.EndToEnd (bench)
 import Data.Aeson (eitherDecodeFileStrict', encodeFile)
 import Hydra.Ledger.Simple (genSequenceOfValidTransactions, utxoRefs)
+import Hydra.Test.Prelude (createSystemTempDirectory)
 import System.FilePath (takeDirectory, (</>))
 import Test.QuickCheck (generate, scale)
 
