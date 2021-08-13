d03a934ed46cef1e886ac3e1a9b82035f98a3e10
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-08-13 08:12:01 +0200

    Scaffold a new, empty package 'hydra-tui'

---
+++ hydra-tui/exe/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Hydra.Prelude
+
+import qualified Hydra.TUI as TUI
+
+main :: IO ()
+main = TUI.run
---
+++ hydra-tui/src/Hydra/TUI.hs
@@ -0,0 +1,6 @@
+module Hydra.TUI where
+
+import Hydra.Prelude
+
+run :: IO ()
+run = putStrLn "Hello world"
