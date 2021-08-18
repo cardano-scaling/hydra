5caa25565377e69215ed6ba0e6b2ca283aed7ed1
KtorZ <matthias.benkort@gmail.com>
2021-08-18 10:59:14 +0200

    Continue writing ToJSON instances for Cardano ledger types.

--- hydra-node/src/Hydra/Ledger/Cardano.hs
+++ hydra-node/src/Hydra/Ledger/Cardano.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wno-missing-methods #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
@@ -8,8 +9,10 @@ import Hydra.Prelude hiding (id)
 import Cardano.Binary (decodeFull', serialize')
 import Cardano.Ledger.Crypto (Crypto, StandardCrypto)
 import Cardano.Ledger.Mary (MaryEra)
-import Data.Aeson (Value (String), withText)
+import qualified Cardano.Ledger.Mary.Value as Cardano
+import Data.Aeson (Value (String), object, withText, (.=))
 import Data.ByteString.Base16 (decodeBase16, encodeBase16)
+import qualified Data.Set as Set
 import Hydra.Ledger (Tx (..))
 import qualified Shelley.Spec.Ledger.API as Cardano
 import Text.Read (readPrec)
@@ -36,16 +39,50 @@ instance Read (Cardano.UTxO era) where
 instance Read (Cardano.TxId era) where
   readPrec = error "Read: Cardano.TxId"
 
+txIdToText :: forall crypto. Crypto crypto => Cardano.TxId crypto -> Text
+txIdToText = encodeBase16 . serialize'
+
+txIdFromText :: (MonadFail m, Crypto crypto) => Text -> m (Cardano.TxId crypto)
+txIdFromText t =
+  case decodeBase16 (encodeUtf8 t) of
+    Left e -> fail $ show e
+    Right bytes -> case decodeFull' bytes of
+      Left e -> fail $ show e
+      Right a -> pure a
+
 instance Crypto crypto => ToJSON (Cardano.TxId crypto) where
-  toJSON = String . encodeBase16 . serialize'
+  toJSON = String . txIdToText @crypto
 
 instance Crypto crypto => FromJSON (Cardano.TxId crypto) where
-  parseJSON = withText "base16 encoded TxId" $ \t ->
-    case decodeBase16 (encodeUtf8 t) of
-      Left e -> fail $ show e
-      Right bytes -> case decodeFull' bytes of
-        Left e -> fail $ show e
-        Right a -> pure a
+  parseJSON = withText "base16 encoded TxId" txIdFromText
+
+instance FromJSON CardanoTxBody where
+  parseJSON = error "TODO: FromJSON CardanoTxBody"
+
+instance ToJSON CardanoTxBody where
+  toJSON (Cardano.TxBody inputs outputs _certs _wdrls _txfee _vldt _update _mdHash) =
+    object
+      [ "inputs" .= Set.map inputToJson inputs
+      , "outputs" .= fmap outputToJson outputs
+      ]
+
+inputToJson :: Cardano.TxIn StandardCrypto -> Value
+inputToJson (Cardano.TxIn id ix) =
+  toJSON (txIdToText id <> "#" <> show ix)
+
+outputToJson :: Cardano.TxOut CardanoEra -> Value
+outputToJson (Cardano.TxOut addr value) =
+  object
+    -- TODO: Use ledger's instance, which is a base16-encoded
+    -- serialized address. We might want to use bech32
+    -- serialization in the end.
+    [ "address" .= addr
+    , "value" .= valueToJson value
+    ]
+
+valueToJson :: Cardano.Value StandardCrypto -> Value
+valueToJson =
+  error "TODO: valueToJson"
 
 instance Semigroup (Cardano.UTxO CardanoEra) where
   Cardano.UTxO u1 <> Cardano.UTxO u2 = Cardano.UTxO (u1 <> u2)
