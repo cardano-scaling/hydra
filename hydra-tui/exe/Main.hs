7d5e63979abeb7ac1b23ae0a575a094f654eedfb
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-08-22 15:00:14 +0200

    Parse command line and connect to given node

--- hydra-tui/exe/Main.hs
+++ hydra-tui/exe/Main.hs
@@ -3,6 +3,9 @@ module Main where
 import Hydra.Prelude
 
 import qualified Hydra.TUI as TUI
+import Hydra.TUI.Options (parseOptions)
+import Options.Applicative (execParser, info)
 
 main :: IO ()
-main = void TUI.run
+main =
+  execParser (info parseOptions mempty) >>= void . TUI.run
