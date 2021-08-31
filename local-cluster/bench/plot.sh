62af4089c62606ebe821533b68c64ee9b45de676
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-08-31 21:35:13 +0200

    Expect path to results.csv to plot.sh

--- local-cluster/bench/plot.sh
+++ local-cluster/bench/plot.sh
@@ -3,9 +3,14 @@
 
 set -e
 
-[ $# -eq 1 ] || { echo "Expecting benchmark output directory as argument"; exit 1; }
+[ $# -eq 1 ] && [ -f $1 ] || {
+    echo "Expecting path to benchmark results.csv as FILE argument"
+    echo "Usage: $0 FILE"
+    exit 1
+}
 
-DIR=$1
+RESULTS=$1
+DIR=$(dirname ${RESULTS})
 
 gnuplot <<EOF
 set term pngcairo size 2048,1532
@@ -17,7 +22,7 @@ set xtics out rotate by -80
 set xdata time
 set format x "%H:%M:%S"
 set datafile separator ","
-plot "${DIR}/results.csv" u (timecolumn(1,"%Y-%m-%d %H:%M:%S")):2 t 'Tx Confirmation over time'
+plot "${RESULTS}" u (timecolumn(1,"%Y-%m-%d %H:%M:%S")):2 t 'Tx Confirmation over time'
 EOF
 
 echo "Created plot: ${DIR}/results.png"
