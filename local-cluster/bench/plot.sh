85680c241f3ddff63b565f6191c4ba9ee6558503
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-08-31 21:25:42 +0200

    Directly use results.csv as created by bench-e2e

--- local-cluster/bench/plot.sh
+++ local-cluster/bench/plot.sh
@@ -7,7 +7,6 @@ set -e
 
 DIR=$1
 
-cat "$DIR/results.csv" | tr -d \" > ${DIR}/results.data
 gnuplot <<EOF
 set term pngcairo size 2048,1532
 set output "${DIR}/results.png"
@@ -18,7 +17,7 @@ set xtics out rotate by -80
 set xdata time
 set format x "%H:%M:%S"
 set datafile separator ","
-plot "${DIR}/results.data" u (timecolumn(1,"%Y-%m-%dT%H:%M:%S")):2 t 'Tx Confirmation over time'
+plot "${DIR}/results.csv" u (timecolumn(1,"%Y-%m-%d %H:%M:%S")):2 t 'Tx Confirmation over time'
 EOF
 
-echo "Created plot '${DIR}/results.png'"
+echo "Created plot: ${DIR}/results.png"
