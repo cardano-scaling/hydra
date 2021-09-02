c73abc8d45249594fbd457260146ed9d723b0ca1
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-09-02 10:06:06 +0000

    Decide to do snapshot based on in-flight snapshot

--- hydra-node/test/Hydra/NodeSpec.hs
+++ hydra-node/test/Hydra/NodeSpec.hs
@@ -16,62 +16,66 @@ import Hydra.HeadLogic (
  )
 import Hydra.Ledger (Party, SigningKey, Tx, deriveParty, sign)
 import Hydra.Ledger.Simple (SimpleTx (..), simpleLedger, utxoRef, utxoRefs)
-import Hydra.Logging (showLogsOnFailure)
+import Hydra.Logging (Tracer, showLogsOnFailure)
 import Hydra.Network (Host (..), Network (..))
 import Hydra.Network.Message (Message (..))
-import Hydra.Node (EventQueue (..), HydraNode (..), createEventQueue, createHydraHead, isEmpty, stepHydraNode)
+import Hydra.Node (EventQueue (..), HydraNode (..), HydraNodeLog, createEventQueue, createHydraHead, isEmpty, stepHydraNode)
 import Hydra.Snapshot (Snapshot (..))
 
 spec :: Spec
 spec = do
-  it "emits a single ReqSn and AckSn as leader, even after multiple ReqTxs" $ do
-    -- NOTE(SN): Sequence of parties in OnInitTx of
-    -- 'prefix' is relevant, so 10 is the (initial) snapshot leader
-    let tx1 = SimpleTx{txSimpleId = 1, txInputs = utxoRefs [2], txOutputs = utxoRefs [4]}
-        tx2 = SimpleTx{txSimpleId = 2, txInputs = utxoRefs [4], txOutputs = utxoRefs [5]}
-        tx3 = SimpleTx{txSimpleId = 3, txInputs = utxoRefs [5], txOutputs = utxoRefs [6]}
-        events =
-          prefix
-            <> [ NetworkEvent{message = ReqTx{party = 10, transaction = tx1}}
-               , NetworkEvent{message = ReqTx{party = 10, transaction = tx2}}
-               , NetworkEvent{message = ReqTx{party = 10, transaction = tx3}}
-               ]
-        signedSnapshot = sign 10 $ Snapshot 1 (utxoRefs [1, 3, 6]) [tx1, tx2, tx3]
-    node <- createHydraNode 10 [20, 30] SnapshotAfterEachTx events
-    (node', getNetworkMessages) <- recordNetwork node
-    runToCompletion node'
-    getNetworkMessages `shouldReturn` [ReqSn 10 1 [tx1, tx2, tx3], AckSn 10 signedSnapshot 1]
+  it "emits a single ReqSn and AckSn as leader, even after multiple ReqTxs" $
+    showLogsOnFailure $ \tracer -> do
+      -- NOTE(SN): Sequence of parties in OnInitTx of
+      -- 'prefix' is relevant, so 10 is the (initial) snapshot leader
+      let tx1 = SimpleTx{txSimpleId = 1, txInputs = utxoRefs [2], txOutputs = utxoRefs [4]}
+          tx2 = SimpleTx{txSimpleId = 2, txInputs = utxoRefs [4], txOutputs = utxoRefs [5]}
+          tx3 = SimpleTx{txSimpleId = 3, txInputs = utxoRefs [5], txOutputs = utxoRefs [6]}
+          events =
+            prefix
+              <> [ NetworkEvent{message = ReqTx{party = 10, transaction = tx1}}
+                 , NetworkEvent{message = ReqTx{party = 10, transaction = tx2}}
+                 , NetworkEvent{message = ReqTx{party = 10, transaction = tx3}}
+                 ]
+          signedSnapshot = sign 10 $ Snapshot 1 (utxoRefs [1, 3, 6]) [tx1, tx2, tx3]
+      node <- createHydraNode 10 [20, 30] SnapshotAfterEachTx events
+      (node', getNetworkMessages) <- recordNetwork node
+      runToCompletion tracer node'
+      getNetworkMessages `shouldReturn` [ReqSn 10 1 [tx1, tx2, tx3], AckSn 10 signedSnapshot 1]
 
-  it "rotates snapshot leaders" $ do
-    let tx1 = SimpleTx{txSimpleId = 1, txInputs = utxoRefs [2], txOutputs = utxoRefs [4]}
-        sn1 = Snapshot 1 (utxoRefs [1, 2, 3]) mempty
-        events =
-          prefix
-            <> [ NetworkEvent{message = ReqSn{party = 10, snapshotNumber = 1, transactions = mempty}}
-               , NetworkEvent{message = AckSn 10 (sign 10 sn1) 1}
-               , NetworkEvent{message = AckSn 30 (sign 30 sn1) 1}
-               , NetworkEvent{message = ReqTx{party = 10, transaction = tx1}}
-               ]
+  it "rotates snapshot leaders" $
+    showLogsOnFailure $ \tracer -> do
+      let tx1 = SimpleTx{txSimpleId = 1, txInputs = utxoRefs [2], txOutputs = utxoRefs [4]}
+          sn1 = Snapshot 1 (utxoRefs [1, 2, 3]) mempty
+          sn2 = Snapshot 2 (utxoRefs [1, 3, 4]) [tx1]
+          events =
+            prefix
+              <> [ NetworkEvent{message = ReqSn{party = 10, snapshotNumber = 1, transactions = mempty}}
+                 , NetworkEvent{message = AckSn 10 (sign 10 sn1) 1}
+                 , NetworkEvent{message = AckSn 30 (sign 30 sn1) 1}
+                 , NetworkEvent{message = ReqTx{party = 10, transaction = tx1}}
+                 ]
 
-    node <- createHydraNode 20 [10, 30] SnapshotAfterEachTx events
-    (node', getNetworkMessages) <- recordNetwork node
-    runToCompletion node'
+      node <- createHydraNode 20 [10, 30] SnapshotAfterEachTx events
+      (node', getNetworkMessages) <- recordNetwork node
+      runToCompletion tracer node'
 
-    getNetworkMessages `shouldReturn` [AckSn 20 (sign 20 sn1) 1, ReqSn 20 2 [tx1]]
+      getNetworkMessages `shouldReturn` [AckSn 20 (sign 20 sn1) 1, ReqSn 20 2 [tx1], AckSn 20 (sign 20 sn2) 2]
 
-  it "processes out-of-order AckSn" $ do
-    let snapshot = Snapshot 1 (utxoRefs [1, 2, 3]) []
-        sig20 = sign 20 snapshot
-        sig10 = sign 10 snapshot
-        events =
-          prefix
-            <> [ NetworkEvent{message = AckSn{party = 20, signed = sig20, snapshotNumber = 1}}
-               , NetworkEvent{message = ReqSn{party = 10, snapshotNumber = 1, transactions = []}}
-               ]
-    node <- createHydraNode 10 [20, 30] SnapshotAfterEachTx events
-    (node', getNetworkMessages) <- recordNetwork node
-    runToCompletion node'
-    getNetworkMessages `shouldReturn` [AckSn{party = 10, signed = sig10, snapshotNumber = 1}]
+  it "processes out-of-order AckSn" $
+    showLogsOnFailure $ \tracer -> do
+      let snapshot = Snapshot 1 (utxoRefs [1, 2, 3]) []
+          sig20 = sign 20 snapshot
+          sig10 = sign 10 snapshot
+          events =
+            prefix
+              <> [ NetworkEvent{message = AckSn{party = 20, signed = sig20, snapshotNumber = 1}}
+                 , NetworkEvent{message = ReqSn{party = 10, snapshotNumber = 1, transactions = []}}
+                 ]
+      node <- createHydraNode 10 [20, 30] SnapshotAfterEachTx events
+      (node', getNetworkMessages) <- recordNetwork node
+      runToCompletion tracer node'
+      getNetworkMessages `shouldReturn` [AckSn{party = 10, signed = sig10, snapshotNumber = 1}]
 
 oneReqSn :: [Message tx] -> Bool
 oneReqSn = (== 1) . length . filter isReqSn
@@ -95,13 +99,12 @@ prefix =
   , OnChainEvent{onChainTx = OnCollectComTx}
   ]
 
-runToCompletion :: Tx tx => HydraNode tx IO -> IO ()
-runToCompletion node@HydraNode{eq = EventQueue{isEmpty}} =
-  showLogsOnFailure go
+runToCompletion :: Tx tx => Tracer IO (HydraNodeLog tx) -> HydraNode tx IO -> IO ()
+runToCompletion tracer node@HydraNode{eq = EventQueue{isEmpty}} = go
  where
-  go tracer =
+  go =
     unlessM isEmpty $
-      stepHydraNode tracer node >> go tracer
+      stepHydraNode tracer node >> go
 
 createHydraNode ::
   MonadSTM m =>
