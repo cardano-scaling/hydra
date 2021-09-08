e5d30ec02a2d3f1e2bdeb55614e30cd4511f86be
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-09-08 07:25:53 +0000

    Simplify more events and effects

--- hydra-node/exe/log-filter/Main.hs
+++ hydra-node/exe/log-filter/Main.hs
@@ -30,28 +30,36 @@ filterLog entry = do
   tag <- node ^? key "tag"
   case tag of
     "ProcessingEvent" -> do
-      case node ^? networkMessage . key "tag" of
+      case (node ^? networkMessage . key "tag") <|> (node ^? clientEvent . key "tag") of
         Just "ReqTx" -> processReqTx node
-        Just "ReqSn" -> processReqSn node
+        Just "NewTx" -> replaceTransactionWithTxId node clientEvent
+        Just "ReqSn" -> replaceTransactionsWithTxIds node networkMessage
         _ -> pure node
     "ProcessedEvent" -> do
-      case node ^? networkMessage . key "tag" of
+      case (node ^? networkMessage . key "tag") <|> (node ^? clientEvent . key "tag") of
         Just "ReqTx" -> processReqTx node
-        Just "ReqSn" -> processReqSn node
+        Just "NewTx" -> replaceTransactionWithTxId node clientEvent
+        Just "ReqSn" -> replaceTransactionsWithTxIds node networkMessage
         _ -> pure node
     "ProcessingEffect" -> do
-      case node ^? clientEffect . key "tag" of
+      case node ^? clientEffect . key "tag" <|> node ^? networkEffect . key "tag" of
         Just "SnapshotConfirmed" -> processSnapshotConfirmed node
         Just "Committed" -> replaceUtxoWithNull node
-        Just "TxSeen" -> replaceTransactionWithTxId node
-        Just "TxInvalid" -> replaceTransactionWithTxId node >>= replaceUtxoWithNull
+        Just "TxSeen" -> replaceTransactionWithTxId node clientEffect
+        Just "TxInvalid" -> replaceTransactionWithTxId node clientEffect >>= replaceUtxoWithNull
+        Just "TxValid" -> replaceTransactionWithTxId node clientEffect
+        Just "ReqTx" -> replaceTransactionWithTxId node networkEffect
+        Just "ReqSn" -> replaceTransactionsWithTxIds node networkEffect
         _ -> pure node
     "ProcessedEffect" -> do
-      case node ^? clientEffect . key "tag" of
+      case node ^? clientEffect . key "tag" <|> node ^? networkEffect . key "tag" of
         Just "SnapshotConfirmed" -> processSnapshotConfirmed node
         Just "Committed" -> replaceUtxoWithNull node
-        Just "TxSeen" -> replaceTransactionWithTxId node
-        Just "TxInvalid" -> replaceTransactionWithTxId node >>= replaceUtxoWithNull
+        Just "TxSeen" -> replaceTransactionWithTxId node clientEffect
+        Just "TxInvalid" -> replaceTransactionWithTxId node clientEffect >>= replaceUtxoWithNull
+        Just "TxValid" -> replaceTransactionWithTxId node clientEffect
+        Just "ReqTx" -> replaceTransactionWithTxId node networkEffect
+        Just "ReqSn" -> replaceTransactionsWithTxIds node networkEffect
         _ -> pure node
     _ -> pure node
  where
@@ -59,9 +67,10 @@ filterLog entry = do
     txid <- node ^? networkMessage . key "transaction" . key "id"
     pure $ node & networkMessage . key "transaction" .~ txid
 
-  processReqSn node = do
-    txids <- node ^? networkMessage . key "transactions" . _Array . traverse . key "id"
-    pure $ node & networkMessage . key "transactions" .~ txids
+  replaceTransactionsWithTxIds :: Value -> Traversal' Value Value -> Maybe Value
+  replaceTransactionsWithTxIds node path = do
+    txids <- node ^? path . key "transactions" . _Array . traverse . key "id"
+    pure $ node & path . key "transactions" .~ txids
 
   processSnapshotConfirmed node = do
     txids <- node ^? clientEffect . key "snapshot" . key "confirmedTransactions" . _Array . traverse . key "id"
@@ -72,12 +81,19 @@ filterLog entry = do
 
   replaceUtxoWithNull node = pure $ node & clientEffect . key "utxo" .~ Null
 
-  replaceTransactionWithTxId node = do
-    txid <- node ^? clientEffect . key "transaction" . key "id"
-    pure $ node & clientEffect . key "transaction" .~ txid
+  replaceTransactionWithTxId :: Value -> Traversal' Value Value -> Maybe Value
+  replaceTransactionWithTxId node path = do
+    txid <- node ^? path . key "transaction" . key "id"
+    pure $ node & path . key "transaction" .~ txid
 
   networkMessage :: Traversal' Value Value
   networkMessage = key "event" . key "message"
+
+  networkEffect :: Traversal' Value Value
+  networkEffect = key "effect" . key "message"
+
+  clientEvent :: Traversal' Value Value
+  clientEvent = key "event" . key "clientInput"
 
   clientEffect :: Traversal' Value Value
   clientEffect = key "effect" . key "serverOutput"
