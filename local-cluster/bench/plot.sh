687ab4a1245049889e731965d7b25aac1658706b
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-09-09 08:23:18 +0000

    Plot throughput on top of confirmation time

--- local-cluster/bench/plot.sh
+++ local-cluster/bench/plot.sh
@@ -16,13 +16,17 @@ gnuplot <<EOF
 set term pngcairo size 2048,1532
 set output "${DIR}/results.png"
 set xlabel "Time"
-set ylabel "Confirmation delay (s)"
 set ylabel "Tx confirmation time (s)"
-set xtics out rotate by -80
+set xtics out rotate
 set xdata time
 set format x "%H:%M:%S"
+set ytics nomirror
+set y2tics 0, 10
+set y2range [0:]
+set y2label "Tx submitted/time interval (5s)"
 set datafile separator ","
-plot "${RESULTS}" u (timecolumn(1,"%Y-%m-%d %H:%M:%S")):2 t 'Tx Confirmation over time'
+plot "${RESULTS}" u (timecolumn(1,"%Y-%m-%d %H:%M:%S")):2 t'Tx Confirmation over time' axis x1y1, \
+  "${RESULTS}" u (timecolumn(1, "%Y-%m-%d %H:%M:%S UTC")):(1.0) bins binwidth=5 w histeps axis x1y2 t '# Tx submitted'
 EOF
 
 echo "Created plot: ${DIR}/results.png"
