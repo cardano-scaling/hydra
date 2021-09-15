a5d9c7bfafa93726792bb716749e39ff901e4de5
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-09-15 15:11:55 +0200

    Bench: Plot and label submitted TPS
    More natural than transactions per 5 seconds

--- local-cluster/bench/plot.sh
+++ local-cluster/bench/plot.sh
@@ -26,11 +26,11 @@ set format x "%H:%M:%S"
 set ytics nomirror
 set y2tics 0, 10
 set y2range [0:]
-set y2label "Tx submitted/time interval (5s)"
+set y2label "Submitted Tx / s"
 set datafile separator ","
 plot "${RESULTS}" u (timecolumn(1,"%Y-%m-%d %H:%M:%S")):3 t 'Tx Confirmation over time' axis x1y1, \
      "${RESULTS}" u (timecolumn(1,"%Y-%m-%d %H:%M:%S")):2 t 'Tx Validation time' w histeps axis x1y1, \
-     "${RESULTS}" u (timecolumn(1, "%Y-%m-%d %H:%M:%S UTC")):(1.0) bins binwidth=5 w histeps axis x1y2 t '# Tx submitted'
+     "${RESULTS}" u (timecolumn(1, "%Y-%m-%d %H:%M:%S UTC")):(1.0) bins binwidth=1 w histeps axis x1y2 t 'Submitted Tx/s'
 EOF
 
 echo "Created plot: ${DIR}/results.png"
