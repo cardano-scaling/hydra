7f74dc95126c9b0a8e644e37eb2717d80bffe12e
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-09-16 16:14:18 +0200

    Use cardano-ledger-specs instead of cardano-api
    We have an 'Arbitrary TxIn' from their test code, but this really has
    less safe-guards in constructing the tx itself -> we would get errors
    only when applying the tx to the ledger, it would not be a 'ValidatedTx'?

--- hydra-node/test/Hydra/Chain/Direct/TxSpec.hs
+++ hydra-node/test/Hydra/Chain/Direct/TxSpec.hs
@@ -2,15 +2,18 @@
 -- "direct" chain component.
 module Hydra.Chain.Direct.TxSpec where
 
+import Cardano.Binary (serialize)
+import Data.ByteString.Lazy as LBS
 import Hydra.Chain.Direct.Tx (initTx)
 import Hydra.Prelude
 import Test.Hydra.Prelude
-import Test.QuickCheck (counterexample, property)
+import Test.QuickCheck (counterexample)
+import Test.Shelley.Spec.Ledger.Serialisation.EraIndepGenerators ()
 
 spec :: Spec
 spec =
   parallel $ do
-    prop "can construct initTx" $ \params txIn ->
-      case initTx params txIn of
-        Left err -> counterexample ("TxBodyError: " <> show err) False
-        Right _ -> property True
+    prop "can construct & serialize initTx" $ \params txIn ->
+      let cbor = serialize (initTx params txIn)
+          len = LBS.length cbor
+       in counterexample (show cbor) $ len > 0
