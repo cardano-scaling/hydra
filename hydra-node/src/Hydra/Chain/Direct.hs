47d9729277df5a260d95bf881e39d7c108c53073
KtorZ <matthias.benkort@gmail.com>
2021-09-16 09:25:03 +0200

    Add boilerplate for ouroboros-network + codecs

--- hydra-node/src/Hydra/Chain/Direct.hs
+++ hydra-node/src/Hydra/Chain/Direct.hs
@@ -1,13 +1,40 @@
+{-# LANGUAGE TypeApplications #-}
+
 -- | Chain component implementation which uses directly the Node-to-Client
 -- protocols to submit "hand-rolled" transactions including Plutus validators and
 -- observing the chain using it as well.
 module Hydra.Chain.Direct where
 
-import Cardano.Binary (serialize)
-import Hydra.Chain (Chain (..), ChainComponent, toOnChainTx)
-import Hydra.Logging (Tracer)
 import Hydra.Prelude
-import Ouroboros.Network.Channel (Channel (Channel, send))
+
+import Cardano.Binary (serialize)
+import Hydra.Chain (Chain (..), ChainComponent, OnChainTx, toOnChainTx)
+import Hydra.Ledger (Tx)
+import Hydra.Logging (Tracer)
+
+import Cardano.Chain.Slotting (EpochSlots (..))
+import Cardano.Ledger.Crypto (StandardCrypto)
+import Control.Tracer (nullTracer)
+import Data.Map.Strict ((!))
+import qualified Data.Map.Strict as Map
+import Network.TypedProtocol.Codec
+import Ouroboros.Consensus.Byron.Ledger.Config (CodecConfig (..))
+import Ouroboros.Consensus.Cardano (CardanoBlock)
+import Ouroboros.Consensus.Cardano.Block (CodecConfig (..), GenTx)
+import Ouroboros.Consensus.Network.NodeToClient (ClientCodecs, Codecs' (..), clientCodecs)
+import Ouroboros.Consensus.Node.NetworkProtocolVersion (SupportedNetworkProtocolVersion (..))
+import Ouroboros.Consensus.Shelley.Ledger.Config (CodecConfig (..))
+import Ouroboros.Network.Block
+import Ouroboros.Network.Channel
+import Ouroboros.Network.Codec
+import Ouroboros.Network.Driver.Simple
+import Ouroboros.Network.Mux
+import Ouroboros.Network.NodeToClient
+import Ouroboros.Network.Protocol.ChainSync.Client (ChainSyncClient, chainSyncClientPeer)
+import Ouroboros.Network.Protocol.ChainSync.Server (ChainSyncServer, chainSyncServerPeer)
+import qualified Ouroboros.Network.Protocol.ChainSync.Type as ChainSync
+import Ouroboros.Network.Protocol.LocalTxSubmission.Client (LocalTxSubmissionClient, localTxSubmissionClientPeer)
+import Ouroboros.Network.Protocol.LocalTxSubmission.Server (LocalTxSubmissionServer, localTxSubmissionServerPeer)
 
 withDirectChain ::
   IO (Channel IO LByteString) ->
@@ -24,3 +51,66 @@ withDirectChain connect _tracer callback action = do
     callback (toOnChainTx now tx)
 
 data DirectChainLog
+
+chainSyncClient ::
+  ChainSyncClient header point tip m a
+chainSyncClient = undefined
+
+txSubmissionClient ::
+  LocalTxSubmissionClient tx reject m a
+txSubmissionClient = undefined
+
+--
+-- Mock Server
+--
+
+mockChainSyncServer ::
+  TQueue m tx ->
+  ChainSyncServer header point tip m a
+mockChainSyncServer = undefined
+
+mockTxSubmissionServer ::
+  TQueue m tx ->
+  LocalTxSubmissionServer tx reject m a
+mockTxSubmissionServer = undefined
+
+--
+-- Codecs
+--
+
+nodeToClientVLatest :: NodeToClientVersion
+nodeToClientVLatest =
+  fst $ Map.findMax $ supportedNodeToClientVersions proxy
+ where
+  proxy = Proxy @(CardanoBlock StandardCrypto)
+
+codecs ::
+  forall m block.
+  (MonadST m, block ~ CardanoBlock StandardCrypto) =>
+  EpochSlots ->
+  NodeToClientVersion ->
+  ClientCodecs block m
+codecs epochSlots nodeToClientV =
+  clientCodecs cfg (supportedVersions ! nodeToClientV) nodeToClientV
+ where
+  supportedVersions = supportedNodeToClientVersions (Proxy @block)
+  cfg = CardanoCodecConfig byron shelley allegra mary alonzo
+   where
+    byron = ByronCodecConfig epochSlots
+    shelley = ShelleyCodecConfig
+    allegra = ShelleyCodecConfig
+    mary = ShelleyCodecConfig
+    alonzo = ShelleyCodecConfig
+
+--
+-- Example
+--
+
+main :: IO ()
+main = do
+  (chanA, chanB) <- createConnectedChannels
+  concurrently_
+    (runPeer nullTracer codec chanA $ chainSyncClientPeer chainSyncClient)
+    (runPeer nullTracer codec chanB $ chainSyncServerPeer mockChainSyncServer)
+ where
+  codec = codecs (EpochSlots 432000) nodeToClientVLatest & cChainSyncCodec
