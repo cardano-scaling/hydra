dce0acd27f31de71d27c4ab0477a7d98320ee8bb
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-09-19 17:32:29 +0200

    Add a roundtrip property for onchain txs

--- hydra-node/test/Hydra/Chain/Direct/TxSpec.hs
+++ hydra-node/test/Hydra/Chain/Direct/TxSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 -- | Unit tests for our "hand-rolled" transactions as they are used in the
 -- "direct" chain component.
 module Hydra.Chain.Direct.TxSpec where
@@ -11,8 +12,8 @@ import Cardano.Ledger.Alonzo.Tx (ValidatedTx (ValidatedTx, wits))
 import Cardano.Ledger.Alonzo.TxWitness (TxWitness (txdats), nullDats, unTxDats)
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Map as Map
-import Hydra.Chain (HeadParameters (..))
-import Hydra.Chain.Direct.Tx (initTx)
+import Hydra.Chain (HeadParameters (..), toOnChainTx)
+import Hydra.Chain.Direct.Tx (initTx, observeTx, constructTx)
 import Hydra.Contract.ContestationPeriod (contestationPeriodFromDiffTime)
 import Hydra.Contract.Head (State (Initial))
 import Hydra.Contract.Party (partyFromVerKey)
@@ -20,17 +21,22 @@ import Hydra.Party (vkey)
 import Plutus.V1.Ledger.Api (toBuiltinData, toData)
 import Test.Cardano.Ledger.Alonzo.Serialisation.Generators ()
 import Test.QuickCheck (counterexample, (===))
+import Hydra.Ledger.Simple (SimpleTx)
 
 spec :: Spec
 spec =
-  parallel $
+  parallel $ do
+    prop "observeTx . constructTx rountrip" $ \postTx txIn time ->
+      observeTx (constructTx txIn postTx) === Just (toOnChainTx @SimpleTx time postTx)
+
     describe "initTx" $ do
       prop "can construct & serialize unsigned initTx" $ \txIn params ->
         let tx = initTx params txIn
             cbor = serialize tx
             len = LBS.length cbor
          in counterexample ("Tx: " <> show tx) $
-              counterexample ("Cbor: " <> show cbor) $ len > 0
+              counterexample ("Tx serialized size: " <> show len) $
+                len < 16000 -- TODO(SN): use real max tx size
 
       prop "contains some datums" $ \txIn params ->
         let ValidatedTx{wits} = initTx params txIn
