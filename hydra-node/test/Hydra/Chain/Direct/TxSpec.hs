886bc71b6c900de7538e56cef94174446a674bc6
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-09-19 20:05:46 +0200

    Add a spec to assert minting/distribution of NFTs
    Although re-using the validator is probably a better idea.

--- hydra-node/test/Hydra/Chain/Direct/TxSpec.hs
+++ hydra-node/test/Hydra/Chain/Direct/TxSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeApplications #-}
+
 -- | Unit tests for our "hand-rolled" transactions as they are used in the
 -- "direct" chain component.
 module Hydra.Chain.Direct.TxSpec where
@@ -8,7 +9,7 @@ import Test.Hydra.Prelude
 
 import Cardano.Binary (serialize)
 import Cardano.Ledger.Alonzo.Data (Data (Data))
-import Cardano.Ledger.Alonzo.Tx (ValidatedTx (ValidatedTx, wits))
+import Cardano.Ledger.Alonzo.Tx (ValidatedTx (ValidatedTx, wits, body), outputs)
 import Cardano.Ledger.Alonzo.TxWitness (TxWitness (txdats), nullDats, unTxDats)
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Map as Map
@@ -22,6 +23,11 @@ import Plutus.V1.Ledger.Api (toBuiltinData, toData)
 import Test.Cardano.Ledger.Alonzo.Serialisation.Generators ()
 import Test.QuickCheck (counterexample, (===))
 import Hydra.Ledger.Simple (SimpleTx)
+import Hydra.Chain.Direct (Era)
+import Cardano.Ledger.Mary.Value (Value(Value), PolicyID, AssetName)
+import Cardano.Ledger.Alonzo (TxOut)
+import Cardano.Ledger.Alonzo.TxBody (TxOut(TxOut))
+import Cardano.Ledger.Crypto (StandardCrypto)
 
 spec :: Spec
 spec =
@@ -52,3 +58,24 @@ spec =
             onChainParties = map (partyFromVerKey . vkey) parties
             datum = Initial onChainPeriod onChainParties
          in Map.elems (unTxDats dats) === [Data . toData $ toBuiltinData datum]
+
+      -- TODO(SN): assert monetary policy?
+      prop "distributes participation tokens" $ \txIn params ->
+        let ValidatedTx{body} = initTx params txIn
+            nfts = foldMap txOutNFT $ outputs body
+         in counterexample ("NFTs: " <> show nfts) $
+            length nfts == length (parties params)
+
+-- | Extract NFT candidates. any single quantity assets not being ADA is a
+-- candidate.
+txOutNFT :: TxOut Era -> [(PolicyID StandardCrypto, AssetName)]
+txOutNFT (TxOut _ value _ ) =
+  mapMaybe findUnitAssets $ Map.toList assets
+ where
+  (Value _ assets) = value
+
+  findUnitAssets (policy, as) = do
+    (name, _q) <- find unitQuantity $ Map.toList as
+    pure (policy, name)
+
+  unitQuantity (_name,q) = q == 1
