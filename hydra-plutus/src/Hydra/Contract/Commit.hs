1431bbc561556b4a097e138740758a4af1efaaac
KtorZ <matthias.benkort@gmail.com>
2021-09-23 15:26:15 +0200

    Provide real implementation for the Commit validator

--- hydra-plutus/src/Hydra/Contract/Commit.hs
+++ hydra-plutus/src/Hydra/Contract/Commit.hs
@@ -8,24 +8,44 @@ module Hydra.Contract.Commit where
 import Ledger hiding (validatorHash)
 import PlutusTx.Prelude
 
+import Hydra.Contract.Head (Head, Input (..))
+import Hydra.OnChain.Util (mustReimburse, mustRunContract)
 import Ledger.Typed.Scripts (TypedValidator, ValidatorType, ValidatorTypes (..))
 import qualified Ledger.Typed.Scripts as Scripts
 import PlutusTx (CompiledCode)
 import qualified PlutusTx
+import PlutusTx.IsData.Class (ToData (..))
 
 data Commit
 
 instance Scripts.ValidatorTypes Commit where
-  type DatumType Commit = TxOut
+  type DatumType Commit = (Dependencies, TxOut)
   type RedeemerType Commit = ()
 
+-- See note on Hydra.Contract.Initial#Dependencies
+data Dependencies = Dependencies
+  { headScript :: ValidatorHash
+  }
+
+PlutusTx.makeLift ''Dependencies
+PlutusTx.unstableMakeIsData ''Dependencies
+
 validator ::
-  TxOut ->
+  (Dependencies, TxOut) ->
   () ->
   ScriptContext ->
   Bool
-validator _commit () _ctx =
-  True
+validator (Dependencies{headScript}, committedOut) () ctx =
+  consumedByCollectCom || consumedByAbort
+ where
+  consumedByCollectCom =
+    mustRunContract @(RedeemerType Head) headScript CollectCom ctx
+
+  consumedByAbort =
+    and
+      [ mustRunContract @(RedeemerType Head) headScript Abort ctx
+      , mustReimburse committedOut ctx
+      ]
 
 compiledValidator :: CompiledCode (ValidatorType Commit)
 compiledValidator = $$(PlutusTx.compile [||validator||])
@@ -41,3 +61,9 @@ typedValidator = Scripts.mkTypedValidator @Commit
 
 validatorHash :: ValidatorHash
 validatorHash = Scripts.validatorHash typedValidator
+
+datum :: DatumType Commit -> Datum
+datum a = Datum (toBuiltinData a)
+
+address :: Address
+address = scriptHashAddress validatorHash
