825c28e97342e1609f9e81b3f6b63b7e3af3b918
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-09-24 05:30:54 +0000

    Fix compilation error
    Still one test of aborttx failing due to failing validator

--- hydra-node/test/Hydra/Chain/DirectSpec.hs
+++ hydra-node/test/Hydra/Chain/DirectSpec.hs
@@ -32,8 +32,8 @@ spec = parallel $ do
     aliceKeys@(aliceVk, _) <- generateKeyPair
     bobKeys@(bobVk, _) <- generateKeyPair
     withMockServer $ \networkMagic iocp socket submitTx -> do
-      withDirectChain (contramap show stdoutTracer) networkMagic iocp socket aliceKeys (putMVar calledBackAlice) $ \Chain{postTx} -> do
-        withDirectChain (contramap show stdoutTracer) networkMagic iocp socket bobKeys (putMVar calledBackBob) $ \_ -> do
+      withDirectChain nullTracer networkMagic iocp socket aliceKeys (putMVar calledBackAlice) $ \Chain{postTx} -> do
+        withDirectChain nullTracer networkMagic iocp socket bobKeys (putMVar calledBackBob) $ \_ -> do
           let parameters = HeadParameters 100 [alice, bob, carol]
           generate (genPaymentTo aliceVk) >>= submitTx
           generate (genPaymentTo bobVk) >>= submitTx
