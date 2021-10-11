6c5a675b41e4cccbb93d124e2afa45ba8c15190e
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-10-11 13:34:32 +0000

    Serialise some datums and redeemers for manual submission

--- hydra-plutus/exe/inspect-script/Main.hs
+++ hydra-plutus/exe/inspect-script/Main.hs
@@ -9,7 +9,9 @@ import Data.Text (pack)
 import Hydra.Contract.Commit as Commit
 import Hydra.Contract.Head as Head
 import Hydra.Contract.Initial as Initial
+import Ledger (Datum (..))
 import Ledger.Value
+import Plutus.V1.Ledger.Api (Redeemer (Redeemer), dataToBuiltinData, toData)
 
 -- | Serialise Hydra scripts to files for submission through cardano-cli
 -- This small utility is useful to manually construct transactions payload for Hydra on-chain
@@ -25,11 +27,26 @@ main = do
           ( unAssetClass $
               assetClass (currencySymbol $ encodeUtf8 currency) (tokenName $ encodeUtf8 token)
           )
-  putTextLn "Serialise scripts"
+  putTextLn "Serialise scripts:"
   forM_ (scripts policyId) $ \(script, scriptName) -> do
     let scriptFile = scriptName <> ".plutus"
-    BL.writeFile scriptFile script
-    putTextLn $ "  " <> pack scriptFile <> ":     " <> sizeInKb (headScript policyId)
+        serialised = Aeson.encode $ serialiseToTextEnvelope (Just $ fromString scriptName) script
+    BL.writeFile scriptFile serialised
+    putTextLn $ "  " <> pack scriptFile <> ":     " <> sizeInKb serialised
+
+  putTextLn "Serialise datums:"
+  forM_ datums $ \(aDatum, datumName) -> do
+    let datumFile = datumName <> ".plutus"
+        serialised = Aeson.encode $ serialiseToTextEnvelope (Just $ fromString datumName) aDatum
+    BL.writeFile datumFile serialised
+    putTextLn $ "  " <> pack datumFile <> ":     " <> sizeInKb serialised
+
+  putTextLn "Serialise redeemers:"
+  forM_ redeemers $ \(aRedeemer, redeemerName) -> do
+    let redeemerFile = redeemerName <> ".plutus"
+        serialised = Aeson.encode $ serialiseToTextEnvelope (Just $ fromString redeemerName) aRedeemer
+    BL.writeFile redeemerFile serialised
+    putTextLn $ "  " <> pack redeemerFile <> ":     " <> sizeInKb serialised
  where
   sizeInKb = (<> " KB") . show . (`div` 1024) . BL.length
 
@@ -39,14 +56,21 @@ main = do
     , (commitScript, "commitScript")
     ]
 
-  headScript policyId =
-    Aeson.encode $
-      serialiseToTextEnvelope Nothing $ Head.validatorScript policyId
+  headScript policyId = Head.validatorScript policyId
 
-  commitScript =
-    Aeson.encode $
-      serialiseToTextEnvelope Nothing Commit.validatorScript
+  commitScript = Commit.validatorScript
 
-  initialScript =
-    Aeson.encode $
-      serialiseToTextEnvelope Nothing Initial.validatorScript
+  initialScript = Initial.validatorScript
+
+  datums =
+    [ (headDatum, "headDatum")
+    , (abortDatum, "abortDatum")
+    ]
+
+  headDatum = Datum $ dataToBuiltinData $ toData $ Head.Initial 1_000_000_000_000 []
+
+  abortDatum = Datum $ dataToBuiltinData $ toData $ Head.Final
+
+  redeemers = [(headRedeemer, "headRedeemer")]
+
+  headRedeemer = Redeemer $ dataToBuiltinData $ toData Head.Abort
