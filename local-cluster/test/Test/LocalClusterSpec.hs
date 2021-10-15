cd1a5209217a94b42b5310883337ea7a6abce2c0
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-10-15 09:34:11 +0000

    Check one payment from initial fund in cluster

--- local-cluster/test/Test/LocalClusterSpec.hs
+++ local-cluster/test/Test/LocalClusterSpec.hs
@@ -4,17 +4,22 @@ import Hydra.Prelude
 import Test.Hydra.Prelude
 
 import CardanoCluster (ClusterConfig (..), ClusterLog (..), RunningCluster (..), withCluster)
-import CardanoNode (ChainTip (..), NodeId, RunningNode (..), cliQueryTip)
+import CardanoNode (ChainTip (..), RunningNode (..), cliQueryTip)
 import Hydra.Logging (Tracer, showLogsOnFailure)
+import qualified Paths_local_cluster as Pkg
+import System.Environment (getEnvironment)
+import System.Exit (ExitCode (..))
+import System.FilePath ((</>))
+import System.Process
 
 spec :: Spec
 spec =
-  it "should produce blocks" $ do
+  it "should produce blocks and provide funds" $ do
     showLogsOnFailure $ \tr ->
       withTempDir "hydra-local-cluster" $ \tmp -> do
         withCluster tr (ClusterConfig tmp) $ \cluster -> do
           failAfter 30 $ assertNetworkIsProducingBlock tr cluster
-
+          assertCanSpendInitialFunds cluster
 
 assertNetworkIsProducingBlock :: Tracer IO ClusterLog -> RunningCluster -> IO ()
 assertNetworkIsProducingBlock tracer = go (-1)
@@ -29,15 +34,32 @@ assertNetworkIsProducingBlock tracer = go (-1)
     _ ->
       error "empty cluster?"
 
+assertCanSpendInitialFunds :: HasCallStack => RunningCluster -> IO ()
+assertCanSpendInitialFunds = \case
+  RunningCluster (ClusterConfig parentDirectory) (RunningNode nodeId socket : _) ->
+    runTestScript (parentDirectory </> "node-" <> show nodeId) socket
   _ ->
     error "empty cluster?"
 
-runTestScript :: NodeId -> FilePath -> IO (ExitCode, String, String)
-runTestScript _nodeId _socket =
-  readCreateProcessWithExitCode sh (Text.unpack inputScript)
+runTestScript :: FilePath -> FilePath -> IO ()
+runTestScript nodeDirectory socket = do
+  inputScript <- Pkg.getDataFileName "test_submit.sh"
+  currentEnv <- getEnvironment
+  let scriptOutput = nodeDirectory </> "test_submit.out"
+  withFile' scriptOutput $ \fileOut ->
+    withCreateProcess (sh currentEnv inputScript fileOut) $ \_stdin _stdout _stderr hdl ->
+      waitForProcess hdl >>= \case
+        ExitFailure{} -> readFile scriptOutput >>= failure . ("Initial funds spending script failed, output: " <>)
+        ExitSuccess -> pure ()
  where
-  sh = proc "/bin/sh" []
-  inputScript = unlines ["exit 1 ;"]
+  socketEnv = ("CARDANO_NODE_SOCKET_PATH", socket)
+  sh baseEnv script out =
+    (proc "/bin/sh" [script])
+      { env = Just (socketEnv : baseEnv)
+      , cwd = Just nodeDirectory
+      , std_out = UseHandle out
+      , std_err = UseHandle out
+      }
 
 waitForNewBlock :: IO ()
 waitForNewBlock = threadDelay (2 * slotLength)
