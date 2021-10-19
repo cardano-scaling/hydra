93c0a00e24d006e4734c823aafcdfdc70e1513c9
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-10-19 12:24:44 +0000

    Compute amount and input to payment transaction in client
    Replaces 2 cardano-cli calls with a function querying the node for a
    UTxO by a given addresse, and extracting input reference and amount
    from the UTxO.

--- local-cluster/src/CardanoClient.hs
+++ local-cluster/src/CardanoClient.hs
@@ -5,10 +5,28 @@ module CardanoClient where
 
 import Hydra.Prelude
 
-import Cardano.Api (Address, NetworkId, ShelleyAddr)
-import Cardano.Api.Shelley (VerificationKey (PaymentVerificationKey))
+import Cardano.Api (
+  Address,
+  AddressAny (AddressShelley),
+  AlonzoEra,
+  CardanoEra (AlonzoEra),
+  ConsensusModeParams (CardanoModeParams),
+  EpochSlots (EpochSlots),
+  EraInMode (AlonzoEraInCardanoMode),
+  LocalNodeConnectInfo (LocalNodeConnectInfo),
+  NetworkId,
+  QueryInEra (QueryInShelleyBasedEra),
+  QueryInMode (QueryInEra),
+  QueryInShelleyBasedEra (QueryUTxO),
+  QueryUTxOFilter (QueryUTxOByAddress),
+  ShelleyAddr,
+  UTxO,
+ )
+import Cardano.Api.Shelley (ShelleyBasedEra (ShelleyBasedEraAlonzo), VerificationKey (PaymentVerificationKey))
 import Cardano.CLI.Shelley.Run.Address (buildShelleyAddress)
+import Cardano.CLI.Shelley.Run.Query (executeQuery)
 import qualified Cardano.Ledger.Keys as Keys
+import qualified Data.Set as Set
 import qualified Hydra.Chain.Direct.Wallet as Hydra
 
 type NodeSocket = FilePath
@@ -22,7 +40,28 @@ buildAddress vKey networkId = do
     Left err -> throwIO $ BuildAddressException (show err)
     Right addr -> pure addr
 
-newtype BuildAddressException = BuildAddressException Text
+data BuildAddressException = BuildAddressException Text | QueryException Text
   deriving (Show)
 
 instance Exception BuildAddressException
+
+-- |Query UTxO for all given addresses.
+--
+-- This query is specialised for Shelley addresses in Alonzo era.
+queryUtxo :: NetworkId -> FilePath -> [Address ShelleyAddr] -> IO (UTxO AlonzoEra)
+queryUtxo networkId socket addresses =
+  let query =
+        QueryInEra
+          AlonzoEraInCardanoMode
+          ( QueryInShelleyBasedEra
+              ShelleyBasedEraAlonzo
+              ( QueryUTxO
+                  (QueryUTxOByAddress (Set.fromList $ map AddressShelley addresses))
+              )
+          )
+      defaultByronEpochSlots = 21600 :: Word64
+      cardanoModeParams = CardanoModeParams $ EpochSlots defaultByronEpochSlots
+      localNodeConnectInfo = LocalNodeConnectInfo cardanoModeParams networkId socket
+   in runExceptT (executeQuery AlonzoEra cardanoModeParams localNodeConnectInfo query) >>= \case
+        Left err -> throwIO $ QueryException (show err)
+        Right utxo -> pure utxo
--- local-cluster/test_submit.sh
+++ local-cluster/test_submit.sh
@@ -6,19 +6,14 @@ set -vx
 [ -x $(which cardano-cli) ] || { echo "cardano-cli not found, check PATH environment variable" ; exit 1 ; }
 
 utxo_addr=$1
+utxo=$2
+amount=$3
 
 transfer_amount=100000000
 
-# take first utxo available
-alice_addr=$(cardano-cli address build --payment-verification-key-file ../alice.vk --testnet-magic 42)
-alice_txin=$(cardano-cli query utxo --testnet-magic 42 --address $alice_addr | tail -n +3 | tr -s ' ' | cut -d ' ' -f1,2 | tr ' ' '#')
-alice_fortune=$(cardano-cli query utxo --testnet-magic 42 --address $alice_addr | tail -n +3 | tr -s ' ' | cut -d ' ' -f3)
-
-transfer_amount=100000000
-
-# dispatch some ADAs from alice to bob
-cardano-cli transaction build-raw --tx-in $alice_txin \
-            --tx-out $alice_addr+$transfer_amount \
+# dispatch some ADAs to alice
+cardano-cli transaction build-raw --tx-in $utxo \
+            --tx-out $utxo_addr+$transfer_amount \
             --invalid-hereafter 0 --fee 0 --out-file tx.draft
 
 fees=$(cardano-cli transaction calculate-min-fee --tx-body-file tx.draft --tx-in-count 1 --tx-out-count 2 --witness-count 1 --testnet-magic 42 --genesis genesis-shelley.json | cut -d ' ' -f1)
