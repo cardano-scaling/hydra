ac6cefc26edec15d12d04202a377cdda5dfe7e76
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-10-21 07:27:58 +0000

    Completely replace script with cardano-api calls

--- local-cluster/test/Test/LocalClusterSpec.hs
+++ local-cluster/test/Test/LocalClusterSpec.hs
@@ -5,28 +5,34 @@ import Test.Hydra.Prelude
 
 import Cardano.Api (
   Address,
+  Lovelace,
   MultiAssetSupportedInEra (MultiAssetInAlonzoEra),
+  NetworkId,
   ShelleyAddr,
   TxOut (TxOut),
   TxOutDatumHash (TxOutDatumHashNone),
-  TxOutValue (TxOutAdaOnly, TxOutValue),
+  TxOutValue (TxOutValue),
   UTxO (..),
   lovelaceToValue,
-  selectLovelace,
-  serialiseToBech32,
   shelleyAddressInEra,
  )
-import CardanoClient (Sizes (..), buildAddress, buildRaw, calculateMinFee, defaultSizes, queryProtocolParameters, queryTipSlotNo, queryUtxo, sign, submit)
+import CardanoClient (
+  Sizes (..),
+  buildAddress,
+  buildRaw,
+  calculateMinFee,
+  defaultSizes,
+  queryProtocolParameters,
+  queryTipSlotNo,
+  queryUtxo,
+  sign,
+  submit,
+  txOutLovelace,
+ )
 import CardanoCluster (ClusterConfig (..), ClusterLog (..), RunningCluster (..), keysFor, testClusterConfig, withCluster)
 import CardanoNode (ChainTip (..), RunningNode (..), cliQueryTip)
 import qualified Data.Map as Map
-import Data.Text (unpack)
 import Hydra.Logging (Tracer, showLogsOnFailure)
-import qualified Paths_local_cluster as Pkg
-import System.Environment (getEnvironment)
-import System.Exit (ExitCode (..))
-import System.FilePath ((</>))
-import System.Process
 
 spec :: Spec
 spec =
@@ -36,7 +42,7 @@ spec =
         let config = testClusterConfig tmp
         withCluster tr config $ \cluster -> do
           failAfter 30 $ assertNetworkIsProducingBlock tr cluster
-          assertCanSpendInitialFunds cluster
+          failAfter 30 $ assertCanSpendInitialFunds cluster
 
 assertNetworkIsProducingBlock :: Tracer IO ClusterLog -> RunningCluster -> IO ()
 assertNetworkIsProducingBlock tracer = go (-1)
@@ -53,56 +59,37 @@ assertNetworkIsProducingBlock tracer = go (-1)
 
 assertCanSpendInitialFunds :: HasCallStack => RunningCluster -> IO ()
 assertCanSpendInitialFunds = \case
-  cluster@(RunningCluster ClusterConfig{parentStateDirectory, networkId} (RunningNode nodeId socket : _)) -> do
+  cluster@(RunningCluster ClusterConfig{networkId} (RunningNode _ socket : _)) -> do
     (vk, sk) <- keysFor "alice" cluster
     addr <- buildAddress vk networkId
     UTxO utxo <- queryUtxo networkId socket [addr]
-    let (txIn, TxOut _ val _) = case Map.toList utxo of
+    let (txIn, out) = case Map.toList utxo of
           [] -> error "No Utxo found"
           (tx : _) -> tx
-        -- NOTE(AB): this is txOutValueToLovelace in more recent cardano-api versions
-        amount = case val of
-          TxOutAdaOnly _ l -> l
-          TxOutValue _ v -> selectLovelace v
 
-        nodeDirectory = parentStateDirectory </> "node-" <> show nodeId
-        paymentOutput = TxOut (shelleyAddressInEra addr) (TxOutValue MultiAssetInAlonzoEra (lovelaceToValue 100_000_000)) TxOutDatumHashNone
+        amount = txOutLovelace out
+        amountToPay = 100_000_001
+        paymentOutput = TxOut (shelleyAddressInEra addr) (TxOutValue MultiAssetInAlonzoEra (lovelaceToValue amountToPay)) TxOutDatumHashNone
     rawTx <- buildRaw [txIn] [] 0 0
     pparams <- queryProtocolParameters networkId socket
     let fee = calculateMinFee networkId rawTx defaultSizes{inputs = 1, outputs = 2, witnesses = 1} pparams
     slotNo <- queryTipSlotNo networkId socket
-    let changeOutput = TxOut (shelleyAddressInEra addr) (TxOutValue MultiAssetInAlonzoEra (lovelaceToValue $ amount - 100_000_000 - fee)) TxOutDatumHashNone
+    let changeOutput = TxOut (shelleyAddressInEra addr) (TxOutValue MultiAssetInAlonzoEra (lovelaceToValue $ amount - amountToPay - fee)) TxOutDatumHashNone
     draftTx <- buildRaw [txIn] [paymentOutput, changeOutput] (slotNo + 100) fee
     let signedTx = sign sk draftTx
     submit networkId socket signedTx
-    runTestScript nodeDirectory addr socket
+    waitForPayment networkId socket amountToPay addr
   _ ->
     error "empty cluster?"
 
-runTestScript :: FilePath -> Address ShelleyAddr -> FilePath -> IO ()
-runTestScript nodeDirectory addr socket = do
-  inputScript <- Pkg.getDataFileName "test_submit.sh"
-  currentEnv <- getEnvironment
-  let scriptOutput = nodeDirectory </> "test_submit.out"
-  withFile' scriptOutput $ \fileOut ->
-    withCreateProcess (sh currentEnv inputScript fileOut) $ \_stdin _stdout _stderr hdl ->
-      waitForProcess hdl >>= \case
-        ExitFailure{} -> readFile scriptOutput >>= failure . ("Initial funds spending script failed, output: " <>)
-        ExitSuccess -> pure ()
+waitForPayment :: NetworkId -> FilePath -> Lovelace -> Address ShelleyAddr -> IO ()
+waitForPayment networkId socket amount addr = go
  where
-  socketEnv = ("CARDANO_NODE_SOCKET_PATH", socket)
-  sh baseEnv script out =
-    ( proc
-        "/bin/sh"
-        [ script
-        , unpack $ serialiseToBech32 addr
-        ]
-    )
-      { env = Just (socketEnv : baseEnv)
-      , cwd = Just nodeDirectory
-      , std_out = UseHandle out
-      , std_err = UseHandle out
-      }
+  go = do
+    UTxO utxo <- queryUtxo networkId socket [addr]
+    unless (containsPayment utxo) $ threadDelay 1 >> go
+  containsPayment utxo =
+    Map.filter ((== amount) . txOutLovelace) utxo /= mempty
 
 waitForNewBlock :: IO ()
 waitForNewBlock = threadDelay (2 * slotLength)
