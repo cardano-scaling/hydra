af1370f1943591e146dbcc097c31c7f3d3fded43
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-07 15:30:14 +0000

    Still need to wait for seed input to appear

--- hydra-node/test/Hydra/Chain/DirectSpec.hs
+++ hydra-node/test/Hydra/Chain/DirectSpec.hs
@@ -17,15 +17,16 @@ import Control.Concurrent (newEmptyMVar, putMVar, takeMVar)
 import Hydra.Chain (
   Chain (..),
   HeadParameters (HeadParameters),
+  InvalidTxError (NoSeedInput),
   OnChainTx (OnAbortTx, OnInitTx),
   PostChainTx (AbortTx, InitTx),
  )
 import Hydra.Chain.Direct (DirectChainLog, withDirectChain)
 import Hydra.Chain.Direct.MockServer (withMockServer)
-import Hydra.Chain.Direct.Util (Era, VerificationKey)
+import Hydra.Chain.Direct.Util (Era, VerificationKey, retry)
 import Hydra.Chain.Direct.Wallet (generateKeyPair)
 import Hydra.Chain.Direct.WalletSpec (genPaymentTo)
-import Hydra.Ledger.Cardano (NetworkMagic)
+import Hydra.Ledger.Cardano (CardanoTx, NetworkMagic)
 import Hydra.Logging (showLogsOnFailure)
 import Hydra.Party (Party, deriveParty, generateKey)
 import Test.QuickCheck (generate)
@@ -46,7 +47,7 @@ spec = do
               let parameters = HeadParameters 100 [alice, bob, carol]
               mkSeedPayment magic aliceVk submitTx
 
-              postTx $ InitTx parameters
+              retry (== NoSeedInput @CardanoTx) $ postTx $ InitTx parameters
               takeMVar calledBackAlice `shouldReturn` OnInitTx 100 [alice, bob, carol]
               takeMVar calledBackBob `shouldReturn` OnInitTx 100 [alice, bob, carol]
 
