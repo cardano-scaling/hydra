d4e9cbb5df41ee88a00fa2f84be19dde15dd7c4b
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-12-08 07:16:50 +0100

    BrickTest outputs to IORef and takes inputs

--- hydra-tui/test/Hydra/TUISpec.hs
+++ hydra-tui/test/Hydra/TUISpec.hs
@@ -3,17 +3,30 @@ module Hydra.TUISpec where
 import Hydra.Prelude
 import Test.Hydra.Prelude
 
-import Control.Monad.Class.MonadTimer (timeout)
+import Control.Monad.Class.MonadSTM (MonadSTM (newTBQueueIO, newTQueueIO), MonadSTMTx (writeTQueue), readTQueue, tryReadTQueue)
 import qualified Data.ByteString as BS
-import Data.ByteString.Char8 (takeWhileEnd)
-import Graphics.Vty (Config (outputFd), Event, Vty (shutdown, update), defaultConfig, mkVty)
+import Foreign (plusPtr, withForeignPtr)
+import Graphics.Vty (
+  Config (outputFd),
+  DisplayContext (..),
+  Event,
+  Input (shutdownInput),
+  Output (..),
+  Vty (Vty, inputIface, isShutdown, nextEvent, nextEventNonblocking, outputIface, refresh, shutdown, update),
+  defaultConfig,
+  displayContext,
+  initialAssumedState,
+  inputForConfig,
+  mkVty,
+  outputPicture,
+  reserveDisplay,
+ )
 import Hydra.Network (Host (..))
 import Hydra.TUI (runWithVty)
 import Hydra.TUI.Options (Options (..))
 import System.IO (SeekMode (AbsoluteSeek))
 import System.IO.Temp (withSystemTempFile)
-import System.Posix (closeFd, createFile, fdSeek, fdWrite, handleToFd, stdFileMode)
-import System.Posix.Files (removeLink)
+import System.Posix (fdSeek, handleToFd)
 
 spec :: Spec
 spec =
@@ -21,7 +34,7 @@ spec =
     it "starts & renders" $
       withBrickTest $ \BrickTest{buildVty, getPicture} -> do
         race_ (runWithVty buildVty Options{nodeHost = Host{hostname = "127.0.0.1", port = 4001}}) $ do
-          threadDelay 1
+          threadDelay 3
           getPicture >>= putBSLn
 
 data BrickTest = BrickTest
@@ -32,30 +45,80 @@ data BrickTest = BrickTest
 
 withBrickTest :: (BrickTest -> Expectation) -> Expectation
 withBrickTest action = do
-  withSystemTempFile "brick-test" $ \fp h -> do
-    fd <- handleToFd h
-    action $
-      BrickTest
-        { buildVty = buildVty fd
-        , getPicture = getPicture fp
-        , sendInputEvent
-        }
+  outRef <- newIORef mempty
+  q <- newTQueueIO
+  action $
+    BrickTest
+      { buildVty = buildVty q outRef
+      , getPicture = readIORef outRef
+      , sendInputEvent = atomically . writeTQueue q
+      }
  where
-  buildVty fd = do
-    vty <- mkVty defaultConfig{outputFd = Just fd}
+  buildVty q outRef = do
+    putStrLn "buildVty"
+    input <- inputForConfig defaultConfig
+    -- This is used by outputPicture and we hack it such that it always has the
+    -- initial state to get a full rendering of the picture
+    as <- newIORef initialAssumedState
+    output <- testOut as outRef
     pure $
-      vty
-        { update = onUpdate vty fd
-        , shutdown = onShutdown vty fd
+      Vty
+        { inputIface = input
+        , nextEvent = do
+            putStrLn "nextEvent"
+            atomically $ readTQueue q
+        , nextEventNonblocking = do
+            putStrLn "nextEventNonBlocking"
+            atomically $ tryReadTQueue q
+        , outputIface = output
+        , update = \p -> do
+            putStrLn "update, clear assumed state & buffer"
+            writeIORef as initialAssumedState
+            atomicModifyIORef'_ outRef (const mempty)
+            dc <- displayContext output (50, 50)
+            outputPicture dc p
+        , refresh = do
+            putStrLn "refresh"
+        , shutdown = do
+            putStrLn "shutdown"
+            shutdownInput input
+        , isShutdown = pure True
         }
 
-  getPicture fp = readFileBS fp
-
-  sendInputEvent = undefined
-
-  onUpdate vty _fd p = do
-    putStrLn "update called"
-    update vty p
-
-  onShutdown vty _fd = do
-    shutdown vty
+  testOut as outRef = do
+    pure $
+      Output
+        { terminalID = "BrickTest terminal"
+        , releaseTerminal = pure ()
+        , reserveDisplay = pure ()
+        , releaseDisplay = pure ()
+        , ringTerminalBell = pure ()
+        , supportsBell = pure False
+        , supportsItalics = pure False
+        , supportsStrikethrough = pure False
+        , setDisplayBounds = const $ pure ()
+        , displayBounds = pure (100, 100)
+        , outputByteBuffer = \bytes -> do
+            putStrLn "outputByteBuffer"
+            atomicModifyIORef'_ outRef (<> bytes)
+        , contextColorCount = 16
+        , supportsCursorVisibility = True
+        , supportsMode = const False
+        , setMode = const $ const $ pure ()
+        , getModeStatus = const $ pure False
+        , assumedStateRef = as
+        , mkDisplayContext = \tActual rActual -> do
+            putStrLn "mkDisplayContext"
+            pure $
+              DisplayContext
+                { contextRegion = rActual
+                , contextDevice = tActual
+                , writeMoveCursor = \_x _y -> trace "writeMoveCursor" mempty
+                , writeShowCursor = trace "writeShowCursor" mempty
+                , writeHideCursor = trace "writeHideCursor" mempty
+                , writeSetAttr = \_ _fattr _diffs _attr -> trace "writeSetAttr" mempty
+                , writeDefaultAttr = trace "writeDefaultAttr" mempty
+                , writeRowEnd = trace "writeRowEnd" mempty
+                , inlineHack = pure ()
+                }
+        }
