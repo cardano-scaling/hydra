5f1f291a1e97825b91fda56a96b0e96504929cb6
KtorZ <matthias.benkort@gmail.com>
2021-12-10 10:50:46 +0100

    Use temporary file for materialise.sh

--- materialise.sh
+++ materialise.sh
@@ -4,18 +4,22 @@
 
 sed -i -e 's/checkMaterialization = false/checkMaterialization = true/g' default.nix
 
-nix-build -A hydra-node.components.exes.hydra-node 2>&1 | tee nix.out
+tmp=$(mktemp)
 
-NEW_SHA=$(grep 'got:    sha256:' nix.out | sed -e 's/.*sha256:\(.*\)/\1/')
+nix-build -A hydra-node.components.exes.hydra-node 2>&1 | tee $tmp
+
+NEW_SHA=$(grep 'got:    sha256:' $tmp | sed -e 's/.*sha256:\(.*\)/\1/')
 
 [ -n "$NEW_SHA" ]  && sed -i -e "s/\(.*\)plan-sha256 =.*/\1plan-sha256 = \"$NEW_SHA\"\;/g" default.nix
 
-nix-build -A hydra-node.project.plan-nix.passthru.updateMaterialized 2> nix.out | bash
+nix-build -A hydra-node.project.plan-nix.passthru.updateMaterialized 2> $tmp | bash
 
-NIX_SCRIPT=$(grep 'To fix run' nix.out |  sed -e 's/.*To fix run: \(.*\)/\1/')
+NIX_SCRIPT=$(grep 'To fix run' $tmp |  sed -e 's/.*To fix run: \(.*\)/\1/')
 
 echo "Running $NIX_SCRIPT"
 
 eval "${NIX_SCRIPT}"
 
 sed -i -e 's/checkMaterialization = true/checkMaterialization = false/g' default.nix
+
+rm $tmp
