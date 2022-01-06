9614b526183c2e95e96f94f8a6331f8eb8fa9253
KtorZ <matthias.benkort@gmail.com>
2022-01-06 10:13:29 +0100

    Start implementing fromList/toList for Merkle Tree.

--- plutus-merkle-tree/src/Plutus/MerkleTree.hs
+++ plutus-merkle-tree/src/Plutus/MerkleTree.hs
@@ -1,11 +1,38 @@
 module Plutus.MerkleTree where
 
 import PlutusTx.Prelude
+import qualified Prelude as Haskell
+
+type Hash = BuiltinByteString
 
 data MerkleTree
+  = MerkleEmpty
+  | MerkleNode Hash MerkleTree MerkleTree
+  | MerkleLeaf Hash BuiltinByteString
+  deriving (Haskell.Eq, Haskell.Show)
+
+instance Eq MerkleTree where
+  MerkleEmpty == MerkleEmpty = True
+  (MerkleLeaf h0 _) == (MerkleLeaf h1 _) = h0 == h1
+  (MerkleNode h0 _ _) == (MerkleNode h1 _ _) = h0 == h1
+  _ == _ = False
 
 fromList :: [BuiltinByteString] -> MerkleTree
-fromList = traceError "not implemented"
+fromList =
+  foldr insert MerkleEmpty
 
 toList :: MerkleTree -> [BuiltinByteString]
-toList = traceError "not implemented"
+toList = go []
+ where
+  go xs = \case
+    MerkleEmpty -> xs
+    _ -> xs
+
+insert :: BuiltinByteString -> MerkleTree -> MerkleTree
+insert e = \case
+  MerkleEmpty -> MerkleLeaf (sha2_256 e) e
+  leaf@(MerkleLeaf h' _) ->
+    let h = sha2_256 e
+        hNode = sha2_256 (appendByteString h' h)
+     in MerkleNode hNode leaf (MerkleLeaf h e)
+  _ -> MerkleEmpty
--- plutus-merkle-tree/test/Plutus/MerkleTreeSpec.hs
+++ plutus-merkle-tree/test/Plutus/MerkleTreeSpec.hs
@@ -1,14 +1,17 @@
 module Plutus.MerkleTreeSpec where
 
 import Hydra.Prelude
+import Test.Hydra.Prelude
+
+import qualified Data.ByteString as BS
 import Plutus.MerkleTree (MerkleTree)
 import qualified Plutus.MerkleTree as MT
-import Test.Hydra.Prelude
-import Test.QuickCheck (Property, (===))
+import qualified PlutusTx.Builtins as Plutus
+import Test.QuickCheck (Property, forAllShrink, (===))
 
 spec :: Spec
 spec =
-  prop "fromList . toList roundtrips MT" $ prop_roundtripFromToList
+  prop "fromList . toList roundtrips MT" prop_roundtripFromToList
 
 prop_roundtripFromToList :: Property
 prop_roundtripFromToList =
@@ -16,4 +19,11 @@ prop_roundtripFromToList =
     MT.fromList (MT.toList tree) === tree
 
 forAllMerkleTree :: (MerkleTree -> Property) -> Property
-forAllMerkleTree = error "not implemented"
+forAllMerkleTree = forAllShrink genMerkleTree shrinkMerkleTree
+
+genMerkleTree :: Gen MerkleTree
+genMerkleTree =
+  MT.fromList . fmap (Plutus.toBuiltin . BS.pack) <$> arbitrary
+
+shrinkMerkleTree :: MerkleTree -> [MerkleTree]
+shrinkMerkleTree _ = []
