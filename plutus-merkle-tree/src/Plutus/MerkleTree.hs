599c49e9825eae8d996e9b03c153f473e6df2d8b
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-01-06 08:47:33 +0000

    Add first property for building treee

--- plutus-merkle-tree/src/Plutus/MerkleTree.hs
+++ plutus-merkle-tree/src/Plutus/MerkleTree.hs
@@ -1,1 +1,11 @@
 module Plutus.MerkleTree where
+
+import PlutusTx.Prelude
+
+data MerkleTree
+
+fromList :: [BuiltinByteString] -> MerkleTree
+fromList = traceError "not implemented"
+
+toList :: MerkleTree -> [BuiltinByteString]
+toList = traceError "not implemented"
--- plutus-merkle-tree/test/Plutus/MerkleTreeSpec.hs
+++ plutus-merkle-tree/test/Plutus/MerkleTreeSpec.hs
@@ -1,7 +1,19 @@
 module Plutus.MerkleTreeSpec where
 
 import Hydra.Prelude
+import Plutus.MerkleTree (MerkleTree)
+import qualified Plutus.MerkleTree as MT
 import Test.Hydra.Prelude
+import Test.QuickCheck (Property, (===))
 
 spec :: Spec
-spec = error "not implemented"
+spec =
+  prop "fromList . toList roundtrips MT" $ prop_roundtripFromToList
+
+prop_roundtripFromToList :: Property
+prop_roundtripFromToList =
+  forAllMerkleTree $ \tree ->
+    MT.fromList (MT.toList tree) === tree
+
+forAllMerkleTree :: (MerkleTree -> Property) -> Property
+forAllMerkleTree = error "not implemented"
