1dc74c57dcc0d5d78bf2791370334c2b30d58b26
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-01-11 07:00:15 +0000

    Separate on-chain encoding logic to own module

--- hydra-plutus/src/Hydra/Contract/MockHead.hs
+++ hydra-plutus/src/Hydra/Contract/MockHead.hs
@@ -12,36 +12,18 @@ import Control.Monad (guard)
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Void (Void)
 import GHC.Generics (Generic)
+import Hydra.Contract.Encoding (serialiseTxOuts)
 import Hydra.Data.ContestationPeriod (ContestationPeriod)
 import Hydra.Data.Party (Party (UnsafeParty))
 import Ledger.Constraints (TxConstraints)
 import qualified Ledger.Typed.Scripts as Scripts
 import Plutus.Codec.CBOR.Encoding (
-  Encoding,
-  encodeBeginList,
-  encodeBreak,
-  encodeByteString,
   encodeInteger,
-  encodeListLen,
-  encodeMap,
-  encodeMapIndef,
   encodingToBuiltinByteString,
  )
 import Plutus.Contract.StateMachine.OnChain (StateMachine)
 import qualified Plutus.Contract.StateMachine.OnChain as SM
-import Plutus.V1.Ledger.Api (
-  Credential (PubKeyCredential, ScriptCredential),
-  CurrencySymbol (CurrencySymbol),
-  StakingCredential (..),
-  TokenName (TokenName),
-  adaSymbol,
-  adaToken,
-  getValue,
- )
-import Plutus.V1.Ledger.Value (valueOf)
 import qualified PlutusTx
-import PlutusTx.AssocMap (Map)
-import qualified PlutusTx.AssocMap as Map
 import Text.Show (Show)
 
 type SnapshotNumber = Integer
@@ -125,94 +107,6 @@ hashTxOuts =
   sha2_256 . serialiseTxOuts
 {-# INLINEABLE hashTxOuts #-}
 
-serialiseTxOuts :: [TxOut] -> BuiltinByteString
-serialiseTxOuts outs =
-  encodingToBuiltinByteString $
-    encodeBeginList <> foldMap encodeTxOut outs <> encodeBreak
-{-# INLINEABLE serialiseTxOuts #-}
-
-encodeTxOut :: TxOut -> Encoding
-encodeTxOut TxOut{txOutAddress, txOutValue, txOutDatumHash} =
-  case txOutDatumHash of
-    Just h ->
-      encodeListLen 3
-        <> encodeAddress txOutAddress
-        <> encodeValue txOutValue
-        <> encodeDatum h
-    Nothing ->
-      encodeListLen 2
-        <> encodeAddress txOutAddress
-        <> encodeValue txOutValue
-
--- See for details: https://github.com/cardano-foundation/CIPs/blob/master/CIP-0019/CIP-0019-cardano-addresses.abnf
--- we only take care of type 6 or 7 addresses rn
---
--- FIXME: This currently assumes that all addresses are TESTNET addresses.
--- Eventually, we need to pass the network id as argument and add `1` to the
--- address prefixes.
-encodeAddress :: Address -> Encoding
-encodeAddress Address{addressCredential, addressStakingCredential} =
-  encodeByteString bytes
- where
-  bytes =
-    case (addressCredential, addressStakingCredential) of
-      (PubKeyCredential (PubKeyHash h), Just (StakingHash (PubKeyCredential (PubKeyHash h')))) ->
-        addrType00 `consByteString` (h `appendByteString` h')
-      (ScriptCredential (ValidatorHash h), Just (StakingHash (PubKeyCredential (PubKeyHash h')))) ->
-        addrType01 `consByteString` (h `appendByteString` h')
-      (PubKeyCredential (PubKeyHash h), Just (StakingHash (ScriptCredential (ValidatorHash h')))) ->
-        addrType02 `consByteString` (h `appendByteString` h')
-      (ScriptCredential (ValidatorHash h), Just (StakingHash (ScriptCredential (ValidatorHash h')))) ->
-        addrType03 `consByteString` (h `appendByteString` h')
-      (PubKeyCredential (PubKeyHash h), Nothing) ->
-        addrType06 `consByteString` h
-      (ScriptCredential (ValidatorHash h), Nothing) ->
-        addrType07 `consByteString` h
-      _ ->
-        traceError "encodeAddress: ptr"
-  addrType00 = 0
-  addrType01 = 16
-  addrType02 = 32
-  addrType03 = 48
-  addrType06 = 96
-  addrType07 = 112
-
-encodeValue :: Value -> Encoding
-encodeValue val =
-  if Map.null assets
-    then encodeInteger coins
-    else encodeListLen 2 <> encodeInteger coins <> encodeAssets assets
- where
-  coins = valueOf val adaSymbol adaToken
-  assets = Map.delete adaSymbol (getValue val)
-{-# INLINEABLE encodeValue #-}
-
-encodeAssets :: Map CurrencySymbol (Map TokenName Integer) -> Encoding
-encodeAssets m
-  | length m <= 23 = encodeMap encodeCurrencySymbol encodeSingleAsset m
-  | otherwise = encodeMapIndef encodeCurrencySymbol encodeSingleAsset m
-{-# INLINEABLE encodeAssets #-}
-
-encodeSingleAsset :: Map TokenName Integer -> Encoding
-encodeSingleAsset m
-  | length m <= 23 = encodeMap encodeTokenName encodeInteger m
-  | otherwise = encodeMapIndef encodeTokenName encodeInteger m
-{-# INLINEABLE encodeSingleAsset #-}
-
-encodeCurrencySymbol :: CurrencySymbol -> Encoding
-encodeCurrencySymbol (CurrencySymbol symbol) =
-  encodeByteString symbol
-{-# INLINEABLE encodeCurrencySymbol #-}
-
-encodeTokenName :: TokenName -> Encoding
-encodeTokenName (TokenName token) =
-  encodeByteString token
-{-# INLINEABLE encodeTokenName #-}
-
-encodeDatum :: DatumHash -> Encoding
-encodeDatum (DatumHash h) = encodeByteString h
-{-# INLINEABLE encodeDatum #-}
-
 {-# INLINEABLE verifySnapshotSignature #-}
 verifySnapshotSignature :: [Party] -> SnapshotNumber -> [Signature] -> Bool
 verifySnapshotSignature parties snapshotNumber sigs =
@@ -252,7 +146,7 @@ mockSign vkey msg = appendByteString (sliceByteString 0 8 hashedMsg) (encodingTo
 --   transactions transitioning the state machine and provide "contract
 --   continuity"
 --
--- TODO: Add a NetworkId so that we can prioperly serialise address hashes
+-- TODO: Add a NetworkId so that we can properly serialise address hashes
 -- see 'encodeAddress' for details
 typedValidator :: MintingPolicyHash -> Scripts.TypedValidator (StateMachine State Input)
 typedValidator policyId =
