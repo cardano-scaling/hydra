87fc0e40165a9158be7b258b65883b9a981a56bc
KtorZ <matthias.benkort@gmail.com>
2022-02-01 17:52:24 +0100

    resolve import cycle issue problem with UTxO.

--- hydra-cardano-api/src/Hydra/Cardano/Api/UTxO.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api/UTxO.hs
@@ -1,8 +1,3 @@
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-
--- | NOTE: This module is meant to be imported qualified as 'UTxO'.
 module Hydra.Cardano.Api.UTxO where
 
 import Hydra.Cardano.Api.Prelude
@@ -10,53 +5,21 @@ import Hydra.Cardano.Api.TxId (toLedgerTxId)
 import Hydra.Cardano.Api.TxIn (fromLedgerTxIn, toLedgerTxIn)
 import Hydra.Cardano.Api.TxOut (fromLedgerTxOut, toLedgerTxOut)
 
+import qualified Cardano.Api.UTxO as UTxO
 import qualified Cardano.Ledger.Alonzo.TxBody as Ledger
 import qualified Cardano.Ledger.Shelley.UTxO as Ledger
 import qualified Cardano.Ledger.TxIn as Ledger
 import qualified Data.Map as Map
-import qualified Data.Text as T
 
-type UTxO = UTxO' (TxOut CtxUTxO Era)
-
--- | Newtype with phantom types mostly required to work around the poor interface
--- of 'Ledger.UTXO'and provide 'Monoid' and 'Foldable' instances to make utxo
--- manipulation bareable.
-newtype UTxO' out = UTxO
-  { toMap :: Map TxIn out
-  }
-  deriving newtype
-    ( Eq
-    , Show
-    , Functor
-    , Foldable
-    , Semigroup
-    , Monoid
-    )
-
-instance Traversable UTxO' where
-  traverse fn (UTxO m) = UTxO <$> traverse fn m
-
-instance ToCBOR UTxO where
-  toCBOR = toCBOR . toLedgerUTxO
-  encodedSizeExpr sz _ = encodedSizeExpr sz (Proxy @(Ledger.UTxO LedgerEra))
-
-instance FromCBOR UTxO where
-  fromCBOR = fromLedgerUTxO <$> fromCBOR
-  label _ = label (Proxy @(Ledger.UTxO LedgerEra))
-
--- | Create a 'UTxO' from a single unspent transaction output.
-singleton :: (TxIn, TxOut CtxUTxO Era) -> UTxO
-singleton (i, o) = UTxO $ Map.singleton i o
-
--- | Turn a 'UTxO' into a list of pairs.
-pairs :: UTxO' out -> [(TxIn, out)]
-pairs = Map.toList . toMap
+-- | Get a human-readable pretty text representation of a UTxO.
+renderUTxO :: (TxIn, TxOut ctx era) -> Text
+renderUTxO = UTxO.render
 
 -- | Construct a UTxO from a transaction. This constructs artificial `TxIn`
 -- (a.k.a output reference) from the transaction itself, zipping them to the
 -- outputs they correspond to.
-fromTx :: Tx Era -> UTxO
-fromTx (Tx body@(ShelleyTxBody _ ledgerBody _ _ _ _) _) =
+utxoFromTx :: Tx Era -> UTxO
+utxoFromTx (Tx body@(ShelleyTxBody _ ledgerBody _ _ _ _) _) =
   let txOuts = toList $ Ledger.outputs' ledgerBody
       txIns =
         [ Ledger.TxIn (toLedgerTxId $ getTxId body) ix
@@ -64,20 +27,11 @@ fromTx (Tx body@(ShelleyTxBody _ ledgerBody _ _ _ _) _) =
         ]
    in fromLedgerUTxO $ Ledger.UTxO $ Map.fromList $ zip txIns txOuts
 
--- | Get a human-readable pretty text representation of a UTxO.
-render :: (TxIn, TxOut ctx era) -> Text
-render (k, TxOut _ (txOutValueToValue -> v) _) =
-  T.drop 54 (renderTxIn k) <> " ↦ " <> renderValue v
-
--- | See 'render'.
-renderUTxO :: (TxIn, TxOut ctx era) -> Text
-renderUTxO = render
-
 -- * Type Conversions
 
 toLedgerUTxO :: UTxO -> Ledger.UTxO LedgerEra
 toLedgerUTxO =
-  Ledger.UTxO . Map.foldMapWithKey fn . toMap
+  Ledger.UTxO . Map.foldMapWithKey fn . UTxO.toMap
  where
   fn ::
     TxIn ->
