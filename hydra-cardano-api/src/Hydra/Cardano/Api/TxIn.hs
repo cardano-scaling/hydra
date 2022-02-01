4c8ddcbc9e3b4e2abf436c30a5381cb478377899
KtorZ <matthias.benkort@gmail.com>
2022-02-01 16:07:36 +0100

    Add extra functions for 'TxOutValue'.

--- hydra-cardano-api/src/Hydra/Cardano/Api/TxIn.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api/TxIn.hs
@@ -4,6 +4,19 @@ import Hydra.Cardano.Api.Prelude
 
 import qualified Cardano.Ledger.TxIn as Ledger
 
+-- * Extras
+
+-- | Create a 'TxIn' (a.k.a UTXO) from a transaction and output index.
+mkTxIn :: Tx era -> Word -> TxIn
+mkTxIn (getTxId . getTxBody -> txId) index =
+  TxIn txId (TxIx index)
+
+-- | Access inputs of a transaction, as an ordered list.
+txIns' :: Tx era -> [TxIn]
+txIns' (getTxBody -> txBody) =
+  let TxBody TxBodyContent{txIns} = txBody
+   in fst <$> txIns
+
 -- * Type Conversions
 
 -- | Convert a cardano-ledger 'TxIn' to a cardano-api 'TxIn'
@@ -13,9 +26,3 @@ fromLedgerTxIn = fromShelleyTxIn
 -- | Convert a cardano-api 'TxIn' to a cardano-ledger 'TxIn'
 toLedgerTxIn :: TxIn -> Ledger.TxIn StandardCrypto
 toLedgerTxIn = toShelleyTxIn
-
--- * Extras
-
--- | Create a 'TxIn' from a transaction body and index.
-mkTxIn :: TxBody era -> Word -> TxIn
-mkTxIn txBody index = TxIn (getTxId txBody) (TxIx index)
--- hydra-cardano-api/src/Hydra/Cardano/Api/TxOut.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api/TxOut.hs
@@ -3,8 +3,8 @@ module Hydra.Cardano.Api.TxOut where
 import Hydra.Cardano.Api.Prelude
 
 txOuts' :: Tx Era -> [TxOut CtxTx Era]
-txOuts' tx =
-  let TxBody TxBodyContent{txOuts} = getTxBody tx
+txOuts' (getTxBody -> txBody) =
+  let TxBody TxBodyContent{txOuts} = txBody
    in txOuts
 
 {-# DEPRECATED getOutputs "use txOuts' instead." #-}
