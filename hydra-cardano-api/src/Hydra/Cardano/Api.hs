a7f35c27b62c0705dfda66a9cc6bc50eafa80e31
KtorZ <matthias.benkort@gmail.com>
2022-02-01 16:07:36 +0100

    Add extra functions for 'TxOutValue'.

--- hydra-cardano-api/src/Hydra/Cardano/Api.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api.hs
@@ -3,6 +3,8 @@ module Hydra.Cardano.Api (
   module Cardano.Api.Byron,
   module Cardano.Api.Shelley,
   module Hydra.Cardano.Api.AddressInEra,
+  module Hydra.Cardano.Api.KeyWitness,
+  module Hydra.Cardano.Api.Lovelace,
   module Hydra.Cardano.Api.PlutusScriptVersion,
   module Hydra.Cardano.Api.ScriptData,
   module Hydra.Cardano.Api.ScriptDatum,
@@ -12,13 +14,21 @@ module Hydra.Cardano.Api (
   module Hydra.Cardano.Api.TxIn,
   module Hydra.Cardano.Api.TxOut,
   module Hydra.Cardano.Api.TxOutDatum,
+  module Hydra.Cardano.Api.TxOutValue,
+  module Hydra.Cardano.Api.TxScriptValidity,
+  StandardCrypto,
+  Era,
+  LedgerEra,
 ) where
 
 import Cardano.Api
 import Cardano.Api.Byron
 import Cardano.Api.Shelley
 import Hydra.Cardano.Api.AddressInEra
+import Hydra.Cardano.Api.KeyWitness
+import Hydra.Cardano.Api.Lovelace
 import Hydra.Cardano.Api.PlutusScriptVersion
+import Hydra.Cardano.Api.Prelude (Era, LedgerEra, StandardCrypto)
 import Hydra.Cardano.Api.ScriptData
 import Hydra.Cardano.Api.ScriptDatum
 import Hydra.Cardano.Api.ScriptHash
@@ -27,3 +37,5 @@ import Hydra.Cardano.Api.TxBody
 import Hydra.Cardano.Api.TxIn
 import Hydra.Cardano.Api.TxOut
 import Hydra.Cardano.Api.TxOutDatum
+import Hydra.Cardano.Api.TxOutValue
+import Hydra.Cardano.Api.TxScriptValidity
