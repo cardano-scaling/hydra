79e33f6a5bc5637a9af1430923f4aa0d3376a481
KtorZ <matthias.benkort@gmail.com>
2022-02-11 17:56:56 +0100

    Add extra functions for modifying 'Tx' to the hydra-cardano-api
      Those functions are mostly unsound outside of a testing context, but still useful to have around.

--- hydra-cardano-api/src/Hydra/Cardano/Api/Tx.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api/Tx.hs
@@ -2,26 +2,40 @@ module Hydra.Cardano.Api.Tx where
 
 import Hydra.Cardano.Api.Prelude
 
+import Hydra.Cardano.Api.ExecutionUnits (fromLedgerExUnits, toLedgerExUnits)
 import Hydra.Cardano.Api.KeyWitness (
   fromLedgerTxWitness,
   toLedgerBootstrapWitness,
   toLedgerKeyWitness,
  )
 import Hydra.Cardano.Api.Lovelace (fromLedgerCoin)
+import Hydra.Cardano.Api.RdmrPtr (RdmrPtr (..))
+import Hydra.Cardano.Api.ScriptData (FromScriptData, fromLedgerData, toLedgerData)
+import Hydra.Cardano.Api.ScriptDataSupportedInEra (HasScriptData (..))
+import Hydra.Cardano.Api.TxIn (toLedgerTxIn)
+import Hydra.Cardano.Api.TxOut (fromLedgerTxOut, toLedgerTxOut)
 import Hydra.Cardano.Api.TxScriptValidity (toLedgerScriptValidity)
 
+import Cardano.Api.Byron (TxBody (..))
 import Cardano.Binary (serialize)
 import qualified Cardano.Ledger.Alonzo as Ledger
+import qualified Cardano.Ledger.Alonzo.Data as Ledger
 import qualified Cardano.Ledger.Alonzo.PParams as Ledger
 import qualified Cardano.Ledger.Alonzo.Scripts as Ledger
 import qualified Cardano.Ledger.Alonzo.Tx as Ledger
 import qualified Cardano.Ledger.Alonzo.TxBody as Ledger
 import qualified Cardano.Ledger.Alonzo.TxWitness as Ledger
+import qualified Cardano.Ledger.Core as Ledger.Core
 import qualified Cardano.Ledger.Era as Ledger
 import qualified Cardano.Ledger.Mary.Value as Ledger
+import qualified Cardano.Ledger.TxIn as Ledger
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Map as Map
 import Data.Maybe.Strict (maybeToStrictMaybe, strictMaybeToMaybe)
+import qualified Data.Sequence.Strict as StrictSeq
+import qualified Data.Set as Set
+import GHC.Records (HasField (..))
+import qualified Plutus.V1.Ledger.Api as Plutus
 
 -- * Extras
 
@@ -54,6 +68,25 @@ totalExecutionCost pparams tx =
       _ ->
         mempty
 
+-- | Apply some mapping function over a transaction's  outputs.
+adjustTxOuts ::
+  ([TxOut CtxTx Era] -> [TxOut CtxTx Era]) ->
+  Tx Era ->
+  Tx Era
+adjustTxOuts adjust (Tx body wits) =
+  case body of
+    ShelleyTxBody era ledgerBody scripts scriptsData auxData validity ->
+      Tx (ShelleyTxBody era (fn ledgerBody) scripts scriptsData auxData validity) wits
+ where
+  fn ledgerBody =
+    ledgerBody
+      { Ledger.outputs =
+          StrictSeq.fromList . mapOutputs . toList $ Ledger.outputs ledgerBody
+      }
+
+  mapOutputs =
+    fmap (toLedgerTxOut . toCtxUTxOTxOut) . adjust . fmap fromLedgerTxOut
+
 -- | Find and deserialise from 'ScriptData', a redeemer from the transaction
 -- associated to the given input.
 findRedeemerSpending ::
@@ -81,6 +114,118 @@ findRedeemerSpending (getTxBody -> ShelleyTxBody _ body _ scriptData _ _) txIn =
       mempty
     TxBodyScriptData _ _ (Ledger.Redeemers rs) ->
       rs
+
+-- | Remove any redeemer spending the given input from the redeemer set.
+--
+-- This function is partial and fails if given a Byron transaction body or, if
+-- the provided input isn't part of the transaction.
+--
+-- TODO: This function does NOT maintain the transaction's script integrity hash.
+removeRedeemerSpending ::
+  ( HasCallStack
+  , Ledger.Era (ShelleyLedgerEra era)
+  , HasField
+      "inputs"
+      (Ledger.Core.TxBody (ShelleyLedgerEra era))
+      (Set (Ledger.TxIn StandardCrypto))
+  ) =>
+  TxIn ->
+  Tx era ->
+  Tx era
+removeRedeemerSpending i (Tx body wits) =
+  case body of
+    ByronTxBody{} ->
+      error "removeRedeemerFor: called on a Byron transaction. What are you trying to do?"
+    ShelleyTxBody era ledgerBody scripts scriptsData auxData validity ->
+      case Set.lookupIndex (toLedgerTxIn i) (getField @"inputs" ledgerBody) of
+        Nothing ->
+          -- NOTE: We _could_ return the unchanged transaction here, but that would
+          -- silently make the function continue where the caller probably did not
+          -- mean to call this function with a wrong 'TxIn'. So it is arguably
+          -- better to loudly fail and indicate an invariant failure.
+          error "removeRedeemerFor: provided 'TxIn' is not part of the transaction."
+        Just ix ->
+          let ptr = RdmrPtr Ledger.Spend (fromIntegral ix)
+           in Tx (ShelleyTxBody era ledgerBody scripts (fn ptr scriptsData) auxData validity) wits
+ where
+  fn ptr = \case
+    TxBodyNoScriptData ->
+      TxBodyNoScriptData
+    TxBodyScriptData scriptDataInEra dats (Ledger.Redeemers redeemers) ->
+      TxBodyScriptData
+        scriptDataInEra
+        dats
+        (Ledger.Redeemers (Map.delete ptr redeemers))
+
+-- | Alter a transaction's  redeemers map given some mapping function.
+--
+-- This function is partial and fails if given a Byron transaction.
+--
+-- TODO: This function does NOT maintain the transaction's script integrity hash.
+adjustRedeemers ::
+  ( HasCallStack
+  , Ledger.Era (ShelleyLedgerEra era)
+  ) =>
+  (RdmrPtr -> (ScriptData, ExecutionUnits) -> (ScriptData, ExecutionUnits)) ->
+  Tx era ->
+  Tx era
+adjustRedeemers adjust (Tx body wits) =
+  case body of
+    ByronTxBody{} ->
+      error "adjustRedeemers: called on a Byron transaction. What are you trying to do?"
+    ShelleyTxBody era ledgerBody scripts scriptsData auxData validity ->
+      Tx (ShelleyTxBody era ledgerBody scripts (fn scriptsData) auxData validity) wits
+ where
+  fn = \case
+    TxBodyNoScriptData ->
+      TxBodyNoScriptData
+    TxBodyScriptData era dats (Ledger.Redeemers redeemers) ->
+      TxBodyScriptData
+        era
+        dats
+        ( Ledger.Redeemers $
+            Map.mapWithKey
+              (\k (fromLedger -> v) -> toLedger (adjust k v))
+              redeemers
+        )
+
+  fromLedger = bimap fromLedgerData fromLedgerExUnits
+  toLedger = bimap toLedgerData toLedgerExUnits
+
+-- | Adds given 'TxOutDatum' and corresponding hash to the transaction's scripts.
+--
+-- This function is partial and fails if given a Byron transaction.
+--
+-- TODO: This function does NOT maintain the transaction's script integrity hash.
+addDatum ::
+  ( HasCallStack
+  , HasScriptData era
+  , Ledger.Era (ShelleyLedgerEra era)
+  ) =>
+  ScriptData ->
+  Tx era ->
+  Tx era
+addDatum (toLedgerData -> datum) (Tx body wits) =
+  case body of
+    ByronTxBody{} ->
+      error "addDatum: called on a Byron transaction. What are you trying to do?"
+    ShelleyTxBody era ledgerBody scripts scriptsData auxData validity ->
+      Tx (ShelleyTxBody era ledgerBody scripts (fn scriptsData) auxData validity) wits
+ where
+  fn = \case
+    TxBodyNoScriptData ->
+      TxBodyScriptData
+        scriptDataSupportedInEra
+        (insertDatum mempty)
+        (Ledger.Redeemers mempty)
+    TxBodyScriptData scriptDataInEra (Ledger.TxDats dats) redeemers ->
+      TxBodyScriptData
+        scriptDataInEra
+        (insertDatum dats)
+        redeemers
+
+  insertDatum =
+    Ledger.TxDats . Map.insert (Ledger.hashData datum) datum
 
 -- | Obtain a human-readable pretty text representation of a transaction.
 renderTx :: Tx Era -> Text
