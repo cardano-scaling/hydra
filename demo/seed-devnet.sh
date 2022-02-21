5698296eb2797eefc798102c0bc3ba65f911c477
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-02-21 18:27:24 +0100

    Send initial funds only to faucet and use it in seed-devnet.sh

--- demo/seed-devnet.sh
+++ demo/seed-devnet.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Seed a "devnet" by marking some ADA as "payment outputs" for the Hydra Head
-# ("the fuel tank").
+# Seed a "devnet" by distributing some Ada to commit and also some marked as
+# "fuel" for the Hydra Head.
 set -e
 
-TESTNET_MAGIC=42
 MARKER_DATUM_HASH="a654fb60d21c1fed48db2c320aa6df9737ec0204c0ba53b9b94a09fb40e757f3"
-DEVNET_FUNDS=900000000000
-STANDARD_FEE=167393
 DEVNET_DIR=/data
 
 function ccli() {
@@ -15,28 +12,41 @@ function ccli() {
   docker-compose exec cardano-node cardano-cli ${@} --testnet-magic 42
 }
 
-function seedCommit() {
+# Retrieve some lovelace from faucet, marked as "fuel" if requested
+function seedFaucet() {
     ACTOR=${1}
-    COMMIT_AMOUNT=${2}
-    FUEL_AMOUNT=$((${DEVNET_FUNDS}-${COMMIT_AMOUNT}-${STANDARD_FEE}))
-    echo >&2 "Seeding a commit UTXO for ${ACTOR} with ${COMMIT_AMOUNT}Ł (${FUEL_AMOUNT}Ł of fuel)"
+    AMOUNT=${2}
+    MARKED=${3:-false}
+    echo >&2 "Seeding a UTXO from faucet to ${ACTOR} with ${AMOUNT}Ł"
 
-    ADDR=$(ccli address build --payment-verification-key-file ${DEVNET_DIR}/credentials/${ACTOR}.vk)
-    GENESIS_TXIN=$(ccli genesis initial-txin --verification-key-file ${DEVNET_DIR}/credentials/${ACTOR}-genesis.vk | tr -d '\n\r')
+    # Determine faucet address and just the **first** txin addressed to it
+    FAUCET_ADDR=$(ccli address build --payment-verification-key-file ${DEVNET_DIR}/credentials/faucet.vk)
+    FAUCET_TXIN=$(ccli query utxo --address ${FAUCET_ADDR} --out-file /dev/stdout | jq -r 'keys[0]')
+
+    ACTOR_ADDR=$(ccli address build --payment-verification-key-file ${DEVNET_DIR}/credentials/${ACTOR}.vk)
+
+    # Optionally mark output
+    MARKER=""
+    if ${MARKED}; then
+        MARKER="--tx-out-datum-hash ${MARKER_DATUM_HASH}"
+    fi
 
     ccli transaction build --alonzo-era --cardano-mode \
-        --change-address ${ADDR} \
-        --tx-in ${GENESIS_TXIN} \
-        --tx-out ${ADDR}+${FUEL_AMOUNT} \
-        --tx-out-datum-hash ${MARKER_DATUM_HASH} \
-        --out-file ${DEVNET_DIR}/${ACTOR}.draft
+        --change-address ${FAUCET_ADDR} \
+        --tx-in ${FAUCET_TXIN} \
+        --tx-out ${ACTOR_ADDR}+${AMOUNT} \
+        ${MARKER} \
+        --out-file ${DEVNET_DIR}/seed-${ACTOR}.draft
     ccli transaction sign \
-        --tx-body-file ${DEVNET_DIR}/${ACTOR}.draft \
-        --signing-key-file ${DEVNET_DIR}/credentials/${ACTOR}.sk \
-        --out-file ${DEVNET_DIR}/${ACTOR}.signed
-    ccli transaction submit --tx-file ${DEVNET_DIR}/${ACTOR}.signed
+        --tx-body-file ${DEVNET_DIR}/seed-${ACTOR}.draft \
+        --signing-key-file ${DEVNET_DIR}/credentials/faucet.sk \
+        --out-file ${DEVNET_DIR}/seed-${ACTOR}.signed
+    ccli transaction submit --tx-file ${DEVNET_DIR}/seed-${ACTOR}.signed
 }
 
-seedCommit "alice" 1000000000
-seedCommit "bob" 500000000
-seedCommit "carol" 250000000
+seedFaucet "alice" 1000000000 # 1000 Ada to commit
+seedFaucet "alice" 100000000 true # 100 Ada marked as "fuel"
+seedFaucet "bob" 500000000 # 500 Ada to commit
+seedFaucet "bob" 100000000 true # 100 Ada marked as "fuel"
+seedFaucet "carol" 250000000 # 250 Ada to commit
+seedFaucet "carol" 100000000 true # 100 Ada marked as "fuel"
