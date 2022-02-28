4a2c0a8462267eb745cacc6a03355c2bdf1b066d
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-02-28 12:44:50 +0100

    Add v_head to HeadTokens minting policy
    This way, we can look for the head datum and determine how many/whom to
    mint PTs for

--- hydra-plutus/src/Hydra/Contract/HeadTokens.hs
+++ hydra-plutus/src/Hydra/Contract/HeadTokens.hs
@@ -6,7 +6,8 @@ module Hydra.Contract.HeadTokens where
 
 import PlutusTx.Prelude
 
-import Ledger (ScriptContext (..), TxInfo (txInfoMint), ownCurrencySymbol)
+import qualified Hydra.Contract.Head as Head
+import Ledger (ScriptContext (..), TxInfo (txInfoMint), ValidatorHash, ownCurrencySymbol)
 import Ledger.Typed.Scripts (wrapMintingPolicy)
 import Plutus.V1.Ledger.Api (
   MintingPolicy,
@@ -23,8 +24,14 @@ hydraHeadV1 :: BuiltinByteString
 hydraHeadV1 = "HydraHeadV1"
 
 {-# INLINEABLE validate #-}
-validate :: TxOutRef -> () -> ScriptContext -> Bool
-validate _ _ context =
+validate ::
+  -- | Head validator
+  ValidatorHash ->
+  TxOutRef ->
+  () ->
+  ScriptContext ->
+  Bool
+validate _headValidator _ _ context =
   let currency = ownCurrencySymbol context
       minted = txInfoMint txInfo
    in valueOf minted currency (TokenName hydraHeadV1) == 1
@@ -49,7 +56,8 @@ validate _ _ context =
 mintingPolicy :: TxOutRef -> MintingPolicy
 mintingPolicy txOutRef =
   mkMintingPolicyScript $
-    $$(PlutusTx.compile [||wrapMintingPolicy . validate||])
+    $$(PlutusTx.compile [||\vHead ref -> wrapMintingPolicy (validate vHead ref)||])
+      `PlutusTx.applyCode` PlutusTx.liftCode Head.validatorHash
       `PlutusTx.applyCode` PlutusTx.liftCode txOutRef
 
 validatorScript :: TxOutRef -> Script
