6919d60aeebf110a4f99f8c610cdfbeb66fb24b0
KtorZ <matthias.benkort@gmail.com>
2022-03-01 09:31:51 +0100

    Retrieve number of parties from head's datum when minting.

--- hydra-plutus/src/Hydra/Contract/HeadTokens.hs
+++ hydra-plutus/src/Hydra/Contract/HeadTokens.hs
@@ -4,21 +4,16 @@
 -- | Minting policy for a single head tokens.
 module Hydra.Contract.HeadTokens where
 
+import Ledger hiding (Mint)
 import PlutusTx.Prelude
 
+import Hydra.Contract.Head (Head)
 import qualified Hydra.Contract.Head as Head
+import qualified Hydra.Contract.HeadState as Head
 import Hydra.Contract.MintAction (MintAction (Burn, Mint))
-import Ledger (ScriptContext (..), TxInfo (txInfoMint), ValidatorHash, ownCurrencySymbol)
-import Ledger.Typed.Scripts (wrapMintingPolicy)
-import Plutus.V1.Ledger.Api (
-  MintingPolicy,
-  Script,
-  TokenName (TokenName),
-  TxOutRef,
-  getMintingPolicy,
-  mkMintingPolicyScript,
- )
-import Plutus.V1.Ledger.Value (valueOf)
+import Ledger.Typed.Scripts (ValidatorTypes (..), wrapMintingPolicy)
+import Plutus.V1.Ledger.Api (fromBuiltinData)
+import Plutus.V1.Ledger.Value (TokenName (..), valueOf)
 import qualified PlutusTx
 
 hydraHeadV1 :: BuiltinByteString
@@ -32,30 +27,29 @@ validate ::
   MintAction ->
   ScriptContext ->
   Bool
-validate _headValidator _ action context =
+validate headValidator _ action context =
   let currency = ownCurrencySymbol context
       minted = txInfoMint txInfo
       quantityST = valueOf minted currency (TokenName hydraHeadV1)
    in case action of
-        Mint -> traceIfFalse "minted not 1" $ quantityST == 1
-        Burn -> traceIfFalse "burnt not 1" $ quantityST == -1
+        Mint ->
+          traceIfFalse "minted not 1" $ quantityST == 1
+        Burn ->
+          traceIfFalse "burnt not 1" $ quantityST == -1
  where
   ScriptContext{scriptContextTxInfo = txInfo} = context
 
--- TODO: Do determine parameters/party from datum as soon as v_head is not
--- parameterized anymore (with something like this below)
-
--- parameters =
---   case scriptOutputsAt headValidator txInfo of
---     [(dh, _)] ->
---       case getDatum <$> findDatum dh txInfo of
---         Nothing -> traceError "expected optional commit datum"
---         Just da ->
---           case fromBuiltinData @(DatumType Head.State) da of
---             Nothing -> traceError "expected commit datum type, got something else"
---             Just Head.Initial{} -> True
---             Just _ -> traceError "unexpected State in datum"
---     _ -> traceError "expected single head output"
+  nParties =
+    case scriptOutputsAt headValidator txInfo of
+      [(dh, _)] ->
+        case getDatum <$> findDatum dh txInfo of
+          Nothing -> traceError "expected optional commit datum"
+          Just da ->
+            case fromBuiltinData @(DatumType Head) da of
+              Nothing -> traceError "expected commit datum type, got something else"
+              Just Head.Initial{Head.parties = parties} -> length parties
+              Just _ -> traceError "unexpected State in datum"
+      _ -> traceError "expected single head output"
 
 mintingPolicy :: TxOutRef -> MintingPolicy
 mintingPolicy txOutRef =
