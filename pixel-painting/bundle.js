fd7c358e677bf7d15d2f2a7e349688db9a119a44
KtorZ <matthias.benkort@gmail.com>
2022-03-15 16:36:03 +0100

    Decode transaction's auxiliary data and paint call 'drawPixel'

--- pixel-painting/bundle.js
+++ pixel-painting/bundle.js
@@ -1,16 +1,19 @@
 const client = new WebSocket("ws://13.38.35.167:4001");
 
-client.addEventListener("open", () => {
-  console.log("SOCKET OPEN");
-});
+const metadataLabel = 14;
 
 client.addEventListener("message", e => {
   const msg = JSON.parse(e.data);
   switch (msg.tag) {
     case "TxSeen":
+      console.log("New transaction seen", msg.transaction.id);
       if (msg.transaction.auxiliaryData != null) {
-
+        console.log("Transaction has auxiliary data", msg.transaction.auxiliaryData);
+        const [x, y, r, g, b] = cbor.decode(msg.transaction.auxiliaryData).get(metadataLabel);
+        drawPixel(x, y, [r, g, b]);
       }
+    default:
+      console.log("Irrelevant message", msg);
   }
 });
 
