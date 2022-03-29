0e404b95f0cb1da8556e72a55e8f8d3ac721b012
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-03-29 10:52:25 +0000

    Run tx-cost benchmark as part of CI process #246

--- .github/workflows/ci-test.sh
+++ .github/workflows/ci-test.sh
@@ -11,3 +11,8 @@ cabal test hydra-tui
 # Sanity check benchmark still runs fine
 # TODO: turn into a test
 cabal bench hydra-cluster --benchmark-options '--scaling-factor 1'
+
+# benchmarking transactions costs
+# TODO: output benchmark to some place where it will be picked up by
+# website publication process
+cabal bench tx-cost
