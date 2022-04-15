b3a5ea92832a1369f89c21a182ea65279b4cc10c
KtorZ <matthias.benkort@gmail.com>
2022-04-15 16:02:56 +0200

    Define 'Eq' instance for on-chain head state(s) and state-machine.

--- hydra-node/src/Hydra/Chain/Direct/State.hs
+++ hydra-node/src/Hydra/Chain/Direct/State.hs
@@ -71,7 +71,7 @@ data OnChainHeadState (st :: HeadStateKind) = OnChainHeadState
   , ownParty :: Party
   , stateMachine :: HydraStateMachine st
   }
-  deriving (Show)
+  deriving (Eq, Show)
 
 -- NOTE (1): The state machine UTxO produced by the Init transaction (a.k.a
 -- 'threadOutput') is always present and 'threaded' across all transactions.
@@ -106,6 +106,7 @@ data HydraStateMachine (st :: HeadStateKind) where
     HydraStateMachine 'StClosed
 
 deriving instance Show (HydraStateMachine st)
+deriving instance Eq (HydraStateMachine st)
 
 getKnownUTxO ::
   OnChainHeadState st ->
@@ -137,10 +138,19 @@ data SomeOnChainHeadState where
 instance Show SomeOnChainHeadState where
   show (SomeOnChainHeadState st) = show st
 
---  Just (closeHappenedAt stClosed)
-
--- rewind :: SomeOnChainHeadState -> SomeOnChainHeadState
--- rewind = error "rewind"
+instance Eq SomeOnChainHeadState where
+  (SomeOnChainHeadState st) == (SomeOnChainHeadState st') =
+    case (reifyState st, reifyState st') of
+      (TkIdle, TkIdle) ->
+        st == st'
+      (TkInitialized, TkInitialized) ->
+        st == st'
+      (TkOpen, TkOpen) ->
+        st == st'
+      (TkClosed, TkClosed) ->
+        st == st'
+      _ ->
+        False
 
 -- | Some Kind for witnessing Hydra state-machine's states at the type-level.
 --
