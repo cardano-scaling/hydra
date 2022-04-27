44c000963b506d63ef0fb43dd0f26207f1e4fd23
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-04-27 11:26:42 +0200

    Resolve/comment on TODOs in Hydra.Data.Party

--- hydra-plutus/src/Hydra/Data/Party.hs
+++ hydra-plutus/src/Hydra/Data/Party.hs
@@ -1,9 +1,3 @@
-{-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeApplications #-}
-
--- TODO(SN): rename to /= Data but something which hints at "OnChain"
 module Hydra.Data.Party where
 
 import Hydra.Prelude hiding (init)
@@ -14,15 +8,15 @@ import PlutusTx.Builtins (BuiltinByteString, fromBuiltin, toBuiltin)
 import PlutusTx.IsData
 import Test.QuickCheck (vector)
 
--- TODO(SN): Copied party + json instances for deserializing in 'init' endpoint
--- and we were struggling to define 'Lift' and 'IsData'. Ideally we would be
--- able to define all necessary instances on 'Hydra.Party' directly
-
+-- | On-chain representation of a Hydra party.
+--
+-- NOTE: This roughly corresponds to the 'Hydra.Party.Party', but is greatly
+-- simplified to allow usage of this type in plutus-tx. If we would use the
+-- complex type directly, which is based on `Cardano.Crypto.DSIGN`, we would get
+-- errors like "Error: Unsupported feature: Kind: GHC.Types.Nat".
 newtype Party = UnsafeParty BuiltinByteString
   deriving stock (Eq, Generic)
   deriving newtype (Show)
-
-PlutusTx.makeLift ''Party
 
 instance Arbitrary Party where
   arbitrary = UnsafeParty . toBuiltin . BS.pack <$> vector 32
