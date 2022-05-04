723e4f9771a62e00732bc0fc35f849f68730548b
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-05-04 18:24:09 +0200

    Not use vanished Signature type from plutus-ledger-api
    Seems like it should have never existed in plutus?

--- hydra-plutus/src/Hydra/Contract/Head.hs
+++ hydra-plutus/src/Hydra/Contract/Head.hs
@@ -10,7 +10,8 @@ import PlutusTx.Prelude
 import Hydra.Contract.Commit (Commit (..))
 import qualified Hydra.Contract.Commit as Commit
 import Hydra.Contract.Encoding (serialiseTxOuts)
-import Hydra.Contract.HeadState (Input (..), SnapshotNumber, State (..))
+import Hydra.Contract.HeadState (Input (..), Signature, SnapshotNumber, State (..))
+import qualified Hydra.Contract.Initial as Initial
 import Hydra.Data.ContestationPeriod (ContestationPeriod, addContestationPeriod)
 import Hydra.Data.Party (Party (vkey))
 import Plutus.Codec.CBOR.Encoding (
@@ -49,7 +50,6 @@ import Plutus.V1.Ledger.Api (
   mkValidatorScript,
  )
 import Plutus.V1.Ledger.Contexts (findDatum, findDatumHash, findOwnInput, getContinuingOutputs)
-import Plutus.V1.Ledger.Crypto (Signature (getSignature))
 import Plutus.V1.Ledger.Interval (Extended (Finite))
 import Plutus.V1.Ledger.Value (valueOf)
 import PlutusTx (CompiledCode)
@@ -493,7 +493,7 @@ verifySnapshotSignature parties snapshotNumber utxoHash sigs =
 verifyPartySignature :: SnapshotNumber -> BuiltinByteString -> Party -> Signature -> Bool
 verifyPartySignature snapshotNumber utxoHash party signed =
   traceIfFalse "party signature verification failed" $
-    verifySignature (vkey party) message (getSignature signed)
+    verifySignature (vkey party) message signed
  where
   message =
     encodingToBuiltinByteString $
