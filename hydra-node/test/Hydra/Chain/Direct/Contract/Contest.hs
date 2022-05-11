b5ae15111f8124d3fa2d4b68e3d4dedcb688a38a
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-05-11 07:55:51 +0000

    [wip] Introduce mutations for Contest

--- hydra-node/test/Hydra/Chain/Direct/Contract/Contest.hs
+++ hydra-node/test/Hydra/Chain/Direct/Contract/Contest.hs
@@ -7,6 +7,7 @@ import Hydra.Cardano.Api
 import Hydra.Prelude hiding (label)
 
 import Cardano.Api.UTxO as UTxO
+import Hydra.Chain.Direct.Contract.Mutation (SomeMutation)
 import Hydra.Chain.Direct.Fixture (genForParty, testNetworkId, testPolicyId)
 import Hydra.Chain.Direct.Tx (assetNameFromVerificationKey, contestTx, mkHeadOutput)
 import qualified Hydra.Contract.HeadState as Head
@@ -104,74 +105,35 @@ healthySignature number = aggregate [sign sk snapshot | sk <- healthySigningKeys
  where
   snapshot = healthySnapshot{number}
 
--- data CloseMutation
---   = MutateSignatureButNotSnapshotNumber
---   | MutateSnapshotNumberButNotSignature
---   | MutateSnapshotToIllFormedValue
---   | MutateParties
---   | MutateRequiredSigner
---   | MutateCloseUTxOHash
---   deriving (Generic, Show, Enum, Bounded)
+data ContestMutation
+  = MutateSignatureButNotSnapshotNumber
+  deriving (Generic, Show, Enum, Bounded)
 
--- genCloseMutation :: (Tx, UTxO) -> Gen SomeMutation
--- genCloseMutation (tx, _utxo) =
---   -- FIXME: using 'closeRedeemer' here is actually too high-level and reduces
---   -- the power of the mutators, we should test at the level of the validator.
---   -- That is, using the on-chain types. 'closeRedeemer' is also not used
---   -- anywhere after changing this and can be moved into the closeTx
---   oneof
---     [ SomeMutation MutateSignatureButNotSnapshotNumber . ChangeHeadRedeemer <$> do
---         closeRedeemer (number healthySnapshot) <$> (arbitrary :: Gen (MultiSignature (Snapshot Tx)))
---     , SomeMutation MutateSnapshotNumberButNotSignature . ChangeHeadRedeemer <$> do
---         mutatedSnapshotNumber <- arbitrarySizedNatural `suchThat` (\n -> n /= healthySnapshotNumber && n > 0)
---         pure (closeRedeemer mutatedSnapshotNumber $ healthySignature healthySnapshotNumber)
---     , SomeMutation MutateSnapshotToIllFormedValue . ChangeHeadRedeemer <$> do
---         mutatedSnapshotNumber <- arbitrary `suchThat` (< 0)
---         let mutatedSignature =
---               aggregate [sign sk $ serialize' mutatedSnapshotNumber | sk <- healthySigningKeys]
---         pure
---           Head.Close
---             { snapshotNumber = mutatedSnapshotNumber
---             , signature = toPlutusSignatures mutatedSignature
---             , utxoHash = ""
---             }
---     , SomeMutation MutateParties . ChangeHeadDatum <$> do
---         mutatedParties <- arbitrary `suchThat` (/= healthyOnChainParties)
---         pure $
---           Head.Open
---             { parties = mutatedParties
---             , utxoHash = ""
---             }
---     , SomeMutation MutateParties . ChangeHeadDatum <$> arbitrary `suchThat` \case
---         Head.Open{Head.parties = parties} ->
---           parties /= Head.parties healthyCloseDatum
---         _ ->
---           True
---     , SomeMutation MutateParties . ChangeHeadDatum <$> arbitrary `suchThat` \case
---         Head.Open{Head.parties = parties} ->
---           parties /= Head.parties healthyCloseDatum
---         _ ->
---           True
---     , SomeMutation MutateRequiredSigner <$> do
---         newSigner <- verificationKeyHash <$> genVerificationKey
---         pure $ ChangeRequiredSigners [newSigner]
---     , SomeMutation MutateCloseUTxOHash . ChangeOutput 0 <$> mutateCloseUTxOHash
---     ]
---  where
---   headTxOut = fromJust $ txOuts' tx !!? 0
+genContestMutation :: (Tx, UTxO) -> Gen SomeMutation
+genContestMutation (tx, _utxo) =
+  -- FIXME: using 'closeRedeemer' here is actually too high-level and reduces
+  -- the power of the mutators, we should test at the level of the validator.
+  -- That is, using the on-chain types. 'closeRedeemer' is also not used
+  -- anywhere after changing this and can be moved into the closeTx
+  oneof
+    [ SomeMutation MutateSignatureButNotSnapshotNumber . ChangeHeadRedeemer <$> do
+        closeRedeemer (number healthySnapshot) <$> (arbitrary :: Gen (MultiSignature (Snapshot Tx)))
+    ]
+ where
+  headTxOut = fromJust $ txOuts' tx !!? 0
 
---   closeRedeemer snapshotNumber sig =
---     Head.Close
---       { snapshotNumber = toInteger snapshotNumber
---       , signature = toPlutusSignatures sig
---       , utxoHash = ""
---       }
+  closeRedeemer snapshotNumber sig =
+    Head.Close
+      { snapshotNumber = toInteger snapshotNumber
+      , signature = toPlutusSignatures sig
+      , utxoHash = ""
+      }
 
---   mutateCloseUTxOHash :: Gen (TxOut CtxTx)
---   mutateCloseUTxOHash = do
---     mutatedUTxOHash <- genHash
---     pure $ changeHeadOutputDatum (mutateState mutatedUTxOHash) headTxOut
+  mutateCloseUTxOHash :: Gen (TxOut CtxTx)
+  mutateCloseUTxOHash = do
+    mutatedUTxOHash <- genHash
+    pure $ changeHeadOutputDatum (mutateState mutatedUTxOHash) headTxOut
 
---   mutateState mutatedUTxOHash = \case
---     Head.Closed{snapshotNumber} -> Head.Closed{snapshotNumber, utxoHash = toBuiltin mutatedUTxOHash}
---     st -> error $ "unexpected state " <> show st
+  mutateState mutatedUTxOHash = \case
+    Head.Closed{snapshotNumber} -> Head.Closed{snapshotNumber, utxoHash = toBuiltin mutatedUTxOHash}
+    st -> error $ "unexpected state " <> show st
