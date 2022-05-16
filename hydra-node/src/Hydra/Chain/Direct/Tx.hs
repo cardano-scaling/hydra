feb9b469dfef11f4a0b073f685b555796a5ba5a0
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-05-16 15:20:05 +0200

    Align off-chain serialization of TxOut in commit
    It's important that we use Plutus.TxOut and its ToCBOR instance

--- hydra-node/src/Hydra/Chain/Direct/Tx.hs
+++ hydra-node/src/Hydra/Chain/Direct/Tx.hs
@@ -42,6 +42,7 @@ import Hydra.Party (Party, partyFromChain, partyToChain)
 import Hydra.Snapshot (Snapshot (..), SnapshotNumber)
 import Plutus.V1.Ledger.Api (POSIXTime, fromBuiltin, fromData, toBuiltin)
 import qualified Plutus.V1.Ledger.Api as Plutus
+import Plutus.V2.Ledger.Api (toData)
 
 -- | Needed on-chain data to create Head transactions.
 type UTxOWithScript = (TxIn, TxOut CtxUTxO, ScriptData)
@@ -189,7 +190,8 @@ mkCommitDatum party headValidatorHash utxo =
     Nothing ->
       Nothing
     Just (_i, o) ->
-      Just $ Commit.SerializedTxOut (toBuiltin $ serialize' $ toLedgerTxOut o)
+      -- REVIEW: Depends on the ToCBOR instance of 'Data' in plutus, is this fine?
+      Commit.SerializedTxOut . toBuiltin . serialize' . toData <$> toPlutusTxOut o
 
 -- | Create a transaction collecting all "committed" utxo and opening a Head,
 -- i.e. driving the Head script state.
