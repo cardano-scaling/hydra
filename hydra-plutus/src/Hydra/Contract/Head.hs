643f1c276331118dbf6f301f8c99c4130c46f5fe
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-05-17 11:34:52 +0200

    [wip] Try to fix snapshot signature alignment

--- hydra-plutus/src/Hydra/Contract/Head.hs
+++ hydra-plutus/src/Hydra/Contract/Head.hs
@@ -47,6 +47,8 @@ import qualified PlutusTx.AssocMap as Map
 import qualified PlutusTx.Builtins as Builtins
 
 -- REVIEW: Functions not re-exported "as V2", but using the same data types.
+
+import qualified Cardano.Crypto.Util as Snapshot
 import Plutus.V1.Ledger.Address (scriptHashAddress)
 import Plutus.V1.Ledger.Value (assetClass, assetClassValue, valueOf)
 
@@ -483,6 +485,7 @@ verifyPartySignature snapshotNumber utxoHash party signed =
     verifySignature (vkey party) message signed
  where
   message =
+    -- TODO: document CDDL format, either here or in 'Hydra.Snapshot.getSignableRepresentation'
     Builtins.serialiseData (toBuiltinData snapshotNumber)
       <> Builtins.serialiseData (toBuiltinData utxoHash)
 {-# INLINEABLE verifyPartySignature #-}
