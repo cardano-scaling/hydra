a840f06bfe72936c0ecbd2f51d705cae2cfdc16e
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-05-24 10:26:42 +0200

    Use a babbage protocol-parameters with 0 fees

--- vasil-testnet/hydra-node.sh
+++ vasil-testnet/hydra-node.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-# Query network's protocol parameters and re-use them for hydra-node
-export CARDANO_NODE_SOCKET_PATH=ipc/node.socket
-cardano-cli query protocol-parameters --testnet-magic 9 > protocol-parameters.json
-
 exec cabal exec hydra-node -- \
   --node-id 314 \
   --node-socket "ipc/node.socket" \
   --network-id 9 \
   --ledger-genesis "config/shelley-genesis.json" \
-  --ledger-protocol-parameters "protocol-parameters.json" \
+  --ledger-protocol-parameters "config/protocol-parameters.json" \
   --hydra-signing-key "credentials/sebastian.hydra.sk" \
   --cardano-signing-key "credentials/sebastian.cardano.sk" \
   --start-chain-from "440772.7870838f05071a30b364832490cfbf9f882382a7d5524e821debaad099c4ed99"
