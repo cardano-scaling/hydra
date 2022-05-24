76be7882fdae33ad0f2caeaa26bf31b491dd0cb5
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-05-24 07:58:14 +0000

    Simplify property for comparing time
    expecting 1/3rd of each Ordering ctor from randomly generated values does not make sense
    as EQ is much likely to appear than the other ones.

--- hydra-plutus/test/Hydra/Data/ContestationPeriodSpec.hs
+++ hydra-plutus/test/Hydra/Data/ContestationPeriodSpec.hs
@@ -10,8 +10,7 @@ import Hydra.Data.ContestationPeriod (
 import Plutus.Orphans ()
 import Test.Hspec (Spec, describe)
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (tabulate, (===))
-import Test.QuickCheck.Property (coverTable)
+import Test.QuickCheck (collect, (===))
 
 spec :: Spec
 spec = do
@@ -24,5 +23,4 @@ spec = do
     prop "is homomorphic w.r.t to Ord" $ \t1 t2 ->
       let ordering = compare t1 t2
        in ordering === compare (posixToUTCTime t1) (posixToUTCTime t2)
-            & tabulate "Ord" (map show $ enumFrom LT)
-            & coverTable "Cover" [("LT", 33), ("EQ", 33), ("GT", 33)]
+            & collect ordering
