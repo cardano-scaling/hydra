cbeeeaa3e1799d0fc7f1fd5505d20814c262df91
Arnaud Bailly <79840582+abailly-iohk@users.noreply.github.com>
2022-06-01 08:46:58 +0200

    Update hydra-node/src/Hydra/Chain/Direct/Handlers.hs
    Co-authored-by: Sebastian Nagel <ch1bo@users.noreply.github.com>

--- hydra-node/src/Hydra/Chain/Direct/Handlers.hs
+++ hydra-node/src/Hydra/Chain/Direct/Handlers.hs
@@ -6,7 +6,7 @@
 -- | Provide infrastructure-independent "handlers" for posting transactions and following the chain.
 --
 -- This module encapsulates the transformation logic between cardano transactions and `HydraNode` abstractions
--- `ChainTx` and `OnChainTx`, and maintainance of on-chain relevant state.
+-- `PostChainTx` and `OnChainTx`, and maintainance of on-chain relevant state.
 module Hydra.Chain.Direct.Handlers where
 
 import Hydra.Prelude
