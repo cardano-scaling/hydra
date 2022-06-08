e3d12fdf0ee92db02c6b08e9de7d0c2af196e7e5
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-06-08 11:54:13 +0000

    Simplify testing machinery for StateModel

--- hydra-node/test/Hydra/ModelSpec.hs
+++ hydra-node/test/Hydra/ModelSpec.hs
@@ -24,61 +24,19 @@ import Hydra.ServerOutput (ServerOutput (..))
 import Test.QuickCheck (Property, counterexample, property)
 import Test.QuickCheck.Gen.Unsafe (Capture (Capture), capture)
 import Test.QuickCheck.Monadic (PropertyM, assert, monadic', monitor, run)
-import Test.QuickCheck.StateModel (Actions, Env, initialState, runActionsInState, pattern Actions)
-
-newtype WrapIOSim a = WrapIOSim {unwrapIOSim :: forall s. IOSim s a}
-
-instance Functor WrapIOSim where
-  fmap f (WrapIOSim m) = WrapIOSim (fmap f m)
-
-instance Applicative WrapIOSim where
-  WrapIOSim f <*> WrapIOSim a = WrapIOSim (f <*> a)
-  pure a = WrapIOSim (pure a)
-
-instance Monad WrapIOSim where
-  WrapIOSim m >>= k = WrapIOSim (m >>= unwrapIOSim . k)
+import Test.QuickCheck.StateModel (Actions, runActions, pattern Actions)
 
 spec :: Spec
 spec =
   modifyMaxSuccess (const 10000) $
     prop "implementation respects model" prop_checkModel
 
-runIOSimProp :: (forall s. Gen (StateT (Nodes (IOSim s)) (IOSim s) Property)) -> Gen Property
-runIOSimProp p = do
-  Capture eval <- capture
-  case runSim $ evalStateT (eval p) mempty of
-    Left f -> pure $ counterexample (show f) $ property False
-    Right p' -> pure p'
-
-newtype AnyActions = AnyActions {unAnyActions :: forall s. Actions (WorldState (IOSim s))}
-
-instance Show AnyActions where
-  show (AnyActions acts) = Prelude.show (acts @())
-
-instance Arbitrary AnyActions where
-  arbitrary = do
-    Capture eval <- capture
-    return (AnyActions (eval arbitrary))
-
-  shrink (AnyActions actions) = case actions of
-    Actions [] -> []
-    acts -> [AnyActions (unsafeCoerceActions act) | act <- shrink acts]
-
-unsafeCoerceActions :: Actions (WorldState (IOSim s)) -> Actions (WorldState (IOSim s'))
-unsafeCoerceActions = unsafeCoerce
-
--- NOTE: This is only sound to run in IOSim, because delays are instant. It
--- allows to make sure we wait long-enough for remaining asynchronous actions /
--- events to complete before we make any test assertion.
-waitUntilTheEndOfTime :: MonadDelay m => m ()
-waitUntilTheEndOfTime = threadDelay 1000000000000
-
 prop_checkModel :: AnyActions -> Property
 prop_checkModel (AnyActions actions) =
   property $
     runIOSimProp $
       monadic' $ do
-        (WorldState world, _symEnv) <- runActions' actions
+        (WorldState world, _symEnv) <- runActions actions
         run $ lift waitUntilTheEndOfTime
         let parties = Map.keysSet world
         nodes <- run get
@@ -142,7 +100,36 @@ assertOpenHeadWithAllExpectedCommits world nodes p = do
     _ -> do
       pure ()
 
-runActions' ::
-  Actions (WorldState (IOSim s)) ->
-  PropertyM (StateT (Nodes (IOSim s)) (IOSim s)) (WorldState (IOSim s), Env)
-runActions' = runActionsInState initialState
+-- NOTE: This is only sound to run in IOSim, because delays are instant. It
+-- allows to make sure we wait long-enough for remaining asynchronous actions /
+-- events to complete before we make any test assertion.
+waitUntilTheEndOfTime :: MonadDelay m => m ()
+waitUntilTheEndOfTime = threadDelay 1000000000000
+
+--
+
+-- * Utilities for `IOSim`
+
+--
+
+-- | Specialised runner similar to <runSTGen https://hackage.haskell.org/package/QuickCheck-2.14.2/docs/src/Test.QuickCheck.Monadic.html#runSTGen>.
+runIOSimProp :: (forall s. Gen (StateT (Nodes (IOSim s)) (IOSim s) Property)) -> Gen Property
+runIOSimProp p = do
+  Capture eval <- capture
+  case runSim $ evalStateT (eval p) mempty of
+    Left f -> pure $ counterexample (show f) $ property False
+    Right p' -> pure p'
+
+newtype AnyActions = AnyActions {unAnyActions :: forall s. Actions (WorldState (IOSim s))}
+
+instance Show AnyActions where
+  show (AnyActions acts) = Prelude.show (acts @())
+
+instance Arbitrary AnyActions where
+  arbitrary = do
+    Capture eval <- capture
+    return (AnyActions (eval arbitrary))
+
+  shrink (AnyActions actions) = case actions of
+    Actions [] -> []
+    acts -> [AnyActions (unsafeCoerce act) | act <- shrink acts]
