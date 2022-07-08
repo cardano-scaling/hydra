8087cafef461db19340dfb456a44c379e71ce214
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-07-08 11:05:32 +0200

    Fix the release.nix for docker build and hydra (CI)

--- release.nix
+++ release.nix
@@ -2,13 +2,13 @@
 
 { ... }:
 let
-  hsPkgs = import ./default.nix { };
+  project = import ./default.nix { };
 in
 {
   # Build shell derivation to cache it
   shell = import ./shell.nix { };
 
   # Build executables only (for now)
-  hydra-node = hsPkgs.hydra-node.components.exes.hydra-node;
-  hydra-tui = hsPkgs.hydra-tui.components.exes.hydra-tui;
+  hydra-node = project.hsPkgs.hydra-node.components.exes.hydra-node;
+  hydra-tui = project.hsPkgs.hydra-tui.components.exes.hydra-tui;
 }
