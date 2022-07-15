5a5f92d3f293d47bd92e3d35069ebd172795a38b
KtorZ <matthias.benkort@gmail.com>
2022-07-15 10:15:16 +0200

    Write skeleton implementation of queryScriptRegistry: will always throw.

--- hydra-node/src/Hydra/Chain/Direct/ScriptRegistry.hs
+++ hydra-node/src/Hydra/Chain/Direct/ScriptRegistry.hs
@@ -9,9 +9,11 @@ import Hydra.Cardano.Api (
   CtxUTxO,
   NetworkId,
   TxId,
-  TxIn,
+  TxIn (..),
+  TxIx (..),
   TxOut,
  )
+import Hydra.Chain.CardanoClient (QueryPoint (..), queryUTxOByTxIn)
 
 -- | Hydra scripts published as reference scripts at these UTxO.
 newtype ScriptRegistry = ScriptRegistry
@@ -22,6 +24,12 @@ newtype ScriptRegistry = ScriptRegistry
 registryUtxo :: ScriptRegistry -> UTxO
 registryUtxo = undefined
 
+-- TODO: Give more context to this exception.
+data UnableToConstructRegistry = UnableToConstructRegistry
+  deriving (Show)
+
+instance Exception UnableToConstructRegistry
+
 -- | Query for 'TxIn's in the search for outputs containing all the reference
 -- scripts of the 'ScriptRegistry'.
 --
@@ -31,4 +39,14 @@ registryUtxo = undefined
 -- NOTE: This is limited to an upper bound of 10 to not query too much before
 -- providing an error.
 queryScriptRegistry :: NetworkId -> FilePath -> TxId -> IO ScriptRegistry
-queryScriptRegistry = undefined
+queryScriptRegistry networkId nodeSocket txId =
+  loop [TxIx 0 .. maxIteration]
+ where
+  maxIteration = TxIx 10 -- Arbitrary but, high-enough.
+  loop = \case
+    [] ->
+      throwIO UnableToConstructRegistry
+    ixs -> do
+      _utxo <- queryUTxOByTxIn networkId nodeSocket QueryTip (TxIn txId <$> ixs)
+      print _utxo
+      loop []
