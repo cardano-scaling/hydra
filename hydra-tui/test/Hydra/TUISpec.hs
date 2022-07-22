448dc446a244e0dc736ff7e08e1be8098fee92a8
Franco Testagrossa <franco.testagrossa@iohk.io>
2022-07-22 13:16:12 +0200

    Add test to render negative times

--- hydra-tui/test/Hydra/TUISpec.hs
+++ hydra-tui/test/Hydra/TUISpec.hs
@@ -104,6 +104,7 @@ spec = do
         days = hours * 24  
         time = 10 * days + 1 * hours + 1 * minutes + 15 * seconds
       renderTime (time :: NominalDiffTime) `shouldBe` "10d 1h 1m 15s"
+      renderTime (-time :: NominalDiffTime) `shouldBe` "-10d -1h -1m -15s"
 
 -- XXX: The same hack as in EndToEndSpec
 gracePeriod :: NominalDiffTime
