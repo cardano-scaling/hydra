1fd1d0f17fd586bea40f3881bf5061df2b7cec5c
Hristo Kochev <h.l.kochev@gmail.com>
2022-07-27 20:39:50 +0300

    small run-docker script fix

--- demo/run-docker.sh
+++ demo/run-docker.sh
@@ -9,11 +9,11 @@ BASEDIR=$(realpath $(dirname $(realpath $0))/..)
 COMPOSE_YAML="$BASEDIR/demo/docker-compose.yaml"
 
 # Sanity check to prevent accidentally tripping oneself with an existing demo
-if ( docker-compose -f $COMPOSE_YAML ps | grep hydra-node > /dev/null 2>&1 ); then
+if ( docker compose -f $COMPOSE_YAML ps | grep hydra-node | grep Up > /dev/null 2>&1 ); then
   echo "Demo already in progress, exiting"
   exit 1
 fi
 
 "$BASEDIR/demo/prepare-devnet.sh"
 
-exec docker-compose -f "$COMPOSE_YAML" up "$@"
+exec docker compose -f "$COMPOSE_YAML" up "$@"
