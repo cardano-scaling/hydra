d738df05661965259b864e65283ccb31b2522ad0
KtorZ <matthias.benkort@gmail.com>
2022-07-28 10:15:22 +0200

    Continue faucet spec
      Goal is to spin up many concurrent 'client' redeeming from the faucet,
      hoping to create a race condition.

--- hydra-cluster/test/Test/Hydra/Cluster/FaucetSpec.hs
+++ hydra-cluster/test/Test/Hydra/Cluster/FaucetSpec.hs
@@ -1,9 +1,22 @@
 module Test.Hydra.Cluster.FaucetSpec where
 
+import CardanoNode (withCardanoNodeDevnet)
+import Control.Concurrent.Async (replicateConcurrently_)
+import Hydra.Cluster.Faucet (Marked (Normal), seedFromFaucet_)
+import Hydra.Ledger.Cardano (genVerificationKey)
+import Hydra.Logging (showLogsOnFailure)
 import Hydra.Prelude
+import HydraNode (EndToEndLog (FromCardanoNode))
 import Test.Hydra.Prelude
+import Test.QuickCheck (generate)
 
 spec :: Spec
-spec = do
-    it "should be true" $ do
-      True `shouldBe` True
+spec = around showLogsOnFailure $ do
+  describe "seed from faucet" $ do
+    it "should work concurrently" $ \tracer -> do
+      failAfter 30 $
+        withTempDir "end-to-end-cardano-node" $ \tmpDir -> do
+          withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \node -> do
+            replicateConcurrently_ 10 $ do
+              vk <- generate genVerificationKey
+              seedFromFaucet_ node vk 1_000_000 Normal
