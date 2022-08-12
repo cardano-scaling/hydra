2da817203df055d16c2662bdd48d765be694f236
Franco Testagrossa <franco.testagrossa@iohk.io>
2022-08-12 17:37:54 +0200

    Improve rendering of relative time since last change using git api.

--- docs/src/components/DocumentMetadata/index.tsx
+++ docs/src/components/DocumentMetadata/index.tsx
@@ -3,27 +3,17 @@ import styles from './styles.module.css'
 
 interface Props {
   lastUpdatedAt: string
+  relativeTimeSince: string
   commitHash: string
   lastTranslatedAt?: string
 }
 
-const isNumber = (str: string): boolean =>
-  !isNaN(parseInt(str))
-
-const isValidDate = (date: string): boolean =>
-  isNumber(date) && isNaN(Date.parse(date))
-
 const isPlaceholder = (date: string): boolean =>
   date.startsWith("{{") && date.endsWith("}}")
 
-const dateToFormatString = (date: string): string =>
-  new Date(parseInt(date)).toUTCString().replace("GMT", "")
-
-const daysSince = (dateSeconds: number): number =>
-  Math.floor((new Date().getTime() - dateSeconds * 1000) / 8.64e7)
-
 export default function DocumentMetadata({
   lastUpdatedAt,
+  relativeTimeSince,
   commitHash,
   lastTranslatedAt
 }: Props): JSX.Element {
@@ -31,32 +21,32 @@ export default function DocumentMetadata({
   const link =
     `https://github.com/input-output-hk/hydra-poc/commit/${commitHash}`
 
-  const renderLastUpdatedAt = (lastUpdatedAtDate: string, daysSinceLastUpdatedAt: number) =>
+  const renderLastUpdatedAt = (lastUpdatedAt: string, relativeTimeSince: string) =>
     <i className={styles.info}>
-      Last updated at: <b>{lastUpdatedAtDate}</b> (
-      <b>{daysSinceLastUpdatedAt}</b> days since last change
+      Last updated at: <b>{lastUpdatedAt}</b> (
+      <b>{relativeTimeSince}</b> since last change
       )
     </i>
 
   const renderCommitHash = (commitHash: string) =>
-    <i className={styles.info}>Last commit: <a href={link}><b>{commitHash}</b></a></i>
+    <i className={styles.info}>Last commit hash: <a href={link}><b>{commitHash}</b></a></i>
 
   const renderLastTranslatedAt = (lastTranslatedAt: string) =>
     <i className={styles.info}>Last translated at: <b>{lastTranslatedAt}</b></i>
 
   return (
     <div className={styles.block}>
-      {isValidDate(lastUpdatedAt) &&
+      {!isPlaceholder(lastUpdatedAt) &&
         renderLastUpdatedAt(
-          dateToFormatString(lastUpdatedAt),
-          daysSince(parseInt(lastUpdatedAt))
+          lastUpdatedAt,
+          relativeTimeSince
         )
       }
       {!isPlaceholder(commitHash) &&
         renderCommitHash(commitHash)
       }
-      {lastTranslatedAt && isValidDate(lastTranslatedAt) &&
-        renderLastTranslatedAt(dateToFormatString(lastTranslatedAt))
+      {lastTranslatedAt && !isPlaceholder(lastTranslatedAt) &&
+        renderLastTranslatedAt(lastTranslatedAt)
       }
     </div >
   )
