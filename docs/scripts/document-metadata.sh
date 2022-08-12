58724fd8c2b688b284b49110824113f7f2f12499
Franco Testagrossa <franco.testagrossa@iohk.io>
2022-08-12 21:05:20 +0200

    Try CI fix ~ v5

--- docs/scripts/document-metadata.sh
+++ docs/scripts/document-metadata.sh
@@ -9,18 +9,22 @@ for MD in $DOCUMENTS; do
 
     PLACEHOLDER="{{last-updated-at}}"
     REPLACEMENT=$(git --no-pager log -1 --pretty=format:'%ad' --date=local $MD)
-    sed -i '' "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
+    
+    echo $(sed "s/$PLACEHOLDER/$REPLACEMENT/g" $MD)
+
+    sed -i "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
     
     PLACEHOLDER="{{relative-time-since}}"
     REPLACEMENT=$(git --no-pager log -1 --pretty=format:'%cr' $MD)
-    sed -i '' "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
+    sed -i "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
 
     PLACEHOLDER="{{commit-hash}}"
     REPLACEMENT=$(git --no-pager log -1 --pretty=format:'%h' $MD)
-    sed -i '' "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
+    sed -i "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
 
     PLACEHOLDER="{{last-translated-at}}"
     REPLACEMENT=$(git --no-pager log -1 --pretty=format:'%ad' --date=local docs/i18n)
-    sed -i '' "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
+    sed -i "s/$PLACEHOLDER/$REPLACEMENT/g" $MD
+
 
 done
