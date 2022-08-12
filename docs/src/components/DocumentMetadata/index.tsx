cb2e92f88f0579c1f0121159218a536f23f8868f
Franco Testagrossa <franco.testagrossa@iohk.io>
2022-08-12 14:19:58 +0200

    Minor format

--- docs/src/components/DocumentMetadata/index.tsx
+++ docs/src/components/DocumentMetadata/index.tsx
@@ -23,7 +23,9 @@ export default function DocumentMetadata({
     <div className={styles.block}>
       <i className={styles.info}>Last updated at: <b>{lastUpdatedAt}</b></i>
       <i className={styles.info}>Author: <b>{authorInfo}</b></i>
-      {lastTranslatedAt && <i className={styles.info}>Last translated at: <b>{lastTranslatedAt}</b></i>}
+      {lastTranslatedAt &&
+        <i className={styles.info}>Last translated at: <b>{lastTranslatedAt}</b></i>
+      }
     </div>
   );
 }
