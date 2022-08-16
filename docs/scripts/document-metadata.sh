f2d8b522f89c7f48811257e4d1d66ead13170735
Franco Testagrossa <franco.testagrossa@iohk.io>
2022-08-16 13:51:27 +0200

    Format improvements

--- docs/scripts/document-metadata.sh
+++ docs/scripts/document-metadata.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-yarn run enrich-document-metadata
-
 PLACEHOLDER="<SiteMetadata />"
 REPLACEMENT=$(git --no-pager log -1 --pretty=format:'%ad' --date=local docs)
 sed -i "s|$PLACEHOLDER|$REPLACEMENT|g" 'docusaurus.config.js'
--- docs/scripts/enrich-document-metadata.js
+++ docs/scripts/enrich-document-metadata.js
@@ -5,9 +5,8 @@ const exec = util.promisify(require('child_process').exec)
 const fs = require('fs')
 const glob = require("glob")
 
-const sysCall = async (cmd) => exec(cmd).then(dirs => dirs.stdout)
-
 const Utils = {
+    sysCall: async (cmd) => exec(cmd).then(dirs => dirs.stdout),
     getItems: async (path, options) => {
         return new Promise((resolve, reject) => {
             glob(path, options, (err, res) => {
@@ -19,11 +18,11 @@ const Utils = {
         })
     }
     , getLastUpdatedAt: async (doc) =>
-        sysCall(`git --no-pager log -1 --pretty=format:'%ad' --date=local ${doc}`)
+        Utils.sysCall(`git --no-pager log -1 --pretty=format:'%ad' --date=local ${doc}`)
     , getRelativeTimeSince: async (doc) =>
-        sysCall(`git --no-pager log -1 --pretty=format:'%cr' ${doc}`)
+        Utils.sysCall(`git --no-pager log -1 --pretty=format:'%cr' ${doc}`)
     , getCommitHash: async (doc) =>
-        sysCall(`git --no-pager log -1 --pretty=format:'%h' ${doc}`)
+        Utils.sysCall(`git --no-pager log -1 --pretty=format:'%h' ${doc}`)
     , pathToLocation: (path) => {
         const basePath = path.replace('.md', '')
             .replace('i18n/', '')
@@ -37,7 +36,7 @@ const Utils = {
             return basePath.replace('docusaurus-plugin-content-', '')
         }
     }
-    , getDocumentMetadata: async (doc) => {
+    , getMetadatasJson: async (doc) => {
         console.log("Processing: ", doc)
         const lastUpdatedAt = await Utils.getLastUpdatedAt(doc)
         const relativeTimeSince = await Utils.getRelativeTimeSince(doc)
@@ -51,24 +50,27 @@ const Utils = {
             }
         }
     }
-    , mergeMetadatas: (metadatas) =>
-        metadatas.reduce((obj, item) => {
+    , mergeJsons: (jsons) =>
+        jsons.reduce((obj, item) => {
             for (const key in item) {
                 obj[key] = item[key]
             }
             return obj
         }, {})
+    , writeJsonToFile: (file) => (json) => {
+        fs.writeFileSync(file, JSON.stringify(json, null, 2))
+    }
 }
 
 async function main() {
 
     Utils.getItems("**/*.md", { "ignore": ['**/node_modules/**'] })
         .then(docs => {
-            const metadatas = docs.map(doc => Utils.getDocumentMetadata(doc))
+            const metadatas = docs.map(Utils.getMetadatasJson)
             Promise
                 .all(metadatas)
-                .then(metadatas => Utils.mergeMetadatas(metadatas))
-                .then(json => fs.writeFileSync("static/metadatas.json", JSON.stringify(json, null, 2)))
+                .then(Utils.mergeJsons)
+                .then(Utils.writeJsonToFile("static/metadatas.json"))
         })
 }
 
