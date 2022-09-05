887741a42d542bee641cd5a701e32e7ef86e0412
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-09-05 17:36:26 +0200

    Add a NixOS example configuration

---
+++ sample-node-config/nixos/README.md
@@ -0,0 +1,50 @@
+This directory does contain an example configuration to run a `cardano-node`,
+`hydra-node` and the example `hydraw` application on a NixOS host.
+
+It is not using fancy NixOps or proper NixOS services, but the basic declarative
+`virtualisation.oci-containers` NixOS options to make it very similar to the
+`docker-compose` way of orchestrating the containers.
+
+## Add & start containers
+
+To use this, copy or symlink the `hydraw.nix` module and import it into your `NixOS`' `configuration.nix`:
+
+```nix
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./hydraw.nix
+  ];
+  
+  # ... the rest of your configuration.nix
+}
+```
+
+Then, after the next `nixos-rebuild switch`, the relevant docker containers should be started by systemd and you can see their logs in either:
+
+``` sh
+journalctl -u docker-cardano-node.service
+docker logs cardano-node
+```
+
+## Copying credentials
+
+As not everything is automated yet, we will need to copy the configured
+credentials of `hydraw.nix` to the target host and create relevant directories.
+For example using `ssh` and `scp` with hostname `example`:
+
+``` sh
+ssh example "mkdir -p /data/cardano-node /data/hydra-node/credentials"
+scp sebastian.cardano.sk sebastian.hydra.sk example:/data/hydra-node/credentials/
+```
+
+Also copy any additional Hydra verification keys into the
+`/data/hydra-node/credentials` and extend the command line of the `hydra-node`
+container accordingly.
+
+Lastly, the `hydra-node` requires some `protocol-parameters.json` to configure the ledger in the `/data/hydra-node` directory. Use the ones from the e2e tests `hydra-cluster/config/protocol-parameters.json` or use the L1 ones via the `cardano-cli` on the target host:
+
+``` sh
+ssh example "CARDANO_NODE_SOCKET_PATH=/data/cardano-node/node.socket cardano-cli query protocol-parameters --testnet-magic 2 > /data/hydra-node/protocol-parameters.json"
+```
---
+++ sample-node-config/nixos/hydraw.nix
@@ -0,0 +1,98 @@
+# Hydraw running on hydra running on cardano
+
+{ config, pkgs, lib, ... }:
+
+let
+  cardano-node = import
+    (pkgs.fetchgit {
+      url = "https://github.com/input-output-hk/cardano-node";
+      rev = "1.35.3";
+      sha256 = "020fwimsm24yblr1fmnwx240wj8r3x715p89cpjgnnd8axwf32p0";
+    })
+    { };
+in
+{
+  # Add iohk substituters
+  nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://hydra.iohk.io"
+  ];
+  nix.settings.trusted-public-keys = [
+    "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+
+  # services.nginx.virtualHosts."hydraw.fk.ncoding.at" = {
+  #   forceSSL = true;
+  #   enableACME = true;
+  #   locations."/" = {
+  #     proxyPass = "http://127.0.0.1:2342";
+  #     proxyWebsockets = true;
+  #     extraConfig = ''
+  #       proxy_buffering off;
+  #       client_max_body_size 500M;
+  #     '';
+  #   };
+  # };
+
+  # Using entrypoint from: https://github.com/input-output-hk/cardano-world/blob/master/nix/cardano/entrypoints.nix
+  virtualisation.oci-containers.containers.cardano-node = {
+    image = "inputoutput/cardano-node:1.35.3-new";
+    volumes = [
+      "/data/cardano-node:/data"
+    ];
+    environment = {
+      DATA_DIR = "/data";
+      ENVIRONMENT = "preview";
+      SOCKET_PATH = "/data/node.socket";
+    };
+  };
+
+  # The 1.35.3-new cardano-node image does not contain a cli, so let's add it
+  # using nix instead.
+  environment.systemPackages = [ cardano-node.cardano-cli ];
+
+  # Our hydra-node instance
+  virtualisation.oci-containers.containers.hydra-node =
+    # TODO: lookup by network (preview)
+    let
+      hydraScriptsTxId = "bde2ca1f404200e78202ec37979174df9941e96fd35c05b3680d79465853a246";
+      networkMagic = "2";
+      nodeId = "314";
+    in
+    {
+      image = "ghcr.io/input-output-hk/hydra-node:0.7.0";
+      volumes = [
+        "/data/cardano-node:/cardano-node:ro"
+        "/data/hydra-node:/data:ro"
+      ];
+      ports = [
+        "4001:4001"
+        "5001:5001"
+      ];
+      cmd = [
+        "--node-id"
+        nodeId
+        "--api-host"
+        "0.0.0.0"
+        "--host"
+        "0.0.0.0"
+        "--monitoring-port"
+        "6001"
+        "--hydra-scripts-tx-id"
+        hydraScriptsTxId
+        "--hydra-signing-key"
+        "/data/credentials/sebastian.hydra.sk"
+        "--cardano-signing-key"
+        "/data/credentials/sebastian.cardano.sk"
+        "--ledger-genesis"
+        "/cardano-node/config/preview/shelley-genesis.json"
+        "--ledger-protocol-parameters"
+        "/data/protocol-parameters.json"
+        "--network-id"
+        networkMagic
+        "--node-socket"
+        "/cardano-node/node.socket"
+      ];
+    };
+}
