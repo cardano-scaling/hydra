c3ee6396f40ae7e730d5109065649c673f28b050
Sasha Bogicevic <Sasha.Bogicevic@iohk.io>
2022-09-23 16:26:20 +0200

    Address two TODO items and refactor a bit
    - getTimeHandle is now moved to TimeHandle module since arbitrary instance
    needs it

--- hydra-cluster/src/Hydra/Cluster/Scenarios.hs
+++ hydra-cluster/src/Hydra/Cluster/Scenarios.hs
@@ -7,15 +7,19 @@ import Hydra.Prelude
 
 import CardanoClient (queryTip)
 import CardanoNode (RunningNode (..))
+import Control.Lens ((^?))
+import Data.Aeson (object, (.=))
+import Data.Aeson.Lens (key, _JSON)
+import qualified Data.Set as Set
 import Hydra.Cardano.Api (Lovelace, TxId, selectLovelace)
 import Hydra.Cluster.Faucet (Marked (Fuel), queryMarkedUTxO, seedFromFaucet)
-import Hydra.Cluster.Fixture (Actor (Alice), actorName, aliceSk)
+import Hydra.Cluster.Fixture (Actor (Alice), actorName, alice, aliceSk)
 import Hydra.Cluster.Util (chainConfigFor, keysFor)
 import Hydra.Ledger (IsTx (balance))
 import Hydra.Ledger.Cardano (Tx)
 import Hydra.Logging (Tracer, traceWith)
 import Hydra.Options (networkId, startChainFrom)
-import HydraNode (EndToEndLog (..), withHydraNode)
+import HydraNode (EndToEndLog (..), input, output, send, waitFor, waitMatch, withHydraNode)
 
 singlePartyHeadFullLifeCycle ::
   Tracer IO EndToEndLog ->
@@ -31,8 +35,27 @@ singlePartyHeadFullLifeCycle tracer workDir node@RunningNode{networkId} hydraScr
     chainConfigFor Alice workDir nodeSocket []
       <&> \config -> config{networkId, startChainFrom = Just tip}
   withHydraNode tracer aliceChainConfig workDir 1 aliceSk [] [1] hydraScriptsTxId $ \n1 -> do
-    -- TODO: revert this back as we don't want to keep this kind of test around
-    threadDelay 20
+    -- Initialize & open head
+    let contestationPeriod = 1 :: Natural
+    send n1 $ input "Init" ["contestationPeriod" .= contestationPeriod]
+    waitFor tracer 600 [n1] $
+      output "ReadyToCommit" ["parties" .= Set.fromList [alice]]
+    -- Commit nothing for now
+    send n1 $ input "Commit" ["utxo" .= object mempty]
+    waitFor tracer 600 [n1] $
+      output "HeadIsOpen" ["utxo" .= object mempty]
+    -- Close head
+    send n1 $ input "Close" []
+    deadline <- waitMatch 600 n1 $ \v -> do
+      guard $ v ^? key "tag" == Just "HeadIsClosed"
+      v ^? key "contestationDeadline" . _JSON
+    -- Expect to see ReadyToFanout within 3 seconds after deadline
+    remainingTime <- diffUTCTime deadline <$> getCurrentTime
+    waitFor tracer (truncate $ remainingTime + 3) [n1] $
+      output "ReadyToFanout" []
+    send n1 $ input "Fanout" []
+    waitFor tracer 600 [n1] $
+      output "HeadIsFinalized" ["utxo" .= object mempty]
   traceRemainingFunds Alice
  where
   RunningNode{nodeSocket} = node
