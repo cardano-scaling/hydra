925e875117355aee5c105989bba86dad72eaa22c
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-09-23 11:38:40 +0200

    Re-instantiate eraParams as they are used in showing the exception

--- hydra-node/src/Hydra/Ledger/Cardano/Evaluate.hs
+++ hydra-node/src/Hydra/Ledger/Cardano/Evaluate.hs
@@ -23,7 +23,7 @@ import Cardano.Ledger.BaseTypes (ProtVer (..), boundRational)
 import Cardano.Ledger.Coin (Coin (Coin))
 import Cardano.Ledger.Val (Val ((<+>)), (<×>))
 import Cardano.Slotting.EpochInfo (EpochInfo, fixedEpochInfo)
-import Cardano.Slotting.Slot (EpochSize (EpochSize), SlotNo (SlotNo), EpochNo (EpochNo))
+import Cardano.Slotting.Slot (EpochNo (EpochNo), EpochSize (EpochSize), SlotNo (SlotNo))
 import Cardano.Slotting.Time (RelativeTime (RelativeTime), SlotLength (getSlotLength), SystemStart (SystemStart), mkSlotLength, toRelativeTime)
 import qualified Data.ByteString as BS
 import Data.Default (def)
@@ -59,6 +59,7 @@ import Hydra.Cardano.Api (
 import Hydra.Data.ContestationPeriod (posixToUTCTime)
 import Ouroboros.Consensus.Cardano.Block (CardanoEras)
 import Ouroboros.Consensus.HardFork.History (
+  Bound (Bound, boundEpoch, boundSlot, boundTime),
   EraEnd (EraEnd, EraUnbounded),
   EraParams (..),
   EraSummary (..),
@@ -66,7 +67,6 @@ import Ouroboros.Consensus.HardFork.History (
   Summary (Summary),
   initBound,
   mkInterpreter,
-  mkUpperBound, Bound (Bound, boundTime, boundSlot, boundEpoch)
  )
 import Ouroboros.Consensus.Util.Counting (NonEmpty (NonEmptyOne))
 import Test.Cardano.Ledger.Alonzo.PlutusScripts (testingCostModelV1, testingCostModelV2)
@@ -214,10 +214,12 @@ eraHistory =
 -- at a known or earliest possible end of the current era + a safe zone.
 --
 -- See 'Ouroboros.Consensus.HardFork.History.EraParams' for details.
--- NOTE: This era is using not so realistic epoch sizes (1)
+--
+-- NOTE: This era is using not so realistic epoch sizes of 1 and sets a slot
+-- length of 1
 eraHistoryWithHorizonAt :: SlotNo -> EraHistory CardanoMode
 eraHistoryWithHorizonAt slotNo@(SlotNo n) =
- EraHistory CardanoMode (mkInterpreter summary)
+  EraHistory CardanoMode (mkInterpreter summary)
  where
   summary :: Summary (CardanoEras StandardCrypto)
   summary =
@@ -227,12 +229,21 @@ eraHistoryWithHorizonAt slotNo@(SlotNo n) =
         , eraEnd =
             EraEnd $
               Bound
-              { boundTime = RelativeTime $ fromIntegral n
-              , boundSlot = slotNo
-              , boundEpoch = EpochNo n
-              }
-        , eraParams = error "This should be unused"
+                { boundTime = RelativeTime $ fromIntegral n
+                , boundSlot = slotNo
+                , boundEpoch = EpochNo n
+                }
+        , eraParams
         }
+
+  eraParams =
+    EraParams
+      { eraEpochSize = EpochSize 1
+      , eraSlotLength = mkSlotLength 1
+      , -- NOTE: unused if the 'eraEnd' is already defined, but would be used to
+        -- extend the last era accordingly in the real cardano-node
+        eraSafeZone = UnsafeIndefiniteSafeZone
+      }
 
 epochInfo :: Monad m => EpochInfo m
 epochInfo = fixedEpochInfo epochSize slotLength
