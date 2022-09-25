332842a252eaf11fbe3d22658a41d6fa928b7c5d
Franco Testagrossa <franco.testagrossa@iohk.io>
2022-09-25 19:46:55 +0200

    Always show tracing after test finishes (with success or error)

--- hydra-cluster/test/Test/Hydra/Cluster/FaucetSpec.hs
+++ hydra-cluster/test/Test/Hydra/Cluster/FaucetSpec.hs
@@ -7,17 +7,17 @@ import CardanoNode (withCardanoNodeDevnet)
 import Control.Concurrent.Async (replicateConcurrently_)
 import Hydra.Cluster.Faucet (Marked (Normal), seedFromFaucet_)
 import Hydra.Ledger.Cardano (genVerificationKey)
-import Hydra.Logging (showLogsOnFailure)
+import Hydra.Logging (showLogsOnFailureOrFinish)
 import Test.QuickCheck (generate)
 
 spec :: Spec
 spec =
   describe "seed from faucet" $
     it "should work concurrently" $
-      showLogsOnFailure $ \tracer ->
+      showLogsOnFailureOrFinish $ \tracer ->
         failAfter 30 $
           withTempDir "end-to-end-cardano-node" $ \tmpDir ->
             withCardanoNodeDevnet tracer tmpDir $ \node ->
-              replicateConcurrently_ 30 $ do
+              replicateConcurrently_ 10 $ do
                 vk <- generate genVerificationKey
                 seedFromFaucet_ node vk 1_000_000 Normal tracer
