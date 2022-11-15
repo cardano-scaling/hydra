77b5f3a740e93c0fdaf8b6570cee96fd6e451cd4
Sasha Bogicevic <Sasha.Bogicevic@iohk.io>
2022-11-15 10:38:31 +0100

    Introduce simple generators

--- hydra-node/test/Hydra/PersistenceSpec.hs
+++ hydra-node/test/Hydra/PersistenceSpec.hs
@@ -1,11 +1,15 @@
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
+
 module Hydra.PersistenceSpec where
 
 import Hydra.Prelude
 import Test.Hydra.Prelude
 
+import Data.Aeson (Value (..))
 import qualified Data.Aeson as Aeson
+import qualified Data.Text as Text
 import Hydra.Persistence (PersistenceIncremental (..), createPersistenceIncremental)
-import Test.QuickCheck (generate, oneof)
+import Test.QuickCheck (elements, generate, oneof)
 import Test.QuickCheck.Gen (listOf)
 
 spec :: Spec
@@ -14,9 +18,19 @@ spec =
     it "is consistent after multiple append calls" $
       withTempDir "hydra-persistence" $ \tmpDir -> do
         items :: [Aeson.Value] <- generate $ listOf genPersistenceItem
+        print items
         PersistenceIncremental{loadAll, append} <- createPersistenceIncremental Proxy $ tmpDir <> "/data"
         forM_ items append
         loadAll `shouldReturn` items
 
 genPersistenceItem :: Gen Aeson.Value
-genPersistenceItem = oneof []
+genPersistenceItem =
+  oneof
+    [ pure Null
+    , String <$> genSomeText
+    ]
+
+genSomeText :: Gen Text
+genSomeText = do
+  let t = ['A' .. 'z'] <> ['\n', '\t', '\r']
+  Text.pack <$> listOf (elements t)
