859d574d2feca5eccc6319b59c14f24c88c137dd
Franco Testagrossa <franco.testagrossa@iohk.io>
2022-12-01 10:24:07 +0100

    Remove

--- hydra-cluster/src/Hydra/Cluster/Options.hs
+++ hydra-cluster/src/Hydra/Cluster/Options.hs
@@ -26,8 +26,7 @@ parseOptions =
     <*> parsePublishHydraScripts
  where
   parseKnownNetwork =
-    flag' Testnet (long "testnet" <> help "The public testnet (soon EOL)")
-      <|> flag' Preview (long "preview" <> help "The preview testnet")
+    flag' Preview (long "preview" <> help "The preview testnet")
       <|> flag' Preproduction (long "preprod" <> help "The pre-production testnet")
 
   parseStateDirectory =
