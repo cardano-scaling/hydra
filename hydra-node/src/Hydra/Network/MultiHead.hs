5bbdaa371259d78be68edaa70b4e39d8d7b07b0b
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-01-01 17:11:17 +0100

    Add UDP and MultiHead network instances
    These are building blocks for a multi-headed node:
    * The `MultiHead` network allows adding a `HeadId` to identify
      messages
    * The `UDP` network component uses "naive" UDP as transport layer for
      messages, as well as a peers selection mechanism to select where to
      send messages to.

---
+++ hydra-node/src/Hydra/Network/MultiHead.hs
@@ -0,0 +1,53 @@
+{-# OPTIONS_GHC -Wwarn #-}
+
+-- | An experimental network component for Hydra node that wraps several heads.
+--
+-- All messages are encapsulated in an `Enveloped` that identifies the `Head` to which the message
+-- pertains. Sender attaches the id to the message which is decoded and correctly dispatched by
+-- the receiver.
+module Hydra.Network.MultiHead where
+
+import Hydra.Prelude
+
+import Control.Monad.Class.MonadAsync (MonadAsync)
+import Control.Monad.Class.MonadSTM (MonadSTM (readTVarIO))
+import Hydra.Chain (HeadId)
+import Hydra.Network (Network (..), NetworkCallback, NetworkComponent)
+
+data Enveloped msg = Enveloped {headId :: HeadId, message :: msg}
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToCBOR, FromCBOR)
+
+withMultiNode ::
+  (MonadAsync m) =>
+  -- | The headId for this network instance.
+  -- This variable will be set externally when the `Init` transaction is posted and
+  -- observed on-chain. When unset, no communication happens and all messages will
+  -- be simply dropped.
+  TVar m (Maybe HeadId) ->
+  NetworkComponent m (Enveloped msg) a ->
+  NetworkComponent m msg a
+withMultiNode headId withNetwork callback action = do
+  withNetwork (messageDispatcher headId callback) $ \network ->
+    action (decorateMessage headId network)
+
+decorateMessage ::
+  MonadSTM m =>
+  TVar m (Maybe HeadId) ->
+  Network m (Enveloped msg) ->
+  Network m msg
+decorateMessage headId Network{broadcast} =
+  Network $ \msg ->
+    readTVarIO headId >>= \case
+      Nothing -> pure ()
+      Just hid -> broadcast (Enveloped hid msg)
+
+messageDispatcher ::
+  MonadSTM m =>
+  TVar m (Maybe HeadId) ->
+  NetworkCallback msg m ->
+  NetworkCallback (Enveloped msg) m
+messageDispatcher headId callback (Enveloped hid msg) =
+  readTVarIO headId >>= \case
+    Just hid' | hid' == hid -> callback msg
+    _ -> pure ()
---
+++ hydra-node/src/Hydra/Network/UDP.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wwarn #-}
+
+-- | An experimental network component for Hydra node that uses UDP.
+module Hydra.Network.UDP where
+
+import Hydra.Prelude
+
+import Codec.CBOR.Read (deserialiseFromBytes)
+import Codec.CBOR.Write (toStrictByteString)
+import qualified Data.ByteString.Lazy as LBS
+import Data.Text (unpack)
+import Hydra.Network (Host (..), Network (..), NetworkCallback, NetworkComponent)
+import Network.Socket (Socket, SocketType (Datagram), addrAddress, addrFamily, bind, close, connect, defaultProtocol, getAddrInfo, socket)
+import Network.Socket.ByteString (recv, sendAll)
+
+type PeersResolver m a = (a -> m [Host])
+
+withUDPNetwork ::
+  (FromCBOR msg, ToCBOR msg) =>
+  -- | The address to bind the server on.
+  Host ->
+  -- | An `IO` action to "resolve" the peers to send a message to.
+  PeersResolver IO msg ->
+  NetworkComponent IO msg a
+withUDPNetwork localhost peersResolver callback action =
+  withAsync (udpServer localhost callback) $ \_ ->
+    action (udpClient peersResolver)
+
+udpClient :: ToCBOR msg => PeersResolver IO msg -> Network IO msg
+udpClient peersResolver =
+  Network $ \msg ->
+    let payload = toStrictByteString $ toCBOR msg
+     in peersResolver msg >>= traverse_ (sendUDP payload)
+
+sendUDP :: ByteString -> Host -> IO ()
+sendUDP payload Host{hostname, port} = do
+  addrinfos <- getAddrInfo Nothing (Just $ unpack hostname) (Just $ show port)
+  case addrinfos of
+    (serveraddr : _) -> do
+      sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+      connect sock (addrAddress serveraddr)
+      -- TODO: chunk the payload if too large. In theory, UDP messages can be several
+      -- 10s of KBs large but in practice, routers can drop messages larger than ~512 bytes
+      -- so it's necessary to chunk and reassemble larger messages.
+      sendAll sock payload
+      close sock
+    _ -> error "TODO: what if address is not resolved"
+
+udpServer :: FromCBOR msg => Host -> NetworkCallback msg IO -> IO ()
+udpServer Host{hostname, port} callback = do
+  addrinfos <- getAddrInfo Nothing (Just $ unpack hostname) (Just $ show port)
+  case addrinfos of
+    (serveraddr : _) -> do
+      sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+      bind sock (addrAddress serveraddr)
+      forever (receiveMsg sock callback)
+    _ -> error "TODO: what if address is not resolved"
+
+receiveMsg :: forall msg. FromCBOR msg => Socket -> NetworkCallback msg IO -> IO ()
+receiveMsg sock callback = do
+  -- TODO: reassemble large messages
+  bytes <- recv sock 4096
+  case deserialiseFromBytes (fromCBOR @msg) (LBS.fromStrict bytes) of
+    Left err -> error $ "TODO: handle error: " <> show err
+    Right (_, msg) -> callback msg
