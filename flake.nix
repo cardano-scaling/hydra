f04e6e089c61ce53ba890cd687dce1b8cc8a1417
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-03-01 07:39:53 +0100

    Remove flake recursion from cardano-node input
    Seems like the benchmarking of the cardano-node was doing funky things
    with flake inputs which yielded warnings on invocations and failures in
    Hydra CI. This is a workaround to fix the tagged release (might be
    addressed on master / for next release).

--- flake.nix
+++ flake.nix
@@ -8,7 +8,15 @@
       url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
       flake = false;
     };
-    cardano-node.url = "github:input-output-hk/cardano-node/1.35.5";
+
+    # The workbench flake inputs have some broken recursion in cardano-node
+    # https://github.com/input-output-hk/cardano-node/pull/4865
+    cardano-node = {
+      url = "github:input-output-hk/cardano-node/1.35.5";
+      inputs.node-measured.follows = "empty-flake";
+      inputs.cardano-node-workbench.follows = "empty-flake";
+    };
+    empty-flake.url = "github:input-output-hk/empty-flake";
   };
 
   outputs =
