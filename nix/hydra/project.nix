74e25d7ce7bdbf41c30c6aaa72ab042f60b4faba
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-03-03 09:32:17 +0100

    Disable checkMaterialization
    This should only be enabled when we want to see whether materialized
    files are not consistent and to update them.

--- nix/hydra/project.nix
+++ nix/hydra/project.nix
@@ -64,7 +64,10 @@ let
       )
     ];
     materialized = ./hydra.materialized;
-    checkMaterialization = true;
+
+    # NOTE: Enable this to verify materialized files (do all the work of
+    # materializing) and provide output how to update the materialized files.
+    checkMaterialization = false;
   };
 in
 {
