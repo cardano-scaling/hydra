d64448e32c62869a87196bee561313bf397362b4
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-04-12 15:03:18 +0200

    Upstreamed ScriptWitnessInCtx
    The same class and instances are now in cardano-api

--- hydra-cardano-api/src/Hydra/Cardano/Api.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api.hs
@@ -120,7 +120,6 @@ import Hydra.Cardano.Api.ScriptDataSupportedInEra as Extras
 import Hydra.Cardano.Api.ScriptDatum as Extras
 import Hydra.Cardano.Api.ScriptHash as Extras
 import Hydra.Cardano.Api.ScriptLanguageInEra as Extras
-import Hydra.Cardano.Api.ScriptWitnessInCtx as Extras
 import Hydra.Cardano.Api.Tx as Extras
 import Hydra.Cardano.Api.TxBody as Extras
 import Hydra.Cardano.Api.TxId as Extras
