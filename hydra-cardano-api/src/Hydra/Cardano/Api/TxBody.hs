272d19b3111181092047869d7fd372d9bc2dc581
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-04-12 18:37:15 +0200

    Avoid using removed "HasField" instances
    The "minted" view on the TxBody has been removed. Let's avoid dealing
    with the TxBody directly by using 'rdptr' function from the
    ledger (spec).

--- hydra-cardano-api/src/Hydra/Cardano/Api/TxBody.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api/TxBody.hs
@@ -6,18 +6,16 @@ import Hydra.Cardano.Api.PlutusScript (fromLedgerScript)
 import Hydra.Cardano.Api.PolicyId (toLedgerScriptHash)
 import Hydra.Cardano.Api.ScriptData (FromScriptData)
 import Hydra.Cardano.Api.TxIn (toLedgerTxIn)
+import Hydra.Cardano.Api.Value (toLedgerPolicyID)
 
 import qualified Cardano.Ledger.Alonzo.Data as Ledger
-import qualified Cardano.Ledger.Alonzo.Scripts as Ledger
-import qualified Cardano.Ledger.Alonzo.Tx as Ledger
 import qualified Cardano.Ledger.Alonzo.TxWitness as Ledger
-import qualified Cardano.Ledger.Era as Ledger
-import qualified Cardano.Ledger.Hashes as Ledger
-import qualified Data.Map as Map
-import Data.Maybe.Strict (strictMaybeToMaybe)
-import GHC.Records (HasField (..))
-import qualified PlutusLedgerApi.V2 as Plutus
+import qualified Cardano.Ledger.Babbage.Tx as Ledger
+import Cardano.Ledger.BaseTypes (strictMaybeToMaybe)
+import qualified Cardano.Ledger.Core as Ledger
 import Data.List (find)
+import qualified Data.Map as Map
+import qualified PlutusLedgerApi.V2 as Plutus
 
 -- | Find and deserialise from 'ScriptData', a redeemer from the transaction
 -- associated to the given input.
@@ -28,12 +26,8 @@ findRedeemerSpending ::
   TxIn ->
   Maybe a
 findRedeemerSpending (getTxBody -> ShelleyTxBody _ body _ scriptData _ _) txIn = do
-  idx <- strictMaybeToMaybe $ Ledger.indexOf needle haystack
-  let ptr = Ledger.RdmrPtr Ledger.Spend idx
+  ptr <- strictMaybeToMaybe $ Ledger.rdptr body (Ledger.Spending $ toLedgerTxIn txIn)
   lookupRedeemer ptr scriptData
- where
-  needle = toLedgerTxIn txIn
-  haystack = getField @"inputs" body
 
 findRedeemerMinting ::
   forall a.
@@ -43,12 +37,8 @@ findRedeemerMinting ::
   PolicyId ->
   Maybe a
 findRedeemerMinting (getTxBody -> ShelleyTxBody _ body _ scriptData _ _) pid = do
-  idx <- strictMaybeToMaybe $ Ledger.indexOf needle haystack
-  let ptr = Ledger.RdmrPtr Ledger.Mint idx
+  ptr <- strictMaybeToMaybe $ Ledger.rdptr body (Ledger.Minting $ toLedgerPolicyID pid)
   lookupRedeemer ptr scriptData
- where
-  needle = toLedgerScriptHash pid
-  haystack = getField @"minted" body :: Set (Ledger.ScriptHash StandardCrypto)
 
 findScriptMinting ::
   forall lang.
@@ -56,12 +46,11 @@ findScriptMinting ::
   Tx Era ->
   PolicyId ->
   Maybe (PlutusScript lang)
-findScriptMinting (getTxBody -> ShelleyTxBody _ body scripts _ _ _) pid = do
-  _idx <- strictMaybeToMaybe $ Ledger.indexOf needle haystack
-  fromLedgerScript @_ @lang <$> find ((== needle) . Ledger.hashScript @(ShelleyLedgerEra Era)) scripts
+findScriptMinting (getTxBody -> ShelleyTxBody _ _ scripts _ _ _) pid = do
+  fromLedgerScript @_ @lang
+    <$> find ((== needle) . Ledger.hashScript @(ShelleyLedgerEra Era)) scripts
  where
   needle = toLedgerScriptHash pid
-  haystack = getField @"minted" body :: Set (Ledger.ScriptHash StandardCrypto)
 
 --
 -- Internals
