ae17cea87b143848211631857dcd7ec6b26732d1
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-04-25 08:42:22 +0200

    Also build spec on Hydra CI
    This will make the spec part of cache.zw3rk.com and result in quick CI
    runs when spec didn't change as we get cache hits.

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
 
         # Build selected derivations in CI for caching
         hydraJobs = {
-          packages = { inherit (packages) hydra-node hydra-tui hydraw; };
+          packages = { inherit (packages) hydra-node hydra-tui hydraw spec; };
           devShells = { inherit (devShells) default ci; };
         };
       });
