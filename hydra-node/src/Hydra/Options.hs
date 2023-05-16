9c6d80d1eeb5d9f1a0f6dafe36660fb917faa08e
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-05-16 06:39:04 +0000

    Use VERSION file if it exists to compute full version

--- hydra-node/src/Hydra/Options.hs
+++ hydra-node/src/Hydra/Options.hs
@@ -33,7 +33,7 @@ import Hydra.Ledger.Cardano ()
 import Hydra.Logging (Verbosity (..))
 import Hydra.Network (Host, NodeId (NodeId), PortNumber, readHost, readPort)
 import Hydra.Party (Party)
-import Hydra.Version (gitDescribe)
+import Hydra.Version (gitDescribe, gitRevision)
 import Options.Applicative (
   Parser,
   ParserInfo,
@@ -264,16 +264,16 @@ cardanoLedgerProtocolParametersParser =
     )
 
 data ChainConfig = DirectChainConfig
-  { networkId :: NetworkId
-  -- ^ Network identifer to which we expect to connect.
-  , nodeSocket :: FilePath
-  -- ^ Path to a domain socket used to connect to the server.
-  , cardanoSigningKey :: FilePath
-  -- ^ Path to the cardano signing key of the internal wallet.
-  , cardanoVerificationKeys :: [FilePath]
-  -- ^ Paths to other node's verification keys.
-  , startChainFrom :: Maybe ChainPoint
-  -- ^ Point at which to start following the chain.
+  { -- | Network identifer to which we expect to connect.
+    networkId :: NetworkId
+  , -- | Path to a domain socket used to connect to the server.
+    nodeSocket :: FilePath
+  , -- | Path to the cardano signing key of the internal wallet.
+    cardanoSigningKey :: FilePath
+  , -- | Paths to other node's verification keys.
+    cardanoVerificationKeys :: [FilePath]
+  , -- | Point at which to start following the chain.
+    startChainFrom :: Maybe ChainPoint
   , contestationPeriod :: ContestationPeriod
   }
   deriving (Eq, Show, Generic, ToJSON, FromJSON)
@@ -571,8 +571,11 @@ hydraNodeCommand =
  where
   versionInfo =
     infoOption
-      (fromMaybe (showVersion version) gitDescribe)
+      (fromMaybe displayVersion gitDescribe)
       (long "version" <> help "Show version")
+
+  displayVersion =
+    showVersion version <> maybe "" ("-" <>) gitRevision
 
   scriptInfo =
     infoOption
@@ -626,7 +629,7 @@ validateRunOptions :: RunOptions -> Either InvalidOptions ()
 validateRunOptions RunOptions{hydraVerificationKeys, chainConfig}
   | numberOfOtherParties + 1 > maximumNumberOfParties = Left MaximumNumberOfPartiesExceeded
   | length (cardanoVerificationKeys chainConfig) /= length hydraVerificationKeys =
-      Left CardanoAndHydraKeysMissmatch
+    Left CardanoAndHydraKeysMissmatch
   | otherwise = Right ()
  where
   -- let's take the higher number of loaded cardano/hydra keys
--- hydra-prelude/src/Hydra/Version.hs
+++ hydra-prelude/src/Hydra/Version.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
 -- | Provides version numbers from git identifiers. Based on 'gitrev' package
 -- with a 'Maybe' interface around it.
@@ -6,6 +7,8 @@ module Hydra.Version where
 
 import Hydra.Prelude
 
+import Data.FileEmbed (embedFileIfExists)
+import qualified Data.Text as Text
 import qualified Development.GitRev as GitRev
 
 -- | Determine the version during build time using `git describe`.
@@ -18,15 +21,22 @@ gitDescribe
   -- not be run, then this will be "UNKNOWN".
   fromGit = $(GitRev.gitDescribe)
 
--- | Determine the version during build time using `git rev-parse`.
+-- | Determine the version during build time.  Uses either the content of a
+-- `VERSION` file if it exists, the output of `git rev-parse` if available, or
+-- `Nothing`.
 gitRevision :: Maybe String
 gitRevision
+  | isJust fromFile = fromFile
   | fromGit == unknownFromGit = Nothing
   | otherwise = Just fromGit
  where
   -- Git revision found during compilation by running git. If
   -- git could not be run, then this will be "UNKNOWN".
   fromGit = $(GitRev.gitHash)
+
+  -- Git revision found in a file named `VERSION` at top-level of
+  -- project's directory.
+  fromFile = fmap (Text.unpack . decodeUtf8 @_ @ByteString) $(embedFileIfExists "VERSION")
 
 -- According to 'gitrev' docs, this is the default value returned on errors.
 unknownFromGit :: String
