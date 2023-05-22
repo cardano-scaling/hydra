1116049371fcea62887310080957a1c3b6ec3c56
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-05-22 08:45:51 +0200

    Add serialisation time to micro-benchmark

--- hydra-node/exe/micro-bench/Main.hs
+++ hydra-node/exe/micro-bench/Main.hs
@@ -1,41 +1,56 @@
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Hydra.Prelude
 
-import Criterion (bench, bgroup, whnf)
-import Criterion.Main (defaultMain)
-import Hydra.Ledger.Cardano ( Tx, cardanoLedger, genFixedSizeSequenceOfSimplePaymentTransactions )
-import Hydra.Ledger (Ledger (applyTransactions), ValidationError, ChainSlot (ChainSlot))
+import qualified Data.Aeson as Aeson
 import qualified Cardano.Ledger.BaseTypes as Ledger
 import qualified Cardano.Ledger.Shelley.Rules.Ledger as Ledger
 import qualified Cardano.Slotting.Time as Slotting
+import Criterion (bench, bgroup, whnf, nf)
+import Criterion.Main (defaultMain)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Hydra.Cardano.Api (
   ExecutionUnitPrices (ExecutionUnitPrices),
   LedgerEra,
-  ProtocolParameters (..), UTxO,
+  ProtocolParameters (..),
+  UTxO, serialiseToCBOR,
  )
+import Hydra.Ledger (ChainSlot (ChainSlot), Ledger (applyTransactions), ValidationError)
+import Hydra.Ledger.Cardano (Tx, cardanoLedger, genFixedSizeSequenceOfSimplePaymentTransactions)
 import Hydra.Ledger.Cardano.Configuration (newLedgerEnv)
 import Hydra.Ledger.Cardano.Evaluate (epochInfo, pparams)
 import Test.QuickCheck (generate)
+import Hydra.API.ClientInput (ClientInput(NewTx))
+import Data.Aeson ((.=), object, Value(String))
+import qualified Data.List as List
 
 main :: IO ()
 main = do
-    (utxo, txs) <- prepareTxs 10_000
-    defaultMain
-        [ bgroup "Cardano Ledger" [bench "Apply Tx" $ whnf benchApplyTxs (utxo, txs)]
+  (utxo, tx) <- prepareTx
+  let jsonNewTx = (Aeson.encode . NewTx) tx
+      toNewTx bs = object [ "tag" .= ("NewTx" :: Text),  "transaction" .= String (decodeUtf8 bs) ]
+      cborNewTx = (Aeson.encode . toNewTx . serialiseToCBOR) tx
+  defaultMain
+    [ bgroup
+        "Cardano Ledger"
+        [ bench "Apply Tx" $ whnf benchApplyTxs (utxo, tx)
+        , bench "Serialize NewTx (JSON)" $ nf (Aeson.encode . NewTx)  tx
+        , bench "Serialize NewTx (CBOR)" $ nf serialiseToCBOR tx
+        , bench "Deserialize NewTx (JSON)" $ whnf (Aeson.decode @(ClientInput Tx)) jsonNewTx
+        , bench "Deserialize NewTx (CBOR-in-JSON)" $ whnf (Aeson.decode @(ClientInput Tx)) cborNewTx
         ]
+    ]
 
-prepareTxs :: Int -> IO (UTxO, [Tx])
-prepareTxs =
-  generate . genFixedSizeSequenceOfSimplePaymentTransactions
+prepareTx :: IO (UTxO, Tx)
+prepareTx =
+  second List.head <$> generate (genFixedSizeSequenceOfSimplePaymentTransactions 1)
 
-
-benchApplyTxs :: (UTxO, [Tx]) -> Either (Tx, ValidationError) UTxO
-benchApplyTxs (utxo, txs) = applyTransactions defaultLedger (ChainSlot 1) utxo txs
+benchApplyTxs :: (UTxO, Tx) -> Either (Tx, ValidationError) UTxO
+benchApplyTxs (utxo, tx) = applyTransactions defaultLedger (ChainSlot 1) utxo [tx]
 
 defaultLedger :: Ledger Tx
 defaultLedger = cardanoLedger defaultGlobals defaultLedgerEnv
