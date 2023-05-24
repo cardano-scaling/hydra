6d405a7092779b3d44be402e33eef7570b400790
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-05-24 15:20:51 +0200

    Use eventId/effectId pair to dedup effects logged content

--- hydra-node/test/Hydra/Logging/MonitoringSpec.hs
+++ hydra-node/test/Hydra/Logging/MonitoringSpec.hs
@@ -18,7 +18,7 @@ import Hydra.Logging (nullTracer, traceWith)
 import Hydra.Logging.Messages (HydraLog (Node))
 import Hydra.Logging.Monitoring
 import Hydra.Network.Message (Message (ReqTx))
-import Hydra.Node (HydraNodeLog (BeginEvent, EndEffect))
+import Hydra.Node (HydraNodeLog (BeginEffect, BeginEvent))
 import Hydra.Snapshot (Snapshot (Snapshot))
 import Network.HTTP.Req (GET (..), NoReqBody (..), bsResponse, defaultHttpConfig, http, port, req, responseBody, runReq, (/:))
 import Test.Hydra.Fixture (alice)
@@ -33,7 +33,7 @@ spec =
         traceWith tracer (Node $ BeginEvent alice 0 (NetworkEvent defaultTTL (ReqTx alice (aValidTx 42))))
         traceWith tracer (Node $ BeginEvent alice 1 (NetworkEvent defaultTTL (ReqTx alice (aValidTx 43))))
         threadDelay 0.1
-        traceWith tracer (Node $ EndEffect alice (ClientEffect (SnapshotConfirmed testHeadId (Snapshot 1 (utxoRefs [1]) [aValidTx 43, aValidTx 42]) mempty)))
+        traceWith tracer (Node $ BeginEffect alice 0 0 (ClientEffect (SnapshotConfirmed testHeadId (Snapshot 1 (utxoRefs [1]) [aValidTx 43, aValidTx 42]) mempty)))
 
         metrics <-
           Text.lines . decodeUtf8 . responseBody
