a5bf22f875b2ede099577b0219f040110b06c638
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-06-07 12:08:26 +0200

    Introduce proper type for recording traces

--- hydra-cluster/src/Hydra/LogFilter.hs
+++ hydra-cluster/src/Hydra/LogFilter.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
 -- | Utility functions to filter and simplify raw logs.
 module Hydra.LogFilter where
 
-import Hydra.Prelude
+import Hydra.Prelude hiding (id)
 
 import Data.Aeson (Value, object, (.=))
 import qualified Data.Map as Map
@@ -9,29 +12,30 @@ import Hydra.API.ClientInput (ClientInput (NewTx))
 import Hydra.Cardano.Api (Tx)
 import Hydra.HeadLogic (Event (..))
 import Hydra.Ledger (IsTx (..))
-import Hydra.Logging (Envelope (..))
+import Hydra.Logging (Envelope (Envelope))
 import Hydra.Logging.Messages (HydraLog (Node))
-import Hydra.Network.Message (Message)
 import Hydra.Node (HydraNodeLog (BeginEvent, EndEvent))
 
-tracePerformance :: Envelope (HydraLog Tx (Message Tx)) -> State (Map Word64 (TxIdType Tx, UTCTime)) (Maybe Value)
+data TraceEvent = TraceEvent
+  { timestamp :: UTCTime
+  , id :: TxIdType Tx
+  , us :: NominalDiffTime
+  , event :: Text
+  }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToJSON)
+
+tracePerformance :: Envelope (HydraLog Tx (Message Tx)) -> State (Map Word64 TraceEvent) (Maybe TraceEvent)
 tracePerformance envelope = do
   pending <- get
   case envelope of
-    (Envelope ut _n _txt (Node (BeginEvent _pa eventID (ClientEvent (NewTx tx))))) -> do
-      put (Map.insert eventID (txId tx, ut) pending)
+    (Envelope timestamp _n _txt (Node (BeginEvent _pa eventID (ClientEvent (NewTx tx))))) -> do
+      put (Map.insert eventID (TraceEvent{event = "NewTx", timestamp, id = txId tx, us = 0}) pending)
       pure Nothing
     (Envelope ut _n _txt (Node (EndEvent _pa eventID))) ->
       case Map.lookup eventID pending of
-        Just (txid, begin) -> do
+        Just e -> do
           put $ Map.delete eventID pending
-          pure $
-            Just $
-              object
-                [ "timestamp" .= begin
-                , "id" .= txid
-                , "event" .= ("NewTx" :: Text)
-                , "us" .= (1_000_000 * diffUTCTime ut begin)
-                ]
+          pure $ Just $ e{us = 1_000_000 * diffUTCTime ut (timestamp e)}
         Nothing -> pure Nothing
     _ -> pure Nothing
