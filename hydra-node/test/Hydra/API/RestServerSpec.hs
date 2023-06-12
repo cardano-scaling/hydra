bb4c7c7ee884441de67562223baa3c3129386a75
Franco Testagrossa <franco.testagrossa@iohk.io>
2023-06-12 14:14:22 +0200

    Add plain old json schema validation for draft commit request

--- hydra-node/test/Hydra/API/RestServerSpec.hs
+++ hydra-node/test/Hydra/API/RestServerSpec.hs
@@ -5,10 +5,14 @@ module Hydra.API.RestServerSpec where
 import Hydra.Prelude
 import Test.Hydra.Prelude
 
+import Data.Aeson.Lens (key)
 import Hydra.API.RestServer (DraftCommitTxRequest, DraftCommitTxResponse)
 import Hydra.Chain.Direct.State ()
+import Hydra.JSONSchema (SpecificationSelector, prop_specIsComplete, prop_validateToJSON, withJsonSpecifications)
 import Hydra.Ledger.Cardano (Tx)
+import System.FilePath ((</>))
 import Test.Aeson.GenericSpecs (roundtripAndGoldenSpecs)
+import Test.QuickCheck.Property (conjoin, property, withMaxSuccess)
 
 spec :: Spec
 spec = parallel $ do
@@ -17,3 +21,15 @@ spec = parallel $ do
 
   roundtripAndGoldenSpecs
     (Proxy @(ReasonablySized (DraftCommitTxRequest Tx)))
+
+  aroundAll withJsonSpecifications $ do
+    specify "DraftCommitTxRequest" $ \dir -> do
+      property $
+        withMaxSuccess 1 $
+          conjoin
+            [ prop_validateToJSON @(ReasonablySized (DraftCommitTxRequest Tx)) (dir </> "api.yaml") "messages" (dir </> "DraftCommitTxRequest")
+            , prop_specIsComplete @(ReasonablySized (DraftCommitTxRequest Tx)) (dir </> "api.yaml") apiSpecificationSelector
+            ]
+
+apiSpecificationSelector :: SpecificationSelector
+apiSpecificationSelector = key "components" . key "message"
