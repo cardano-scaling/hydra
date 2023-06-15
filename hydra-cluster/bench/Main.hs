3e648d04ede62f05743ea9c9710522ac3eb4442a
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-06-15 18:03:22 +0200

    Can generate markdown benchmark results in a directory

--- hydra-cluster/bench/Main.hs
+++ hydra-cluster/bench/Main.hs
@@ -5,6 +5,7 @@ import Test.Hydra.Prelude
 
 import Bench.EndToEnd (Summary (..), bench)
 import Data.Aeson (eitherDecodeFileStrict', encodeFile)
+import Data.ByteString (hPut)
 import Hydra.Cardano.Api (
   ShelleyBasedEra (..),
   ShelleyGenesis (..),
@@ -31,11 +32,13 @@ import Options.Applicative (
 import System.Directory (createDirectory, doesDirectoryExist)
 import System.Environment (withArgs)
 import System.FilePath ((</>))
+import System.IO.Unsafe (unsafePerformIO)
 import Test.HUnit.Lang (HUnitFailure (..), formatFailureReason)
 import Test.QuickCheck (generate, getSize, scale)
 
 data Options = Options
   { workDirectory :: Maybe FilePath
+  , outputDirectory :: Maybe FilePath
   , scalingFactor :: Int
   , timeoutSeconds :: DiffTime
   , clusterSize :: Word64
@@ -54,6 +57,15 @@ benchOptionsParser =
                 \   'utxo.json' files, \
                 \ * If the directory does not exist, it will be created and \
                 \   populated with new transactions and UTxO set."
+          )
+      )
+    <*> optional
+      ( strOption
+          ( long "output-directory"
+              <> metavar "DIR"
+              <> help
+                "The directory where to output markdown-formatted benchmark results. \
+                \ If not set, raw text summary will be printed to the console. (default: none)"
           )
       )
     <*> option
@@ -103,10 +115,11 @@ main =
       if existsDir
         then replay o benchDir
         else createDirectory benchDir >> play o benchDir
-    o ->
-      createSystemTempDirectory "bench" >>= play o
+    o -> do
+      tmpDir <- createSystemTempDirectory "bench"
+      play o tmpDir
  where
-  play Options{scalingFactor, timeoutSeconds, clusterSize} benchDir = do
+  play options@Options{scalingFactor, clusterSize} benchDir = do
     numberOfTxs <- generate $ scale (* scalingFactor) getSize
     pparams <-
       eitherDecodeFileStrict' ("config" </> "devnet" </> "genesis-shelley.json") >>= \case
@@ -115,21 +128,21 @@ main =
           pure $ fromLedgerPParams ShelleyBasedEraShelley (sgProtocolParams shelleyGenesis)
     dataset <- generateConstantUTxODataset pparams (fromIntegral clusterSize) numberOfTxs
     saveDataset benchDir dataset
-    run timeoutSeconds benchDir dataset clusterSize
+    run options benchDir dataset
 
-  replay Options{timeoutSeconds, clusterSize} benchDir = do
+  replay options benchDir = do
     datasets <- either die pure =<< eitherDecodeFileStrict' (benchDir </> "dataset.json")
     putStrLn $ "Using UTxO and Transactions from: " <> benchDir
-    run timeoutSeconds benchDir datasets clusterSize
+    run options benchDir datasets
 
-  run timeoutSeconds benchDir datasets clusterSize = do
+  run options@Options{timeoutSeconds, clusterSize} benchDir datasets = do
     putStrLn $ "Test logs available in: " <> (benchDir </> "test.log")
     withArgs [] $
       try (bench timeoutSeconds benchDir datasets clusterSize) >>= \case
         Left (err :: HUnitFailure) ->
           benchmarkFailedWith benchDir err
         Right summary ->
-          benchmarkSucceeded benchDir summary
+          benchmarkSucceeded options benchDir summary
 
   saveDataset tmpDir dataset = do
     let txsFile = tmpDir </> "dataset.json"
@@ -142,8 +155,54 @@ benchmarkFailedWith benchDir (HUnitFailure _ reason) = do
   putStrLn $ "To re-run with same dataset, pass '--work-directory=" <> benchDir <> "' to the executable"
   exitFailure
 
-benchmarkSucceeded :: FilePath -> Summary -> IO ()
-benchmarkSucceeded _ Summary{numberOfTxs, averageConfirmationTime, percentBelow100ms} = do
-  putTextLn $ "Confirmed txs: " <> show numberOfTxs
-  putTextLn $ "Average confirmation time (ms): " <> show averageConfirmationTime
-  putTextLn $ "Confirmed below 100ms: " <> show percentBelow100ms <> "%"
+benchmarkSucceeded :: Options -> FilePath -> Summary -> IO ()
+benchmarkSucceeded Options{outputDirectory, clusterSize} _ Summary{numberOfTxs, averageConfirmationTime, percentBelow100ms} =
+  maybe dumpToStdout writeMarkdownReportTo outputDirectory
+ where
+  dumpToStdout = do
+    putTextLn $ "Confirmed txs: " <> show numberOfTxs
+    putTextLn $ "Average confirmation time (ms): " <> show averageConfirmationTime
+    putTextLn $ "Confirmed below 100ms: " <> show percentBelow100ms <> "%"
+
+  writeMarkdownReportTo outputDir = do
+    existsDir <- doesDirectoryExist outputDir
+    unless existsDir $ createDirectory outputDir
+    withFile (outputDir </> "end-to-end-benchmarks.md") WriteMode $ \hdl -> do
+      hPut hdl $ encodeUtf8 $ unlines pageHeader
+      hPut hdl $ encodeUtf8 $ unlines $ formattedSummary
+
+  pageHeader :: [Text]
+  pageHeader =
+    [ "--- "
+    , "sidebar_label: 'End-to-End Benchmarks' "
+    , "sidebar_position: 4 "
+    , "--- "
+    , ""
+    , "# End-To-End Benchmark Results "
+    , ""
+    , "This page is intended to collect the latest end-to-end benchmarks \
+      \ results produced by Hydra's Continuous Integration system from \
+      \ the latest `master` code. Please take those results with a grain of \
+      \ salt as they are produced from basic cloud VMs and not controlled \
+      \ hardware. Instead of focusing on the _absolute_ results, the emphasis \
+      \ should be on relative results, eg. how the timings for a scenario \
+      \ evolve as the code changes."
+    , ""
+    , "_Generated at_  " <> show now
+    , ""
+    ]
+
+  {-# NOINLINE now #-}
+  now :: UTCTime
+  now = unsafePerformIO getCurrentTime
+
+  summaryTitle = "Benchmark (nodes=" <> show clusterSize <> ")"
+
+  formattedSummary :: [Text]
+  formattedSummary =
+    [ "## " <> summaryTitle
+    , ""
+    , "| _Number of txs_ | " <> show numberOfTxs <> " |"
+    , "| _Avg. Confirmation Time (ms)_ | " <> show averageConfirmationTime <> " |"
+    , "| _Share of Txs (%) < 100ms_ | " <> show percentBelow100ms <> " |"
+    ]
