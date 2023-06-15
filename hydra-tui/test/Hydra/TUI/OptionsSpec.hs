565cc2eff616924ef63306233bf3a843a3220a49
Pascal Grange <pascal@grange.nom.fr>
2023-06-15 10:31:08 +0200

    Show version in tui

--- hydra-tui/test/Hydra/TUI/OptionsSpec.hs
+++ hydra-tui/test/Hydra/TUI/OptionsSpec.hs
@@ -11,24 +11,33 @@ import Hydra.TUI.Options (
  )
 import Options.Applicative (
   Parser,
-  ParserResult (Success),
+  ParserResult (Failure, Success),
   defaultPrefs,
   execParserPure,
   info,
+  renderFailure,
  )
+import Text.Regex.TDFA ((=~))
 
 spec :: Spec
 spec = parallel $ do
-  it "parses --connect option" $ do
-    shouldParseWith parseOptions ["--connect", "127.0.0.2:4002"] defaultOptions{hydraNodeHost = Host "127.0.0.2" 4002}
   it "no arguments yield default options" $ do
     shouldParseWith parseOptions [] defaultOptions
+  it "parses --connect option" $ do
+    shouldParseWith parseOptions ["--connect", "127.0.0.2:4002"] defaultOptions{hydraNodeHost = Host "127.0.0.2" 4002}
   it "parses --testnet-magic option" $ do
     shouldParseWith parseOptions ["--testnet-magic", "123"] defaultOptions{cardanoNetworkId = Testnet $ NetworkMagic 123}
   it "parses --cardano-signing-key option" $ do
     shouldParseWith parseOptions ["--cardano-signing-key", "foo.sk"] defaultOptions{cardanoSigningKey = "foo.sk"}
   it "parses --node-socket option" $ do
     shouldParseWith parseOptions ["--node-socket", "something.socket"] defaultOptions{cardanoNodeSocket = "something.socket"}
+  it "parses --version option" $ do
+    case execParserPure defaultPrefs (info parseOptions mempty) ["--version"] of
+      Failure theFailure ->
+        let (version, _exitCode) = renderFailure theFailure "test"
+         in version
+              `shouldSatisfy` (=~ ("[0-9]+\\.[0-9]+\\.[0-9]+(:?-[a-zA-Z0-9]+)" :: String))
+      _ -> failure "expected a version but did get something else"
 
 defaultOptions :: Options
 defaultOptions =
