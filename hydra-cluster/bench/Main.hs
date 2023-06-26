1fb6e2c565010e78b086210ee8d4d69a39f409da
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-06-26 22:39:05 +0200

    Add slight delay between each bench
    ... in the hope it will allow OS to clean resources and prevent
    spurious errors across different scenarios

--- hydra-cluster/bench/Main.hs
+++ hydra-cluster/bench/Main.hs
@@ -20,7 +20,7 @@ import Hydra.Generator (generateConstantUTxODataset)
 import Options.Applicative (
   execParser,
  )
-import System.Directory (createDirectory, doesDirectoryExist, createDirectoryIfMissing)
+import System.Directory (createDirectory, createDirectoryIfMissing, doesDirectoryExist)
 import System.Environment (withArgs)
 import System.FilePath ((</>))
 import Test.HUnit.Lang (HUnitFailure (..), formatFailureReason)
@@ -29,21 +29,20 @@ import Test.QuickCheck (generate, getSize, scale)
 main :: IO ()
 main =
   execParser benchOptionsParser >>= \case
-    StandaloneOptions{workDirectory = Just benchDir,  outputDirectory, timeoutSeconds, startingNodeId, scalingFactor, clusterSize } -> do
+    StandaloneOptions{workDirectory = Just benchDir, outputDirectory, timeoutSeconds, startingNodeId, scalingFactor, clusterSize} -> do
       existsDir <- doesDirectoryExist benchDir
       if existsDir
         then replay outputDirectory timeoutSeconds startingNodeId benchDir
         else createDirectory benchDir >> play outputDirectory timeoutSeconds scalingFactor clusterSize startingNodeId benchDir
-    StandaloneOptions{workDirectory = Nothing,  outputDirectory, timeoutSeconds, scalingFactor, clusterSize, startingNodeId} -> do
+    StandaloneOptions{workDirectory = Nothing, outputDirectory, timeoutSeconds, scalingFactor, clusterSize, startingNodeId} -> do
       tmpDir <- createSystemTempDirectory "bench"
-      play  outputDirectory timeoutSeconds scalingFactor clusterSize  startingNodeId tmpDir
-    DatasetOptions{datasetFiles,  outputDirectory, timeoutSeconds, startingNodeId} -> do
+      play outputDirectory timeoutSeconds scalingFactor clusterSize startingNodeId tmpDir
+    DatasetOptions{datasetFiles, outputDirectory, timeoutSeconds, startingNodeId} -> do
       benchDir <- createSystemTempDirectory "bench"
       datasets <- mapM (eitherDecodeFileStrict' >=> either die pure) datasetFiles
       let targets = zip datasets $ (benchDir </>) . show <$> [1 .. length datasets]
       forM_ (snd <$> targets) (createDirectoryIfMissing True)
       run outputDirectory timeoutSeconds startingNodeId targets
-
  where
   play outputDirectory timeoutSeconds scalingFactor clusterSize startingNodeId benchDir = do
     numberOfTxs <- generate $ scale (* scalingFactor) getSize
@@ -59,19 +58,21 @@ main =
   replay outputDirectory timeoutSeconds startingNodeId benchDir = do
     dataset <- either die pure =<< eitherDecodeFileStrict' (benchDir </> "dataset.json")
     putStrLn $ "Using UTxO and Transactions from: " <> benchDir
-    run outputDirectory timeoutSeconds startingNodeId  [(dataset, benchDir)]
+    run outputDirectory timeoutSeconds startingNodeId [(dataset, benchDir)]
 
   run outputDirectory timeoutSeconds startingNodeId targets = do
     results <- forM targets $ \(dataset, dir) -> do
       putStrLn $ "Test logs available in: " <> (dir </> "test.log")
-      withArgs [] $
+      withArgs [] $ do
+        -- XXX: Wait between each bench run to give the OS time to cleanup resources??
+        threadDelay 10
         try @_ @HUnitFailure (bench startingNodeId timeoutSeconds dir dataset) >>= \case
-          Left exc ->  pure $ Left (dataset, dir, exc)
+          Left exc -> pure $ Left (dataset, dir, exc)
           Right summary -> pure $ Right summary
     let (failures, summaries) = partitionEithers results
     case failures of
       [] -> benchmarkSucceeded outputDirectory summaries
-      errs -> mapM_ (\ (_, dir, exc) -> benchmarkFailedWith dir exc) errs >> exitFailure
+      errs -> mapM_ (\(_, dir, exc) -> benchmarkFailedWith dir exc) errs >> exitFailure
 
   saveDataset tmpDir dataset = do
     let txsFile = tmpDir </> "dataset.json"
