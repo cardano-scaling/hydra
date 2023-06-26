f2594daff9507bcccf610d4e86654569e14a9d2c
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-06-26 15:47:57 +0200

    Document possible responses
    Also remove tests that were not covering all cases properly

--- hydra-node/test/Hydra/API/RestServerSpec.hs
+++ hydra-node/test/Hydra/API/RestServerSpec.hs
@@ -8,10 +8,9 @@ import Test.Hydra.Prelude
 import Data.Aeson.Lens (key)
 import Hydra.API.RestServer (DraftCommitTxRequest, DraftCommitTxResponse)
 import Hydra.Chain.Direct.State ()
-import Hydra.JSONSchema (SpecificationSelector, prop_specIsComplete, prop_validateJSONSchema)
-import Hydra.Ledger.Cardano (Tx)
+import Hydra.JSONSchema (prop_validateJSONSchema)
 import Test.Aeson.GenericSpecs (roundtripAndGoldenSpecs)
-import Test.QuickCheck.Property (conjoin, property, withMaxSuccess)
+import Test.QuickCheck.Property (property, withMaxSuccess)
 
 spec :: Spec
 spec = parallel $ do
@@ -23,18 +22,10 @@ spec = parallel $ do
 
   prop "Validate /commit publish api schema" $
     property $
-      withMaxSuccess 1 $ do
-        conjoin
-          [ prop_validateJSONSchema @DraftCommitTxRequest "api.json" (key "components" . key "messages")
-          ]
+      withMaxSuccess 1 $
+        prop_validateJSONSchema @DraftCommitTxRequest "api.json" (key "components" . key "messages")
 
   prop "Validate /commit subscribe api schema" $
     property $
-      withMaxSuccess 1 $ do
-        conjoin
-          [ prop_validateJSONSchema @DraftCommitTxResponse "api.json" (key "channels" . key "/commit" . key "subscribe")
-          , prop_specIsComplete @DraftCommitTxResponse "api.json" apiSpecificationSelector
-          ]
-
-apiSpecificationSelector :: SpecificationSelector
-apiSpecificationSelector = key "components" . key "messages"
+      withMaxSuccess 1 $
+        prop_validateJSONSchema @DraftCommitTxResponse "api.json" (key "channels" . key "/commit" . key "subscribe")
