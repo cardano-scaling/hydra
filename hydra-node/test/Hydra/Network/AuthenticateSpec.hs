adb61ed9721d7d46c720337866c56519092f8654
Pascal Grange <pascal@grange.nom.fr>
2023-07-03 15:30:02 +0200

    WIP

--- hydra-node/test/Hydra/Network/AuthenticateSpec.hs
+++ hydra-node/test/Hydra/Network/AuthenticateSpec.hs
@@ -6,7 +6,9 @@ import Test.Hydra.Prelude
 import Control.Concurrent.Class.MonadSTM (MonadSTM (readTVarIO), modifyTVar', newTVarIO)
 import Control.Monad.IOSim (IOSim, runSimOrThrow)
 import Hydra.Network (Network (..), NetworkComponent, NodeId (..))
+import Hydra.Network.HeartbeatSpec (noop)
 import Test.Hydra.Fixture (alice, bob)
+import Hydra.Crypto (Signature)
 
 spec :: Spec
 spec = parallel $ do
@@ -20,7 +22,7 @@ spec = parallel $ do
 
       otherNodeId = NodeId "node_id-2"
 
-  fit "pass the messages around" $ do
+  fit "pass the authenticated messages around" $ do
     let receivedMsgs = runSimOrThrow $ do
           receivedMessages <- newTVarIO ([] :: [Integer])
 
@@ -31,7 +33,33 @@ spec = parallel $ do
 
     receivedMsgs `shouldBe` [1]
 
-newtype Authenticated msg = Authenticated {payload :: msg}
+  fit "drop unauthenticated messages" $ do
+    let receivedMsgs = runSimOrThrow $ do
+          receivedMessages <- newTVarIO ([] :: [Integer])
+
+          withAuthentication nodeId (\incoming _ -> incoming (Authenticated 1) >> incoming (Authenticated 2)) (captureIncoming receivedMessages) $ \_ ->
+            threadDelay 1
+
+          readTVarIO receivedMessages
+
+    receivedMsgs `shouldBe` [1]
+
+  fit "authenticate the message to broadcast" $ do
+    let someMessage = 1
+        sentMsgs = runSimOrThrow $ do
+          sentMessages <- newTVarIO ([] :: [Authenticated Integer])
+
+          withAuthentication nodeId (captureOutgoing sentMessages) noop $ \Network{broadcast} -> do
+            threadDelay 0.6
+            broadcast someMessage
+            threadDelay 1
+
+          readTVarIO sentMessages
+
+    sentMsgs `shouldBe` [Authenticated 1]
+
+
+data Authenticated msg = Authenticated {payload :: msg, signature :: Signature msg} deriving (Eq, Show)
 
 withAuthentication ::
   ( MonadAsync m
@@ -42,8 +70,10 @@ withAuthentication ::
   NetworkComponent m (Authenticated msg) a ->
   NetworkComponent m msg a
 withAuthentication nodeId withRawNetwork callback action = do
-  withRawNetwork unpack pack
+  withRawNetwork verify sign
  where
-  unpack (Authenticated msg) = callback msg
-  pack = \rawNetwork ->
-    undefined
+  verify (Authenticated msg _sig) = callback msg
+  sign = action . contramap Authenticated
+
+
+
