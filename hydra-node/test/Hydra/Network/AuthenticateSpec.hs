fdbdb0c96983dd210a1732929d5fd454f3b6bad6
Franco Testagrossa <franco.testagrossa@iohk.io>
2023-07-03 16:24:50 +0200

    Introduce signatures for Authenticated messages

--- hydra-node/test/Hydra/Network/AuthenticateSpec.hs
+++ hydra-node/test/Hydra/Network/AuthenticateSpec.hs
@@ -3,12 +3,14 @@ module Hydra.Network.AuthenticateSpec where
 import Hydra.Prelude
 import Test.Hydra.Prelude
 
+import Cardano.Crypto.Util (SignableRepresentation)
 import Control.Concurrent.Class.MonadSTM (MonadSTM (readTVarIO), modifyTVar', newTVarIO)
 import Control.Monad.IOSim (IOSim, runSimOrThrow)
+import Hydra.Crypto (Signature, sign, verify)
 import Hydra.Network (Network (..), NetworkComponent, NodeId (..))
 import Hydra.Network.HeartbeatSpec (noop)
-import Test.Hydra.Fixture (alice, bob)
-import Hydra.Crypto (Signature)
+import Hydra.Party (Party(..))
+import Test.Hydra.Fixture (alice, aliceSk, bob, bobSk)
 
 spec :: Spec
 spec = parallel $ do
@@ -18,62 +20,75 @@ spec = parallel $ do
       captureIncoming receivedMessages msg =
         atomically $ modifyTVar' receivedMessages (msg :)
 
-      nodeId = NodeId "node_id-1"
-
-      otherNodeId = NodeId "node_id-2"
-
   fit "pass the authenticated messages around" $ do
     let receivedMsgs = runSimOrThrow $ do
-          receivedMessages <- newTVarIO ([] :: [Integer])
+          receivedMessages <- newTVarIO ([] :: [ByteString])
 
-          withAuthentication nodeId (\incoming _ -> incoming (Authenticated 1)) (captureIncoming receivedMessages) $ \_ ->
-            threadDelay 1
+          withAuthentication
+            [bob]
+            ( \incoming _ -> do
+                incoming (Authenticated "1" (sign bobSk "1"))
+            )
+            (captureIncoming receivedMessages)
+            $ \_ ->
+              threadDelay 1
 
           readTVarIO receivedMessages
 
-    receivedMsgs `shouldBe` [1]
+    receivedMsgs `shouldBe` ["1"]
 
   fit "drop unauthenticated messages" $ do
     let receivedMsgs = runSimOrThrow $ do
-          receivedMessages <- newTVarIO ([] :: [Integer])
+          receivedMessages <- newTVarIO ([] :: [ByteString])
 
-          withAuthentication nodeId (\incoming _ -> incoming (Authenticated 1) >> incoming (Authenticated 2)) (captureIncoming receivedMessages) $ \_ ->
-            threadDelay 1
+          withAuthentication
+            [bob]
+            ( \incoming _ -> do
+                incoming (Authenticated "1" (sign bobSk "1"))
+                incoming (Authenticated "2" (sign aliceSk "1"))
+            )
+            (captureIncoming receivedMessages)
+            $ \_ ->
+              threadDelay 1
 
           readTVarIO receivedMessages
 
-    receivedMsgs `shouldBe` [1]
+    receivedMsgs `shouldBe` ["1"]
 
   fit "authenticate the message to broadcast" $ do
-    let someMessage = 1
+    let someMessage = "1"
         sentMsgs = runSimOrThrow $ do
-          sentMessages <- newTVarIO ([] :: [Authenticated Integer])
+          sentMessages <- newTVarIO ([] :: [Authenticated ByteString])
 
-          withAuthentication nodeId (captureOutgoing sentMessages) noop $ \Network{broadcast} -> do
+          withAuthentication [] (captureOutgoing sentMessages) noop $ \Network{broadcast} -> do
             threadDelay 0.6
             broadcast someMessage
             threadDelay 1
 
           readTVarIO sentMessages
 
-    sentMsgs `shouldBe` [Authenticated 1]
+    sentMsgs `shouldBe` [Authenticated "1" (sign bobSk "1")]
 
-
-data Authenticated msg = Authenticated {payload :: msg, signature :: Signature msg} deriving (Eq, Show)
+data Authenticated msg = Authenticated
+  { payload :: msg
+  , signature :: Signature msg
+  }
+  deriving (Eq, Show)
 
 withAuthentication ::
   ( MonadAsync m
   , MonadDelay m
   , MonadMonotonicTime m
+  , SignableRepresentation msg
   ) =>
-  NodeId ->
+  [Party] ->
   NetworkComponent m (Authenticated msg) a ->
   NetworkComponent m msg a
-withAuthentication nodeId withRawNetwork callback action = do
-  withRawNetwork verify sign
+withAuthentication parties withRawNetwork callback action = do
+  withRawNetwork checkSignature authenticate
  where
-  verify (Authenticated msg _sig) = callback msg
-  sign = action . contramap Authenticated
-
-
-
+  checkSignature (Authenticated msg sig) = do
+    when (any (\Party{vkey} -> verify vkey sig msg) parties) $
+      callback msg
+  authenticate = \Network{broadcast} ->
+    action $ Network{broadcast = \msg -> broadcast (Authenticated msg (sign bobSk msg))}
