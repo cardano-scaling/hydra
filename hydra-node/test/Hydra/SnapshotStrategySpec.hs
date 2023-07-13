7577d6ccf090dcfa5b488a7b5009a235c87e008b
Pascal Grange <pascal@grange.nom.fr>
2023-07-13 15:11:35 +0200

    Adapat SnapshotStrategySpec

--- hydra-node/test/Hydra/SnapshotStrategySpec.hs
+++ hydra-node/test/Hydra/SnapshotStrategySpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Hydra.SnapshotStrategySpec where
 
@@ -11,25 +12,28 @@ import Hydra.HeadLogic (
   CoordinatedHeadState (..),
   Effect (..),
   Environment (..),
+  Event (NetworkEvent),
   NoSnapshotReason (..),
   Outcome (..),
   SeenSnapshot (..),
   SnapshotOutcome (..),
+  collectEffects,
+  defaultTTL,
   emitSnapshot,
   isLeader,
   newSn,
+  update,
  )
 import Hydra.HeadLogicSpec (inOpenState')
-import Hydra.Ledger (Ledger (..))
+import Hydra.Ledger (Ledger (..), txId)
 import Hydra.Ledger.Simple (SimpleTx (..), aValidTx, simpleLedger)
 import Hydra.Network.Message (Message (..))
 import Hydra.Options (defaultContestationPeriod)
 import Hydra.Party (deriveParty)
 import Hydra.Snapshot (ConfirmedSnapshot (..), Snapshot (..), getSnapshot)
 import Test.Hydra.Fixture (alice, aliceSk, bob, bobSk, carol, cperiod)
-import Test.QuickCheck (Property, counterexample, forAll, label, oneof, (===), (==>))
+import Test.QuickCheck (Property, counterexample, forAll, oneof, (==>))
 import Test.QuickCheck.Monadic (monadicST, pick)
-import qualified Prelude
 
 spec :: Spec
 spec = do
@@ -63,6 +67,7 @@ spec = do
         newSn (envFor aliceSk) params st `shouldBe` ShouldSnapshot 1 [tx]
 
       prop "always ReqSn given head has 1 member and there's a seen tx" prop_singleMemberHeadAlwaysSnapshot
+      -- prop "given head has 1 member, always ReqSn after confirming a snapshot " prop_singleMemberHeadAlwaysSnapshotOnAckSn
 
       prop "there's always a leader for every snapshot number" prop_thereIsAlwaysALeader
 
@@ -102,31 +107,32 @@ prop_singleMemberHeadAlwaysSnapshot sn = monadicST $ do
         [ pure NoSeenSnapshot
         , LastSeenSnapshot <$> arbitrary
         ]
-  let tx = aValidTx 1
-      aliceEnv =
-        let party = alice
-         in Environment
-              { party
-              , signingKey = aliceSk
-              , otherParties = []
-              , contestationPeriod = defaultContestationPeriod
-              }
-      st =
-        CoordinatedHeadState
-          { seenUTxO = mempty
-          , allTxs = mempty
-          , seenTxs = [tx]
-          , confirmedSnapshot = sn
-          , seenSnapshot
-          }
-      params = HeadParameters cperiod [alice]
-      decision = newSn aliceEnv params st
-      Snapshot{number} = getSnapshot sn
+  tx <- pick $ aValidTx <$> arbitrary
+  let
+    aliceEnv =
+      let party = alice
+       in Environment
+            { party
+            , signingKey = aliceSk
+            , otherParties = []
+            , contestationPeriod = defaultContestationPeriod
+            }
+    st =
+      CoordinatedHeadState
+        { seenUTxO = mempty
+        , allTxs = mempty
+        , seenTxs = []
+        , confirmedSnapshot = sn
+        , seenSnapshot
+        }
+    outcome = update aliceEnv simpleLedger (inOpenState' [alice] st) $ NetworkEvent defaultTTL alice $ ReqTx tx
+    Snapshot{number = confirmedSn} = getSnapshot sn
+    nextSn = confirmedSn + 1
   pure $
-    decision
-      === ShouldSnapshot (succ number) [tx]
-      & counterexample ("decision: " <> show decision)
-      & label (Prelude.head . Prelude.words . show $ sn)
+    ( collectEffects outcome
+        `shouldContain` [NetworkEffect (ReqSn nextSn [txId tx])]
+    )
+      & counterexample (show outcome)
 
 prop_thereIsAlwaysALeader :: Property
 prop_thereIsAlwaysALeader =
