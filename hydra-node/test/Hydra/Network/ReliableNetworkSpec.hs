ec934dda43f42921e6dc25c6cb0b9bb7453a9531
Pascal Grange <pascal@grange.nom.fr>
2023-09-05 15:43:28 +0200

    Better naming

--- hydra-node/test/Hydra/Network/ReliableNetworkSpec.hs
+++ hydra-node/test/Hydra/Network/ReliableNetworkSpec.hs
@@ -5,60 +5,62 @@ module Hydra.Network.ReliableNetworkSpec where
 
 import Hydra.Prelude hiding (Any, label)
 
+import Control.Monad.IOSim (IOSim)
 import Test.Hspec (Spec)
 import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck (Property, Testable (property))
 import Test.QuickCheck.DynamicLogic (DL, DynLogicModel, anyActions_, forAllDL)
-import Test.QuickCheck.Monadic (assert, PropertyM)
+import Test.QuickCheck.Monadic (PropertyM, assert)
 import Test.QuickCheck.StateModel (Action, Actions, Any (Some), LookUp, Realized, RunModel (..), StateModel (..), VarContext, runActions)
 import Test.QuickCheck.StateModel.Variables (HasVariables (..))
-import Control.Monad.IOSim (IOSim)
 
 spec :: Spec
 spec = do
   prop "State between nodes is eventually consistent" prop_eventuallyConsistentState
 
-data ClusterState = ClusterState
+data ClusterModel = ClusterModel
   deriving (Show, Eq, Generic)
 
-instance DynLogicModel ClusterState
-instance HasVariables (Action ClusterState a) where
+data Cluster = Cluster
+
+instance DynLogicModel ClusterModel
+instance HasVariables (Action ClusterModel a) where
   getAllVariables = mempty
-deriving instance Show (Action ClusterState a)
-deriving instance Eq (Action ClusterState a)
+deriving instance Show (Action ClusterModel a)
+deriving instance Eq (Action ClusterModel a)
 
-newtype OurMonad m a = OurMonad {ourMonad :: ReaderT ClusterState m a}
-  deriving (Functor, Applicative, Monad, MonadReader ClusterState)
+newtype RunMonad m a = RunMonad {runMonad :: ReaderT Cluster m a}
+  deriving (Functor, Applicative, Monad, MonadReader Cluster)
 
-type instance Realized (OurMonad m) a = a
+type instance Realized (RunMonad m) a = a
 
-instance Monad m => RunModel ClusterState (OurMonad m) where
+instance Monad m => RunModel ClusterModel (RunMonad m) where
   perform st action _ = case action of
     Noop -> pure ()
 
-instance StateModel ClusterState where
-  data Action ClusterState a where
-    Noop :: Action ClusterState ()
+instance StateModel ClusterModel where
+  data Action ClusterModel a where
+    Noop :: Action ClusterModel ()
 
-  arbitraryAction :: VarContext -> ClusterState -> Gen (Any (Action ClusterState))
+  arbitraryAction :: VarContext -> ClusterModel -> Gen (Any (Action ClusterModel))
   arbitraryAction _ _ = pure $ Some Noop
 
-  initialState = ClusterState
+  initialState = ClusterModel
 
-runNetworkActions :: Actions ClusterState -> Property
+runNetworkActions :: Actions ClusterModel -> Property
 runNetworkActions actions = property $
   runIOSimProp $ do
     _ <- runActions actions
     assert True
 
 -- | Specialised runner similar to <runSTGen https://hackage.haskell.org/package/QuickCheck-2.14.2/docs/src/Test.QuickCheck.Monadic.html#runSTGen>.
-runIOSimProp :: Testable a => (forall s. PropertyM (OurMonad (IOSim s)) a) -> Gen Property
+runIOSimProp :: Testable a => (forall s. PropertyM (RunMonad (IOSim s)) a) -> Gen Property
 runIOSimProp p = undefined
 
 prop_eventuallyConsistentState :: Property
 prop_eventuallyConsistentState =
   forAllDL stateIsEventuallyConsistent runNetworkActions
 
-stateIsEventuallyConsistent :: DL ClusterState ()
+stateIsEventuallyConsistent :: DL ClusterModel ()
 stateIsEventuallyConsistent = do
   anyActions_
