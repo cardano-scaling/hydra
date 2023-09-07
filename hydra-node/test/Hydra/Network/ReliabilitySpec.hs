58a9915cc63f8369ea55f1bdd7bcbb698e1abfdf
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-09-07 14:44:49 +0200

    Introduce message id for reliability layer

--- hydra-node/test/Hydra/Network/ReliabilitySpec.hs
+++ hydra-node/test/Hydra/Network/ReliabilitySpec.hs
@@ -6,26 +6,29 @@ import Hydra.Prelude
 import Test.Hydra.Prelude
 
 import Control.Concurrent.Class.MonadSTM (MonadSTM (readTQueue, readTVarIO, writeTQueue), modifyTVar', newTQueueIO, newTVarIO)
+import Control.Concurrent.Class.MonadSTM.TVar (modifyTVar)
 import Control.Monad.IOSim (runSimOrThrow)
+import Data.Sequence ((|>))
 import Hydra.Network (Network (..), NetworkComponent)
 import Test.QuickCheck (generate)
 
 spec :: Spec
 spec = parallel $ do
   let captureOutgoing msgqueue _cb action =
-        action $ Network{broadcast = \msg -> atomically $ modifyTVar' msgqueue (msg :)}
+        action $ Network{broadcast = \msg -> atomically $ modifyTVar' msgqueue (|> msg)}
 
       captureIncoming receivedMessages msg =
         atomically $ modifyTVar' receivedMessages (msg :)
 
   msg <- runIO $ generate @String arbitrary
+
   it "forward received messages" $ do
     let receivedMsgs = runSimOrThrow $ do
           receivedMessages <- newTVarIO []
 
           withReliability
             ( \incoming _ -> do
-                incoming msg
+                incoming (Msg 1 msg)
             )
             (captureIncoming receivedMessages)
             $ \_ ->
@@ -35,23 +38,24 @@ spec = parallel $ do
 
     receivedMsgs `shouldBe` [msg]
 
-  it "broadcast messages to the network" $ do
+  prop "broadcast messages to the network assigning a sequential id" $ \(messages :: [String]) ->
     let sentMsgs = runSimOrThrow $ do
-          sentMessages <- newTVarIO []
+          sentMessages <- newTVarIO mempty
 
           withReliability (captureOutgoing sentMessages) noop $ \Network{broadcast} -> do
-            broadcast msg
+            mapM_ broadcast messages
 
-          readTVarIO sentMessages
-
-    sentMsgs `shouldBe` [msg]
+          toList <$> readTVarIO sentMessages
+     in messageId <$> sentMsgs `shouldBe` [1 .. (length messages)]
 
   it "broadcasts messages to single connected peer" $ do
     let receivedMsgs = runSimOrThrow $ do
           receivedMessages <- newTVarIO []
           queue <- newTQueueIO
+
           let aliceNetwork _ action = do
                 action (Network{broadcast = atomically . writeTQueue queue})
+
           let bobNetwork callback action = do
                 withAsync
                   ( forever $ do
@@ -60,23 +64,41 @@ spec = parallel $ do
                   )
                   $ \_ ->
                     action (Network{broadcast = const $ pure ()})
+
           withReliability aliceNetwork (const $ pure ()) $ \Network{broadcast} ->
             withReliability bobNetwork (captureIncoming receivedMessages) $ \_ -> do
               broadcast msg
               threadDelay 1
+
           readTVarIO receivedMessages
 
     receivedMsgs `shouldBe` [msg]
 
+data Msg msg = Msg
+  { messageId :: Int
+  , message :: msg
+  }
+  deriving (Eq, Show)
+
 withReliability ::
-  NetworkComponent m msg a ->
+  (MonadSTM m) =>
+  NetworkComponent m (Msg msg) a ->
   NetworkComponent m msg a
 withReliability withRawNetwork callback action = do
-  withRawNetwork dummyCallback dummyBroadcast
+  counter <- newTVarIO 0
+  withRawNetwork dummyCallback (dummyBroadcast counter)
  where
-  dummyCallback msg = callback msg
-  dummyBroadcast Network{broadcast} =
-    action $ Network{broadcast = \msg -> broadcast msg}
+  dummyCallback (Msg _ msg) = callback msg
+  dummyBroadcast messageCounter Network{broadcast} =
+    action $
+      Network
+        { broadcast = \msg -> do
+            counter <- atomically $ do
+              modifyTVar messageCounter (+ 1)
+              readTVar messageCounter
+
+            broadcast (Msg counter msg)
+        }
 
 noop :: Monad m => b -> m ()
 noop = const $ pure ()
