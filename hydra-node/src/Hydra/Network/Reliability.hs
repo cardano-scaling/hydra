83b3811109a2468c042fa6568ddc012140aec77a
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-09-12 12:36:01 +0200

    Use Vectors instead of Lists
    To be more performant let's use Vectors in the Msg type
    as well as trace messages.

--- hydra-node/src/Hydra/Network/Reliability.hs
+++ hydra-node/src/Hydra/Network/Reliability.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 -- | A `Network` layer that guarantees delivery of `msg` in order even in the
 -- face of transient connection failures.
 --
@@ -32,7 +34,7 @@
 -- before communicating with the applicative layer.
 module Hydra.Network.Reliability where
 
-import Hydra.Prelude
+import Hydra.Prelude hiding (fromList, replicate, zipWith)
 
 import Cardano.Binary (serialize')
 import Cardano.Crypto.Util (SignableRepresentation (getSignableRepresentation))
@@ -47,13 +49,15 @@ import Control.Tracer (Tracer)
 import qualified Data.List as List
 import Data.Maybe (fromJust)
 import Data.Sequence ((!?), (|>))
+import Data.Vector (Vector, fromList, generate, replicate, zipWith, (!))
 import Hydra.Logging (traceWith)
 import Hydra.Network (Network (..), NetworkComponent)
 import Hydra.Network.Authenticate (Authenticated (..))
 import Hydra.Party (Party)
+import Test.QuickCheck (getPositive, listOf)
 
 data Msg msg = Msg
-  { messageId :: [Int]
+  { messageId :: Vector Int
   , message :: msg
   }
   deriving stock (Eq, Show, Generic)
@@ -92,7 +96,7 @@ withReliability tracer us allParties withRawNetwork callback action = do
             counter <- atomically $ do
               acks <- readTVar messageCounter
               let ourIndex = fromJust $ List.elemIndex us allParties
-              let newAcks = zipWith (\ack i -> if i == ourIndex then ack + 1 else ack) acks [0 ..]
+              let newAcks = zipWith (\ack i -> if i == ourIndex then ack + 1 else ack) acks partyIndexes
               writeTVar messageCounter newAcks
               modifyTVar' sentMessages (|> msg)
               readTVar messageCounter
@@ -101,49 +105,56 @@ withReliability tracer us allParties withRawNetwork callback action = do
 
             broadcast $ Authenticated (Msg counter msg) us
         }
+  partyIndexes = generate (length allParties) id
 
-  reliableCallback broadcastCounter sentMessages resend (Authenticated (Msg acks msg) party) = do
-    let partyIndex = fromJust $ List.elemIndex party allParties
-    let n = acks List.!! partyIndex
-    counter <- readTVarIO broadcastCounter
-    let count = (List.!! partyIndex) counter
+  reliableCallback broadcastCounter sentMessages resend (Authenticated (Msg acks msg) party) =
+    if length acks /= length allParties
+      then error "Expected acks from a Msg is not the same length as the party list."
+      else do
+        let partyIndex = fromJust $ List.elemIndex party allParties
+        let n = acks ! partyIndex
+        counter <- readTVarIO broadcastCounter
+        let count = (! partyIndex) counter
 
-    -- handle message from party iff it's next in line
-    when (n == count + 1) $ do
-      let newAcks = zipWith (\ack i -> if i == partyIndex then ack + 1 else ack) counter [0 ..]
-      atomically $ writeTVar broadcastCounter newAcks
-      callback (Authenticated msg party)
+        -- handle message from party iff it's next in line
+        when (n == count + 1) $ do
+          let newAcks = zipWith (\ack i -> if i == partyIndex then ack + 1 else ack) counter partyIndexes
+          atomically $ writeTVar broadcastCounter newAcks
+          callback (Authenticated msg party)
 
-    -- resend messages if party did not acknowledge our latest idx
-    let myIndex = fromJust $ List.elemIndex us allParties
-    let acked = acks List.!! myIndex
-    let latestMsg = (List.!! myIndex) counter
-    when (acked < latestMsg) $ do
-      let missing = [acked + 1 .. latestMsg]
-      traceWith tracer (Resending missing acks counter party)
-      atomically $ do
-        messages <- readTVar sentMessages
-        forM_ missing $ \idx -> do
-          case messages !? (idx - 1) of
-            Nothing ->
-              error $
-                "FIXME: this should never happen, there's no sent message at index "
-                  <> show idx
-                  <> ", messages length = "
-                  <> show (length messages)
-                  <> ", latest: "
-                  <> show latestMsg
-                  <> ", acked: "
-                  <> show acked
-            Just missingMsg -> do
-              let counter' = zipWith (\ack i -> if i == myIndex then idx else ack) counter [0 ..]
-              resend $ Authenticated (Msg counter' missingMsg) us
+        -- resend messages if party did not acknowledge our latest idx
+        let myIndex = fromJust $ List.elemIndex us allParties
+        let acked = acks ! myIndex
+        let latestMsg = (! myIndex) counter
+        when (acked < latestMsg) $ do
+          let missing = fromList [acked + 1 .. latestMsg]
+          traceWith tracer (Resending missing acks counter party)
+          atomically $ do
+            messages <- readTVar sentMessages
+            forM_ missing $ \idx -> do
+              case messages !? (idx - 1) of
+                Nothing ->
+                  error $
+                    "FIXME: this should never happen, there's no sent message at index "
+                      <> show idx
+                      <> ", messages length = "
+                      <> show (length messages)
+                      <> ", latest: "
+                      <> show latestMsg
+                      <> ", acked: "
+                      <> show acked
+                Just missingMsg -> do
+                  let counter' = zipWith (\ack i -> if i == myIndex then idx else ack) counter partyIndexes
+                  resend $ Authenticated (Msg counter' missingMsg) us
 
 data ReliabilityLog
-  = Resending {missing :: [Int], acknowledged :: [Int], localCounter :: [Int], party :: Party}
-  | BroadcastCounter {localCounter :: [Int]}
+  = Resending {missing :: Vector Int, acknowledged :: Vector Int, localCounter :: Vector Int, party :: Party}
+  | BroadcastCounter {localCounter :: Vector Int}
   deriving stock (Show, Eq, Generic)
   deriving anyclass (ToJSON, FromJSON)
+
+instance Arbitrary (Vector Int) where
+  arbitrary = fromList <$> listOf (getPositive <$> arbitrary)
 
 instance Arbitrary ReliabilityLog where
   arbitrary = genericArbitrary
