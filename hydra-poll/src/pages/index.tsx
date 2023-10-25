374f80ec4129122eedcd4ab1f4efb7745ff74e0e
Franco Testagrossa <franco.testagrossa@gmail.com>
2023-10-25 21:10:40 +0200

    update node dependencies
    also fix icon and use next/head instead of html/header

--- hydra-poll/src/pages/index.tsx
+++ hydra-poll/src/pages/index.tsx
@@ -1,12 +1,13 @@
+import Head from "next/head"
 import Main from "./poll/main"
 
 export default function Home() {
   return (
     <div className="container">
-      <header className="title">
+      <Head>
         <title>Hydra Poll</title>
         <meta name="description" content="Poll running on Hydra Head protocol" />
-      </header>
+      </Head>
 
       <main className="main">
         <Main />
