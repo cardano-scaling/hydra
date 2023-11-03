e662e419498f0b9c946d3c31fd1d177bc01b9412
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-11-03 10:46:48 +0100

    Provide node socket and network id to hydra-chain-observer

--- hydra-cluster/test/Test/ChainObserverSpec.hs
+++ hydra-cluster/test/Test/ChainObserverSpec.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveAnyClass #-}
-{-# OPTIONS_GHC -Wno-unused-do-bind #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
+-- withCreateProcess interface is annoying
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 -- | Integration tests for the 'hydra-chain-observer' executable. These will run
 -- also 'hydra-node' on a devnet and assert correct observation.
@@ -14,6 +14,7 @@ import Control.Exception (IOException)
 import Control.Lens ((^?))
 import Data.Aeson.Lens (key, _String)
 import Data.ByteString (hGetLine)
+import Hydra.Cardano.Api (NetworkId (..), NetworkMagic (..))
 import Hydra.Cluster.Faucet (FaucetLog, publishHydraScriptsAs, seedFromFaucet_)
 import Hydra.Cluster.Fixture (Actor (..), aliceSk, cperiod)
 import Hydra.Cluster.Util (chainConfigFor, keysFor)
@@ -31,10 +32,10 @@ spec = do
           withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \cardanoNode@RunningNode{nodeSocket} -> do
             -- Prepare a hydra-node
             hydraScriptsTxId <- publishHydraScriptsAs cardanoNode Faucet
-            (aliceCardanoVk, aliceCardanoSk) <- keysFor Alice
+            (aliceCardanoVk, _aliceCardanoSk) <- keysFor Alice
             aliceChainConfig <- chainConfigFor Alice tmpDir nodeSocket [] cperiod
             withHydraNode (contramap FromHydraNode tracer) aliceChainConfig tmpDir 1 aliceSk [] [1] hydraScriptsTxId $ \hydraNode -> do
-              withChainObserver $ \ChainObserverHandle{awaitNext} -> do
+              withChainObserver cardanoNode $ \ChainObserverHandle{awaitNext} -> do
                 seedFromFaucet_ cardanoNode aliceCardanoVk 100_000_000 (contramap FromFaucet tracer)
 
                 -- Init a head using the hydra-node
@@ -60,17 +61,27 @@ data ChainObserverLog
   deriving anyclass (ToJSON)
 
 -- | Starts a 'hydra-chain-observer' on some Cardano network.
-withChainObserver :: (ChainObserverHandle -> IO ()) -> IO ()
-withChainObserver action =
+withChainObserver :: RunningNode -> (ChainObserverHandle -> IO ()) -> IO ()
+withChainObserver cardanoNode action =
   -- XXX: If this throws an IOException, 'withFile' invocations around mislead
   -- to the file path opened (e.g. the cardano-node log file) in the test
   -- failure output. Print the exception here to have some debuggability at
   -- least.
   handle (\(e :: IOException) -> print e >> throwIO e) $
-    withCreateProcess (proc "hydra-chain-observer" []){std_out = CreatePipe} $ \_stdin (Just out) _stderr processHandle ->
+    withCreateProcess process{std_out = CreatePipe} $ \_in (Just out) _err _ph ->
       action $
         ChainObserverHandle
           { awaitNext = do
               x <- hGetLine out
               pure $ decodeUtf8 x
           }
+ where
+  process =
+    proc
+      "hydra-chain-observer"
+      $ ["--node-socket", show nodeSocket]
+        <> case networkId of
+          Mainnet -> ["--mainnet"]
+          Testnet (NetworkMagic magic) -> ["--testnet-magic", show magic]
+
+  RunningNode{nodeSocket, networkId} = cardanoNode
