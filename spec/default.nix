ed3405311aa41704bb8461325fadbb85753b3d2d
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-11-28 18:25:15 +0100

    Factor diagram.lua into spec/

--- spec/default.nix
+++ spec/default.nix
@@ -1,8 +1,4 @@
 { pkgs ? import <nixpkgs> { }
-, diagramFilterSource ? fetchTarball {
-    url = "https://github.com/pandoc-ext/diagram/archive/v1.0.0.tar.gz";
-    sha256 = "04d7krqn2wsa6ngblnjrc8j0b3sbx60q1kn4k17kcjzh5xmwkma3";
-  }
 }:
 let
   buildScript = ./build.sh;
@@ -31,7 +27,6 @@ pkgs.stdenv.mkDerivation rec {
   phases = [ "unpackPhase" "buildPhase" ];
   buildPhase = ''
     export FONTCONFIG_FILE=${fonts}
-    ln -s ${diagramFilterSource} diagram
     ${buildScript} $out
   '';
 }
