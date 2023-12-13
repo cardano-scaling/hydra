9e1d747f030a7dec84dd61d54c00512c8e368125
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-12-13 10:36:56 +0100

    Prevent concurrent loading and appending in PersistenceIncremental

--- hydra-node/src/Hydra/Persistence.hs
+++ hydra-node/src/Hydra/Persistence.hs
@@ -4,6 +4,8 @@ module Hydra.Persistence where
 
 import Hydra.Prelude
 
+import Control.Concurrent.Class.MonadSTM (modifyTVar', newTVarIO, throwSTM)
+import Control.Monad.Class.MonadFork (myThreadId)
 import Data.Aeson qualified as Aeson
 import Data.ByteString qualified as BS
 import Data.ByteString.Char8 qualified as C8
@@ -11,8 +13,9 @@ import System.Directory (createDirectoryIfMissing, doesFileExist)
 import System.FilePath (takeDirectory)
 import UnliftIO.IO.File (withBinaryFile, writeBinaryFileDurableAtomic)
 
-newtype PersistenceException
+data PersistenceException
   = PersistenceException String
+  | IncorrectAccessException String
   deriving stock (Eq, Show)
 
 instance Exception PersistenceException
@@ -54,17 +57,27 @@ data PersistenceIncremental a m = PersistenceIncremental
 
 -- | Initialize persistence handle for given type 'a' at given file path.
 createPersistenceIncremental ::
-  (MonadIO m, MonadThrow m) =>
+  forall a m.
+  (MonadIO m, MonadThrow m, MonadSTM m, MonadThread m, MonadThrow (STM m)) =>
   FilePath ->
   m (PersistenceIncremental a m)
 createPersistenceIncremental fp = do
   liftIO . createDirectoryIfMissing True $ takeDirectory fp
+  authorizedThread <- newTVarIO Nothing
   pure $
     PersistenceIncremental
       { append = \a -> do
+          tid <- myThreadId
+          atomically $ modifyTVar' authorizedThread (const $ Just tid)
           let bytes = toStrict $ Aeson.encode a <> "\n"
           liftIO $ withBinaryFile fp AppendMode (`BS.hPut` bytes)
-      , loadAll =
+      , loadAll = do
+          tid <- myThreadId
+          atomically $ do
+            authTid <- readTVar authorizedThread
+            when (isJust authTid && authTid /= Just tid) $
+              throwSTM (IncorrectAccessException $ "Trying to load persisted data in " <> fp <> " from different thread")
+
           liftIO (doesFileExist fp) >>= \case
             False -> pure []
             True -> do
--- hydra-node/test/Hydra/PersistenceSpec.hs
+++ hydra-node/test/Hydra/PersistenceSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Hydra.PersistenceSpec where
 
 import Hydra.Prelude hiding (label)
@@ -6,10 +8,10 @@ import Test.Hydra.Prelude
 import Data.Aeson (Value (..))
 import Data.Aeson qualified as Aeson
 import Data.Text qualified as Text
-import Hydra.Persistence (Persistence (..), PersistenceIncremental (..), createPersistence, createPersistenceIncremental)
-import Test.QuickCheck (checkCoverage, cover, elements, oneof, (===))
+import Hydra.Persistence (Persistence (..), PersistenceException (..), PersistenceIncremental (..), createPersistence, createPersistenceIncremental)
+import Test.QuickCheck (checkCoverage, counterexample, cover, elements, generate, oneof, suchThat, (===))
 import Test.QuickCheck.Gen (listOf)
-import Test.QuickCheck.Monadic (monadicIO, monitor, pick, run)
+import Test.QuickCheck.Monadic (assert, monadicIO, monitor, pick, run)
 
 spec :: Spec
 spec = do
@@ -53,6 +55,25 @@ spec = do
               forM_ items append
               loadAll
           pure $ actualResult === items
+
+    it "it cannot load from a different thread once having started appending" $
+      monadicIO $ do
+        items <- pick $ listOf genPersistenceItem
+        moreItems <- pick $ listOf genPersistenceItem `suchThat` ((> 2) . length)
+        result :: Either PersistenceException () <- run $ withTempDir "hydra-persistence" $ \tmpDir -> do
+          persistence@PersistenceIncremental{loadAll, append} <- createPersistenceIncremental $ tmpDir <> "/data"
+          forM_ items append
+          loadAll `shouldReturn` items
+          try $ loadAndAppendConcurrently persistence moreItems
+
+        monitor $ counterexample $ show result
+        assert $ isLeft result
+
+loadAndAppendConcurrently :: PersistenceIncremental Value IO -> [Value] -> IO ()
+loadAndAppendConcurrently PersistenceIncremental{loadAll, append} moreItems =
+  race_
+    (forever $ threadDelay 0.01 >> loadAll)
+    (forM_ moreItems $ \item -> append item >> threadDelay 0.01)
 
 genPersistenceItem :: Gen Aeson.Value
 genPersistenceItem =
