678e27534f790497d081e58c7af65fe918578296
Franco Testagrossa <franco.testagrossa@gmail.com>
2023-12-22 08:57:07 +0100

    Draft e2e on restart

--- hydra-cluster/test/Test/EndToEndSpec.hs
+++ hydra-cluster/test/Test/EndToEndSpec.hs
@@ -592,7 +592,49 @@ spec = around (showLogsOnFailure "EndToEndSpec") $ do
 
                 waitFor hydraTracer 3 [n1] $ output "HeadIsOpen" ["utxo" .= committedUTxOByAlice, "headId" .= headId]
 
-                delayEpoch tmpDir args node 10
+                waitUntilEpoch tmpDir args node 10
+
+                send n1 $ input "Close" []
+                waitMatch 3 n1 $ \v -> do
+                  guard $ v ^? key "tag" == Just "HeadIsClosed"
+                  guard $ v ^? key "headId" == Just (toJSON headId)
+                  snapshotNumber <- v ^? key "snapshotNumber"
+                  guard $ snapshotNumber == Aeson.Number 0
+
+      it "support new era on restart" $ \tracer -> do
+        withClusterTempDir "support-new-era-restart" $ \tmpDir -> do
+          args <- setupCardanoDevnet tmpDir
+
+          forkIntoConwayInEpoch tmpDir args 10
+          withCardanoNode (contramap FromCardanoNode tracer) defaultNetworkId tmpDir args $
+            \node@RunningNode{nodeSocket} -> do
+              let lovelaceBalanceValue = 100_000_000
+              -- Funds to be used as fuel by Hydra protocol transactions
+              (aliceCardanoVk, _) <- keysFor Alice
+              seedFromFaucet_ node aliceCardanoVk lovelaceBalanceValue (contramap FromFaucet tracer)
+              -- Get some UTXOs to commit to a head
+              (aliceExternalVk, aliceExternalSk) <- generate genKeyPair
+              committedUTxOByAlice <- seedFromFaucet node aliceExternalVk aliceCommittedToHead (contramap FromFaucet tracer)
+
+              hydraScriptsTxId <- publishHydraScriptsAs node Faucet
+              chainConfig <- chainConfigFor Alice tmpDir nodeSocket [] cperiod
+
+              let hydraTracer = contramap FromHydraNode tracer
+              headId <- withHydraNode hydraTracer chainConfig tmpDir 1 aliceSk [] [1] hydraScriptsTxId $ \n1 -> do
+                waitForNodesConnected hydraTracer 20 [n1]
+                send n1 $ input "Init" []
+                headId <- waitForAllMatch 10 [n1] $ headIsInitializingWith (Set.fromList [alice])
+
+                requestCommitTx n1 committedUTxOByAlice <&> signTx aliceExternalSk >>= submitTx node
+
+                waitFor hydraTracer 3 [n1] $ output "HeadIsOpen" ["utxo" .= committedUTxOByAlice, "headId" .= headId]
+
+                pure headId
+
+              waitUntilEpoch tmpDir args node 10
+
+              withHydraNode hydraTracer chainConfig tmpDir 1 aliceSk [] [1] hydraScriptsTxId $ \n1 -> do
+                waitForNodesConnected hydraTracer 20 [n1]
 
                 send n1 $ input "Close" []
                 waitMatch 3 n1 $ \v -> do
