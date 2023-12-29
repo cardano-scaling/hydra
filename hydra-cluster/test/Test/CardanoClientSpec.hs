8d7e1b7dc51f50c519f325156fbb15fb9bdd1ec8
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-12-29 11:49:22 +0100

    Check cardano-cli version in scope and pparams roundtrip

--- hydra-cluster/test/Test/CardanoClientSpec.hs
+++ hydra-cluster/test/Test/CardanoClientSpec.hs
@@ -3,11 +3,13 @@ module Test.CardanoClientSpec where
 import Hydra.Prelude
 import Test.Hydra.Prelude
 
+import Cardano.Ledger.Core (PParams)
 import CardanoClient (QueryPoint (..), RunningNode (..), queryGenesisParameters)
 import CardanoNode (withCardanoNodeDevnet)
-import Data.Aeson ((.:))
+import Data.Aeson (eitherDecode', encode, (.:))
 import Data.Aeson qualified as Aeson
 import Hydra.Cardano.Api (GenesisParameters (..))
+import Hydra.Cardano.Api.Prelude (LedgerEra)
 import Hydra.Ledger.Cardano.Configuration (readJsonFileThrow)
 import Hydra.Logging (showLogsOnFailure)
 import System.FilePath ((</>))
@@ -15,7 +17,7 @@ import Test.EndToEndSpec (withClusterTempDir)
 
 spec :: Spec
 spec =
-  around (showLogsOnFailure "CardanoClientSpec") $
+  around (showLogsOnFailure "CardanoClientSpec") $ do
     it "queryGenesisParameters works as expected" $ \tracer ->
       failAfter 60 $
         withClusterTempDir "queryGenesisParameters" $ \tmpDir -> do
@@ -31,3 +33,11 @@ spec =
             expectedSystemStart <- readJsonFileThrow parseSystemStart $ tmpDir </> "genesis-shelley.json"
 
             queriedSystemStart `shouldBe` expectedSystemStart
+
+    it "query ProtocolParameters and check JSON roundtrip" $ \tracer ->
+      failAfter 60 $
+        withClusterTempDir "queryProtocolParameters" $ \tmpDir -> do
+          withCardanoNodeDevnet tracer tmpDir $ \RunningNode{pparams} -> do
+            case eitherDecode' (encode pparams) :: Either String (PParams LedgerEra) of
+              Left e -> expectationFailure e
+              Right parsedPParams -> pparams `shouldBe` parsedPParams
