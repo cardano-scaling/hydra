55a15e8eec0cfe2fbeecd9867b1d70164c0e78fd
Daniel Firth <daniel.firth@iohk.io>
2024-01-04 12:05:53 +0000

    simplify cardano-cli pparams test

--- hydra-cluster/test/Test/Hydra/Cluster/CardanoCliSpec.hs
+++ hydra-cluster/test/Test/Hydra/Cluster/CardanoCliSpec.hs
@@ -4,15 +4,13 @@ import Hydra.Prelude
 import Test.Hydra.Prelude
 
 import CardanoClient (RunningNode (..))
-import CardanoNode (withCardanoNodeDevnet)
+import CardanoNode (cliQueryProtocolParameters, withCardanoNodeDevnet)
 import Control.Lens ((^?))
-import Data.Aeson (eitherDecode', encodeFile)
+import Data.Aeson (encodeFile)
 import Data.Aeson.Lens (key, _String)
 import Data.Aeson.Types (parseEither)
-import Data.ByteString.Lazy.Char8 (pack)
 import Hydra.API.HTTPServer (DraftCommitTxResponse (DraftCommitTxResponse))
-import Hydra.Cardano.Api (Tx, unFile, unNetworkMagic)
-import Hydra.Cardano.Api.Prelude (NetworkId (Testnet))
+import Hydra.Cardano.Api (Tx)
 import Hydra.Ledger.Cardano.Configuration (pparamsFromJson)
 import Hydra.Logging (showLogsOnFailure)
 import System.Exit (ExitCode (..))
@@ -43,21 +41,10 @@ spec =
       it "query protocol-parameters is compatible with our FromJSON instance" $ \tracer ->
         withTempDir "cardano-cli-pparams" $ \tmpDir -> do
           withCardanoNodeDevnet tracer tmpDir $ \RunningNode{nodeSocket, networkId} -> do
-            case networkId of
-              Testnet networkId' -> do
-                (exitCode, output, _errors) <-
-                  readCreateProcessWithExitCode
-                    (cardanoCliQueryPParams (unFile nodeSocket) (show $ unNetworkMagic networkId'))
-                    ""
-                exitCode `shouldBe` ExitSuccess
-                either
-                  (\e -> failure $ "Failed to decode JSON: " <> e <> "\n" <> output)
-                  pure
-                  $ void
-                  $ do
-                    x <- eitherDecode' (pack output)
-                    parseEither pparamsFromJson x
-              _ -> failure "Should only run on Testnet"
+            protocolParameters <- cliQueryProtocolParameters nodeSocket (networkId)
+            case (parseEither pparamsFromJson protocolParameters) of
+              Left e -> failure $ "Failed to decode JSON: " <> e <> "\n" <> show protocolParameters
+              Right _ -> pure ()
 
       it "query protocol-parameters matches our schema" $ \_tracer ->
         pendingWith "TODO"
@@ -73,19 +60,6 @@ spec =
       , "config/credentials/alice.sk"
       , "--testnet-magic"
       , "42"
-      , "--out-file"
-      , "/dev/stdout"
-      ]
-
-  cardanoCliQueryPParams nodeSocket magic =
-    proc
-      "cardano-cli"
-      [ "query"
-      , "protocol-parameters"
-      , "--socket-path"
-      , nodeSocket
-      , "--testnet-magic"
-      , magic
       , "--out-file"
       , "/dev/stdout"
       ]
