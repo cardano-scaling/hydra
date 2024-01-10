e368c724ac5e20a543f429cbc55369362925fcf1
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-01-10 16:24:54 +0100

    Restructure server outputs in api.yaml to make it testable
    The asyncapi schema for websocket messages is not compatible with our
    json schema tests, so we need to extract those and refer them differently.

--- hydra-node/test/Hydra/API/ServerOutputSpec.hs
+++ hydra-node/test/Hydra/API/ServerOutputSpec.hs
@@ -3,8 +3,10 @@ module Hydra.API.ServerOutputSpec where
 import Hydra.Prelude
 import Test.Hydra.Prelude
 
+import Data.Aeson.Lens (key)
 import Hydra.API.ServerOutput (ServerOutput, TimedServerOutput)
 import Hydra.Chain.Direct.State ()
+import Hydra.JSONSchema (prop_validateJSONSchema)
 import Hydra.Ledger.Cardano (Tx)
 import Hydra.Ledger.Simple (SimpleTx)
 import Test.Aeson.GenericSpecs (
@@ -12,8 +14,6 @@ import Test.Aeson.GenericSpecs (
   defaultSettings,
   roundtripAndGoldenSpecsWithSettings,
  )
-import Hydra.JSONSchema (prop_validateJSONSchema)
-import Data.Aeson.Lens (key)
 
 spec :: Spec
 spec = parallel $ do
@@ -25,8 +25,9 @@ spec = parallel $ do
     settings
     (Proxy @(ReasonablySized (ServerOutput Tx)))
 
-  prop "matches JSON schema" $ prop_validateJSONSchema @(TimedServerOutput Tx) "api.json" $
-    key "channels" . key "/" . key "subscribe" . key "message"
+  prop "matches JSON schema" $
+    prop_validateJSONSchema @(TimedServerOutput Tx) "api.json" $
+      key "components" . key "schemas" . key "ServerOutput"
 
   -- NOTE: The golden file produced by this is also used by the
   -- 'validate:outputs' target in ./docs/package.json.
