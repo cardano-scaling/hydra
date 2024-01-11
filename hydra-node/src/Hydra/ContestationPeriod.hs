e2e530649fc9230a028d3fe24239799b38bd9aa6
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-01-11 15:58:48 +0100

    Fix the fix of ContestationPeriod fromDiffTime

--- hydra-node/src/Hydra/ContestationPeriod.hs
+++ hydra-node/src/Hydra/ContestationPeriod.hs
@@ -42,7 +42,7 @@ instance Arbitrary ContestationPeriod where
 fromDiffTime :: MonadFail m => DiffTime -> m ContestationPeriod
 fromDiffTime dt =
   if seconds > 0
-    then pure . UnsafeContestationPeriod $ truncate seconds
+    then pure . UnsafeContestationPeriod . max 1 $ truncate seconds
     else fail $ "fromDiffTime: contestation period <= 0: " <> show dt
  where
   seconds :: Pico = realToFrac dt
--- hydra-node/test/Hydra/ContestationPeriodSpec.hs
+++ hydra-node/test/Hydra/ContestationPeriodSpec.hs
@@ -2,10 +2,11 @@ module Hydra.ContestationPeriodSpec where
 
 import Hydra.Prelude
 
-import Hydra.ContestationPeriod (fromDiffTime)
+import Data.Time (picosecondsToDiffTime)
+import Hydra.ContestationPeriod (ContestationPeriod, fromDiffTime)
 import Test.Hspec (Spec, describe)
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (getNonPositive, getPositive)
+import Test.QuickCheck (getNonPositive, getPositive, (===))
 import Test.QuickCheck.Instances.Time ()
 
 spec :: Spec
@@ -16,3 +17,8 @@ spec = do
 
     prop "fails for diff times <= 0" $
       isNothing . fromDiffTime . getNonPositive
+
+    prop "rounds to 1 second" $ \n ->
+      let subSecondPicos = getPositive n `mod` 1_000_000_000_000
+       in fromDiffTime (picosecondsToDiffTime subSecondPicos)
+            === (fromDiffTime 1 :: Maybe ContestationPeriod)
