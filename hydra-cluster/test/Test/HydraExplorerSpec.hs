5a1cb534934f2954774b649a1a57f328755e4882
Franco Testagrossa <franco.testagrossa@gmail.com>
2024-01-11 17:51:47 +0100

    Add e2e spec to explore multiple heads in the past

--- hydra-cluster/test/Test/HydraExplorerSpec.hs
+++ hydra-cluster/test/Test/HydraExplorerSpec.hs
@@ -20,7 +20,7 @@ import Data.ByteString (hGetLine)
 import Data.Text qualified as T
 import Hydra.Cardano.Api (NetworkId (..), NetworkMagic (..), unFile)
 import Hydra.Cluster.Faucet (FaucetLog, publishHydraScriptsAs, seedFromFaucet_)
-import Hydra.Cluster.Fixture (Actor (..), aliceSk, cperiod)
+import Hydra.Cluster.Fixture (Actor (..), aliceSk, bobSk, cperiod)
 import Hydra.Cluster.Util (chainConfigFor, keysFor)
 import Hydra.HeadId (HeadId (..))
 import Hydra.Logging (showLogsOnFailure)
@@ -33,13 +33,11 @@ import System.Process (CreateProcess (std_out), StdStream (..), proc, withCreate
 
 spec :: Spec
 spec = do
-  it "can observe hydra transactions created by hydra-nodes" $
+  it "can observe hydra transactions live" $
     failAfter 60 $
       showLogsOnFailure "HydraExplorerSpec" $ \tracer -> do
-        withTempDir "hydra-explorer" $ \tmpDir -> do
-          -- Start a cardano devnet
+        withTempDir "hydra-explorer-live" $ \tmpDir -> do
           withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \cardanoNode@RunningNode{nodeSocket} -> do
-            -- Prepare a hydra-node
             let hydraTracer = contramap FromHydraNode tracer
             hydraScriptsTxId <- publishHydraScriptsAs cardanoNode Faucet
             (aliceCardanoVk, _aliceCardanoSk) <- keysFor Alice
@@ -73,6 +71,57 @@ spec = do
                 send hydraNode $ input "Fanout" []
 
                 headExplorerSees explorer "HeadFanoutTx" headId
+
+  it "can observe hydra transactions created by multiple hydra-nodes" $
+    failAfter 60 $
+      showLogsOnFailure "HydraExplorerSpec" $ \tracer -> do
+        withTempDir "hydra-explorer-history" $ \tmpDir -> do
+          withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \cardanoNode@RunningNode{nodeSocket} -> do
+            let hydraTracer = contramap FromHydraNode tracer
+            hydraScriptsTxId <- publishHydraScriptsAs cardanoNode Faucet
+            (aliceCardanoVk, _aliceCardanoSk) <- keysFor Alice
+            aliceChainConfig <- chainConfigFor Alice tmpDir nodeSocket hydraScriptsTxId [] cperiod
+            aliceHeadId <- withHydraNode hydraTracer aliceChainConfig tmpDir 1 aliceSk [] [1] $ \hydraNode -> do
+              seedFromFaucet_ cardanoNode aliceCardanoVk 100_000_000 (contramap FromFaucet tracer)
+
+              send hydraNode $ input "Init" []
+
+              headId <- waitMatch 5 hydraNode $ \v -> do
+                guard $ v ^? key "tag" == Just "HeadIsInitializing"
+                v ^? key "headId" . _String
+
+              requestCommitTx hydraNode mempty >>= submitTx cardanoNode
+              waitFor hydraTracer 5 [hydraNode] $
+                output "HeadIsOpen" ["utxo" .= object mempty, "headId" .= headId]
+
+              send hydraNode $ input "Close" []
+
+              pure headId
+
+            (bobCardanoVk, _bobCardanoSk) <- keysFor Bob
+            bobChainConfig <- chainConfigFor Bob tmpDir nodeSocket hydraScriptsTxId [] cperiod
+            bobHeadId <- withHydraNode hydraTracer bobChainConfig tmpDir 2 bobSk [] [2] $ \hydraNode -> do
+              seedFromFaucet_ cardanoNode bobCardanoVk 100_000_000 (contramap FromFaucet tracer)
+
+              send hydraNode $ input "Init" []
+
+              headId <- waitMatch 5 hydraNode $ \v -> do
+                guard $ v ^? key "tag" == Just "HeadIsInitializing"
+                v ^? key "headId" . _String
+
+              requestCommitTx hydraNode mempty >>= submitTx cardanoNode
+              waitFor hydraTracer 5 [hydraNode] $
+                output "HeadIsOpen" ["utxo" .= object mempty, "headId" .= headId]
+
+              send hydraNode $ input "Close" []
+
+              pure headId
+
+            withHydraExplorer cardanoNode $ \explorer -> do
+              headExplorerSees explorer "HeadIsInitializing" aliceHeadId
+              headExplorerSees explorer "HeadIsClosed" aliceHeadId
+              headExplorerSees explorer "HeadIsInitializing" bobHeadId
+              headExplorerSees explorer "HeadIsClosed" aliceHeadId
 
   it "can query for all hydra heads observed" $
     failAfter 60 $
