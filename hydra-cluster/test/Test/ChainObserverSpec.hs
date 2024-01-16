c9e5902561dff837e557e418e2b3673ee5cd05ac
Franco Testagrossa <franco.testagrossa@gmail.com>
2024-01-16 14:40:21 +0400

    Update cluster e2e spec

--- hydra-cluster/test/Test/ChainObserverSpec.hs
+++ hydra-cluster/test/Test/ChainObserverSpec.hs
@@ -23,9 +23,23 @@ import Hydra.Cluster.Faucet (FaucetLog, publishHydraScriptsAs, seedFromFaucet_)
 import Hydra.Cluster.Fixture (Actor (..), aliceSk, cperiod)
 import Hydra.Cluster.Util (chainConfigFor, keysFor)
 import Hydra.Logging (showLogsOnFailure)
+import Hydra.Network (Host (..))
 import HydraNode (HydraNodeLog, input, output, requestCommitTx, send, waitFor, waitMatch, withHydraNode)
+import Network.Socket (
+  AddrInfo (..),
+  SocketType (..),
+  close,
+  connect,
+  defaultHints,
+  defaultProtocol,
+  getAddrInfo,
+  socket,
+  socketToHandle,
+  withSocketsDo,
+ )
+import System.IO (hClose)
 import System.IO.Error (isEOFError, isIllegalOperation)
-import System.Process (CreateProcess (std_out), StdStream (..), proc, withCreateProcess)
+import System.Process (proc, withCreateProcess)
 
 spec :: Spec
 spec = do
@@ -41,7 +55,7 @@ spec = do
             (aliceCardanoVk, _aliceCardanoSk) <- keysFor Alice
             aliceChainConfig <- chainConfigFor Alice tmpDir nodeSocket hydraScriptsTxId [] cperiod
             withHydraNode hydraTracer aliceChainConfig tmpDir 1 aliceSk [] [1] $ \hydraNode -> do
-              withChainObserver cardanoNode $ \observer -> do
+              withChainObserver cardanoNode Host{hostname = "127.0.0.1", port = 8888} $ \observer -> do
                 seedFromFaucet_ cardanoNode aliceCardanoVk 100_000_000 (contramap FromFaucet tracer)
 
                 send hydraNode $ input "Init" []
@@ -50,35 +64,35 @@ spec = do
                   guard $ v ^? key "tag" == Just "HeadIsInitializing"
                   v ^? key "headId" . _String
 
-                chainObserverSees observer "HeadInitTx" headId
+                chainObserverSees observer "Init" headId
 
                 requestCommitTx hydraNode mempty >>= submitTx cardanoNode
                 waitFor hydraTracer 5 [hydraNode] $
                   output "HeadIsOpen" ["utxo" .= object mempty, "headId" .= headId]
 
-                chainObserverSees observer "HeadCommitTx" headId
-                chainObserverSees observer "HeadCollectComTx" headId
+                chainObserverSees observer "Commit" headId
+                chainObserverSees observer "CollectCom" headId
 
                 send hydraNode $ input "Close" []
 
-                chainObserverSees observer "HeadCloseTx" headId
+                chainObserverSees observer "Close" headId
 
                 waitFor hydraTracer 50 [hydraNode] $
                   output "ReadyToFanout" ["headId" .= headId]
 
                 send hydraNode $ input "Fanout" []
 
-                chainObserverSees observer "HeadFanoutTx" headId
+                chainObserverSees observer "Fanout" headId
 
 chainObserverSees :: HasCallStack => ChainObserverHandle -> Value -> Text -> IO ()
 chainObserverSees observer txType headId =
   awaitMatch observer 5 $ \v -> do
-    guard $ v ^? key "message" . key "tag" == Just txType
-    let actualId = v ^? key "message" . key "headId" . _String
+    guard $ v ^? key "tag" == Just txType
+    let actualId = v ^? key "contents" . key "headId" . _String
     guard $ actualId == Just headId
 
 awaitMatch :: HasCallStack => ChainObserverHandle -> DiffTime -> (Aeson.Value -> Maybe a) -> IO a
-awaitMatch chainObserverHandle delay f = do
+awaitMatch ChainObserverHandle{hdl} delay f = do
   seenMsgs <- newTVarIO []
   timeout delay (go seenMsgs) >>= \case
     Just x -> pure x
@@ -92,38 +106,6 @@ awaitMatch chainObserverHandle delay f = do
                 <> unlines (align 20 (decodeUtf8 . Aeson.encode <$> msgs))
             ]
  where
-  go seenMsgs = do
-    msg <- awaitNext chainObserverHandle
-    atomically (modifyTVar' seenMsgs (msg :))
-    maybe (go seenMsgs) pure (f msg)
-
-  align _ [] = []
-  align n (h : q) = h : fmap (T.replicate n " " <>) q
-
-newtype ChainObserverHandle = ChainObserverHandle {awaitNext :: IO Value}
-
-data ChainObserverLog
-  = FromCardanoNode NodeLog
-  | FromHydraNode HydraNodeLog
-  | FromFaucet FaucetLog
-  deriving (Eq, Show, Generic)
-  deriving anyclass (ToJSON)
-
--- | Starts a 'hydra-chain-observer' on some Cardano network.
-withChainObserver :: RunningNode -> (ChainObserverHandle -> IO ()) -> IO ()
-withChainObserver cardanoNode action =
-  -- XXX: If this throws an IOException, 'withFile' invocations around mislead
-  -- to the file path opened (e.g. the cardano-node log file) in the test
-  -- failure output. Print the exception here to have some debuggability at
-  -- least.
-  handle (\(e :: IOException) -> print e >> throwIO e) $
-    withCreateProcess process{std_out = CreatePipe} $ \_in (Just out) _err _ph ->
-      action
-        ChainObserverHandle
-          { awaitNext = awaitNext out
-          }
- where
-  awaitNext :: Handle -> IO Aeson.Value
   awaitNext out = do
     x <- try (hGetLine out)
     case x of
@@ -139,10 +121,73 @@ withChainObserver cardanoNode action =
             awaitNext out
           Right value -> pure value
 
+  go seenMsgs = do
+    msg <- awaitNext hdl
+    atomically (modifyTVar' seenMsgs (msg :))
+    maybe (go seenMsgs) pure (f msg)
+
+  align _ [] = []
+  align n (h : q) = h : fmap (T.replicate n " " <>) q
+
+newtype ChainObserverHandle = ChainObserverHandle {hdl :: Handle}
+
+data ChainObserverLog
+  = FromCardanoNode NodeLog
+  | FromHydraNode HydraNodeLog
+  | FromFaucet FaucetLog
+  | ConnectedToObserver
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToJSON)
+
+-- | Starts a 'hydra-chain-observer' on some Cardano network.
+withChainObserver :: RunningNode -> Host -> (ChainObserverHandle -> IO ()) -> IO ()
+withChainObserver cardanoNode Host{hostname, port} action =
+  handle (\(e :: IOException) -> print e >> throwIO e) $
+    withCreateProcess process $ \_in _out err ph ->
+      race
+        (checkProcessHasNotDied "hydra-chain-observer" ph err)
+        withConnectionToObserver
+        <&> either absurd id
+ where
+  withConnectionToObserver = do
+    connectedOnce <- newIORef False
+    tryConnect connectedOnce (200 :: Int)
+
+  tryConnect connectedOnce n
+    | n == 0 = failure "Timed out waiting for connection to hydra-chain-observer"
+    | otherwise =
+        doConnect connectedOnce `catch` \(e :: IOException) -> do
+          putStrLn $ "Error while establishing connection to hydra-chain-observer: " <> displayException e
+          readIORef connectedOnce >>= \case
+            False -> threadDelay 0.1 >> tryConnect connectedOnce (n - 1)
+            True -> throwIO e
+
+  doConnect connectedOnce =
+    withSocketsDo $ bracket openConnection closeConnection $ \(_socket, hdl) -> do
+      atomicWriteIORef connectedOnce True
+      action ChainObserverHandle{hdl}
+
+  openConnection = do
+    is <- getAddrInfo (Just defaultHints) (Just $ toString hostname) (Just $ show port)
+    sockAddr <- case is of
+      (inf : _) -> pure inf
+      _ -> die "getAdrrInfo failed"
+    sock <- socket (addrFamily sockAddr) Stream defaultProtocol
+    connect sock (addrAddress sockAddr)
+    hdl <- socketToHandle sock ReadWriteMode
+    hSetBuffering hdl LineBuffering
+    return (sock, hdl)
+
+  closeConnection (sock, hdl) = do
+    hClose hdl
+    close sock
+
   process =
     proc
       "hydra-chain-observer"
       $ ["--node-socket", unFile nodeSocket]
+        <> ["--host", toString hostname]
+        <> ["--port", show port]
         <> case networkId of
           Mainnet -> ["--mainnet"]
           Testnet (NetworkMagic magic) -> ["--testnet-magic", show magic]
