24fde0fe8235ddb4f71b8700d2d42d97ac48d062
Franco Testagrossa <franco.testagrossa@gmail.com>
2024-01-24 12:04:04 +0400

    Draft property spec for aggregate head observations
    Added arbitrary instances for head observations.

---
+++ hydra-explorer/test/Hydra/Explorer/ExplorerStateSpec.hs
@@ -0,0 +1,29 @@
+module Hydra.Explorer.ExplorerStateSpec where
+
+import Hydra.Prelude
+import Test.Hydra.Prelude
+
+import Hydra.Chain.Direct.Tx (HeadObservation (..))
+import Hydra.Explorer.ExplorerState (ExplorerState, aggregateHeadObservations, headId)
+import Hydra.HeadId (HeadId)
+import Test.QuickCheck (forAll, suchThat, (=/=))
+
+spec :: Spec
+spec = do
+  describe "aggregate head observation into explorer state" $ do
+    -- This ensures that the explorer always at least knows about the existence of a head.
+    -- Even if we only observe a part of the life cycle of some head.
+    prop "Any head observations (of some head id) must yield an entry of that head id" $
+      forAll genObservations $ \observations ->
+        aggregateHeadObservations observations [] =/= []
+    prop "Given any observations, the resulting list of head ids is a prefix of the original" $
+      forAll genObservations $ \observations ->
+        forAll arbitrary $ \initialState -> do
+          let resultHeads = aggregateHeadObservations observations initialState
+          getHeadIds initialState `isPrefixOf` getHeadIds resultHeads
+ where
+  genObservations :: Gen [HeadObservation]
+  genObservations = arbitrary `suchThat` (not . null)
+
+  getHeadIds :: ExplorerState -> [HeadId]
+  getHeadIds = fmap headId
