0f3ba3b9690390dd2ea69d9472faa5f6a62dc877
Franco Testagrossa <franco.testagrossa@gmail.com>
2024-01-26 12:24:14 +0400

    Refactor http server to use servant

--- hydra-explorer/src/Hydra/Explorer.hs
+++ hydra-explorer/src/Hydra/Explorer.hs
@@ -3,36 +3,86 @@ module Hydra.Explorer where
 import Hydra.ChainObserver qualified
 import Hydra.Prelude
 
+import Control.Concurrent.Class.MonadSTM (modifyTVar', newTVarIO, readTVarIO)
+import Hydra.API.APIServerLog (APIServerLog (..), Method (..), PathInfo (..))
+import Hydra.Chain.Direct.Tx (HeadObservation)
+import Hydra.Explorer.ExplorerState (ExplorerState, HeadState, aggregateHeadObservations)
 import Hydra.Logging (Tracer, Verbosity (..), traceWith, withTracer)
 import Hydra.Network (PortNumber)
-
-import Control.Concurrent.Class.MonadSTM (modifyTVar', newTVarIO, readTVarIO)
-import Data.Aeson qualified as Aeson
-import Hydra.API.APIServerLog (APIServerLog (..), Method (..), PathInfo (..))
-import Hydra.Chain.Direct.Tx (
-  HeadObservation (..),
- )
-import Hydra.Explorer.ExplorerState (ExplorerState, HeadState, aggregateHeadObservations)
-import Network.HTTP.Types (status200)
-import Network.HTTP.Types.Header (HeaderName)
-import Network.HTTP.Types.Status (status404, status500)
-import Network.Wai (
-  Application,
-  Response,
-  pathInfo,
-  rawPathInfo,
-  requestMethod,
-  responseFile,
-  responseLBS,
- )
+import Network.Wai (Middleware, Request (..), Response)
 import Network.Wai.Handler.Warp qualified as Warp
+import Servant (Server, throwError)
+import Servant.API (Get, Header, JSON, addHeader, (:>))
+import Servant.API.ResponseHeaders (Headers)
+import Servant.Server (Application, Handler, err404, err500, serve)
 import System.Environment (withArgs)
+
+type API =
+  "heads"
+    :> Get
+        '[JSON]
+        ( Headers
+            '[ Header "Accept" String
+             , Header "Access-Control-Allow-Origin" String
+             , Header "Access-Control-Allow-Methods" String
+             , Header "Access-Control-Allow-Headers" String
+             ]
+            [HeadState]
+        )
+
+type GetHeads = IO [HeadState]
+
+api :: Proxy API
+api = Proxy
+
+server ::
+  GetHeads ->
+  Server API
+server = handleGetHeads
+
+handleGetHeads ::
+  GetHeads ->
+  Handler
+    ( Headers
+        '[ Header "Accept" String
+         , Header "Access-Control-Allow-Origin" String
+         , Header "Access-Control-Allow-Methods" String
+         , Header "Access-Control-Allow-Headers" String
+         ]
+        [HeadState]
+    )
+handleGetHeads getHeads = do
+  result <- liftIO $ try getHeads
+  case result of
+    Right heads -> do
+      return $ addHeader "application/json" $ addCorsHeaders heads
+    Left (_ :: SomeException) -> throwError err500
+
+handleUnknownResource :: Request -> Handler Response
+handleUnknownResource _req = throwError err404
+
+logMiddleware :: Tracer IO APIServerLog -> Middleware
+logMiddleware tracer app' req sendResponse = do
+  liftIO $
+    traceWith tracer $
+      APIHTTPRequestReceived
+        { method = Method $ requestMethod req
+        , path = PathInfo $ rawPathInfo req
+        }
+  app' req sendResponse
+
+httpApp :: Tracer IO APIServerLog -> GetHeads -> Application
+httpApp tracer getHeads =
+  logMiddleware tracer $ serve api $ server getHeads
 
 observerHandler :: TVar IO ExplorerState -> [HeadObservation] -> IO ()
 observerHandler explorerState observations = do
   atomically $
     modifyTVar' explorerState $
       aggregateHeadObservations observations
+
+readModelGetHeadIds :: TVar IO ExplorerState -> GetHeads
+readModelGetHeadIds = readTVarIO
 
 main :: IO ()
 main = do
@@ -42,7 +92,9 @@ main = do
     args <- getArgs
     race
       -- FIXME: this is going to be problematic on mainnet.
-      (withArgs (args <> ["--start-chain-from", "0"]) $ Hydra.ChainObserver.main (observerHandler explorerState))
+      ( withArgs (args <> ["--start-chain-from", "0"]) $
+          Hydra.ChainObserver.main (observerHandler explorerState)
+      )
       ( traceWith tracer (APIServerStarted (fromIntegral port :: PortNumber))
           *> Warp.runSettings (settings tracer) (httpApp tracer getHeads)
       )
@@ -58,48 +110,12 @@ main = do
       & Warp.setHost "0.0.0.0"
       & Warp.setOnException (\_ e -> traceWith tracer $ APIConnectionError{reason = show e})
 
-  readModelGetHeadIds :: TVar IO ExplorerState -> GetHeads
-  readModelGetHeadIds = readTVarIO
-
-type GetHeads = IO [HeadState]
-
-httpApp :: Tracer IO APIServerLog -> GetHeads -> Application
-httpApp tracer getHeads req send = do
-  traceWith tracer $
-    APIHTTPRequestReceived
-      { method = Method $ requestMethod req
-      , path = PathInfo $ rawPathInfo req
-      }
-  case (requestMethod req, pathInfo req) of
-    ("HEAD", _) -> send $ responseLBS status200 corsHeaders ""
-    ("GET", ["heads"]) -> handleGetHeads getHeads req send
-    (_, _) -> send handleNotFound
-
-handleGetHeads ::
-  -- | Read model of all known head ids
-  GetHeads ->
-  Application
-handleGetHeads getHeads _req send = do
-  heads <- getHeads
-  send . responseLBS status200 (contentTypeHeader : corsHeaders) $ Aeson.encode heads
-
-handleError :: Response
-handleError =
-  responseLBS status500 corsHeaders "INVALID REQUEST"
-
-handleNotFound :: Response
-handleNotFound =
-  responseLBS status404 corsHeaders "NOT FOUND"
-
-handleFile :: FilePath -> Response
-handleFile filepath = responseFile status200 corsHeaders filepath Nothing
-
-corsHeaders :: [(HeaderName, ByteString)]
-corsHeaders =
-  [ ("Access-Control-Allow-Origin", "*")
-  , ("Access-Control-Allow-Methods", "*")
-  , ("Access-Control-Allow-Headers", "*")
-  ]
-
-contentTypeHeader :: (HeaderName, ByteString)
-contentTypeHeader = ("Accept", "application/json")
+addCorsHeaders ::
+  a ->
+  Headers
+    [ Header "Access-Control-Allow-Origin" String
+    , Header "Access-Control-Allow-Methods" String
+    , Header "Access-Control-Allow-Headers" String
+    ]
+    a
+addCorsHeaders = addHeader "*" . addHeader "*" . addHeader "*"
