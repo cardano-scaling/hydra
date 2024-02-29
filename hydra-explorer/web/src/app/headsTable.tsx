08c1e7d7f78b800717e8911b6ff1c153268660c6
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-02-29 12:58:47 +0100

    Switch to using a relative /heads api endpoint

--- hydra-explorer/web/src/app/headsTable.tsx
+++ hydra-explorer/web/src/app/headsTable.tsx
@@ -18,7 +18,7 @@ const HeadsTable = () => {
 
     const getHeads = async () => {
         try {
-            const response = await fetch('http://explorer.hydra.family/heads')
+            const response = await fetch('/heads')
             // The return value is *not* serialized
             // You can return Date, Map, Set, etc.
             if (!response.ok) {
