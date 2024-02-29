44704dc53fee7ca30c631eea082402fb03f8a447
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-02-29 12:28:32 +0100

    Package hydra-explorer-web using buildYarnPackage
    This is not ideal as it requires quite some overriding, but it seems to work.

--- hydra-explorer/web/hydra-explorer.nix
+++ hydra-explorer/web/hydra-explorer.nix
@@ -1,7 +1,11 @@
 { buildYarnPackage }:
 
-buildYarnPackage rec {
-
+(buildYarnPackage rec {
   src = ./.;
-
-}
+  yarnBuildMore = "yarn build";
+}).overrideAttrs (oldAttrs: {
+  installPhase = ''
+    mkdir -p $out
+    cp -r out/* $out
+  '';
+})
