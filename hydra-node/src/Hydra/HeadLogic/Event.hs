179a8e376d56dbdd754202bbe44820811e2a3143
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-03-06 19:40:49 +0100

    Start renaming

--- hydra-node/src/Hydra/HeadLogic/Event.hs
+++ hydra-node/src/Hydra/HeadLogic/Event.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE UndecidableInstances #-}
 
-module Hydra.HeadLogic.Event where
+module Hydra.HeadLogic.Input where
 
 import Hydra.Prelude
 
@@ -12,28 +12,26 @@ import Hydra.Party (Party)
 
 type TTL = Natural
 
--- TODO: Move logic up and types down or re-organize using explicit exports
-
--- | The different events which are processed by the head logic (the "core").
--- Corresponding to each of the "shell" layers, we distinguish between events
--- from the client, the network and the chain.
-data Event tx
-  = -- | Event received from clients via the "Hydra.API".
-    ClientEvent {clientInput :: ClientInput tx}
-  | -- | Event received from peers via a "Hydra.Network".
+-- | Inputs that are processed by the head logic (the "core"). Corresponding to
+-- each of the "shell" layers, we distinguish between inputs from the client,
+-- the network and the chain.
+data Input tx
+  = -- | Input received from clients via the "Hydra.API".
+    ClientInput {clientInput :: ClientInput tx}
+  | -- | Input received from peers via a "Hydra.Network".
     --
     --  * `ttl` is a simple counter that's decreased every time the event is
     --    reenqueued due to a wait. It's default value is `defaultTTL`
-    NetworkEvent {ttl :: TTL, party :: Party, message :: Message tx}
-  | -- | Event received from the chain via a "Hydra.Chain".
-    OnChainEvent {chainEvent :: ChainEvent tx}
+    NetworkInput {ttl :: TTL, party :: Party, message :: Message tx}
+  | -- | Input received from the chain via a "Hydra.Chain".
+    ChainInput {chainInput :: ChainEvent tx}
   deriving stock (Generic)
 
-deriving stock instance IsChainState tx => Eq (Event tx)
-deriving stock instance IsChainState tx => Show (Event tx)
-deriving anyclass instance IsChainState tx => ToJSON (Event tx)
-deriving anyclass instance IsChainState tx => FromJSON (Event tx)
+deriving stock instance IsChainState tx => Eq (Input tx)
+deriving stock instance IsChainState tx => Show (Input tx)
+deriving anyclass instance IsChainState tx => ToJSON (Input tx)
+deriving anyclass instance IsChainState tx => FromJSON (Input tx)
 
-instance (IsTx tx, IsChainState tx) => Arbitrary (Event tx) where
+instance (IsTx tx, IsChainState tx) => Arbitrary (Input tx) where
   arbitrary = genericArbitrary
   shrink = genericShrink
