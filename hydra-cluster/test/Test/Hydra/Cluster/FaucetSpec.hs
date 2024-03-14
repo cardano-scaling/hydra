c065752cd055f0c5acbc6e9445abe6860b40f70b
Franco Testagrossa <franco.testagrossa@gmail.com>
2024-03-14 10:13:46 +0100

    Keep the describe contexts
    To not need to mention seedFromFaucet on each test case.
    
    This will also make thee diff smaller.

--- hydra-cluster/test/Test/Hydra/Cluster/FaucetSpec.hs
+++ hydra-cluster/test/Test/Hydra/Cluster/FaucetSpec.hs
@@ -18,44 +18,46 @@ import Test.QuickCheck (elements, generate)
 
 spec :: Spec
 spec = do
-  it "seedFromFaucet should work concurrently" $
-    showLogsOnFailure "FaucetSpec" $ \tracer ->
-      failAfter 30 $
+  describe "seedFromFaucet" $ do
+    it "should work concurrently" $
+      showLogsOnFailure "FaucetSpec" $ \tracer ->
+        failAfter 30 $
+          withTempDir "hydra-cluster" $ \tmpDir ->
+            withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \node ->
+              replicateConcurrently_ 10 $ do
+                vk <- generate genVerificationKey
+                seedFromFaucet_ node vk 1_000_000 (contramap FromFaucet tracer)
+    it "should work when called multiple times with the same amount of lovelace" $
+      showLogsOnFailure "FaucetSpec" $ \tracer ->
+        failAfter 30 $
+          withTempDir "hydra-cluster" $ \tmpDir ->
+            withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \node -> do
+              walletVk <- fst <$> generate genKeyPair
+              uTxO1 <- seedFromFaucet node walletVk 2_000_000 (contramap FromFaucet tracer)
+              uTxO2 <- seedFromFaucet node walletVk 2_000_000 (contramap FromFaucet tracer)
+              uTxO1 `shouldNotBe` uTxO2
+  describe "returnFundsToFaucet" $
+    it "seedFromFaucet and returnFundsToFaucet should work together" $ do
+      showLogsOnFailure "FaucetSpec" $ \tracer ->
         withTempDir "hydra-cluster" $ \tmpDir ->
-          withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \node ->
-            replicateConcurrently_ 10 $ do
-              vk <- generate genVerificationKey
-              seedFromFaucet_ node vk 1_000_000 (contramap FromFaucet tracer)
-  it "seedFromFaucet should work when called multiple times with the same amount of lovelace" $
-    showLogsOnFailure "FaucetSpec" $ \tracer ->
-      failAfter 30 $
-        withTempDir "hydra-cluster" $ \tmpDir ->
-          withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \node -> do
-            walletVk <- fst <$> generate genKeyPair
-            uTxO1 <- seedFromFaucet node walletVk 2_000_000 (contramap FromFaucet tracer)
-            uTxO2 <- seedFromFaucet node walletVk 2_000_000 (contramap FromFaucet tracer)
-            uTxO1 `shouldNotBe` uTxO2
-  it "seedFromFaucet and returnFundsToFaucet should work together" $ do
-    showLogsOnFailure "FaucetSpec" $ \tracer ->
-      withTempDir "hydra-cluster" $ \tmpDir ->
-        withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \node@RunningNode{networkId, nodeSocket} -> do
-          let faucetTracer = contramap FromFaucet tracer
-          actor <- generate $ elements [Alice, Bob, Carol]
-          (vk, _) <- keysFor actor
-          (faucetVk, _) <- keysFor Faucet
-          initialFaucetFunds <- queryUTxOFor networkId nodeSocket QueryTip faucetVk
-          seeded <- seedFromFaucet node vk 100_000_000 faucetTracer
-          returnFundsToFaucet faucetTracer node actor
-          remaining <- queryUTxOFor networkId nodeSocket QueryTip vk
-          finalFaucetFunds <- queryUTxOFor networkId nodeSocket QueryTip faucetVk
-          foldMap txOutValue remaining `shouldBe` mempty
+          withCardanoNodeDevnet (contramap FromCardanoNode tracer) tmpDir $ \node@RunningNode{networkId, nodeSocket} -> do
+            let faucetTracer = contramap FromFaucet tracer
+            actor <- generate $ elements [Alice, Bob, Carol]
+            (vk, _) <- keysFor actor
+            (faucetVk, _) <- keysFor Faucet
+            initialFaucetFunds <- queryUTxOFor networkId nodeSocket QueryTip faucetVk
+            seeded <- seedFromFaucet node vk 100_000_000 faucetTracer
+            returnFundsToFaucet faucetTracer node actor
+            remaining <- queryUTxOFor networkId nodeSocket QueryTip vk
+            finalFaucetFunds <- queryUTxOFor networkId nodeSocket QueryTip faucetVk
+            foldMap txOutValue remaining `shouldBe` mempty
 
-          -- check the faucet has one utxo extra in the end
-          length seeded `shouldBe` length remaining + 1
+            -- check the faucet has one utxo extra in the end
+            length seeded `shouldBe` length remaining + 1
 
-          let initialFaucetValue = selectAsset (foldMap txOutValue initialFaucetFunds) AdaAssetId
-          let finalFaucetValue = selectAsset (foldMap txOutValue finalFaucetFunds) AdaAssetId
-          let difference = initialFaucetValue - finalFaucetValue
-          -- difference between starting faucet amount and final one should
-          -- just be the amount of paid fees
-          difference `shouldSatisfy` (< 340_000)
+            let initialFaucetValue = selectAsset (foldMap txOutValue initialFaucetFunds) AdaAssetId
+            let finalFaucetValue = selectAsset (foldMap txOutValue finalFaucetFunds) AdaAssetId
+            let difference = initialFaucetValue - finalFaucetValue
+            -- difference between starting faucet amount and final one should
+            -- just be the amount of paid fees
+            difference `shouldSatisfy` (< 340_000)
