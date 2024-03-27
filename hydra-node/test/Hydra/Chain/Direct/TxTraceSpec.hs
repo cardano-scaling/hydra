0cd3442b5d832c863dc1549cbc10c85e3651f071
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2024-03-27 10:46:21 +0100

    Update the model to include list of Snapshots

--- hydra-node/test/Hydra/Chain/Direct/TxTraceSpec.hs
+++ hydra-node/test/Hydra/Chain/Direct/TxTraceSpec.hs
@@ -5,7 +5,9 @@ import Test.Hydra.Prelude
 
 import Data.Map.Strict qualified as Map
 import Hydra.Chain.Direct.Contract.Close (healthyCloseTx)
+import Hydra.Ledger.Cardano (Tx)
 import Hydra.Ledger.Cardano.Evaluate (evaluateTx)
+import Hydra.Snapshot (Snapshot)
 import Test.QuickCheck (Property, Smart (..), checkCoverage, cover, elements, forAll)
 import Test.QuickCheck.Monadic (monadicIO)
 import Test.QuickCheck.StateModel (
@@ -22,61 +24,69 @@ import Test.QuickCheck.StateModel (
   runActions,
  )
 
+data Model = Model {snapshots :: [Snapshot Tx], headState :: State} deriving (Show)
+
 data State
   = Open
   | Closed
   | Final
   deriving (Show)
 
-instance StateModel State where
-  data Action State a where
-    Close :: Action State ()
-    Contest :: Action State ()
-    Fanout :: Action State ()
+instance StateModel Model where
+  data Action Model a where
+    ProduceSnapshots :: [Snapshot Tx] -> Action Model ()
+    Close :: Action Model ()
+    Contest :: Action Model ()
+    Fanout :: Action Model ()
     -- \| Helper action to identify the terminal state 'Final' and shorten
     -- traces using the 'precondition'.
-    Stop :: Action State ()
+    Stop :: Action Model ()
 
-  arbitraryAction :: VarContext -> State -> Gen (Any (Action State))
-  arbitraryAction _lookup = \case
-    Open -> pure $ Some Close
-    Closed -> Some <$> elements [Contest, Fanout]
-    Final -> pure $ Some Stop
+  arbitraryAction :: VarContext -> Model -> Gen (Any (Action Model))
+  arbitraryAction _lookup Model{headState} =
+    case headState of
+      Open -> pure $ Some Close
+      Closed -> Some <$> elements [Contest, Fanout]
+      Final -> pure $ Some Stop
 
-  initialState = Open
+  initialState = Model{snapshots = [], headState = Open}
 
-  nextState :: State -> Action State a -> Var a -> State
-  nextState s t _ =
-    case (s, t) of
-      (_, Stop) -> s
-      (Open, Close) -> Closed
-      (Closed, Contest) -> Closed
-      (Closed, Fanout) -> Final
-      _ -> s
+  nextState :: Model -> Action Model a -> Var a -> Model
+  nextState m Stop _ = m
+  nextState m t _ =
+    m
+      { headState =
+          case (headState m, t) of
+            (Open, Close) -> Closed
+            (Closed, Contest) -> Closed
+            (Closed, Fanout) -> Final
+            _ -> headState m
+      }
 
-  precondition :: State -> Action State a -> Bool
-  precondition Final Stop = False
+  precondition :: Model -> Action Model a -> Bool
+  precondition Model{headState = Final} Stop = False
   precondition _ _ = True
 
-instance HasVariables State where
+instance HasVariables Model where
   getAllVariables = mempty
 
-instance HasVariables (Action State a) where
+instance HasVariables (Action Model a) where
   getAllVariables = mempty
 
-deriving instance Eq (Action State a)
-deriving instance Show (Action State a)
+deriving instance Eq (Action Model a)
+deriving instance Show (Action Model a)
 
-instance RunModel State IO where
-  perform :: State -> Action State a -> LookUp IO -> IO a
-  perform s action _lookup = do
-    case s of
+instance RunModel Model IO where
+  perform :: Model -> Action Model a -> LookUp IO -> IO a
+  perform m action _lookup = do
+    case headState m of
       Open -> putStrLn "=========OPEN======="
       _ -> pure ()
 
     putStrLn $ "performing action: " <> show action
 
     case action of
+      ProduceSnapshots _snapshots -> pure ()
       Close -> do
         let (tx, utxo) = healthyCloseTx
 
@@ -98,13 +108,20 @@ spec = do
 
 prop_traces :: Property
 prop_traces =
-  forAll (arbitrary :: Gen (Actions State)) $ \(Actions_ _ (Smart _ steps)) ->
+  forAll (arbitrary :: Gen (Actions Model)) $ \(Actions_ _ (Smart _ steps)) ->
     checkCoverage $
       True
         & cover 1 (null steps) "empty"
         & cover 10 (hasFanout steps) "reach fanout"
         & cover 5 (countContests steps >= 2) "has multiple contests"
+        & cover 5 (containSomeSnapshots steps) "has some snapshots"
  where
+  containSomeSnapshots =
+    any $
+      \(_ := ActionWithPolarity{polarAction}) -> case polarAction of
+        ProduceSnapshots snapshots -> not $ null snapshots
+        _ -> False
+
   hasFanout =
     any $
       \(_ := ActionWithPolarity{polarAction}) -> case polarAction of
@@ -119,7 +136,7 @@ prop_traces =
             _ -> False
         )
 
-prop_runActions :: Actions State -> Property
+prop_runActions :: Actions Model -> Property
 prop_runActions actions =
   monadicIO $
     void (runActions actions)
