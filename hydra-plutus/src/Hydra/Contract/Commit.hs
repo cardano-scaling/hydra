9175bb227599f2b107c7892192ef76132d9d9561
v0d1ch <sasha.bogicevic@iohk.io>
2024-06-22 16:44:01 +0200

    Add Version: Introduce optimisations to other contracts

--- hydra-plutus/src/Hydra/Contract/Commit.hs
+++ hydra-plutus/src/Hydra/Contract/Commit.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fno-specialize #-}
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:defer-errors #-}
--- Avoid trace calls to be optimized away when inlining functions.
-{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:no-simplifier-inline #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:conservative-optimisation #-}
 -- Plutus core version to compile to. In babbage era, that is Cardano protocol
 -- version 7 and 8, only plutus-core version 1.0.0 is available.
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:target-version=1.0.0 #-}
