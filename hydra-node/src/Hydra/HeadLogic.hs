eea47125f05b4220cf715028bb5720a8ff18e1c4
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-07-16 18:46:30 +0200

    Remove redundant language pragmas

--- hydra-node/src/Hydra/HeadLogic.hs
+++ hydra-node/src/Hydra/HeadLogic.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE OverloadedRecordDot #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-ambiguous-fields #-}
 
 -- | Implements the Head Protocol's /state machine/ as /pure functions/ in an event sourced manner.
