4e17a7b4e956bf0f6fd6bb1e87ee10165415039a
Leandro Dal Molin <leandro.dalmolin@iohk.io>
2024-08-09 15:07:24 +0100

    Add staggered animation to feature grid

--- docs/src/components/homepage/Features.tsx
+++ docs/src/components/homepage/Features.tsx
@@ -2,22 +2,26 @@ import React, { FC } from "react";
 import clsx from "clsx";
 import { motion } from "framer-motion";
 import { FeatureList } from "../../../docs/homepage/features";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import { forTablet } from "../../../helpers/media-queries";
 
 type Props = {
   icon: React.JSX.Element;
   title: string;
   description: string;
   tagLine: string;
+  index: number;
 };
 
-const Feature: FC<Props> = ({ icon, title, description, tagLine }) => {
+const Feature: FC<Props> = ({ icon, title, description, tagLine, index }) => {
+  const isTabletUp = useMediaQuery(forTablet);
   return (
     <motion.div
       className="flex flex-col gap-4"
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      transition={{ duration: 0.35, delay: 0.25 }}
+      transition={{ duration: 0.35, delay: isTabletUp ? index * 0.2 : 0.25 }}
       variants={{
         visible: { opacity: 1, y: 0 },
         hidden: { opacity: 0, y: 100 },
@@ -61,7 +65,7 @@ const Features: FC = () => {
         </motion.h5>
         <motion.div className="grid laptop:grid-cols-3 laptop:grid-rows-2 laptop:grid-flow-row tablet:grid-rows-3 tablet:grid-flow-col gap-x-6 tablet:gap-y-6 laptop:gap-y-14 gap-y-14">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <Feature key={idx} index={idx} {...props} />
           ))}
         </motion.div>
       </div>
