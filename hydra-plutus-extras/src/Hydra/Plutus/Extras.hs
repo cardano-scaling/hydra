0b7c661d4abe4f54548f1d12cf3a8bf43370f0e4
Daniel Firth <daniel.firth@iohk.io>
2024-08-15 11:27:47 +0000

    change wrapMintingPolicy

--- hydra-plutus-extras/src/Hydra/Plutus/Extras.hs
+++ hydra-plutus-extras/src/Hydra/Plutus/Extras.hs
@@ -17,7 +17,7 @@ import Cardano.Api (
  )
 import Cardano.Api.Shelley (PlutusScript (PlutusScriptSerialised))
 import PlutusLedgerApi.Common (SerialisedScript)
-import PlutusLedgerApi.V3 (ScriptHash (..), ScriptInfo (..), ScriptContext (..), Redeemer, getRedeemer, Datum (..))
+import PlutusLedgerApi.V3 (ScriptHash (..), ScriptInfo (..), ScriptContext (..), getRedeemer, Datum (..))
 import PlutusTx (BuiltinData, UnsafeFromData (..))
 import PlutusTx.Prelude (BuiltinUnit, check, toBuiltin)
 
@@ -36,7 +36,7 @@ wrapValidator ::
 wrapValidator f c = let
   context = unsafeFromBuiltinData c
   in check $ case scriptContextScriptInfo context of
-       SpendingScript x (Just d) ->
+       SpendingScript _ (Just d) ->
          let datum = unsafeFromBuiltinData $ getDatum d
              redeemer = unsafeFromBuiltinData $ getRedeemer $ scriptContextRedeemer context
          in f datum redeemer context
@@ -44,19 +44,21 @@ wrapValidator f c = let
 {-# INLINEABLE wrapValidator #-}
 
 -- | Signature of an untyped minting policy script.
-type MintingPolicyType = BuiltinData -> BuiltinData -> BuiltinUnit
+type MintingPolicyType = BuiltinData -> BuiltinUnit
 
 -- | Wrap a typed minting policy to get the basic `MintingPolicyType` signature
 -- which can be passed to `PlutusTx.compile`.
 wrapMintingPolicy ::
-  (UnsafeFromData redeemer, UnsafeFromData context) =>
-  (redeemer -> context -> Bool) ->
+  (UnsafeFromData redeemer) =>
+  (redeemer -> ScriptContext -> Bool) ->
   MintingPolicyType
-wrapMintingPolicy f r c =
-  check $ f redeemer context
- where
-  redeemer = unsafeFromBuiltinData r
+wrapMintingPolicy f c = let
   context = unsafeFromBuiltinData c
+  in check $ case scriptContextScriptInfo context of
+     MintingScript _ ->
+       let redeemer = unsafeFromBuiltinData $ getRedeemer $ scriptContextRedeemer context
+       in f redeemer context
+     _ -> False
 {-# INLINEABLE wrapMintingPolicy #-}
 
 -- * Similar utilities as plutus-ledger
