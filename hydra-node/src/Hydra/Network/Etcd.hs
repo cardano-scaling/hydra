70ccdb412f8a5fa42ffc27a9efae682099ffe4cf
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-09-12 16:54:26 +0200

    Start etcd on network component start

--- hydra-node/src/Hydra/Network/Etcd.hs
+++ hydra-node/src/Hydra/Network/Etcd.hs
@@ -6,16 +6,36 @@ module Hydra.Network.Etcd where
 
 import Hydra.Prelude
 
-import Hydra.Network (NetworkComponent)
-import Hydra.Network.Message (NetworkEvent)
 import Hydra.Logging (Tracer)
+import Hydra.Network (Host, Network (..), NetworkCallback (..), NetworkComponent)
+import Hydra.Network.Message (NetworkEvent (..))
+import Hydra.Tx (deriveParty)
+import Hydra.Tx.Crypto (HydraKey, SigningKey)
+import System.Posix (Handler (Catch), installHandler, sigTERM)
+import System.Process.Typed (proc, stopProcess, waitExitCode, withProcessWait)
 
 -- | Concrete network component that broadcasts messages to an etcd cluster and
 -- listens for incoming messages.
-withEtcdNetwork :: ToCBOR msg =>
- Tracer IO EtcdLog ->
- NetworkComponent IO (NetworkEvent msg) msg ()
-withEtcdNetwork = undefined
+withEtcdNetwork ::
+  ToCBOR msg =>
+  Tracer IO EtcdLog ->
+  -- | This node's signing key, used to sign messages sent to peers.
+  SigningKey HydraKey ->
+  -- \| Remote hosts to connect to.
+  [Host] ->
+  NetworkComponent IO (NetworkEvent msg) msg ()
+withEtcdNetwork _tracer signingKey _remoteHosts NetworkCallback{deliver} action =
+  withProcessWait etcdCmd $ \p -> do
+    -- Ensure the sub-process is also stopped when we get asked to terminate.
+    _ <- installHandler sigTERM (Catch $ stopProcess p) Nothing
+    -- TODO: error handling
+    race_ (waitExitCode p >>= \ec -> die $ "ectd exited with: " <> show ec) $ do
+      action Network{broadcast}
+ where
+  etcdCmd = proc "etcd" []
+
+  broadcast msg = do
+    deliver (ReceivedMessage{sender = deriveParty signingKey, msg})
 
 data EtcdLog = EtcdLog
   deriving stock (Eq, Show, Generic)
