909aa03b2d125fff5a28f85d804e63b768db7fe6
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2024-10-14 13:27:23 +0200

    Draft initial increment mutation

--- hydra-tx/test/Hydra/Tx/Contract/Deposit.hs
+++ hydra-tx/test/Hydra/Tx/Contract/Deposit.hs
@@ -3,6 +3,8 @@ module Hydra.Tx.Contract.Deposit where
 import Hydra.Cardano.Api
 import Hydra.Prelude
 
+import Data.Time (UTCTime (..), secondsToDiffTime)
+import Data.Time.Calendar (fromGregorian)
 import Hydra.Tx (mkHeadId)
 import Hydra.Tx.BlueprintTx (CommitBlueprintTx (..))
 import Hydra.Tx.Deposit (depositTx)
@@ -18,9 +20,10 @@ healthyDepositTx =
       testNetworkId
       (mkHeadId testPolicyId)
       CommitBlueprintTx{blueprintTx = txSpendingUTxO healthyDepositUTxO, lookupUTxO = healthyDepositUTxO}
-      deadline
+      depositDeadline
 
-  deadline = arbitrary `generateWith` 42
+depositDeadline :: UTCTime
+depositDeadline = UTCTime (fromGregorian 2024 15 0) (secondsToDiffTime 0)
 
 healthyDepositUTxO :: UTxO
 healthyDepositUTxO = genUTxOAdaOnlyOfSize 5 `generateWith` 42
