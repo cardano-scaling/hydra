32919210f1d09cb8c2a147c4b81a120770e466d7
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2024-10-15 15:32:05 +0200

    Check the increment version

--- hydra-plutus/src/Hydra/Contract/Head.hs
+++ hydra-plutus/src/Hydra/Contract/Head.hs
@@ -231,7 +231,12 @@ checkIncrement ctx openBefore _redeemer =
   -- FIXME: spec is mentioning the n also needs to be unchanged - what is n here?
   -- "parameters cid, 𝑘̃ H , 𝑛, 𝑇 stay unchanged"
   mustNotChangeParameters (prevParties, nextParties) (prevCperiod, nextCperiod) (prevHeadId, nextHeadId)
+    && mustIncreaseVersion
  where
+  mustIncreaseVersion =
+    traceIfFalse $(errorCode VersionNotIncremented) $
+      nextVersion == prevVersion + 1
+
   OpenDatum
     { parties = prevParties
     , contestationPeriod = prevCperiod
@@ -240,8 +245,7 @@ checkIncrement ctx openBefore _redeemer =
     } = openBefore
 
   OpenDatum
-    { utxoHash = nextUtxoHash
-    , parties = nextParties
+    { parties = nextParties
     , contestationPeriod = nextCperiod
     , headId = nextHeadId
     , version = nextVersion
