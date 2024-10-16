f0ef31e5b0a101d475c853c43ee3c5f7e1f20c32
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2024-10-16 15:02:06 +0200

    Check snapshot sig and corresponding mutation

--- hydra-tx/test/Hydra/Tx/Contract/Increment.hs
+++ hydra-tx/test/Hydra/Tx/Contract/Increment.hs
@@ -25,19 +25,20 @@ import Hydra.Data.Party qualified as OnChain
 import Hydra.Ledger.Cardano.Time (slotNoFromUTCTime)
 import Hydra.Plutus.Orphans ()
 import Hydra.Tx.ContestationPeriod (ContestationPeriod, toChain)
-import Hydra.Tx.Contract.Deposit (depositDeadline, healthyDepositTx)
-import Hydra.Tx.Crypto (HydraKey, MultiSignature (..), aggregate, sign)
+import Hydra.Tx.Contract.Deposit (depositDeadline, healthyDepositTx, healthyDepositUTxO)
+import Hydra.Tx.Crypto (HydraKey, MultiSignature (..), aggregate, sign, toPlutusSignatures)
+import Hydra.Tx.Deposit qualified as Deposit
 import Hydra.Tx.HeadId (headIdToCurrencySymbol, mkHeadId)
 import Hydra.Tx.HeadParameters (HeadParameters (..))
 import Hydra.Tx.Increment (
   incrementTx,
  )
 import Hydra.Tx.Init (mkHeadOutput)
-import Hydra.Tx.IsTx (IsTx (hashUTxO, withoutUTxO))
+import Hydra.Tx.IsTx (IsTx (hashUTxO))
 import Hydra.Tx.Party (Party, deriveParty, partyToChain)
 import Hydra.Tx.ScriptRegistry (registryUTxO)
 import Hydra.Tx.Snapshot (Snapshot (..), SnapshotNumber, SnapshotVersion)
-import Hydra.Tx.Utils (adaOnly, splitUTxO)
+import Hydra.Tx.Utils (adaOnly)
 import PlutusLedgerApi.V2 qualified as Plutus
 import PlutusTx.Builtins (toBuiltin)
 import Test.Hydra.Tx.Fixture (aliceSk, bobSk, carolSk, slotLength, systemStart, testHeadId, testNetworkId, testPolicyId)
@@ -66,7 +67,6 @@ healthyIncrementTx =
       (slotNoFromUTCTime systemStart slotLength depositDeadline)
       healthySignature
 
-  depositUTxO = utxoFromTx (fst healthyDepositTx)
   parameters =
     HeadParameters
       { parties = healthyParties
@@ -81,6 +81,9 @@ healthyIncrementTx =
     mkHeadOutput testNetworkId testPolicyId (toUTxOContext $ mkTxOutDatumInline healthyDatum)
       & addParticipationTokens healthyParticipants
       & modifyTxOutValue (<> foldMap txOutValue healthyUTxO)
+
+depositUTxO :: UTxO
+depositUTxO = utxoFromTx (fst healthyDepositTx)
 
 somePartyCardanoVerificationKey :: VerificationKey PaymentKey
 somePartyCardanoVerificationKey =
@@ -110,24 +113,15 @@ healthySnapshotVersion = 1
 
 healthySnapshot :: Snapshot Tx
 healthySnapshot =
-  let (utxoToDecommit', utxo) = splitUTxO healthyUTxO
-   in Snapshot
-        { headId = mkHeadId testPolicyId
-        , version = healthySnapshotVersion
-        , number = succ healthySnapshotNumber
-        , confirmed = []
-        , utxo
-        , utxoToCommit = Nothing
-        , utxoToDecommit = Just utxoToDecommit'
-        }
-
-splitDecommitUTxO :: UTxO -> (UTxO, UTxO)
-splitDecommitUTxO utxo =
-  case UTxO.pairs utxo of
-    [] -> error "empty utxo in splitDecommitUTxO"
-    (decommit : _rest) ->
-      let decommitUTxO' = UTxO.fromPairs [decommit]
-       in (utxo `withoutUTxO` decommitUTxO', decommitUTxO')
+  Snapshot
+    { headId = mkHeadId testPolicyId
+    , version = healthySnapshotVersion
+    , number = succ healthySnapshotNumber
+    , confirmed = []
+    , utxo = healthyUTxO
+    , utxoToCommit = Just healthyDepositUTxO
+    , utxoToDecommit = Nothing
+    }
 
 healthyContestationPeriod :: ContestationPeriod
 healthyContestationPeriod =
@@ -138,15 +132,14 @@ healthyUTxO = adaOnly <$> generateWith (genUTxOSized 3) 42
 
 healthyDatum :: Head.State
 healthyDatum =
-  let (_utxoToDecommit', utxo) = splitDecommitUTxO healthyUTxO
-   in Head.Open
-        Head.OpenDatum
-          { utxoHash = toBuiltin $ hashUTxO @Tx utxo
-          , parties = healthyOnChainParties
-          , contestationPeriod = toChain healthyContestationPeriod
-          , headId = toPlutusCurrencySymbol testPolicyId
-          , version = toInteger healthySnapshotVersion
-          }
+  Head.Open
+    Head.OpenDatum
+      { utxoHash = toBuiltin $ hashUTxO @Tx healthyUTxO
+      , parties = healthyOnChainParties
+      , contestationPeriod = toChain healthyContestationPeriod
+      , headId = toPlutusCurrencySymbol testPolicyId
+      , version = toInteger healthySnapshotVersion
+      }
 
 data IncrementMutation
   = -- | Move the deadline from the deposit datum back in time
@@ -158,37 +151,52 @@ data IncrementMutation
     IncrementMutateParties
   | -- | New version is incremented correctly
     IncrementUseDifferentSnapshotVersion
+  | -- | Produce invalid signatures
+    ProduceInvalidSignatures
   -- \| -- | Alter the Claim redeemer `TxOutRef`
   -- IncrementDifferentClaimRedeemer
   deriving stock (Generic, Show, Enum, Bounded)
 
 genIncrementMutation :: (Tx, UTxO) -> Gen SomeMutation
 genIncrementMutation (tx, utxo) =
-  let (depositIn, depositOut@(TxOut addr val _ rscript)) = UTxO.pairs (resolveInputsUTxO utxo tx) List.!! 1
-   in oneof
-        [ SomeMutation (pure $ toErrorCode DepositDeadlineSurpassed) DepositMutateDepositDeadline <$> do
-            let datum =
-                  txOutDatum $
-                    flip modifyInlineDatum (toTxContext depositOut) $ \case
-                      DepositDatum (headCS', depositDatumDeadline, commits) ->
-                        DepositDatum (headCS', Plutus.POSIXTime $ Plutus.getPOSIXTime depositDatumDeadline - 1, commits)
-            let newOutput = toCtxUTxOTxOut $ TxOut addr val datum rscript
-            pure $ ChangeInput depositIn newOutput (Just $ toScriptData $ Claim (headIdToCurrencySymbol testHeadId))
-        , SomeMutation (pure $ toErrorCode WrongHeadIdInDepositDatum) DepositMutateHeadId <$> do
-            otherHeadId <- arbitrary
-            let datum =
-                  txOutDatum $
-                    flip modifyInlineDatum (toTxContext depositOut) $ \case
-                      DepositDatum (_headCS, depositDatumDeadline, commits) ->
-                        DepositDatum (otherHeadId, depositDatumDeadline, commits)
-            let newOutput = toCtxUTxOTxOut $ TxOut addr val datum rscript
-            pure $ ChangeInput depositIn newOutput (Just $ toScriptData $ Claim (headIdToCurrencySymbol testHeadId))
-        , SomeMutation (pure $ toErrorCode ChangedParameters) IncrementMutateParties <$> do
-            mutatedParties <- arbitrary `suchThat` (/= healthyOnChainParties)
-            pure $ ChangeOutput 0 $ modifyInlineDatum (replaceParties mutatedParties) headTxOut
-        , SomeMutation (pure $ toErrorCode VersionNotIncremented) IncrementUseDifferentSnapshotVersion <$> do
-            mutatedSnapshotVersion <- arbitrarySizedNatural `suchThat` (/= healthySnapshotVersion + 1)
-            pure $ ChangeOutput 0 $ modifyInlineDatum (replaceSnapshotVersion $ toInteger mutatedSnapshotVersion) headTxOut
-        ]
+  oneof
+    [ SomeMutation (pure $ toErrorCode DepositDeadlineSurpassed) DepositMutateDepositDeadline <$> do
+        let datum =
+              txOutDatum $
+                flip modifyInlineDatum (toTxContext depositOut) $ \case
+                  DepositDatum (headCS', depositDatumDeadline, commits) ->
+                    DepositDatum (headCS', Plutus.POSIXTime $ Plutus.getPOSIXTime depositDatumDeadline - 1, commits)
+        let newOutput = toCtxUTxOTxOut $ TxOut addr val datum rscript
+        pure $ ChangeInput depositIn newOutput (Just $ toScriptData $ Claim (headIdToCurrencySymbol testHeadId))
+    , SomeMutation (pure $ toErrorCode WrongHeadIdInDepositDatum) DepositMutateHeadId <$> do
+        otherHeadId <- arbitrary
+        let datum =
+              txOutDatum $
+                flip modifyInlineDatum (toTxContext depositOut) $ \case
+                  DepositDatum (_headCS, depositDatumDeadline, commits) ->
+                    DepositDatum (otherHeadId, depositDatumDeadline, commits)
+        let newOutput = toCtxUTxOTxOut $ TxOut addr val datum rscript
+        pure $ ChangeInput depositIn newOutput (Just $ toScriptData $ Claim (headIdToCurrencySymbol testHeadId))
+    , SomeMutation (pure $ toErrorCode ChangedParameters) IncrementMutateParties <$> do
+        mutatedParties <- arbitrary `suchThat` (/= healthyOnChainParties)
+        pure $ ChangeOutput 0 $ modifyInlineDatum (replaceParties mutatedParties) headTxOut
+    , SomeMutation (pure $ toErrorCode VersionNotIncremented) IncrementUseDifferentSnapshotVersion <$> do
+        mutatedSnapshotVersion <- arbitrarySizedNatural `suchThat` (/= healthySnapshotVersion + 1)
+        pure $ ChangeOutput 0 $ modifyInlineDatum (replaceSnapshotVersion $ toInteger mutatedSnapshotVersion) headTxOut
+    , SomeMutation (pure $ toErrorCode SignatureVerificationFailed) ProduceInvalidSignatures . ChangeHeadRedeemer <$> do
+        invalidSignature <- toPlutusSignatures <$> (arbitrary :: Gen (MultiSignature (Snapshot Tx)))
+        pure $
+          Head.Increment
+            Head.IncrementRedeemer
+              { signature = invalidSignature
+              , snapshotNumber = fromIntegral healthySnapshotNumber
+              , increment = toPlutusTxOutRef $ fst $ List.head $ UTxO.pairs depositUTxO
+              }
+    ]
  where
   headTxOut = fromJust $ txOuts' tx !!? 0
+  (depositIn, depositOut@(TxOut addr val _ rscript)) =
+    fromJust $
+      find
+        (\(_, TxOut address _ _ _) -> address == Deposit.depositAddress testNetworkId)
+        (UTxO.pairs (resolveInputsUTxO utxo tx))
