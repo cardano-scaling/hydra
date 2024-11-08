8ec693d9eb349576c87c927257ca78d65aa93d4d
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2024-11-08 14:31:22 +0100

    Simplify our dummy script to make all of the tests that use it pass

--- hydra-plutus/src/Hydra/Contract/Dummy.hs
+++ hydra-plutus/src/Hydra/Contract/Dummy.hs
@@ -1,9 +1,4 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# OPTIONS_GHC -fno-specialize #-}
-{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:conservative-optimisation #-}
-{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:defer-errors #-}
--- Plutus core version to compile to. In babbage era, that is Cardano protocol
--- version 7 and 8, only plutus-core version 1.0.0 is available.
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:target-version=1.0.0 #-}
 
 module Hydra.Contract.Dummy where
@@ -12,49 +7,25 @@ import Hydra.Cardano.Api (PlutusScriptVersion (PlutusScriptV3))
 import Hydra.Plutus.Extras (ValidatorType, scriptValidatorHash, wrapValidator)
 import Hydra.Prelude
 
-import PlutusLedgerApi.V3 (ScriptContext (scriptContextScriptInfo), ScriptHash, ScriptInfo (RewardingScript), SerialisedScript, UnsafeFromData (unsafeFromBuiltinData), getRedeemer, scriptContextRedeemer, serialiseCompiledCode, toBuiltinData)
+import PlutusLedgerApi.V3 (BuiltinData, ScriptContext, ScriptHash, SerialisedScript, serialiseCompiledCode, toOpaque)
 import PlutusTx (CompiledCode, compile)
-import PlutusTx.Prelude (check)
 
-type D = ()
-type R = ()
-
-dummyValidator :: D -> R -> ScriptContext -> Bool
+dummyValidator :: BuiltinData -> BuiltinData -> ScriptContext -> Bool
 dummyValidator _ _ _ = True
 
 compiledDummyValidator :: CompiledCode ValidatorType
 compiledDummyValidator =
-  $$(PlutusTx.compile [||wrap dummyValidator||])
+  $$(PlutusTx.compile [||fakeWrap dummyValidator||])
  where
-  wrap = wrapValidator @D @R
+  wrap = wrapValidator @BuiltinData @BuiltinData
 
-wrapFakeStakeValidator ::
-  (UnsafeFromData datum, UnsafeFromData redeemer) =>
+fakeWrap ::
   (datum -> redeemer -> ScriptContext -> Bool) ->
   ValidatorType
-wrapFakeStakeValidator f c =
-  let
-    context = unsafeFromBuiltinData c
-   in
-    check $ case scriptContextScriptInfo context of
-      RewardingScript _cred ->
-        let datum = unsafeFromBuiltinData $ toBuiltinData ()
-            redeemer = unsafeFromBuiltinData $ getRedeemer $ scriptContextRedeemer context
-         in f datum redeemer context
-      _ -> False
-{-# INLINEABLE wrapFakeStakeValidator #-}
-
-compiledDummyStakeValidator :: CompiledCode ValidatorType
-compiledDummyStakeValidator =
-  $$(PlutusTx.compile [||wrapFakeStakeValidator dummyValidator||])
- where
-  wrap = wrapValidator @D @R
+fakeWrap _ _ = toOpaque ()
 
 dummyValidatorScript :: SerialisedScript
 dummyValidatorScript = serialiseCompiledCode compiledDummyValidator
-
-dummyStakeValidatorScript :: SerialisedScript
-dummyStakeValidatorScript = serialiseCompiledCode compiledDummyStakeValidator
 
 dummyValidatorHash :: ScriptHash
 dummyValidatorHash = scriptValidatorHash PlutusScriptV3 dummyValidatorScript
