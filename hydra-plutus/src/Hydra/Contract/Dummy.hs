0020849b87fc27f233a2c4f4c6275d3e5ea772cf
Daniel Firth <daniel.firth@iohk.io>
2024-11-18 11:51:33 +0000

    Import schnorkell plutus
    add singlePartyUsesSchnorrkelScriptOnL2 scenario test
    
    Finalize schnorkel test case
    
    There is some refactoring in order but the test itself is fine.
    
    Update plutus source-repository-package
    
    treefmt
    
    fixup
    
    Have some ada for the min UTxO
    
    Almost there; insufficient collateral now
    
    Trying to be more clear about the UTxOs to pay for things
    
    WIP on schnorrkel; just missing script as input

--- hydra-plutus/src/Hydra/Contract/Dummy.hs
+++ hydra-plutus/src/Hydra/Contract/Dummy.hs
@@ -5,12 +5,28 @@
 
 module Hydra.Contract.Dummy where
 
-import Hydra.Prelude
+import Hydra.Cardano.Api (PlutusScriptVersion (PlutusScriptV3))
+import Hydra.Plutus.Extras (ValidatorType, scriptValidatorHash, wrapValidator)
+import Hydra.Prelude hiding ((==))
 
 import Hydra.Cardano.Api (PlutusScript, pattern PlutusScriptSerialised)
 import Hydra.Plutus.Extras (ValidatorType, wrapValidator)
 import PlutusLedgerApi.V3 (BuiltinData, ScriptContext, serialiseCompiledCode, toOpaque)
 import PlutusTx (CompiledCode, compile)
+import PlutusTx.Builtins (schnorrkel)
+import PlutusTx.Prelude (Eq (..))
+
+schnorrkelValidator :: BuiltinData -> BuiltinData -> ScriptContext -> Bool
+schnorrkelValidator _ _ _ = "" == schnorrkel ""
+
+schnorrkelValidatorScript :: PlutusScript
+schnorrkelValidatorScript = PlutusScriptSerialised $ serialiseCompiledCode compiledDummyValidator
+
+compiledSchnorrkelValidator :: CompiledCode ValidatorType
+compiledSchnorrkelValidator =
+  $$(PlutusTx.compile [||fakeWrap schnorrkelValidator||])
+ where
+  wrap = wrapValidator @BuiltinData @BuiltinData
 
 dummyValidator :: BuiltinData -> BuiltinData -> ScriptContext -> Bool
 dummyValidator _ _ _ = True
