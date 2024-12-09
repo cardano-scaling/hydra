a0564ba9b763d0b66c21749407fc8944f8ea52e4
Daniel Firth <daniel.firth@iohk.io>
2024-12-09 05:01:10 +0000

    Remove unused function toLedgerScriptHash

--- hydra-cardano-api/src/Hydra/Cardano/Api/PolicyId.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api/PolicyId.hs
@@ -5,7 +5,6 @@ module Hydra.Cardano.Api.PolicyId where
 import Hydra.Cardano.Api.Prelude
 
 import Cardano.Ledger.Alonzo.Plutus.TxInfo qualified as Ledger
-import Cardano.Ledger.Hashes qualified as Ledger
 import Cardano.Ledger.Mary.Value qualified as Ledger
 import Hydra.Cardano.Api.ScriptHash ()
 import PlutusLedgerApi.V3 (CurrencySymbol, fromBuiltin, unCurrencySymbol)
@@ -16,9 +15,6 @@ instance Arbitrary PolicyId where
   arbitrary = PolicyId <$> arbitrary
 
 -- * Type conversions
-
-toLedgerScriptHash :: PolicyId -> Ledger.ScriptHash StandardCrypto
-toLedgerScriptHash (PolicyId scriptHash) = toShelleyScriptHash scriptHash
 
 -- | Convert Cardano api 'PolicyId' to Cardano ledger `PolicyID`.
 toLedgerPolicyID :: PolicyId -> Ledger.PolicyID StandardCrypto
