3311b4426329f5d83d0ff778147b9a2ecd8abfb8
Noon van der Silk <noon.vandersilk@iohk.io>
2025-01-13 12:09:53 +0000

    No need for tr on 0.19.0

--- demo/seed-devnet.sh
+++ demo/seed-devnet.sh
@@ -98,8 +98,7 @@ function publishReferenceScripts() {
   hnode publish-scripts \
     --testnet-magic ${NETWORK_ID} \
     --node-socket ${DEVNET_DIR}/node.socket \
-    --cardano-signing-key devnet/credentials/faucet.sk \
-    | tr '\n' ','
+    --cardano-signing-key devnet/credentials/faucet.sk
 }
 
 function queryPParams() {
