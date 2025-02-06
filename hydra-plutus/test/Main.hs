d260d3f1bfe1ded601ba63035e93bef35bb0909b
Daniel Firth <daniel.firth@iohk.io>
2025-02-06 14:15:21 +0000

    Remove test results from documentation

--- hydra-plutus/test/Main.hs
+++ hydra-plutus/test/Main.hs
@@ -2,13 +2,9 @@ module Main where
 
 import Hydra.Prelude
 
-import Test.Hspec.Runner (configFormat, defaultConfig, hspecWith)
-import Test.Hydra.Prelude (combinedHspecFormatter)
+import Test.Hspec.Runner (hspec)
 
 import Spec qualified
 
 main :: IO ()
-main =
-  hspecWith
-    defaultConfig{configFormat = Just (combinedHspecFormatter "hydra-plutus")}
-    Spec.spec
+main = hspec Spec.spec
