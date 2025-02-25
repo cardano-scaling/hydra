b7a406ba52596232396b552aeeeb6e891c537e97
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-02-25 18:17:30 +0100

    Updated with review suggestions
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-node/src/Hydra/Events/ApiSink.hs
+++ hydra-node/src/Hydra/Events/ApiSink.hs
@@ -6,16 +6,5 @@ import Hydra.API.Server (Server (..))
 import Hydra.Events (EventSink (..), StateEvent (..))
 import Hydra.Node (HydraNode (..))
 
-wireApiEvents ::
-  MonadSTM m =>
-  Server tx m ->
-  m (EventSink (StateEvent tx) m)
-wireApiEvents server = do
-  let putEvent stateEvt = do
-        sendOutput stateEvt
-  pure EventSink{putEvent}
- where
-  Server{sendOutput} = server
-
-addApiEventSink :: EventSink (StateEvent tx) m -> HydraNode tx m -> HydraNode tx m
-addApiEventSink sink node = node{eventSinks = sink : eventSinks node}
+addEventSink :: EventSink (StateEvent tx) m -> HydraNode tx m -> HydraNode tx m
+addEventSink sink node = node{eventSinks = sink : eventSinks node}
