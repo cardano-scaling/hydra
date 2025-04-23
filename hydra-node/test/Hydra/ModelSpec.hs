d370af1663dcdaa0823324013877c9a150c59ef4
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-04-23 13:29:32 +0200

    Improve ModelSpec counterexample of HUnitFailures

--- hydra-node/test/Hydra/ModelSpec.hs
+++ hydra-node/test/Hydra/ModelSpec.hs
@@ -120,6 +120,7 @@ import Control.Monad.IOSim (Failure (FailureException), IOSim, runSimTrace, trac
 import Data.Map ((!))
 import Data.Map qualified as Map
 import Data.Set qualified as Set
+import Data.Typeable (cast)
 import Hydra.BehaviorSpec (TestHydraClient (..), dummySimulatedChainNetwork)
 import Hydra.Logging.Messages (HydraLog)
 import Hydra.Model (
@@ -143,6 +144,7 @@ import Hydra.Model.Payment qualified as Payment
 import Hydra.Tx.Party (Party (..), deriveParty)
 import System.IO.Temp (writeSystemTempFile)
 import System.IO.Unsafe (unsafePerformIO)
+import Test.HUnit.Lang (formatFailureReason)
 import Test.QuickCheck (Property, Testable, counterexample, forAllShrink, property, withMaxSuccess, within)
 import Test.QuickCheck.DynamicLogic (
   DL,
@@ -388,7 +390,12 @@ runRunMonadIOSimGen f = do
       case traceResult False tr of
         Right a -> property a
         Left (FailureException (SomeException ex)) ->
-          counterexample (show ex) False
+          case cast ex of
+            Just (HUnitFailure loc reason) ->
+              False
+                & counterexample (formatFailureReason reason)
+                & counterexample ("Location: " <> maybe "unknown" prettySrcLoc loc)
+            _ -> counterexample (show ex) False
         Left ex ->
           counterexample (show ex) False
  where
