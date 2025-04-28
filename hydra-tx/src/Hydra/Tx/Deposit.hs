3a7c1442591a912f0e7aa8970ba381b6893e5c74
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-04-28 21:03:24 +0200

    Add an upper validity to depositTx
    This will mark the creation of the deposit.

--- hydra-tx/src/Hydra/Tx/Deposit.hs
+++ hydra-tx/src/Hydra/Tx/Deposit.hs
@@ -3,8 +3,9 @@ module Hydra.Tx.Deposit where
 import Hydra.Prelude hiding (toList)
 
 import Cardano.Api.UTxO qualified as UTxO
-import Cardano.Ledger.Api (bodyTxL, inputsTxBodyL, outputsTxBodyL)
+import Cardano.Ledger.Api (AllegraEraTxBody (vldtTxBodyL), ValidityInterval (..), bodyTxL, inputsTxBodyL, outputsTxBodyL)
 import Control.Lens ((.~), (^.))
+import Data.Maybe.Strict (StrictMaybe (..))
 import Data.Sequence.Strict qualified as StrictSeq
 import Data.Set qualified as Set
 import GHC.IsList (toList)
@@ -13,7 +14,7 @@ import Hydra.Contract.Commit qualified as Commit
 import Hydra.Contract.Deposit qualified as Deposit
 import Hydra.Plutus (depositValidatorScript)
 import Hydra.Plutus.Extras.Time (posixFromUTCTime)
-import Hydra.Tx (CommitBlueprintTx (..), HeadId, currencySymbolToHeadId, headIdToCurrencySymbol)
+import Hydra.Tx (CommitBlueprintTx (..), HeadId, currencySymbolToHeadId, headIdToCurrencySymbol, txId)
 import Hydra.Tx.Utils (addMetadata, mkHydraHeadV1TxName)
 import PlutusLedgerApi.V3 (POSIXTime)
 
@@ -24,14 +25,17 @@ depositTx ::
   NetworkId ->
   HeadId ->
   CommitBlueprintTx Tx ->
-  -- | Deposit deadline
+  -- | Slot to use as upper validity. Will mark the time of creation of the deposit.
+  SlotNo ->
+  -- | Deposit deadline from which onward the deposit can be recovered.
   UTCTime ->
   Tx
-depositTx networkId headId commitBlueprintTx deadline =
+depositTx networkId headId commitBlueprintTx upperSlot deadline =
   fromLedgerTx $
     toLedgerTx blueprintTx
       & addDepositInputs
       & bodyTxL . outputsTxBodyL .~ StrictSeq.singleton (toLedgerTxOut $ mkDepositOutput networkId headId depositUTxO deadline)
+      & bodyTxL . vldtTxBodyL .~ ValidityInterval{invalidBefore = SNothing, invalidHereafter = SJust upperSlot}
       & addMetadata (mkHydraHeadV1TxName "DepositTx") blueprintTx
  where
   addDepositInputs tx =
@@ -72,8 +76,9 @@ depositAddress networkId = mkScriptAddress networkId depositValidatorScript
 
 data DepositObservation = DepositObservation
   { headId :: HeadId
-  , deposited :: UTxO
   , depositTxId :: TxId
+  , deposited :: UTxO
+  , created :: SlotNo
   , deadline :: POSIXTime
   }
   deriving stock (Show, Eq, Generic)
@@ -82,24 +87,36 @@ data DepositObservation = DepositObservation
 -- deadline and deposited utxo in the datum.
 --
 -- This includes checking whether
+-- - the first output is a deposit output
 -- - all inputs of deposited utxo are actually spent,
--- - the deposit script output actually contains the deposited value.
+-- - the deposit script output actually contains the deposited value,
+-- - an upper validity bound has been set (used as creation slot).
 observeDepositTx ::
   NetworkId ->
   Tx ->
   Maybe DepositObservation
 observeDepositTx networkId tx = do
-  -- TODO: could just use the first output and fail otherwise
-  (TxIn depositTxId _, depositOut) <- findTxOutByAddress (depositAddress networkId) tx
-  (headId, deposited, deadline) <- observeDepositTxOut (toShelleyNetwork networkId) (toCtxUTxOTxOut depositOut)
-  guard $ all (`elem` txIns' tx) (UTxO.inputSet deposited)
+  depositOut <- fmap head . nonEmpty $ txOuts' tx
+  (headId, deposited, deadline) <- observeDepositTxOut network (toCtxUTxOTxOut depositOut)
+  guard $ spendsAll deposited
+  created <- getUpperBound
   pure
     DepositObservation
       { headId
+      , depositTxId = txId tx
       , deposited
-      , depositTxId
+      , created
       , deadline
       }
+ where
+  spendsAll = all (`elem` txIns' tx) . UTxO.inputSet
+
+  getUpperBound =
+    case tx & getTxBody & getTxBodyContent & txValidityUpperBound of
+      TxValidityUpperBound{upperBound} -> Just upperBound
+      TxValidityNoUpperBound -> Nothing
+
+  network = toShelleyNetwork networkId
 
 observeDepositTxOut :: Network -> TxOut CtxUTxO -> Maybe (HeadId, UTxO, POSIXTime)
 observeDepositTxOut network depositOut = do
