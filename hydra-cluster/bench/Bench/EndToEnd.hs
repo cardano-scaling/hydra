10e41631755e0f75b62cb83be843fe48a9f44a37
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-04-30 19:25:42 +0200

    Remove depositPeriod from withHydraCluster

--- hydra-cluster/bench/Bench/EndToEnd.hs
+++ hydra-cluster/bench/Bench/EndToEnd.hs
@@ -41,7 +41,6 @@ import Hydra.Logging (
   withTracerOutputTo,
  )
 import Hydra.Network (Host)
-import Hydra.Node.DepositPeriod (DepositPeriod (DepositPeriod))
 import Hydra.Tx (HeadId, txId)
 import Hydra.Tx.ContestationPeriod (ContestationPeriod (UnsafeContestationPeriod))
 import Hydra.Tx.Crypto (generateSigningKey)
@@ -92,8 +91,7 @@ bench startingNodeId timeoutSeconds workDir dataset = do
             putStrLn $ "Starting hydra cluster in " <> workDir
             let hydraTracer = contramap FromHydraNode tracer
             let contestationPeriod = UnsafeContestationPeriod 10
-            let depositPeriod = DepositPeriod 10
-            withHydraCluster hydraTracer workDir nodeSocket startingNodeId cardanoKeys hydraKeys hydraScriptsTxId contestationPeriod depositPeriod $ \clients -> do
+            withHydraCluster hydraTracer workDir nodeSocket startingNodeId cardanoKeys hydraKeys hydraScriptsTxId contestationPeriod $ \clients -> do
               waitForNodesConnected hydraTracer 20 clients
               scenario hydraTracer node workDir dataset clients
         systemStats <- readTVarIO statsTvar
