eae7df64fe783ad2e7800a13ba32977f87da09e1
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-05-14 10:45:44 +0200

    Clean up resources using bracket or resourcet

--- hydra-node/examples/Hydra/Events/UDP.hs
+++ hydra-node/examples/Hydra/Events/UDP.hs
@@ -3,23 +3,49 @@ module Hydra.Events.UDP where
 
 import Hydra.Prelude
 
+import Control.Monad.Trans.Resource (MonadResource, allocate)
 import Data.Aeson (encode)
 import Hydra.Events (EventSink (..))
 import Network.Socket (HostName, ServiceName)
-import Network.UDP (clientSocket, send)
+import Network.UDP (UDPSocket, clientSocket, close, send)
 
 -- | Create a new event sink that sends events as JSON.
-newUDPEventSink ::
+--
+-- This is the so-called bracket or with-style pattern, which can ensure that
+-- resources are free'd after the 'EventSink' is not needed anymore.
+--
+-- See 'newUDPEventSink' for a variant that uses 'MonadResource' to clean up.
+withUDPEventSink ::
   ToJSON e =>
   -- | Destination host
   HostName ->
   -- | Destination port
   ServiceName ->
-  IO (EventSink e IO)
+  -- | Continuation with EventSink
+  (EventSink e IO -> IO a) ->
+  IO a
+withUDPEventSink host port action =
+  -- Make sure to free resources (file descriptor)
+  bracket (clientSocket host port False) close $ \socket ->
+    action $ EventSink{putEvent = sendData socket}
+
+-- | Create a new event sink that sends events as JSON.
+--
+-- This uses 'MonadResource' to register and clean up resources. See
+-- 'withUDPEventSink' for a variant that uses 'bracket' and does not require
+-- 'resourcet'.
+newUDPEventSink ::
+  (ToJSON e, MonadResource m) =>
+  -- | Destination host
+  HostName ->
+  -- | Destination port
+  ServiceName ->
+  m (EventSink e IO)
 newUDPEventSink host port = do
-  socket <- clientSocket host port False
-  pure $
-    EventSink
-      { putEvent = \e ->
-          send socket (toStrict $ encode e <> "\n")
-      }
+  -- Make sure to free resources (file descriptor)
+  (_, socket) <- allocate (clientSocket host port False) close
+  pure EventSink{putEvent = sendData socket}
+
+sendData :: ToJSON e => UDPSocket -> e -> IO ()
+sendData socket e =
+  send socket (toStrict $ encode e <> "\n")
--- hydra-node/test/Hydra/Events/UDPSpec.hs
+++ hydra-node/test/Hydra/Events/UDPSpec.hs
@@ -4,9 +4,10 @@ module Hydra.Events.UDPSpec where
 import Hydra.Prelude
 import Test.Hydra.Prelude
 
+import Control.Monad.Trans.Resource (runResourceT)
 import Data.ByteString.Char8 qualified as BS8
 import Hydra.Events (EventId, EventSink (..), putEvent)
-import Hydra.Events.UDP (newUDPEventSink)
+import Hydra.Events.UDP (newUDPEventSink, withUDPEventSink)
 import Network.UDP (recvFrom, serverSocket, stop)
 import Test.Network.Ports (withFreePort)
 
@@ -16,20 +17,26 @@ spec = do
     it "sends datagram" $ do
       withFreePort $ \port -> do
         bracket (serverSocket ("0.0.0.0", port)) stop $ \socket -> do
-          sink <- newUDPEventSink "0.0.0.0" (show port)
-          let event = 123 :: EventId
-          putEvent sink event
-          (received, _) <- recvFrom socket
-          BS8.unpack received `shouldContain` "123"
+          withUDPEventSink "0.0.0.0" (show port) $ \sink -> do
+            let event = 123 :: EventId
+            putEvent sink event
+            (received, _) <- recvFrom socket
+            BS8.unpack received `shouldContain` "123"
 
     it "allows concurrent usage" $ do
       withFreePort $ \port -> do
-        EventSink{putEvent} <- newUDPEventSink @EventId "localhost" (show port)
-        concurrently_ (putEvent 123) (putEvent 456)
+        withUDPEventSink @EventId "0.0.0.0" (show port) $ \EventSink{putEvent} -> do
+          concurrently_ (putEvent 123) (putEvent 456)
 
   it "supports multiple instances" $ do
     withFreePort $ \port -> do
-      s1 <- newUDPEventSink @EventId "localhost" (show port)
-      putEvent s1 123
-      s2 <- newUDPEventSink @EventId "localhost" (show port)
-      putEvent s2 456
+      withUDPEventSink @EventId "0.0.0.0" (show port) $ \s1 -> do
+        withUDPEventSink @EventId "0.0.0.0" (show port) $ \s2 -> do
+          putEvent s1 123
+          putEvent s2 456
+
+      runResourceT $ do
+        s1 <- newUDPEventSink @EventId "0.0.0.0" (show port)
+        s2 <- newUDPEventSink @EventId "0.0.0.0" (show port)
+        lift $ putEvent s1 123
+        lift $ putEvent s2 456
