9fbc67fd6685f633bcb2237836511888798afd78
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-05-14 13:48:12 +0200

    Implement sourceEvents for S3 event source

--- hydra-node/examples/Hydra/Events/S3.hs
+++ hydra-node/examples/Hydra/Events/S3.hs
@@ -6,20 +6,17 @@ import Hydra.Prelude
 import Amazonka qualified as AWS
 import Amazonka.S3 qualified as AWS
 import Amazonka.S3 qualified as S3
+import Amazonka.S3.Lens qualified as AWS
+import Conduit (concatC, concatMapC, mapC, mapMC, sinkLazy, (.|))
+import Control.Lens (view, (^.))
 import Data.Aeson qualified as Aeson
 import Hydra.Events (EventSink (..), EventSource (..), HasEventId, getEventId)
 
 -- | Create a new event source and sink that stores events in AWS S3.
-newS3EventStore :: (HasEventId e, ToJSON e) => AWS.BucketName -> IO (EventSource e IO, EventSink e IO)
+newS3EventStore :: (HasEventId e, ToJSON e, FromJSON e) => AWS.BucketName -> IO (EventSource e IO, EventSink e IO)
 newS3EventStore bucketName = do
   -- TODO: nothing to clean up?
-  logger <- AWS.newLogger AWS.Debug stdout
-  discoveredEnv <- AWS.newEnv AWS.discover
-  let env =
-        discoveredEnv
-          { AWS.logger = logger
-          , AWS.region = AWS.Paris
-          }
+  env <- AWS.newEnv AWS.discover
   pure
     ( EventSource{sourceEvents = sourceEvents env}
     , EventSink{putEvent = putEvent env}
@@ -27,10 +24,38 @@ newS3EventStore bucketName = do
  where
   putEvent env e = do
     let body = AWS.toBody (Aeson.encode e)
-    res <- AWS.runResourceT $ AWS.send env (S3.newPutObject bucketName (objectKey e) body)
-    print res
+    void $ AWS.runResourceT $ AWS.send env (S3.newPutObject bucketName (objectKey e) body)
 
-  sourceEvents env = undefined
+  sourceEvents env = do
+    AWS.paginate env (AWS.newListObjects bucketName)
+      .| concatMapC (^. AWS.listObjectsResponse_contents)
+      .| concatC
+      .| mapC (^. AWS.object_key)
+      .| mapMC (getEvent env)
+
+  getEvent env k = do
+    body <-
+      AWS.send env (AWS.newGetObject bucketName k)
+        <&> view AWS.getObjectResponse_body
+    bytes <- AWS.sinkBody body sinkLazy
+    case Aeson.eitherDecode bytes of
+      Left err ->
+        fail $ "Failed to decode event: " <> show err
+      Right e -> pure e
 
 objectKey :: HasEventId e => e -> AWS.ObjectKey
 objectKey e = fromString $ "events/" <> show (getEventId e)
+
+-- | Delete all event objects from given S3 bucket.
+purgeEvents :: AWS.Env -> AWS.BucketName -> IO ()
+purgeEvents env bucketName = do
+  -- TODO: pagination
+  allObjects <- AWS.runResourceT . AWS.send env $ AWS.newListObjects bucketName
+  case allObjects ^. AWS.listObjectsResponse_contents of
+    Nothing -> pure ()
+    Just os ->
+      forM_ os $ \object -> do
+        -- TODO: remove log
+        putTextLn $ "deleting " <> show (object ^. AWS.object_key)
+        void . AWS.runResourceT . AWS.send env $
+          S3.newDeleteObject bucketName (object ^. AWS.object_key)
--- hydra-node/test/Hydra/Events/S3Spec.hs
+++ hydra-node/test/Hydra/Events/S3Spec.hs
@@ -6,23 +6,23 @@ import Test.Hydra.Prelude
 
 import Amazonka qualified as AWS
 import Amazonka.Auth qualified as AWS
-import Hydra.Events (EventSink (..), getEvents)
-import Hydra.Events.FileBasedSpec (genContinuousEvents)
-import Hydra.Events.S3 (newS3EventStore)
+import Hydra.Events (EventId, EventSink (..), getEvents)
+import Hydra.Events.S3 (newS3EventStore, purgeEvents)
 import Test.QuickCheck (forAllShrink, ioProperty, property, (===))
 
 spec :: Spec
 spec = do
+  -- Only run tests if the AWS environment can be discovered.
   around onlyWithAWSEnv $ do
     it "roundtrip putEvent and sourceEvents" $ \bucketName ->
       property $
         -- TODO: DRY with FiledBasedSpec -> create propEventsCompleteness
         forAllShrink genContinuousEvents shrink $ \events ->
           ioProperty $ do
-            (source, sink) <- newS3EventStore bucketName
-            forM_ events (putEvent sink)
-            loadedEvents <- getEvents source
-            pure $ loadedEvents === events
+            bracket (newS3EventStore bucketName) (const $ cleanup bucketName) $ \(source, sink) -> do
+              forM_ events (putEvent sink)
+              loadedEvents <- getEvents source
+              pure $ loadedEvents === events
 
     it "handles non-continous events" $ const True
 
@@ -31,20 +31,26 @@ spec = do
     it "allows concurrent usage" $ const True
 
     it "supports multiple instances" $ const True
+ where
+  -- See https://hackage.haskell.org/package/amazonka-2.0/docs/Amazonka-Auth.html#v:fromKeysEnv
+  --
+  -- Also provides the BucketName to tests. We are using 'fromString' to avoid the
+  -- dependency onto amazonka-s3 in the test suite.
+  onlyWithAWSEnv action = do
+    try (AWS.newEnv AWS.fromKeysEnv) >>= \case
+      Left (_ :: AWS.AuthError) -> pendingWith "Requires AWS environment"
+      Right _ -> pure ()
 
--- | Tests only run of amazonka env can be discovered.
---
--- See https://hackage.haskell.org/package/amazonka-2.0/docs/Amazonka-Auth.html#v:fromKeysEnv
---
--- Also provides the BucketName to tests. We are using 'fromString' to avoid the
--- dependency onto amazonka-s3 in the test suite.
-onlyWithAWSEnv :: IsString t => (t -> IO ()) -> IO ()
-onlyWithAWSEnv action = do
-  try (AWS.newEnv AWS.fromKeysEnv) >>= \case
-    Left (_ :: AWS.AuthError) -> pendingWith "Requires AWS environment"
-    Right _ -> pure ()
+    lookupEnv "BUCKET_NAME" >>= \case
+      Nothing -> pendingWith "Requires BUCKET_NAME environment variable"
+      Just bucketName -> do
+        action (fromString bucketName)
 
-  lookupEnv "BUCKET_NAME" >>= \case
-    Nothing -> pendingWith "Requires BUCKET_NAME environment variable"
-    Just bucketName -> do
-      action (fromString bucketName)
+  cleanup bucketName = do
+    env <- AWS.newEnv AWS.fromKeysEnv
+    purgeEvents env bucketName
+
+genContinuousEvents :: Gen [EventId]
+genContinuousEvents = do
+  len <- arbitrary
+  pure [0 .. len - 1]
