2a5695fcdc09f13c63002b2a9318683e60dec92d
Daniel Firth <daniel.firth@iohk.io>
2025-05-30 17:44:20 +0000

    Every file is a flake-parts module
    Uses import-tree to turn every file into a flake-parts module.
    
    Passes compiler, inputMap and hsPkgs through the module argument system.
    
    Passes lib.asZip through the module argument system

---
+++ nix/configuration.nix
@@ -0,0 +1,33 @@
+{ inputs, ... }: {
+
+  imports = [
+    inputs.process-compose-flake.flakeModule
+  ];
+
+  systems = [
+    "x86_64-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+    "aarch64-linux"
+  ];
+
+  perSystem = { pkgs, hsPkgs, config, lib, system, ... }:
+    let
+      compiler = "ghc966";
+
+      inputMap = { "https://intersectmbo.github.io/cardano-haskell-packages" = inputs.CHaP; };
+
+    in
+    {
+
+      _module.args = { inherit compiler inputMap; };
+
+      legacyPackages = pkgs // hsPkgs;
+
+      packages =
+        {
+          spec = inputs.hydra-spec.packages.${system}.default;
+        };
+
+    };
+}
