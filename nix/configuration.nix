a722dfd7398320da22fa8a2e443209c18fe60b56
Daniel Firth <daniel.firth@iohk.io>
2025-05-31 06:53:42 +0000

    Reformat configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,17 +1,12 @@
 { inputs, ... }: {
 
-  perSystem = { pkgs, hsPkgs, config, lib, system, ... }:
+  perSystem = { pkgs, hsPkgs, ... }:
     let
       compiler = "ghc966";
-
       inputMap = { "https://intersectmbo.github.io/cardano-haskell-packages" = inputs.CHaP; };
-
     in
     {
-
       _module.args = { inherit compiler inputMap; };
-
       legacyPackages = pkgs // hsPkgs;
-
     };
 }
