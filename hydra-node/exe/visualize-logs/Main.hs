bcfccbd4725d567a13fd3e293311ba420019478c
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-08-04 14:52:39 +0200

    Use conduit
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-node/exe/visualize-logs/Main.hs
+++ hydra-node/exe/visualize-logs/Main.hs
@@ -1,38 +1,47 @@
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# OPTIONS_GHC -Wno-deprecations #-}
 
 module Main where
 
 import Hydra.Cardano.Api
 import Hydra.Prelude hiding (encodeUtf8)
 
+import Conduit
 import Control.Lens ((^?))
 import Data.Aeson (eitherDecode')
 import Data.Aeson.Lens (key, _String)
 import Data.ByteString.Lazy.Char8 qualified as C8
 import Data.Text.Encoding (encodeUtf8)
 import Hydra.Logging (Envelope (..))
-import Hydra.Logging.Messages (HydraLog)
-import Hydra.Node (HydraNodeLog)
+import Hydra.Logging.Messages (HydraLog (..))
 
-data Decoded tx
-  = DecodedHydraLog (HydraLog tx)
-  | DecodedHydraNodeLog (HydraNodeLog tx)
-  deriving (Show)
+newtype Decoded tx
+  = DecodedHydraLog Text
+  deriving newtype (Show)
 
 main :: IO ()
 main = do
-  logLines <- readFileLBS "../devnet/alice-logs.txt"
-  let allLogLines = zip [1 :: Int ..] (C8.lines logLines)
-  decodedLines <- forM allLogLines $ \(ix, l) ->
-    case l ^? key "message" . _String of
-      Nothing -> error "Failed to find key 'message' which was expected"
-      Just line ->
-        let envelope = fromStrict $ encodeUtf8 line
-         in case decodeAs envelope (undefined :: Envelope (HydraLog Tx)) of
-              Left e -> error $ "Line " <> show ix <> ": error - " <> show e <> " line: " <> show line
-              Right decoded -> pure $ DecodedHydraLog $ decoded.message
-  print decodedLines
-  pure ()
+  decodedLines <-
+    runConduitRes $
+      sourceFileBS "../devnet/alice-logs.txt"
+        .| linesUnboundedAsciiC
+        .| mapMC
+          ( \l ->
+              case l ^? key "message" . _String of
+                Nothing -> error "Failed to find key 'message' which was expected"
+                Just line ->
+                  let envelope = fromStrict $ encodeUtf8 line
+                   in case decodeAs envelope (undefined :: Envelope (HydraLog Tx)) of
+                        Left e -> error $ show e <> line
+                        Right decoded ->
+                          case decoded.message of
+                            NodeOptions opt -> pure $ DecodedHydraLog $ "NODE STARTING: " <> show opt
+                            Node msg -> pure $ DecodedHydraLog $ "NODE LOG: " <> show msg
+                            _ -> pure $ DecodedHydraLog "_____"
+          )
+        .| sinkList
+  forM_ decodedLines $ \l ->
+    putTextLn (show l)
 
 decodeAs :: forall a. FromJSON a => C8.ByteString -> a -> Either String a
 decodeAs l _ =
