490a2ffe6dfb4c2132e2933d1a0631e93556a35f
awcjack <awcjack@protonmail.com>
2025-08-20 07:25:37 +0000

    fix unknown ca cert in docker

--- nix/hydra/docker.nix
+++ nix/hydra/docker.nix
@@ -14,6 +14,7 @@
             name = "hydra-node-env";
             paths = [
               pkgs.busybox
+              pkgs.dockerTools.caCertificates
             ];
           };
           config = {
