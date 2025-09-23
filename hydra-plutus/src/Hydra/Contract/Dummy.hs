1437aec215695bd7f251ea48e8e2079547566324
Sasha Bogicevic <sasa.bogicevic@pm.me>
2025-09-23 11:15:21 +0200

    Document commit process using user scripts
    Describe the user script validator checks that would
    set the Hydra standard when it comes to safely committing
    to specified Head.

--- hydra-plutus/src/Hydra/Contract/Dummy.hs
+++ hydra-plutus/src/Hydra/Contract/Dummy.hs
@@ -10,9 +10,28 @@ import Hydra.Prelude
 
 import Hydra.Cardano.Api (PlutusScript, pattern PlutusScriptSerialised)
 import Hydra.Plutus.Extras (wrapValidator)
-import PlutusLedgerApi.V3 (ScriptContext (..), ScriptInfo (..), serialiseCompiledCode, unsafeFromBuiltinData)
-import PlutusTx (compile)
-import PlutusTx.Prelude (check, traceIfFalse)
+import PlutusLedgerApi.V3 (
+  Address,
+  Credential (..),
+  CurrencySymbol,
+  OutputDatum (..),
+  ScriptContext (..),
+  ScriptHash,
+  ScriptInfo (..),
+  TxInInfo,
+  TxOut,
+  addressCredential,
+  fromBuiltinData,
+  getDatum,
+  serialiseCompiledCode,
+  txInInfoResolved,
+  txInfoInputs,
+  txOutAddress,
+  txOutDatum,
+  unsafeFromBuiltinData,
+ )
+import PlutusTx (compile, unstableMakeIsData)
+import PlutusTx.Prelude (check, traceError, traceIfFalse)
 
 dummyValidatorScript :: PlutusScript
 dummyValidatorScript =
@@ -64,3 +83,73 @@ dummyRewardingScript =
                 _ -> False
             ||]
         )
+
+-------------------------------------------------------------------
+-- Example user script to demonstrate committing to correct Head --
+-------------------------------------------------------------------
+data R
+  = R
+  { expectedHeadId :: CurrencySymbol
+  , expectedInitialValidator :: ScriptHash
+  }
+  deriving stock (Show, Generic)
+
+unstableMakeIsData ''R
+
+exampleValidator ::
+  () ->
+  R ->
+  ScriptContext ->
+  Bool
+exampleValidator _ redeemer ctx =
+  checkInitialInputIsSpent
+    && checkCorrectHeadId
+ where
+  checkInitialInputIsSpent =
+    traceIfFalse "Initial input not found" (isNothing initialInput)
+
+  checkCorrectHeadId =
+    case extractDatum of
+      Nothing -> traceError "Could not decode initial datum"
+      Just headId -> traceIfFalse "HeadId is not correct" $ headId == expectedHeadId
+
+  extractDatum =
+    case initialInput of
+      Nothing -> traceError "Initial input not found"
+      Just i ->
+        case decodeDatum (txInInfoResolved i) of
+          Just cs -> Just cs
+          Nothing -> Nothing
+
+  decodeDatum :: TxOut -> Maybe CurrencySymbol
+  decodeDatum txOut = case txOutDatum txOut of
+    OutputDatum d -> fromBuiltinData (getDatum d)
+    _ -> Nothing
+
+  initialInput = findInitialInput expectedInitialValidator
+
+  findInitialInput :: ScriptHash -> Maybe TxInInfo
+  findInitialInput initialScriptHash =
+    let allInputs = txInfoInputs info
+     in find (isInitialAddress initialScriptHash . txOutAddress . txInInfoResolved) allInputs
+
+  -- Check if an address is a script address with the specified script hash
+  isInitialAddress :: ScriptHash -> Address -> Bool
+  isInitialAddress expectedHash addr =
+    case addressCredential addr of
+      ScriptCredential vh -> vh == expectedHash
+      PubKeyCredential _ -> False
+
+  info = scriptContextTxInfo ctx
+
+  R{expectedHeadId, expectedInitialValidator} = redeemer
+
+exampleSecureValidatorScript :: PlutusScript
+exampleSecureValidatorScript =
+  PlutusScriptSerialised $
+    serialiseCompiledCode
+      $$( PlutusTx.compile
+            [||wrap exampleValidator||]
+        )
+ where
+  wrap = wrapValidator @() @R
