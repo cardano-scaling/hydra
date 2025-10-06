2a914f0d4ef861b794d405cb591a9c2a63301bb3
Franco Testagrossa <franco.testagrossa@iohk.io>
2025-10-06 02:21:54 +0200

    simplify synced status type

--- hydra-node/src/Hydra/Chain/SyncedStatus.hs
+++ hydra-node/src/Hydra/Chain/SyncedStatus.hs
@@ -2,15 +2,11 @@ module Hydra.Chain.SyncedStatus where
 
 import Hydra.Prelude
 
-import Control.Concurrent.Class.MonadSTM (writeTVar)
-import Hydra.Cardano.Api (ChainPoint (..), SlotNo (SlotNo), chainPointToSlotNo)
-import Hydra.Chain.ChainState (ChainSlot (ChainSlot))
+import Hydra.Cardano.Api (ChainPoint (..))
 
 data SyncedStatus
   = SyncedStatus
-  { status :: Bool
-  , diff :: Natural
-  , point :: ChainPoint
+  { point :: Maybe ChainPoint
   , tip :: ChainPoint
   }
   deriving stock (Eq, Show, Generic)
@@ -20,27 +16,11 @@ instance Arbitrary SyncedStatus where
   arbitrary = genericArbitrary
   shrink = genericShrink
 
+status :: SyncedStatus -> Bool
+status SyncedStatus{point, tip} =
+  case point of
+    Just p -> p == tip
+    Nothing -> False
+
 unSynced :: ChainPoint -> SyncedStatus
-unSynced tip = SyncedStatus{status = False, diff, point, tip}
- where
-  point = ChainPointAtGenesis
-  ChainSlot diff = chainSlotFromPoint tip - chainSlotFromPoint point
-
-updateSyncStatus ::
-  MonadSTM m =>
-  TVar m SyncedStatus ->
-  ChainPoint ->
-  ChainPoint ->
-  m ()
-updateSyncStatus syncedStatus tip point = do
-  let ChainSlot diff = chainSlotFromPoint tip - chainSlotFromPoint point
-      synced = tip == point
-  atomically $
-    writeTVar syncedStatus SyncedStatus{status = synced, diff, point, tip}
-
--- TODO! DRY
-chainSlotFromPoint :: ChainPoint -> ChainSlot
-chainSlotFromPoint p =
-  case chainPointToSlotNo p of
-    Nothing -> ChainSlot 0
-    Just (SlotNo s) -> ChainSlot $ fromIntegral s
+unSynced tip = SyncedStatus{point = Nothing, tip}
