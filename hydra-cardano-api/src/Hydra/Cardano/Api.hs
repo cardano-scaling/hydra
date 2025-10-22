d15c7d5ba310a120b7c277cf763ecea73b12aab0
Daniel Firth <daniel.firth@iohk.io>
2025-10-22 19:59:06 +0000

    Use genBlockHeader and genChainPoint functions from cardano-api 10.17

--- hydra-cardano-api/src/Hydra/Cardano/Api.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api.hs
@@ -91,7 +91,6 @@ import Hydra.Cardano.Api.Prelude (
 
 import Hydra.Cardano.Api.Address ()
 import Hydra.Cardano.Api.AddressInEra as Extras
-import Hydra.Cardano.Api.BlockHeader as Extras
 import Hydra.Cardano.Api.ChainPoint as Extras
 import Hydra.Cardano.Api.ExecutionUnits as Extras
 import Hydra.Cardano.Api.Hash as Extras
