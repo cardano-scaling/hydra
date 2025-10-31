0e0c877419da477a34cb595df61f56aba817ac9a
Veronika Romashkina <vrom911@gmail.com>
2025-10-31 15:21:53 +0000

    Introduce HasAccumulatorElement class
    Fix

--- hydra-node/src/Hydra/API/ServerOutput.hs
+++ hydra-node/src/Hydra/API/ServerOutput.hs
@@ -22,6 +22,7 @@ import Hydra.Node.State (NodeState, SyncedStatus)
 import Hydra.Prelude hiding (seq)
 import Hydra.Tx (HeadId, Party, Snapshot, SnapshotNumber, getSnapshot)
 import Hydra.Tx qualified as Tx
+import Hydra.Tx.Accumulator (HasAccumulatorElement)
 import Hydra.Tx.ContestationPeriod (ContestationPeriod)
 import Hydra.Tx.Crypto (MultiSignature)
 import Hydra.Tx.IsTx (IsTx (..))
@@ -313,7 +314,7 @@ data NetworkInfo = NetworkInfo
   deriving anyclass (ToJSON, FromJSON)
 
 -- | Get latest confirmed snapshot UTxO from 'HeadState'.
-getSnapshotUtxo :: IsTx tx => HeadState tx -> Maybe (UTxOType tx)
+getSnapshotUtxo :: HasAccumulatorElement tx => HeadState tx -> Maybe (UTxOType tx)
 getSnapshotUtxo = \case
   HeadState.Idle{} ->
     Nothing
@@ -328,7 +329,7 @@ getSnapshotUtxo = \case
      in Just $ Tx.utxo snapshot <> fromMaybe mempty (Tx.utxoToCommit snapshot)
 
 -- | Get latest seen snapshot from 'HeadState'.
-getSeenSnapshot :: IsTx tx => HeadState tx -> HeadState.SeenSnapshot tx
+getSeenSnapshot :: HasAccumulatorElement tx => HeadState tx -> HeadState.SeenSnapshot tx
 getSeenSnapshot = \case
   HeadState.Idle{} ->
     NoSeenSnapshot
