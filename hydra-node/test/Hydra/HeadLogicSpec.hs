d6d91e7f1f8ff0cc56f7708d2a4c8f0fdfe6d940
Veronika Romashkina <vrom911@gmail.com>
2025-11-07 09:21:57 +0000

    Add useful fns, change the way we create acc

--- hydra-node/test/Hydra/HeadLogicSpec.hs
+++ hydra-node/test/Hydra/HeadLogicSpec.hs
@@ -631,7 +631,6 @@ spec =
         let decommitTx1 = SimpleTx 1 (utxoRef 1) (utxoRef 3)
             decommitTx2 = SimpleTx 2 (utxoRef 2) (utxoRef 4)
             activeUTxO = utxoRefs [1, 2]
-            utxoHash = hashUTxO activeUTxO
             snapshot =
               Snapshot
                 { headId = testHeadId
@@ -641,7 +640,7 @@ spec =
                 , utxo = activeUTxO
                 , utxoToCommit = Nothing
                 , utxoToDecommit = Just $ utxoRefs [3]
-                , accumulator = Accumulator.build [utxoHash, hashUTxO @SimpleTx mempty, hashUTxO @SimpleTx $ utxoRefs [3]]
+                , accumulator = Accumulator.buildFromUTxO activeUTxO
                 }
             s0 =
               inOpenState'
@@ -1058,8 +1057,7 @@ spec =
         prop "reject side load confirmed snapshot because wrong snapshot utxoToDecommit" $ \utxoToDecommit -> do
           getConfirmedSnapshot startingState `shouldBe` Just snapshot1
           let utxo' = utxoRef 3
-              utxoHash = hashUTxO utxo'
-              accumulator = Accumulator.build [utxoHash, hashUTxO @SimpleTx mempty, hashUTxO @SimpleTx $ fromMaybe mempty (Just utxoToDecommit)]
+              accumulator = Accumulator.buildFromUTxO utxo'
               snapshot2 = Snapshot testHeadId 0 2 [tx2] utxo' Nothing (Just utxoToDecommit) accumulator
               multisig2 = aggregate [sign aliceSk snapshot2, sign bobSk snapshot2]
 
@@ -1071,8 +1069,7 @@ spec =
           getConfirmedSnapshot startingState `shouldBe` Just snapshot1
 
           let utxo' = utxoRef 3
-              utxoHash = hashUTxO utxo'
-              accumulator = Accumulator.build [utxoHash, hashUTxO @SimpleTx $ fromMaybe mempty (Just utxoToCommit), hashUTxO @SimpleTx mempty]
+              accumulator = Accumulator.buildFromUTxO utxo'
               snapshot2 = Snapshot testHeadId 0 2 [tx2] utxo' (Just utxoToCommit) Nothing accumulator
               multisig2 = aggregate [sign aliceSk snapshot2, sign bobSk snapshot2]
 
@@ -1644,14 +1641,13 @@ testSnapshot ::
   UTxOType tx ->
   Snapshot tx
 testSnapshot number version confirmed utxo =
-  let utxoHash = hashUTxO utxo
-   in Snapshot
-        { headId = testHeadId
-        , version
-        , number
-        , confirmed
-        , utxo
-        , utxoToCommit = mempty
-        , utxoToDecommit = mempty
-        , accumulator = Accumulator.build [utxoHash, hashUTxO @tx mempty, hashUTxO @tx mempty]
-        }
+  Snapshot
+    { headId = testHeadId
+    , version
+    , number
+    , confirmed
+    , utxo
+    , utxoToCommit = mempty
+    , utxoToDecommit = mempty
+    , accumulator = Accumulator.buildFromUTxO utxo
+    }
