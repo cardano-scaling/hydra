d2447774f43c31ee7db53cab85bc33046c0966b1
Franco Testagrossa <franco.testagrossa@iohk.io>
2025-12-10 14:29:38 -0300

    refactor Tick to track ChainPoint instead of ChainSlot

--- hydra-node/src/Hydra/Chain/Direct/Handlers.hs
+++ hydra-node/src/Hydra/Chain/Direct/Handlers.hs
@@ -352,8 +352,7 @@ chainSyncHandler tracer callback getTimeHandle ctx localChainState =
           Left reason ->
             throwIO TimeConversionException{slotNo, reason}
           Right utcTime -> do
-            let chainSlot = ChainSlot . fromIntegral $ unSlotNo slotNo
-            callback (Tick{chainTime = utcTime, chainSlot})
+            callback (Tick{chainTime = utcTime, chainPoint = point})
 
     forM_ receivedTxs $
       maybeObserveSomeTx timeHandle point >=> \case
