4ff6070ac7070f9e8e76421c215d37659fffa809
Franco Testagrossa <franco.testagrossa@iohk.io>
2025-12-18 13:55:46 -0300

    do not wait for node to be in synced

--- hydra-cluster/src/Hydra/Cluster/Scenarios.hs
+++ hydra-cluster/src/Hydra/Cluster/Scenarios.hs
@@ -304,21 +304,21 @@ resumeFromLatestKnownPoint tracer workDir backend hydraScriptsTxId = do
       <&> setNetworkId networkId
 
   chainPoint :: ChainPoint <-
-    withHydraNode hydraTracer aliceChainConfig workDir 1 aliceSk [] [1] $ \n1 -> do
+    withHydraNodeCatchingUp hydraTracer aliceChainConfig workDir 1 aliceSk [] [1] $ \n1 -> do
       point <- waitMatch 20 n1 $ \v -> do
         guard $ v ^? key "tag" == Just "Greetings"
         guard $ v ^? key "headStatus" == Just (toJSON Idle)
         point <- v ^? key "atChainPoint"
         parseMaybe parseJSON point
 
-      -- wait for some blocks to roll forward
-      threadDelay 5
+      -- wait for some ticks to be observed
+      threadDelay 1
       pure point
 
   let chainSlot = chainSlotFromPoint chainPoint
 
   chainPoint' :: ChainPoint <-
-    withHydraNode hydraTracer aliceChainConfig workDir 1 aliceSk [] [1] $ \n1 -> do
+    withHydraNodeCatchingUp hydraTracer aliceChainConfig workDir 1 aliceSk [] [1] $ \n1 -> do
       waitMatch 20 n1 $ \v -> do
         guard $ v ^? key "tag" == Just "Greetings"
         guard $ v ^? key "headStatus" == Just (toJSON Idle)
