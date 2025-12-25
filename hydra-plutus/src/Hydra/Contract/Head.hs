ff5bd4773538bc05a7cd6a10df40cc121ee5f4c1
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-12-25 12:15:39 +0100

    Fix golden files
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-plutus/src/Hydra/Contract/Head.hs
+++ hydra-plutus/src/Hydra/Contract/Head.hs
@@ -37,6 +37,7 @@ import Hydra.Contract.Util (hasST, hashPreSerializedCommits, hashTxOuts, mustBur
 import Hydra.Data.ContestationPeriod (ContestationPeriod, addContestationPeriod, milliseconds)
 import Hydra.Data.Party (Party (vkey))
 import Hydra.Plutus.Extras (ValidatorType, wrapValidator)
+
 -- import Plutus.Crypto.Accumulator (checkMembership)
 import PlutusLedgerApi.Common (serialiseCompiledCode)
 import PlutusLedgerApi.V1.Time (fromMilliSeconds)
@@ -655,8 +656,9 @@ checkFanout _ctx@ScriptContext{scriptContextTxInfo = txInfo} closedDatum numberO
     && hasSameDecommitUTxOHash
     && afterContestationDeadline
     && crsRefExists
-    -- && checkMembership crs accumulatorCommitment [] proof
  where
+  -- && checkMembership crs accumulatorCommitment [] proof
+
   minted = txInfoMint txInfo
 
   hasSameUTxOHash =
