09413022879f67646f0fab5974a76ea9e71c6288
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-12-30 16:06:09 +0100

    Optimize CRS contract
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-plutus/src/Hydra/Contract/CRS.hs
+++ hydra-plutus/src/Hydra/Contract/CRS.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# OPTIONS_GHC -fplugin PlutusTx.Plugin #-}
+{-# OPTIONS_GHC -fno-specialize #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:conservative-optimisation #-}
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:defer-errors #-}
-{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:target-version=1.1.0 #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:optimize #-}
 
 module Hydra.Contract.CRS where
 
