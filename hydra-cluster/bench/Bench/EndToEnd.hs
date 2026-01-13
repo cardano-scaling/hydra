590e9d8afdc26cc56f4f65c2b919b1034fb208ac
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2026-01-13 16:50:06 +0100

    Introduce number of fanout outputs and number of txs
    Now we pass in wanted number of txs and we also output the
    number of fanout outputs so we can track the fanout performance.
    
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-cluster/bench/Bench/EndToEnd.hs
+++ hydra-cluster/bench/Bench/EndToEnd.hs
@@ -6,6 +6,7 @@ import Hydra.Prelude
 import Test.Hydra.Prelude
 
 import Bench.Summary (Summary (..), SystemStats, makeQuantiles)
+import Cardano.Api.UTxO qualified as UTxO
 import CardanoNode (findRunningCardanoNode', withCardanoNodeDevnet)
 import Control.Concurrent.Class.MonadSTM (
   MonadSTM (readTVarIO),
@@ -19,6 +20,7 @@ import Control.Lens (to, (^..), (^?))
 import Control.Monad.Class.MonadAsync (mapConcurrently)
 import Data.Aeson (Result (Error, Success), Value, encode, fromJSON, (.=))
 import Data.Aeson.Lens (key, values, _JSON, _Number, _String)
+import Data.Aeson.Types (parseEither)
 import Data.ByteString.Lazy qualified as LBS
 import Data.List qualified as List
 import Data.Map qualified as Map
@@ -26,7 +28,7 @@ import Data.Scientific (Scientific)
 import Data.Set ((\\))
 import Data.Set qualified as Set
 import Data.Time (UTCTime (UTCTime), utctDayTime)
-import Hydra.Cardano.Api (NetworkId, SocketPath, Tx, TxId, UTxO, getVerificationKey, lovelaceToValue, signTx)
+import Hydra.Cardano.Api (Era, NetworkId, SocketPath, Tx, TxId, UTxO, getVerificationKey, lovelaceToValue, signTx)
 import Hydra.Chain.Backend (ChainBackend)
 import Hydra.Chain.Backend qualified as Backend
 import Hydra.Cluster.Faucet (FaucetLog (..), publishHydraScriptsAs, returnFundsToFaucet', seedFromFaucet)
@@ -190,9 +192,10 @@ scenario hydraTracer backend workDir Dataset{clientDatasets, title, description}
 
   putTextLn "Finalizing the Head"
   send leader $ input "Fanout" []
-  waitMatch 100 leader $ \v -> do
+  finalUTxOJSON <- waitMatch 100 leader $ \v -> do
     guard (v ^? key "tag" == Just "HeadIsFinalized")
     guard $ v ^? key "headId" == Just (toJSON headId)
+    v ^? key "utxo"
 
   let confTimes = map (\(_, _, a) -> a) res
       numberOfTxs = length confTimes
@@ -201,6 +204,10 @@ scenario hydraTracer backend workDir Dataset{clientDatasets, title, description}
       quantiles = makeQuantiles confTimes
       summaryTitle = fromMaybe "Baseline Scenario" title
       summaryDescription = fromMaybe defaultDescription description
+      numberOfFanoutOutputs =
+        case parseEither (parseJSON @(UTxO.UTxO Era)) finalUTxOJSON of
+          Left _ -> error "Failed to decode Fanout UTxO"
+          Right fanoutUTxO -> UTxO.size fanoutUTxO
 
   pure $
     Summary
@@ -212,6 +219,7 @@ scenario hydraTracer backend workDir Dataset{clientDatasets, title, description}
       , summaryTitle
       , summaryDescription
       , numberOfInvalidTxs
+      , numberOfFanoutOutputs
       }
 
 defaultDescription :: Text
