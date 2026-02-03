6f2e0bcf1bd854be19421331448690ad3687831a
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2026-02-03 09:36:46 +0100

    Make model spec pass by reducing the number of fanout outputs
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-node/test/Hydra/ModelSpec.hs
+++ hydra-node/test/Hydra/ModelSpec.hs
@@ -153,8 +153,9 @@ spec = do
   -- This scenario seeds a head with a single party and an UTxO set of 50 elements,
   -- which is over the budget of the mocked chain implementation.
   -- See https://github.com/cardano-scaling/hydra/issues/2270
-  prop "fails fanout over the limit" $ expectFailure (propFanoutLimit 50)
-  prop "succeeds fanout under the limit" $ propFanoutLimit 49
+  context "fanout limit" $ do
+    prop "fails fanout over the limit" $ expectFailure (propFanoutLimit 48)
+    prop "succeeds fanout under the limit" $ propFanoutLimit 47
   context "logic" $ do
     prop "check conflict-free liveness" $ propDL conflictFreeLiveness
     prop "fanout contains whole confirmed UTxO" $ propDL fanoutContainsWholeConfirmedUTxO
