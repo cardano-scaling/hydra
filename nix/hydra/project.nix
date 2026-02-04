f9e9ceb3b50642d74237e9abb0a88cacd3ded0ea
Daniel Firth <daniel.firth@iohk.io>
2026-02-04 06:12:53 +0000

    Update CHaP and haskell.nix
    Pin etcd to 3.5.x. There is a bug in version in versions 3.6.0 through 3.6.7 that causes the handles expired leases test to fail.

--- nix/hydra/project.nix
+++ nix/hydra/project.nix
@@ -88,7 +88,7 @@
           }
           # Add etcd as build dependency of hydra-node (template haskell embedding not tracked by cabal)
           {
-            packages.hydra-node.components.library.build-tools = [ pkgs.etcd ];
+            packages.hydra-node.components.library.build-tools = [ pkgs.etcd_3_5 ];
           }
         ];
       };
