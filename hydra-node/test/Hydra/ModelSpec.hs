bedafce7c8cfa7a994a75d168b368f3697e52c63
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2026-02-12 10:59:23 +0100

    Bring back correct execution units and make the tests pass.
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-node/test/Hydra/ModelSpec.hs
+++ hydra-node/test/Hydra/ModelSpec.hs
@@ -150,12 +150,12 @@ spec = do
     prop "toTxOuts is distributive" $ propIsDistributive toTxOuts
   prop "check model" propHydraModel
   prop "check model balances" propCheckModelBalances
-  -- This scenario seeds a head with a single party and an UTxO set of 50 elements,
+  -- This scenario seeds a head with a single party and an UTxO set of elements,
   -- which is over the budget of the mocked chain implementation.
   -- See https://github.com/cardano-scaling/hydra/issues/2270
   context "fanout limit" $ do
-    prop "fails fanout over the limit" $ expectFailure (propFanoutLimit 48)
-    prop "succeeds fanout under the limit" $ propFanoutLimit 47
+    prop "fails fanout over the limit" $ expectFailure (propFanoutLimit 21)
+    prop "succeeds fanout under the limit" $ propFanoutLimit 20
   context "logic" $ do
     prop "check conflict-free liveness" $ propDL conflictFreeLiveness
     prop "fanout contains whole confirmed UTxO" $ propDL fanoutContainsWholeConfirmedUTxO
