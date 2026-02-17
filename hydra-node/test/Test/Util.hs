cda4c757e33345ee21eb7f816e460073da75eef9
Noon van der Silk <noon.vandersilk@iohk.io>
2026-02-17 14:17:19 +0000

    remove un-used function

--- hydra-node/test/Test/Util.hs
+++ hydra-node/test/Test/Util.hs
@@ -52,32 +52,6 @@ shouldRunInSim action =
   tr = runSimTrace action
   dumpTrace = say (toString $ printTrace (Proxy :: Proxy (HydraNodeLog SimpleTx)) tr)
 
--- | Run given 'action' in 'IOSim' as a 'Property' including the whole
--- 'HydraNodeLog' trace as a counterexample.
-propRunInSim :: Testable prop => (forall s. IOSim s prop) -> Property
-propRunInSim action =
-  ioProperty $ do
-    fn <- storeTrace
-    pure $
-      runSim
-        & counterexample ("IOSim trace stored in: " <> fn)
- where
-  runSim = case traceResult False tr of
-    Right x ->
-      property x
-    Left (FailureException (SomeException ex)) -> do
-      property False
-        & counterexample ("Failed with exception: " <> show ex)
-    Left ex ->
-      property False
-        & counterexample ("Failed with exception: " <> show ex)
-
-  tr = runSimTrace action
-
-  storeTrace =
-    writeSystemTempFile "io-sim-trace" . toString $
-      printTrace (Proxy :: Proxy (HydraNodeLog SimpleTx)) tr
-
 -- | Utility function to dump logs given a `SimTrace`.
 printTrace :: forall log a. (Typeable log, ToJSON log) => Proxy log -> SimTrace a -> Text
 printTrace _ tr =
