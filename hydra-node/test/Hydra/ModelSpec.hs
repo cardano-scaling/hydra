89e943e3e3f8f252cbba8c1821e3e831a88f876b
Veronika Romashkina <vrom911@gmail.com>
2026-02-22 20:39:47 +0000

    Use plutusTx plugin version 1.1.0 everywhere (#2517)

--- hydra-node/test/Hydra/ModelSpec.hs
+++ hydra-node/test/Hydra/ModelSpec.hs
@@ -156,11 +156,11 @@ spec = do
     prop "toTxOuts is distributive" $ propIsDistributive toTxOuts
   prop "check model" propHydraModel
   prop "check model balances" propCheckModelBalances
-  -- This scenario seeds a head with a single party and an UTxO set of 42 elements,
+  -- This scenario seeds a head with a single party and an UTxO set of 47 elements,
   -- which is over the budget of the mocked chain implementation.
   -- See https://github.com/cardano-scaling/hydra/issues/2270
-  prop "fails fanout over the limit" $ expectFailure (propFanoutLimit 42)
-  prop "succeeds fanout under the limit" $ propFanoutLimit 41
+  prop "fails fanout over the limit" $ expectFailure (propFanoutLimit 48)
+  prop "succeeds fanout under the limit" $ propFanoutLimit 47
   context "logic" $ do
     prop "check conflict-free liveness" $ propDL conflictFreeLiveness
     prop "check head opens if all participants commit" $ propDL headOpensIfAllPartiesCommit
