fc9b7c809c3580ded5edd960bb608010a8466b08
Daniel Firth <daniel.firth@iohk.io>
2026-02-23 17:26:28 +0000

    cardano-api: 10.21

--- hydra-cluster/src/Hydra/Cluster/Faucet.hs
+++ hydra-cluster/src/Hydra/Cluster/Faucet.hs
@@ -237,7 +237,7 @@ createOutputAtAddress networkId backend atAddress datum val = do
   let output = TxOut atAddress val datum ReferenceScriptNone
   buildTransaction backend (mkVkAddress networkId faucetVk) utxo collateralTxIns [output] >>= \case
     Left e ->
-      throwErrorAsException e
+      throwErrorM e
     Right x -> do
       let body = getTxBody x
       let tx = makeSignedTransaction [makeShelleyKeyWitness body (WitnessPaymentKey faucetSk)] body
