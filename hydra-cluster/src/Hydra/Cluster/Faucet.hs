8c06c65dd23107ffc66c3443ad343c3e94e2b783
Veronika Romashkina <vrom911@gmail.com>
2026-03-19 09:30:47 +0000

    Use cardano-api 10.21 proper version
    Got all changes from the capi-10.21 branch here

--- hydra-cluster/src/Hydra/Cluster/Faucet.hs
+++ hydra-cluster/src/Hydra/Cluster/Faucet.hs
@@ -234,7 +234,7 @@ createOutputAtAddress networkId opts atAddress datum val = do
   let output = TxOut atAddress val datum ReferenceScriptNone
   runBackend opts (buildTransaction (mkVkAddress networkId faucetVk) utxo collateralTxIns [output]) >>= \case
     Left e ->
-      failure $ show e
+      throwErrorM e
     Right x -> do
       let body = getTxBody x
       let tx = makeSignedTransaction [makeShelleyKeyWitness body (WitnessPaymentKey faucetSk)] body
