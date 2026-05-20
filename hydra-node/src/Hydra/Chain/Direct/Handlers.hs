b23f2d041596b55b259d764513b341aa21430c64
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2026-05-20 19:38:12 +0200

    Refactor
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- hydra-node/src/Hydra/Chain/Direct/Handlers.hs
+++ hydra-node/src/Hydra/Chain/Direct/Handlers.hs
@@ -73,7 +73,6 @@ import Hydra.Chain.Direct.State (
   recover,
  )
 import Hydra.Chain.Direct.TimeHandle (TimeHandle (..))
-import System.IO.Error (userError)
 import Hydra.Chain.Direct.Wallet (
   ErrCoverFee (..),
   TinyWallet (..),
@@ -105,6 +104,7 @@ import Hydra.Tx.Observe (
  )
 import Hydra.Tx.Recover (RecoverObservation (..))
 import Hydra.Tx.Snapshot (Snapshot (..), getSnapshot)
+import System.IO.Error (userError)
 
 -- | Handle of a mutable local chain state that is kept in the direct chain layer.
 data LocalChainState m tx = LocalChainState
