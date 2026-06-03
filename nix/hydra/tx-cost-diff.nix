79747db9f87528e01d32c1ac5419c1a6835727ca
Veronika Romashkina <vrom911@gmail.com>
2026-06-03 19:25:00 +0100

    Add partialFanout and finalPartialFanout into the tx-cost-diff report (#2633)
    <!-- Describe your change here -->
    
    ---
    
    <!-- Consider each and tick it off one way or the other -->
    * [ ] CHANGELOG updated or not needed
    * [ ] Documentation updated or not needed
    * [ ] Haddocks updated or not needed
    * [ ] No new TODOs introduced or explained herafter
    
    ---------
    
    Co-authored-by: Noon van der Silk <noon.vandersilk@iohk.io>

--- nix/hydra/tx-cost-diff.nix
+++ nix/hydra/tx-cost-diff.nix
@@ -1,12 +1,27 @@
 { self, ... }: {
-  perSystem = { pkgs, ... }: {
-    packages.tx-cost-diff =
-      pkgs.writers.writeHaskellBin
-        "tx-cost-diff"
-        {
-          libraries =
-            with pkgs.haskellPackages;
-            [ aeson text bytestring lens lens-aeson shh ];
-        } ''${builtins.readFile "${self}/scripts/tx-cost-diff.hs"}'';
-  };
+  perSystem = { pkgs, ... }:
+    let
+      tx-cost-diff =
+        pkgs.writers.writeHaskellBin
+          "tx-cost-diff"
+          {
+            libraries =
+              with pkgs.haskellPackages;
+              [ aeson text bytestring lens lens-aeson shh ];
+          } ''${builtins.readFile "${self}/scripts/tx-cost-diff.hs"}'';
+
+      pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        pandas
+        html5lib
+        beautifulsoup4
+        tabulate
+      ]);
+    in
+    {
+      packages.tx-cost-diff = pkgs.writeShellApplication {
+        name = "tx-cost-diff";
+        runtimeInputs = [ pkgs.pandoc pythonEnv ];
+        text = ''exec ${tx-cost-diff}/bin/tx-cost-diff "$@"'';
+      };
+    };
 }
